da4aad1bcb42503d74f4812fac0317a03e9ddbbd
Siddharth Bhat <siddu.druid@gmail.com>
2023-02-26 02:54:10 +0000

    Try to phrase our rewrite correctness in terms of pre/post condition semantics.
    structure Rewrite where
      find: Command
      replace : Command
      correct: ∀ (post: assertion), (replace.sp (find.wp post)).implies post
    
    That is, a rewrite is correct, if for any valid 'post-condition' of the
    'find' pattern, the strongest post-condition of the replacement
    pattern wrt the weakest precondition of the find pattern of post
    should imply the post-condition.
    
    Intuitively, this is true, because we are saying:
    
    1. Assume 'find' runs correctly and produces an environment with state
       'post'.
    2. Find what state the enrivonment had to be in (ie, find the initial
        state of the environment) as 'wp(post)'.
    3. Run the replacement pattern forwards, as 'sp(wp(post))'.
    4. This must create an environment that has at least as much stuff as
       the find pattern! Thus, the user must prove that
       'sp(wp(post)) => post'.
    
    The great great advantage of phrasing stuff this way is that everything
    is _computational_. We crunch the find pattern and compute the
    weakest precondition, and we then crunch the replacement pattern
    and compute the strongest postcondition. What we are left with
    is a pure proposition (albeit a monstrously large one) which
    we can start proving!
    
    ---
    
    In working on this, I realised that I do not in fact understand
    hoare logic properly. I had failed to realize how key partial
    correctness of a program is for hoare logic. Turns out, that
    if we assume total correctness, we cannot compute the strongest
    post-condition for assignment!

--- SSA/Bugred.lean
+++ SSA/Bugred.lean
@@ -1,223 +1,258 @@
-/-
-A file used purely for bug reduction
--/
-import Mathlib.Algebra.Group
+import Aesop
 
-namespace BoringKleisli
+namespace Hoare
 
 abbrev Val := Nat
-abbrev Env := Nat → Nat
-abbrev TopM := (ReaderT Env (Except String))
 
-instance : LawfulMonad TopM := sorry
+structure NamedVal where
+  name : String
+  val : Val
 
-def repeatM: Nat → (Val → TopM Val) → Val → TopM Val
-| 0, _ => pure
-| .succ n, f => f >=> repeatM n f
-
-#print mul_left_comm
-
-theorem repeatM.peel_left: ∀ (n: Nat) (f: Val → TopM Val),
-  repeatM (n+1) f = f >=> repeatM n f := by {
-  intros n f;
-  induction n <;> funext <;>
-    simp[Bind.kleisliRight, bind, ReaderT.bind,
-          repeatM];
-}
-
--- kleisli arrow is associative.
-theorem kleisliRight.assoc {m: Type → Type}
-  [M: Monad m] [LM: LawfulMonad m]
-  {α β γ δ: Type}
-  (f: α → m β) (g: β → m γ) (h: γ → m δ) :
-  (f >=> g) >=> h = f >=> (g >=> h) := by {
-  funext x;
-  simp[Bind.kleisliRight];
-}
-
--- pure is left identity
-@[simp]
-theorem kleisliRight.pure_id_left {m: Type → Type}
-  [M: Monad m] [LM: LawfulMonad m]
-  {α β: Type}
-  (f: α → m β) :
-  pure >=> f = f := by {
-  funext x;
-  simp[Bind.kleisliRight];
-}
-
--- pure is left identity
-@[simp]
-theorem kleisliRight.pure_id_right {m: Type → Type}
-  [M: Monad m] [LM: LawfulMonad m]
-  {α β: Type}
-  (f: α → m β) :
-  f >=> pure = f:= by {
-  funext x;
-  simp[Bind.kleisliRight];
-}
-
--- The above shows that (>=>, pure) forms a category.
-
--- repeatM commutes with f;
-theorem repeatM.commute_f: ∀ (n: Nat) (f: Val → TopM Val),
-  repeatM n f >=> f = f >=> repeatM n f := by {
-  intros n;
-  induction n;
-  case zero => {
-    intros f; funext x env;
-    simp[repeatM, pure, ReaderT.pure, Except.pure,
-     Bind.kleisliRight, bind, ReaderT.bind, Except.bind];
-     cases H:( f x env) <;> simp;
-  }
-  case succ n' IH => {
-    intros f;
-    rw[repeatM.peel_left];
-    rw[kleisliRight.assoc];
-    rw[IH];
-  }
-}
-
--- repeatM allows an 'f' to be extracted to the right.
-theorem repeatM.peel_right: ∀ (n: Nat) (f: Val → TopM Val),
-  repeatM (n+1) f = repeatM n f >=> f := by {
-  intros n;
-  induction n <;> intros f;
-  case zero => {
-    funext x env;
-    simp[repeatM, pure, ReaderT.pure, Except.pure,
-     Bind.kleisliRight, bind, ReaderT.bind, Except.bind];
-     cases (f x env) <;> simp
-  }
-  case succ m IH => {
-    simp;
-    rw[IH];
-    rw[repeatM.peel_left]
-    rw[IH];
-    rw[repeatM.commute_f];
-    rw[kleisliRight.assoc];
-    rw[repeatM.commute_f];
-  }
-}
-
--- composing repeatM is the same as adding the repeat
--- counter.
-theorem repeatM.compose (f: Val → TopM Val) (n : Nat) :
-  ∀ (m: Nat),
-  (repeatM n f) >=> (repeatM m f) = repeatM (n+m) f := by {
-  induction n;
-  case zero => {
-  intros m; simp[repeatM];
-  }
-  case succ n' IH => {
-    intros m;
-    rw[repeatM.peel_left];
-    rw[kleisliRight.assoc];
-    rw[IH];
-    rw[<- repeatM.peel_left];
-    suffices : n' + m + 1 = Nat.succ n' + m;
-    simp[this];
-    linarith;
-  }
-}
+abbrev Env := String → Option Nat
+abbrev TopM := StateT Env Option
 
 
--- pull a function that commutes with f to the left of repeatM
-@[simp] -- pull simple stuff to the left.
-theorem repeatM.commuting_pull_left
-  (f: Val → TopM Val) (g: Val → TopM Val)
-  (COMMUTES: f >=> g = g >=> f) :
-  (repeatM n f) >=> g = g >=> repeatM n f := by {
-  induction n;
-  case zero => { simp[repeatM]; }
-  case succ n' IH => {
-    simp[repeatM];
-    rw[kleisliRight.assoc];
-    rw[IH];
-    rw[<- kleisliRight.assoc];
-    rw[COMMUTES];
-    rw[kleisliRight.assoc];
+inductive Expr
+| const (n : Nat) : Expr
+| var (x : String) : Expr
+| add (e₁ e₂ : Expr) : Expr
+| sub (e₁ e₂ : Expr) : Expr
+| mul (e₁ e₂ : Expr) : Expr
+| div (e₁ e₂ : Expr) : Expr
+
+def Expr.eval (env: Env): Expr → Option Val
+| (Expr.const n) => pure n
+| (Expr.var x) => do
+  env x
+| (Expr.add e₁ e₂) => do
+  let v₁ ← Expr.eval env e₁
+  let v₂ ← Expr.eval env e₂
+  pure (v₁ + v₂)
+| (Expr.sub e₁ e₂) => do
+  let v₁ ← Expr.eval env e₁
+  let v₂ ← Expr.eval env e₂
+  pure (v₁ - v₂)
+| (Expr.mul e₁ e₂) => do
+  let v₁ ← Expr.eval env e₁
+  let v₂ ← Expr.eval env e₂
+  pure (v₁ * v₂)
+| (Expr.div e₁ e₂) => do
+  let v₁ ← Expr.eval env e₁
+  let v₂ ← Expr.eval env e₂
+  if v₂ = 0 then
+    .none
+  else
+    pure (v₁ / v₂)
+
+
+inductive Command
+| skip: Command
+| assign (x : String) (e : Expr) : Command
+| seq (c₁ c₂ : Command) : Command
+| if_ (e : Expr) (c₁ c₂ : Command) : Command
+
+
+def TopM.updateEnv (f : Env → Env) : TopM Unit := do
+  let env ← get
+  let env' := f env
+  set env'
+
+def Env.set (nv: NamedVal) (env: Env) : Env :=
+  fun x => if x = nv.name then nv.val else env x
+
+theorem Env.get_set_eq (env: Env) (n: String) (v: Nat): (Env.set ⟨n, v⟩ env) n = .some v :=
+  by simp[Env.set]
+
+theorem Env.get_set_neq (env: Env)
+  (lookupk setk: String) (NEQ: lookupk ≠ setk)
+  (v: Nat): (Env.set ⟨setk, v⟩ env) lookupk = env lookupk := by {
+    simp[Env.set];
+    rw[if_neg NEQ];
   }
 
+def Command.exec : Command → TopM Unit
+| Command.skip => return ()
+| (Command.assign x e) => do
+  let v ← Expr.eval (← get) e
+  TopM.updateEnv (Env.set ⟨x, v⟩)
+| (Command.seq c₁ c₂) => do
+  Command.exec c₁
+  (Command.exec c₂)
+| (Command.if_ e c₁ c₂) => do
+  let v ← Expr.eval (← get) e
+  if v = 0 then Command.exec c₂ else Command.exec c₁
+
+
+
+def assertion  := Env → Prop
+
+def hoare_triple (P: assertion) (c: Command) (Q: assertion) : Prop :=
+  ∀ (env: Env), P env → ∃ st , c.exec.run env = .some ⟨(), st⟩ ∧ Q st
+
+-- weakest precondition
+def Command.wp (Q: assertion): Command → assertion
+| Command.skip => Q
+| (Command.assign x e) => fun env => ∃ v, Expr.eval env e = .some v ∧ Q (env.set ⟨x, v⟩)
+| (Command.seq c₁ c₂) =>
+    let wp2 := c₂.wp Q
+    let wp1 := c₁.wp wp2
+    wp1
+| (Command.if_ e c₁ c₂) =>
+  fun env => ∃ v, Expr.eval env e = .some v ∧
+  (if v = 0 then Command.wp Q c₂ else Command.wp Q c₁) env
+
+
+def assertion.implies (P Q: assertion) : Prop := ∀ env, P env → Q env
+
+/-
+Prove that the weakest precondition is a precondition.
+-/
+theorem Command.wp.skip.is_precondition:
+  ∀ (Q: assertion), hoare_triple (Command.skip.wp Q) Command.skip Q := by {
+    intros Q;
+    simp[hoare_triple, wp, exec, pure, StateT.run, StateT.pure];
 }
 
-theorem repeatM.commuting_pull_right
-  (f: Val → TopM Val) (g: Val → TopM Val)
-  (COMMUTES: f >=> g = g >=> f) :
-  g >=> (repeatM n f) = repeatM n f >=> g := by {
-    simp[repeatM.commuting_pull_left, COMMUTES];
+theorem Command.wp.assign.is_precondition:
+  ∀ {c: Command} {name: String} {val: Expr} {C: c = Command.assign name val}
+    {Q: assertion}, hoare_triple (c.wp Q) c Q := by {
+    intros c name expr C Q;
+    simp[C];
+    simp[hoare_triple, wp, exec, pure, StateT.run, StateT.pure];
+    intros env expr_val EXPR_VAL Qenv';
+    simp[get, getThe, MonadStateOf.get, StateT.get, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+      StateT.lift];
+    exists (Env.set { name := name, val := expr_val } env);
+    simp[Qenv'];
+    exists (expr_val, env);
+    simp[EXPR_VAL];
+    simp[TopM.updateEnv, set, StateT.set, StateT.run, StateT.pure, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+      StateT.lift, get, getThe, MonadStateOf.get, StateT.get];
 }
 
 
--- TODO: decision procedure for free theory of
--- commuting functions?
-theorem repeatM.commuting_commute
-  (f: Val → TopM Val) (g: Val → TopM Val)
-  (COMMUTES: f >=> g = g >=> f) :
-  repeatM n f >=> repeatM m g = repeatM m g >=> repeatM n f := by {
-    induction n;
-    case zero => {
-      simp[repeatM];
+theorem Command.wp.is_precondition:
+  ∀ (Q: assertion) (c: Command), hoare_triple (c.wp Q) c Q := by {
+
+    intros Q c;
+    revert Q;
+    induction c;
+    case skip => {
+      intros Q;
+      apply Command.wp.skip.is_precondition;
     }
-    case succ n' IH => {
-      simp[repeatM];
-      rw[kleisliRight.assoc]
-      rw[IH];
-      rw[<- kleisliRight.assoc]
-      rw[repeatM.commuting_pull_right (g := f) (f := g) (n := m)
-        (COMMUTES := by simp[COMMUTES])];
-      rw[kleisliRight.assoc];
+    case assign x e => {
+      intros Q;
+      apply Command.wp.assign.is_precondition (C := rfl)
     }
-}
-
--- to show (f >=> k) = (f >=> h), it suffices to show that k = h
-theorem kleisli.cancel_left [M: Monad m] (f: α → m β) (k h : β → m γ) (H: k = h) :
-  f >=> k = f >=> h := by {
-    rw[H];
-}
-
--- Use `match goal` to move terms to the left.
-theorem repeatM.commuting_compose
-  (f: Val → TopM Val) (g: Val → TopM Val)
-  (n: Nat)
-  (COMMUTES: f >=> g = g >=> f) :
-  (repeatM n f) >=> repeatM n g =
-  repeatM n (f >=> g) := by {
-  induction n;
-  case zero => { simp[repeatM]; }
-  case succ n' IH => {
-    conv in (_ >=> _) => { repeat rw[repeatM.peel_left]; }
-    conv in (f >=> repeatM n' f) >=> g >=> repeatM n' g
-    rw[repeatM.peel_left];
+    case seq c1 c2 IH1 IH2 => {
+      simp[wp];
+      intros Q;
+      simp[hoare_triple, exec, pure, StateT.run, StateT.pure] at *;
+      intros env Penv;
+      simp[get, getThe, MonadStateOf.get, StateT.get, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+        StateT.lift];
+      simp[hoare_triple] at IH1;
+      specialize (IH1 _ _ Penv);
+      have ⟨st', EXEC_C1, Pst'⟩ := IH1;
+      specialize (IH2 _ _ Pst');
+      have ⟨st'', EXEC_C2, Qst''⟩ := IH2;
+      exists st'';
+      simp[EXEC_C1, EXEC_C2];
+      exact Qst'';
+    }
+    case if_ e c1 c2 IH1 IH2 => {
+      intros Q;
+      simp[hoare_triple, exec, pure, StateT.run, StateT.pure] at *;
+      intros env Penv;
+      simp[get, getThe, MonadStateOf.get, StateT.get, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+        StateT.lift];
+      simp[wp] at Penv;
+      have ⟨eval, EVAL, Penv'⟩ := Penv;
+      by_cases (eval = 0);
+      case pos EVAL_0 => {
+        simp[EVAL];
+        simp[EVAL_0] at *;
+        apply IH2;
+        exact Penv';
+      }
+      case neg EVAL_0 => {
+        simp[EVAL];
+        simp[EVAL_0] at *;
+        apply IH1;
+        exact Penv';
+      }
+    }
   }
-  simp[repeatM, COMMUTES];
-  case succ n' IH => {
-    calc repeatM (Nat.succ n') f >=> repeatM (Nat.succ n') g =
-          f >=> repeatM n' f >=> repeatM (Nat.succ n') g  := by {
-            simp[repeatM.peel_left];
-            simp[kleisliRight.assoc];
-          }
-    _ = f >=> repeatM n' f >=> g >=> repeatM n' g := by {
-          simp[repeatM.peel_left];
-    }
-    _ = f >=> g >=> repeatM n' f >=> repeatM n' g := by {
-      apply kleisli.cancel_left;
-      rw[repeatM.commuting_pull_left];
-      simp[kleisliRight.assoc];
-      apply kleisli.cancel_left;
-      apply repeatM.commuting_commute;
-      simp[COMMUTES];
 
-    }
-    _ = f >=> g >=> repeatM n' (f >=> g) := by {
-      simp[IH];
-    }
-    _ = repeatM (Nat.succ n') (f >=> g) := by {
-      simp[<- repeatM.peel_left];
-    }
+-- strongest postcondition
+def Command.sp (P: assertion): Command → assertion
+| Command.skip => P
+| (Command.assign x e) => fun env =>
+      ((e.eval env).isSome = True) ->
+          ∃ oldv, let oldenv := env.set ⟨x, oldv⟩; P oldenv /\ e.eval env = .some oldv
+| (Command.seq c₁ c₂) =>
+    let sp1 := c₁.sp P
+    let sp2 := c₂.sp sp1
+    sp2
+| (Command.if_ e c₁ c₂) =>
+  fun env => ∃ v,
+    (Expr.eval env e = .some v ∧
+      (v = 0 ∧ Command.sp P c₁ env) ∨ (v ≠ 0 ∧ Command.sp P c₂ env))
 
 
-  }
+theorem Command.sp.skip.is_postcondition:
+  ∀ (P: assertion), hoare_triple P Command.skip (Command.skip.sp P) := by {
+    intros P;
+    simp[hoare_triple, sp, exec, pure, StateT.run, StateT.pure];
 }
 
-end BoringKleisli
+/-
+TODO: how to prove strongest post-condition for assignment?
+-/
+theorem Command.sp.assign.is_postcondition:
+  ∀ {c: Command} {name: String} {val: Expr} {C: c = Command.assign name val}
+    {P: assertion}, hoare_triple P c (c.sp P) := by {
+    intros c name expr C P;
+    simp[C];
+    simp[hoare_triple, sp, exec, pure, StateT.run, StateT.pure];
+    intros env Penv;
+    simp[get, getThe, MonadStateOf.get, StateT.get, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+      StateT.lift];
+    simp[sp] at Penv;
+    simp[TopM.updateEnv, set, StateT.set, StateT.run, StateT.pure, bind, StateT.bind, pure, liftM, monadLift, MonadLift.monadLift,
+      StateT.lift, get, getThe, MonadStateOf.get, StateT.get];
+    cases EXPR_VAL:(Expr.eval env expr);
+    case none => {
+      simp[EXPR_VAL];
+      contradiction;
+    }
+}
+
+theorem Command.sp.is_postcondition:
+  ∀ (P: assertion) (c: Command), hoare_triple P c (c.sp P) := by { sorry }
+
+structure Rewrite where
+  find: Command
+  replace : Command
+  correct: ∀ (post: assertion), (replace.sp (find.wp post)).implies post
+
+
+def rewriteSubSubXEqZero : Rewrite :=
+  {
+    find := Command.assign "out" (Expr.sub (Expr.var "y") (Expr.var "y")),
+    replace := Command.assign "out" (Expr.const 0),
+    correct := fun post => by {
+      simp[Command.wp, Command.sp, assertion.implies];
+      intros env;
+      intros ENV_EVAL_0;
+      simp[Expr.eval] at ENV_EVAL_0;
+      have ⟨y_val, ⟨zerov, ZEROVAL⟩, ENV_EVAL_0'⟩ := ENV_EVAL_0;
+      clear ENV_EVAL_0;
+      simp[Env.set] at ZEROVAL;
+      sorry
+
+    }
+  }
+
+end Hoare
