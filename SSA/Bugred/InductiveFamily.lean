21ea7a0141983c7d56537f9c60a5097862e5a66e
Siddharth Bhat <siddu.druid@gmail.com>
2023-03-02 01:15:10 +0000

    add example where lean fails to produce nice error for inductive defn

---
+++ SSA/Bugred/InductiveFamily.lean
@@ -0,0 +1,11 @@
+abbrev Kind := Unit
+abbrev ExprKind :=  Unit
+ inductive OpKind : Kind → Kind → Type
+| const : OpKind () ()
+
+inductive Expr : ExprKind → Type where
+| ops: Expr () → Expr () → Expr () -- op ;; ops.
+
+inductive ExprEval {kindMotive : Kind → Type}
+  (opFold: {i o: Kind} → OpKind i o → kindMotive i → kindMotive o) : Expr ek  → Type where
+  | ops (EVAL_OPS: ExprEval opFold ops) : ExprEval opFold (Expr.ops op ops)
