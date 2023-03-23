1beacb5f5194c218c44e39702fcfd722ace061f8
Siddharth Bhat <siddu.druid@gmail.com>
2023-03-23 14:17:30 +0000

    show that having a single region means stuff works?

--- SSA/Experiment/SSARgn2TreeNoProof.lean
+++ SSA/Experiment/SSARgn2TreeNoProof.lean
@@ -55,7 +55,7 @@ notation e "[" name "↦" v "]" => Env.extend name v e
 class UserSemantics (opcode : Type)  where
   -- | Arguments given as (<args>, <rgn1>, ... <rgnN>)
   -- | Consider not allowing users to not be access all of 'Env'.
-  opcodeEval (op: opcode) (vals : Int × Int) (rgns :  List (Int × Int → Int)) : Int
+  opcodeEval (op: opcode) (vals : Int × Int) (rgns : (Int × Int → Int)) : Int
 
 attribute [simp] UserSemantics.opcodeEval
 
@@ -63,29 +63,23 @@ inductive ASTKind : Type where
 | O : ASTKind
 | Os : ASTKind
 | R : ASTKind
-| Rs : ASTKind
 deriving Inhabited, DecidableEq
 
 -- | The operations of the language.
 inductive AST (opcode : Type): ASTKind → Type where
-| assign (ret : VarName) (op : opcode) (args : VarName × VarName) (rgns : AST opcode .Rs) : AST opcode .O
+| assign (ret : VarName) (op : opcode)
+  (args : VarName × VarName)
+  (rgns : AST opcode .R) : AST opcode .O
 | ops1 (op : AST opcode .O) :  AST opcode .Os
 | opsmany (op : AST opcode .O) (ops : AST opcode .Os) : AST opcode .Os
-| rgn  (args : VarName × VarName) (body : AST opcode .Os) : AST opcode .R
-| rgns0 : AST opcode .Rs
-| rgnsmany (rgn : AST opcode .R) (rgns :  AST opcode .Rs) :  AST opcode .Rs
-
-def Region.toRegions : AST opcode .R → AST opcode .Rs
-| r => .rgnsmany r .rgns0
-
-instance : Coe (AST opcode .R) (AST opcode .Rs) where
-  coe := Region.toRegions
+| rgn (args : VarName × VarName) (body : AST opcode .Os) : AST opcode .R
+| rgn0 : AST opcode .R 
 
 def AST.retname : AST opcode .O → VarName
 | .assign (ret := ret) .. => ret
 
 instance [Inhabited opcode] : Inhabited (AST opcode .O) where
-  default := .assign .x0 default (.x0, .x0) .rgns0
+  default := .assign .x0 default (.x0, .x0) .rgn0
 
 instance [Inhabited opcode] : Inhabited (AST opcode .Os) where
   default := .ops1 default
@@ -94,21 +88,8 @@ instance [Inhabited opcode] : Inhabited (AST opcode .R) where
   default := .rgn (.x0, .x0) default
 
 instance : Inhabited (AST opcode .Rs) where
-  default := .rgns0
+  default := .rgn0
 
-@[simp]
-def Regions.ofRegion : AST opcode .R → AST opcode .Rs 
-| r => .rgnsmany r .rgns0 
-
-instance : Coe (AST opcode .R) (AST opcode .Rs) := ⟨Regions.ofRegion⟩
-#check DecidableEq
-@[simp]
-def Regions.ofList : List (AST opcode .R) → AST opcode .Rs
-| [] => .rgns0
-| r :: rs => .rgnsmany r (Regions.ofList rs)
-
-@[reducible, simp]
-instance : Coe (List (AST opcode .R)) (AST opcode .Rs) := ⟨Regions.ofList⟩
 
 @[simp]
 def Ops.ofList [Inhabited opcode] : List (AST opcode .O) → AST opcode .Os
@@ -124,14 +105,13 @@ def ASTKind.eval : ASTKind → Type
 | .O => Int
 | .Os => Int
 | .R => (Int × Int → Int)
-| .Rs => List (Int × Int → Int)
 -- evaluate an operation with repect to a particular user semantics.
 @[simp]
 def AST.eval [S: UserSemantics opcode]
   {astk: ASTKind} (e: Env Int): AST opcode astk → astk.eval × Env Int
-| .assign ret op args rgns =>
+| .assign ret op args r =>
     let (arg1, arg2) := args
-    let retval := S.opcodeEval op (e arg1, e arg2) (rgns.eval e).fst
+    let retval := S.opcodeEval op (e arg1, e arg2) (r.eval e).fst
     (retval, e.extend ret retval)
 | .ops1 op =>
    let (out, env) := op.eval e
@@ -145,11 +125,7 @@ def AST.eval [S: UserSemantics opcode]
       let e2 := e1.extend args.snd vals.snd
       let (outval, _) := body.eval e2
       outval, e)
-| .rgns0 => ([], e)
-| .rgnsmany r rs =>
-    let outr := (r.eval e).fst
-    let outrs := (rs.eval e).fst
-    (List.cons outr outrs, e)
+| .rgn0 => (fun _ => default, e)
 
 end StxSem
 
@@ -159,8 +135,6 @@ section Tree
 inductive CtreeKind
 | O -- op
 | R -- region (higher order)
-| Rs -- regions (higher order)
-
 
 -- closed trees, leaves are integers.
 inductive Ctree  (opcode : Type) : CtreeKind → Type where
@@ -168,10 +142,9 @@ inductive Ctree  (opcode : Type) : CtreeKind → Type where
   (op : opcode)
   (lhs : Ctree opcode .O)
   (rhs : Ctree opcode .O)
-  (rgns: Ctree opcode .Rs) : Ctree opcode .O
+  (rgns: Ctree opcode .R) : Ctree opcode .O
 | rgn (f : Int × Int → Ctree opcode .O) : Ctree opcode .R
-| rgns0 : Ctree opcode .Rs
-| rgns (rgn : Ctree opcode .R) (rgns : Ctree opcode .Rs) : Ctree opcode .Rs
+| rgn0 : Ctree opcode .R
 | leaf (val : Int) : Ctree opcode .O
 
 -- convert an AST into a closed tree under the given environment
@@ -184,17 +157,15 @@ def ASTKind.toCTree (opcode : Type) : ASTKind → Type
 | .O => Ctree opcode .O
 | .Os => Ctree opcode .O
 | .R => Ctree opcode .R
-| .Rs => Ctree opcode .Rs
 
 @[simp]
 def AST.toCtree_ {astk: ASTKind} (e : Env (Ctree opcode .O)) :
   AST opcode astk → (astk.toCTree opcode) × Env (Ctree opcode .O)
-| .assign ret opcode (u, v) rgns =>
-    let rgnsval := (rgns.toCtree_ e).fst
-    let t := .binop opcode (e u) (e v) rgnsval
+| .assign ret opcode (u, v) r =>
+    let rval := (r.toCtree_ e).fst
+    let t := .binop opcode (e u) (e v) rval
     (t, e.extend ret t)
-| .rgns0 => (.rgns0, e)
-| .rgnsmany r rs => (.rgns (r.toCtree_ e).fst (rs.toCtree_ e).fst, e)
+| .rgn0 => (.rgn0, e)
 | .rgn args body =>
     (.rgn fun vals  =>
       let e1 := e.extend args.fst (.leaf vals.fst)
@@ -227,7 +198,6 @@ def Ops.toCtree (a: AST opcode .Os) (e: Env Int) : Ctree opcode .O :=
 def CtreeKind.eval : CtreeKind → Type
 | .O => Int
 | .R => Int × Int → Int
-| .Rs => List (Int × Int → Int)
 
 -- Note: is this literally "just" staging the partial evaluation against the environment?
 @[simp]
@@ -235,9 +205,8 @@ def Ctree.eval [UserSemantics opcode] : Ctree opcode treek → treek.eval
 | .binop o l r rs =>
   UserSemantics.opcodeEval o (l.eval, r.eval) rs.eval
 | .leaf v => v
-| .rgns0 => []
+| .rgn0 => fun _ => default
 | .rgn f => fun args => (f args).eval
-| .rgns r rs => r.eval :: rs.eval
 
 end Tree
 
@@ -275,31 +244,29 @@ instance :  UserSemantics Opcode  where
 def x_add_4_times_mul_val_eq (env: Env Int):
   let p : AST Opcode .Os :=
       Ops.ofList [
-      .assign .x1 .add (.x0, .x0) .rgns0,
-      .assign .x2 .add (.x1, .x1) .rgns0
+      .assign .x1 .add (.x0, .x0) .rgn0,
+      .assign .x2 .add (.x1, .x1) .rgn0
       ]
   let q : AST Opcode .Os := Ops.ofList [
-        .assign .x1 (.const 4) (.x0, .x0) .rgns0
-      , .assign .x2 .mul (.x1, .x0) .rgns0
+        .assign .x1 (.const 4) (.x0, .x0) .rgn0
+      , .assign .x2 .mul (.x1, .x0) .rgn0
     ]
   (Ops.toCtree p env).eval = (Ops.toCtree q env).eval := by {
     simp only [Ops.ofList, AST.eval, Ops.toCtree,  AST.toCtree_];
     -- see that there are environments, which are folded away when calling
     -- Ctree.eval.
     simp[Ctree.eval];
-    simp[CtreeKind.eval];
-    linarith
   }
 
 
 def run_inline (env: Env Int) (o : AST Opcode .Os):
   let p : AST Opcode .Os :=
       Ops.ofList [
-      .assign .x2 .add (.x0, .x1) .rgns0 
+      .assign .x2 .add (.x0, .x1) .rgn0 
       ]
   let q : AST Opcode .Os := Ops.ofList [
       .assign .x2 .run (.x0, .x1) (AST.rgn ⟨.x5, .x6⟩ (Ops.ofList [
-        .assign .y1 .add (.x5, .x6) .rgns0
+        .assign .y1 .add (.x5, .x6) .rgn0
       ]))
     ]
   (Ops.toCtree p env).eval = (Ops.toCtree q env).eval := by {
@@ -311,8 +278,8 @@ def run_inline (env: Env Int) (o : AST Opcode .Os):
 def runnot_inline (env: Env Int) (r : AST Opcode .R):
   let p : AST Opcode .Os :=
       Ops.ofList [
-      .assign .y1 .not ⟨.x0, .null⟩ .rgns0
-      , .assign .y2 .run (.x1, .y1) r
+      .assign .y1 .not ⟨.x0, .null⟩ .rgn0
+      , .assign .y2 .run (.y1, .x1) r
       ]
   let q : AST Opcode .Os := Ops.ofList [
       .assign .x2 .runnot (.x0, .x1) (AST.rgn ⟨.z1, .z2⟩ (Ops.ofList [
@@ -321,29 +288,8 @@ def runnot_inline (env: Env Int) (r : AST Opcode .R):
     ]
   (Ops.toCtree p env).eval = (Ops.toCtree q env).eval := by {
     simp
-    -- see that this cannot be simplified away by pure 'simp'. Some thought
-    -- is needed here.
-    sorry
   }
 
-def ite_switch_branch_negate_cond (env: Env Int) (r1 r2 : AST Opcode .R):
-  let p : AST Opcode .Os :=
-      Ops.ofList [
-      .assign .x1 .not (.x0, .null) .rgns0,
-      .assign .x2 .ite (.x1, .null) (Regions.ofList [r2, r1])
-      ]
-  let q : AST Opcode .Os := Ops.ofList [
-      .assign .x2 .ite (.x0, .null) (Regions.ofList [r1, r2])
-    ]
-  (Ops.toCtree p env).eval = (Ops.toCtree q env).eval := by {
-    simp
-    by_cases X0:env VarName.x0 = 0  <;> simp[X0]
-    
-    -- see that this cannot be simplified away by pure 'simp'. Some thought
-    -- is needed here.
-    sorry
-    sorry
-  }
 end MultipleInstructionTree
 
 end SSARgn2Tree
