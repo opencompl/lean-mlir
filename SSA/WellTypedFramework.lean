340ba04073d60bec54e906438a114e66103604b1
Siddharth Bhat <siddu.druid@gmail.com>
2023-04-27 21:40:40 +0100

    feat: make code bollu style. @goens: feel invited to revert if you don't like it

--- SSA/WellTypedFramework.lean
+++ SSA/WellTypedFramework.lean
@@ -5,8 +5,11 @@ import Mathlib.Data.Option.Basic
 Typeclass for a `baseType` which is a godel code of 
 Lean types.
 -/
-class Godel (BaseType : Type)  : Type 1 where
-  toType : BaseType → Type
+class Godel (β : Type)  : Type 1 where
+  toType : β → Type
+open Godel /- make toType publically visible in module. -/
+
+notation "⟦" x "⟧" => Godel.toType x
 
 instance : Godel Unit where toType := fun _ => Unit
 
@@ -18,110 +21,113 @@ inductive UserType (T : Type) : Type where
 
 namespace UserType
 
-instance: Inhabited (UserType BaseType) where default := UserType.unit
+instance: Inhabited (UserType β) where default := UserType.unit
 
-def toType [Godel BaseType] : UserType BaseType → Type
-  | .base t => Godel.toType t
+def toType [Godel β] : UserType β → Type
+  | .base t =>  ⟦t⟧
   | .pair k₁ k₂ => (toType k₁) × (toType k₂)
   | .triple k₁ k₂ k₃ => toType k₁ × toType k₂ × toType k₃
   | .unit => Unit
 
-def mkPair {BaseType : Type} [Godel BaseType] {k₁ k₂ : UserType BaseType}: toType k₁ → toType k₂ → toType (.pair k₁ k₂)
+instance [Godel β] : Godel (UserType β) where 
+  toType := toType 
+
+def mkPair [Godel β] {k₁ k₂ : UserType β}: ⟦k₁⟧ → ⟦k₂⟧ → ⟦k₁.pair k₂⟧
   | k₁, k₂ => (k₁, k₂)
 
-def mkTriple {BaseType : Type} [Godel BaseType] {k₁ k₂ k₃ : UserType BaseType}: toType k₁ → toType k₂ → toType k₃ → toType (.triple k₁ k₂ k₃)
+def mkTriple [Godel β] {k₁ k₂ k₃ : UserType β}: ⟦k₁⟧ → ⟦k₂⟧ → ⟦k₃⟧ → ⟦k₁.triple k₂ k₃⟧
   | k₁, k₂, k₃ => (k₁, k₂, k₃)
 
-def fstPair {BaseType : Type} [Godel BaseType] {k₁ k₂ : UserType BaseType} : toType (.pair k₁ k₂) → toType k₁
+def fstPair [Godel β] {k₁ k₂ : UserType β} : ⟦k₁.pair k₂⟧ → ⟦k₁⟧
   | (k₁, _) => k₁
 
-def sndPair {BaseType : Type} [Godel BaseType] {k₁ k₂ : UserType BaseType} : toType (.pair k₁ k₂) → toType k₂
+def sndPair [Godel β] {k₁ k₂ : UserType β} : ⟦k₁.pair k₂⟧ → ⟦k₂⟧
   | (_, k₂) => k₂
 
-def fstTriple {BaseType : Type} [Godel BaseType] {k₁ k₂ k₃ : UserType BaseType} : toType (.triple k₁ k₂ k₃) → toType k₁
+def fstTriple [Godel β] {k₁ k₂ k₃ : UserType β} : ⟦k₁.triple k₂ k₃⟧ → ⟦k₁⟧
   | (k₁, _, _) => k₁
 
-def sndTriple {BaseType : Type} [Godel BaseType] {k₁ k₂ k₃ : UserType BaseType} : toType (.triple k₁ k₂ k₃) → toType k₂
+def sndTriple [Godel β] {k₁ k₂ k₃ : UserType β} : ⟦k₁.triple k₂ k₃⟧ → ⟦k₂⟧
   | (_, k₂, _) => k₂
 
-def trdTriple {BaseType : Type} [Godel BaseType] {k₁ k₂ k₃ : UserType BaseType} : toType (.triple k₁ k₂ k₃) → toType k₃
+def trdTriple [Godel β] {k₁ k₂ k₃ : UserType β} : ⟦k₁.triple k₂ k₃⟧ → ⟦k₃⟧
   | (_, _, k₃) => k₃
 
 end UserType
 
 
-structure UserData {BaseType : Type} [Godel BaseType] where
-  type : UserType BaseType
-  value : type.toType
+structure UserData [Godel β] where
+  type : UserType β
+  value : toType type
 
 
--- @goens: I suggest removing [DecidableEq ...] [Godel ...]
--- We do not keep typeclass constraints on definitions, only on use sites.
--- See the Haskell rationale: 
-inductive  TypeData (BaseType : Type) : Type
-  | some : BaseType → TypeData BaseType
-  | unit : TypeData BaseType
-  | pair : TypeData BaseType → TypeData BaseType → TypeData BaseType
-  | triple : TypeData BaseType → TypeData BaseType → TypeData BaseType → TypeData BaseType
-  | any : TypeData BaseType
-  | unused : TypeData BaseType -- Or bound
+inductive  TypeData (β : Type) : Type
+  | some : β → TypeData β
+  | unit : TypeData β
+  | pair : TypeData β → TypeData β → TypeData β
+  | triple : TypeData β → TypeData β → TypeData β → TypeData β
+  | any : TypeData β
+  | unused : TypeData β -- Or bound
 
 @[coe]
-def UserType.toTypeData [Godel BaseType] : UserType BaseType → TypeData BaseType
+def UserType.toTypeData [Godel β] : UserType β → TypeData β
   | UserType.base t => TypeData.some t
   | UserType.unit => TypeData.unit
   | UserType.pair k₁ k₂ => TypeData.pair (UserType.toTypeData k₁) (UserType.toTypeData k₂)
   | UserType.triple k₁ k₂ k₃ => TypeData.triple (UserType.toTypeData k₁) (UserType.toTypeData k₂)
     (UserType.toTypeData k₃)
 
-variable {BaseType : Type} [instDecidableEqBaseType : DecidableEq BaseType] [instGodel : Godel BaseType]
-instance : Coe (UserType BaseType) (TypeData BaseType) := ⟨UserType.toTypeData⟩
+variable {β : Type} [instDecidableEqBaseType : DecidableEq β] [instGodel : Godel β]
+instance : Coe (UserType β) (TypeData β) := ⟨UserType.toTypeData⟩
 
-class TypedUserSemantics (Op : Type) (BaseType : Type) [Godel BaseType] where
-  argKind : Op → UserType BaseType
-  rgnDom : Op → UserType BaseType
-  rgnCod : Op → UserType BaseType
-  outKind : Op → UserType BaseType
-  eval : ∀ (o : Op), (argKind o).toType → ((rgnDom o).toType →
-    (rgnCod o).toType) → (outKind o).toType
+class TypedUserSemantics (Op : Type) (β : Type) [Godel β] where
+  argKind : Op → UserType β
+  rgnDom : Op → UserType β
+  rgnCod : Op → UserType β
+  outKind : Op → UserType β
+  eval : ∀ (o : Op), toType (argKind o)  → (toType (rgnDom o) →
+    toType (rgnCod o)) → toType (outKind o)
 
 @[simp]
-def TypeData.toType : TypeData BaseType → Type
+def TypeData.toType : TypeData β → Type
   | TypeData.some t => Godel.toType t
   | TypeData.unit => Unit
   | TypeData.pair t₁ t₂ => (TypeData.toType  t₁) × (TypeData.toType t₂)
   | TypeData.triple t₁ t₂ t₃ => (TypeData.toType t₁) × (TypeData.toType t₂) × (TypeData.toType t₃)
-  | TypeData.any => Σ (k : UserType BaseType), UserType.toType k
+  | TypeData.any => Σ (k : UserType β), ⟦k⟧
   | TypeData.unused => Unit
 
-theorem TypeDataToTypeEqUserTypeToType (a : UserType BaseType) : a.toType = TypeData.toType a.toTypeData :=
+instance : Godel (TypeData β) where 
+  toType := TypeData.toType
+
+theorem TypeDataToTypeEqUserTypeToType (a : UserType β) : 
+  toType a = TypeData.toType a.toTypeData :=
   by
-    induction a <;> (try rfl)
-    case pair k₁ k₂ ih₁ ih₂ => simp [TypeData.toType, UserType.toType, ih₁, ih₂]
-    case triple k₁ k₂ k₃ ih₁ ih₂ ih₃ => simp [TypeData.toType, UserType.toType, ih₁, ih₂, ih₃]
+    induction a <;> 
+      (simp [Godel.toType, TypeData.toType, UserType.toType] at * <;> aesop)
 
 
 @[simp]
-def SSAIndex.ieval (Op : Type) (BaseType : Type) : SSAIndex → Type
-  | .TERMINATOR => TypeData BaseType
-  | .EXPR => TypeData BaseType
-  | .REGION => TypeData BaseType × TypeData BaseType
+def SSAIndex.ieval (Op : Type) (β : Type) : SSAIndex → Type
+  | .TERMINATOR => TypeData β
+  | .EXPR => TypeData β
+  | .REGION => TypeData β × TypeData β
   | .STMT => List Var
 
-abbrev K (BaseType : Type)  
-  : Type → Type := StateT ((Var → TypeData BaseType) × (Var → (TypeData  BaseType × TypeData BaseType))) Option
+abbrev K (β : Type)  
+  : Type → Type := StateT ((Var → TypeData β) × (Var → (TypeData  β × TypeData β))) Option
 
 -- Lean gets very picky here and wants to get all instances passed explicit to be sure they're the same
-def getVars : K BaseType (Var → TypeData BaseType) :=
+def getVars : K β (Var → TypeData β) :=
   do return (← StateT.get).1
 
-def getVarType (v : Var) : K BaseType (TypeData BaseType) :=
+def getVarType (v : Var) : K β (TypeData β) :=
   do return (← getVars) v
 
-def updateType (v : Var) (t : TypeData BaseType) : K BaseType Unit := do
+def updateType (v : Var) (t : TypeData β) : K β Unit := do
   StateT.modifyGet (fun s => ((), fun v' => if v' = v then t else s.1 v', s.2))
 
-def assignVar (v : Var) (t : TypeData BaseType) : K BaseType Unit := do
+def assignVar (v : Var) (t : TypeData β) : K β Unit := do
   let k ← getVarType v
   match k with
   | .unused => updateType v t
@@ -129,7 +135,7 @@ def assignVar (v : Var) (t : TypeData BaseType) : K BaseType Unit := do
 
 /-- Does not change the state. Fails if impossible to unify or if either
 input is `unused`. TypeData is a meet semilattice with top element `any`.  -/
-def unify : (t₁ t₂ : TypeData BaseType) → K BaseType (TypeData BaseType)
+def unify : (t₁ t₂ : TypeData β) → K β (TypeData β)
   | .unused, _ => failure
   | _, .unused => failure
   | .any, k => return k
@@ -150,25 +156,25 @@ def unify : (t₁ t₂ : TypeData BaseType) → K BaseType (TypeData BaseType)
   | .unit, .unit => return .unit
   | _, _ => failure
 
-def unifyVar (v : Var) (t : TypeData BaseType) : K BaseType  (TypeData BaseType) := do
+def unifyVar (v : Var) (t : TypeData β) : K β  (TypeData β) := do
   let k ← getVarType v
   let k' ← unify k t
   updateType v k'
   return k'
 
-def unifyRVar (v : Var) (dom : TypeData BaseType) (cod : TypeData BaseType) : K BaseType (TypeData  BaseType × TypeData BaseType) := do
+def unifyRVar (v : Var) (dom : TypeData β) (cod : TypeData β) : K β (TypeData  β × TypeData β) := do
   let s ← StateT.get
   let dom' ← unify dom (s.2 v).1
   let cod' ← unify cod (s.2 v).2
   return (dom', cod')
 
 @[simp]
-def assignAny (t : Var → TypeData BaseType) (v : Var) : K BaseType Unit :=
+def assignAny (t : Var → TypeData β) (v : Var) : K β Unit :=
   match t v with
   | .unused => assignVar v .any
   | _ => failure
 
-def unassignVars (l : List Var) : K BaseType Unit := do
+def unassignVars (l : List Var) : K β Unit := do
   l.forM (fun v => updateType v .unused)
 
 /-- Given an expected type, tries to infer the type of an expression.
@@ -183,11 +189,11 @@ Return the type of the returned variable. Fails when bound variable is not `unus
 * If `EXPR`, then return type of expression, and do not change the type of bound
 variables, only free variables.
 * If `REGION` then as for `EXPR`  -/
--- @chris: the type signature should be `SSA Op i → K (Context BaseType)`?
-def inferType {Op : Type}  [TUS : TypedUserSemantics Op BaseType] :
+-- @chris: the type signature should be `SSA Op i → K (Context β)`?
+def inferType {Op : Type}  [TUS : TypedUserSemantics Op β] :
     {i : SSAIndex} → SSA Op i →
-    (i.ieval Op BaseType)
-    → K BaseType (i.ieval Op BaseType)
+    (i.ieval Op β)
+    → K β (i.ieval Op β)
   | _,  .assign lhs rhs rest, _ => do
     let k ← inferType rhs (← getVarType lhs)
     assignVar lhs k
@@ -222,7 +228,7 @@ def inferType {Op : Type}  [TUS : TypedUserSemantics Op BaseType] :
     return (dom, cod)
 
 -- @chris: TODO: implement `eval`:
---   `SSA Op i → Environment (Option Context BaseType) → Option i.eval` or some such.
+--   `SSA Op i → Environment (Option Context β) → Option i.eval` or some such.
 
 -- @[simp]
 -- def TypeData.inf {Kind : Type} [DecidableEq Kind] :
@@ -344,18 +350,18 @@ def inferType {Op : Type}  [TUS : TypedUserSemantics Op BaseType] :
 
 @[simp]
 def SSAIndex.teval (Op : Type)
-    [TypedUserSemantics Op BaseType] (sem : Var → TypeData BaseType) :
+    [TypedUserSemantics Op β] (sem : Var → TypeData β) :
     SSAIndex → Type
   | .TERMINATOR => Var
-  | .EXPR => Σ (t : TypeData BaseType) (_ : Var → TypeData BaseType),
+  | .EXPR => Σ (t : TypeData β) (_ : Var → TypeData β),
       ((∀ v, (sem v).toType) → t.toType)
-  | .REGION => Σ (dom cod : TypeData BaseType), ((∀ v, (sem v).toType) → dom.toType → cod.toType)
-  | .STMT => (∀ v, (sem v).toType) → Σ (s : Var → TypeData BaseType),  (∀ v, (s v).toType)
+  | .REGION => Σ (dom cod : TypeData β), ((∀ v, (sem v).toType) → dom.toType → cod.toType)
+  | .STMT => (∀ v, (sem v).toType) → Σ (s : Var → TypeData β),  (∀ v, (s v).toType)
 
 open TypeData
 
-def SSA.teval {Op : Type} [TypedUserSemantics Op BaseType]
-    : (sem : Var → TypeData BaseType) → {k : SSAIndex} → SSA Op k →
+def SSA.teval {Op : Type} [TypedUserSemantics Op β]
+    : (sem : Var → TypeData β) → {k : SSAIndex} → SSA Op k →
     k.teval Op sem
 | sem, _, .assign lhs rhs rest => fun s => sorry
   -- let ⟨sem₁, rest⟩ := SSA.teval (Kind := Kind) sem rest s
