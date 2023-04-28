ceb94c5fea012dadf17b593dcd91cf5fcf44a4f4
ChrisHughes24 <chrishughes24@gmail.com>
2023-04-28 16:22:44 +0100

    Start thinking about evaluation

--- SSA/WellTypedFramework.lean
+++ SSA/WellTypedFramework.lean
@@ -278,9 +278,7 @@ def inferTypeCore {Op : Type}  [TUS : TypedUserSemantics Op β] :
   | _, .var v, k => unifyVar v k
   | _, .rgnvar v, k => unifyRVar v k.1 k.2
   | _, .rgn0, k => do
-    let _ ← unify k.1 UserType.unit.toTypeData
-    let _ ← unify k.2 UserType.unit.toTypeData
-    return (UserType.unit.toTypeData, UserType.unit.toTypeData)
+    return (← unify k.1 UserType.unit.toTypeData, ← unify k.2 UserType.unit.toTypeData)
   | _, .rgn arg body, k => do
     let dom ← unifyVar arg k.1
     let cod ← inferTypeCore body k.2
@@ -294,161 +292,44 @@ def inferType {Op : Type} [TypedUserSemantics Op β]
   return (k.1, ← k.2.1.pmap TypeData.toUserType, ← k.2.2.pmap
     fun x => do return (← x.1.toUserType, ← x.2.toUserType))
 
+def EnvC (c : Context (UserType β)) :=
+  ∀ (v : Var), (c.lookup v).elim Unit UserType.toType
+
+variable (β)
+
+def UVal := Σ (k : UserType β), k.toType
+
+def EnvU : Type :=
+  Var → Option (UVal β)
+
+variable {β}
+
+def EnvC.toEnvU {c : Context (UserType β)} (e : EnvC c) : EnvU β := by
+  dsimp [EnvC] at *
+  intro v
+  have ev := e v
+  revert ev
+  cases c.lookup v
+  . exact fun _ => none
+  . exact fun x => some ⟨_, x⟩
+
+-- def SSA.teval {Op : Type} [TypedUserSemantics Op β]
+--   (e : EnvU β) (er : ) :
+--   (e : EnvC c) → SSA Op k → k.eval Val
+-- | .assign lhs rhs rest =>
+--     rest.teval (e.set lhs (rhs.eval e re)) re
+-- | .nop => _
+-- | .ret above v => _
+-- | .pair fst snd => _
+-- | .triple fst snd third => _
+-- | .op o arg r => _
+-- | .var v => _
+-- | .rgnvar v => _
+-- | .rgn0 => _
+-- | .rgn arg body => _
 
 -- @chris: TODO: implement `eval`:
 --   `SSA Op i → Environment (Option Context β) → Option i.eval` or some such.
-
--- @[simp]
--- def TypeData.inf {Kind : Type} [DecidableEq Kind] :
---     TypeData Kind → TypeData Kind → TypeData Kind
---   | .none, _ => .none
---   | _, .none => .none
---   | .some k₁, .some k₂ => if k₁ = k₂ then .some k₁ else .none
---   | .some k, _ => .some k
---   | _, .some k => .some k
---   | .any, _ => .any
---   | _, .any => .any
---   | _, _ => .unused
-
-
-
--- @[simp]
--- def ofInfLeft [DecidableEq Kind] [TypeSemantics Kind] :
---     {t₁ t₂ : TypeData Kind} → (t₁.inf t₂).toType → t₁.toType
---   | .unused, .unused, x => x
---   | .unused, _, _ => ()
---   | .any, .unused, x => x
---   | .any, .any, x => x
---   | .any, .some _, x => ⟨_, x⟩
---   | .some _, .unused, x => x
---   | .some _, .any, x => x
---   | .some k₁, .some k₂, x =>
---       if h : k₁ = k₂
---       then by
---         subst h
---         simp at x
---         exact x
---       else by simp [h] at x; exact Empty.elim x
-
--- @[simp]
--- def ofInfRight [DecidableEq Kind] [TypeSemantics Kind] :
---     {t₁ t₂ : TypeData Kind} → (t₁.inf t₂).toType → t₂.toType
---   | .unused, .unused, x => x
---   | _, .unused, _ => ()
---   | .unused, .any, x => x
---   | .any, .any, x => x
---   | .some _, .any, x => ⟨_, x⟩
---   | .unused, .some _, x => x
---   | .any, .some _, x => x
---   | .some k₁, .some k₂, x =>
---       if h : k₁ = k₂
---       then by
---         subst h
---         simp at x
---         exact x
---       else by simp [h] at x; exact Empty.elim x
-
--- @[simp]
--- def toInfLeft [DecidableEq Kind] [TypeSemantics Kind] :
---     {t₁ t₂ : TypeData Kind} → t₁.toType → t₂.toType → (t₁.inf t₂).toType
---   | .unused, .unused, x, y => by simp; exact ()
---   | .unused, .any, x, y => y
---   | .unused, .some _, x, y => y
---   | .any, .unused, x, y => x
---   | .any, .any, x, y => x
---   | .any, .some _, x, y => y
---   | .some _, .unused, x, y => x
---   | .some _, .any, x, y => x
---   | .some k₁, .some k₂, x, y =>
---     if h : k₁ = k₂
---     then by
---       subst h
---       simp
---       exact x
---     else by simp [h]
-
-
-
--- @[simp]
--- def unassign (t : Var → TypeData Kind) (v : Var) :
---     Var → TypeData Kind :=
---   fun v' => if v = v' then .unused else t v'
-
--- @[simp]
--- def toUnassign [TypeSemantics Kind] (t : Var → TypeData Kind) (v : Var) :
---     ∀ v', (t v').toType → (unassign t v v').toType :=
---   fun v' => if h : v = v' then by subst h; simp; exact fun _ => ()
---     else by simp [h]; exact id
-
-
-
--- @[simp]
--- def ofAssign [DecidableEq Kind] [TypeSemantics Kind] (t : Var → TypeData Kind) (v : Var) (k : TypeData Kind) :
---     ∀ v', (assign t v k v').toType → (t v').toType :=
---   fun v' =>
---   if h : v = v'
---   then by simp only [h, assign, if_true]; exact ofInfLeft
---   else by simp only [h, assign, if_false]; exact id
-
--- @[simp]
--- def toAssign [DecidableEq Kind] [TypeSemantics Kind]
---    (t : Var → TypeData Kind) (v : Var) (k : TypeData Kind) (semk : TypeData.toType k) :
---     ∀ v', (t v').toType → (assign t v k v').toType :=
---   fun v' =>
---   if h : v = v'
---   then by simp only [h, assign, if_true]; _
---   else by simp only [h, assign, if_false]; exact id
-
--- @[simp]
--- def TypeData.rToType {Kind : Type} [TypeSemantics Kind] :
---     TypeData (Kind × Kind) → Type
---   | TypeData.none => Empty
---   | TypeData.some (k₁, k₂) => TypeSemantics.toType k₁ → TypeSemantics.toType k₂
---   | TypeData.any => Σ (k₁ k₂ : Kind),
---       TypeSemantics.toType k₁ → TypeSemantics.toType k₂
---   | TypeData.unused => Unit
-
--- @[simp]
--- def SSAIndex.teval (Op : Type) (Kind : Type) [TypeSemantics Kind]
---     [TypedUserSemantics Op Kind] : SSAIndex → Type
---   | .STMT => Σ (sem : Var → TypeData Kind), (∀ v, (sem v).toType)
---   | .REGION => Σ k₁ k₂ : Kind, TypeSemantics.toType k₁ → TypeSemantics.toType k₂
---   | .TERMINATOR => Σ k : Kind, TypeSemantics.toType k
---   | .EXPR => Σ k : Kind, TypeSemantics.toType k
-
-@[simp]
-def SSAIndex.teval (Op : Type)
-    [TypedUserSemantics Op β] (sem : Var → TypeData β) :
-    SSAIndex → Type
-  | .TERMINATOR => Var
-  | .EXPR => Σ (t : TypeData β) (_ : Var → TypeData β),
-      ((∀ v, (sem v).toType) → t.toType)
-  | .REGION => Σ (dom cod : TypeData β), ((∀ v, (sem v).toType) → dom.toType → cod.toType)
-  | .STMT => (∀ v, (sem v).toType) → Σ (s : Var → TypeData β),  (∀ v, (s v).toType)
-
-open TypeData
-
-def SSA.teval {Op : Type} [TypedUserSemantics Op β]
-    : (sem : Var → TypeData β) → {k : SSAIndex} → SSA Op k →
-    k.teval Op sem
-| sem, _, .assign lhs rhs rest => fun s => sorry
-  -- let ⟨sem₁, rest⟩ := SSA.teval (Kind := Kind) sem rest s
-  -- let ⟨t, sem₂, f⟩ := SSA.teval (Kind := Kind) sem rhs
-  -- let sem₁' := unassign sem₁ lhs
-  -- ⟨_root_.assign (fun v => ((sem₁' v).inf (sem₂ v))) lhs t,
-  --   fun v => sorry⟩
-| sem, _, .nop => fun f => ⟨sem, f⟩
-| _, _, .ret above v => v
-| _, _, .pair fst snd =>
-  ⟨TypeData.pair .any .any,
-   _⟩
-| _, _, .triple fst snd third => _
-| _, _, .op o arg r =>
-  ⟨_, _⟩
-| _, _, .var v => _
-| _, _, .rgnvar v => _
-| _, _, .rgn0 => _
-| _, _, .rgn arg body => _
 
 -- We can recover the case with the TypeSemantics as an instance
 def TypeSemantics : Type 1 :=
