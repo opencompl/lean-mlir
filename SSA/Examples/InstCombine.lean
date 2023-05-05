c1e5e5cf9cee984f39d4296e8009785ecceaa05c
Andres Goens <andres.goens@ed.ac.uk>
2023-05-05 18:22:47 +0100

    Revive length-indexed lists
    will try to use three different representations for bitvectors

--- SSA/Examples/InstCombine.lean
+++ SSA/Examples/InstCombine.lean
@@ -3,11 +3,110 @@ import Aesop
 
 namespace InstCombine
 
-abbrev Width := { x : Nat // 0 < x } -- difference with { x : Nat  | 0 < x }?
-abbrev BitVector (w : Width) := Fin (2^w)
-inductive BaseType
-| bitvec (w : Width) : BaseType
+abbrev Width := { x : Nat // x > 0 } -- difference with { x : Nat  | 0 < x }?
 
+inductive BaseType
+  | bitvec (w : Width) : BaseType
+
+inductive LengthIndexedList (α : Type u) : Nat → Type u where
+   | nil : LengthIndexedList α 0
+   | cons : α → LengthIndexedList α n → LengthIndexedList α (n + 1)
+  deriving Repr, DecidableEq
+
+def LengthIndexedList.fromList {α : Type u} (l : List α) : LengthIndexedList α (List.length l) :=
+  match l with
+  | [] => LengthIndexedList.nil
+  | x :: xs => LengthIndexedList.cons x (LengthIndexedList.fromList xs)
+
+theorem _root_.Nat.lt_succ_lt_succ : {a b : Nat} → Nat.succ a < Nat.succ b → a < b := by
+  sorry
+
+def LengthIndexList.nth {α : Type u} {n : Nat} (l : LengthIndexedList α n) (i : Fin n) : α :=
+  match l, i with
+  | LengthIndexedList.cons x _, ⟨0, _⟩ => x
+  | LengthIndexedList.cons _ xs, ⟨i + 1, h⟩ => nth xs ⟨i, Nat.lt_succ_lt_succ h⟩
+
+@[simp]
+def LengthIndexedList.foldl {α β : Type u} {n : Nat} (f : β → α → β) (acc : β) : LengthIndexedList α n → β
+  | nil => acc
+  | cons x xs => LengthIndexedList.foldl f (f acc x) xs
+
+@[simp]
+def LengthIndexedList.zipWith {α β γ : Type u} {n : Nat} (f : α → β → γ) :
+    LengthIndexedList α n → LengthIndexedList β n → LengthIndexedList γ n
+  | nil, nil => nil
+  | cons x xs, cons y ys => cons (f x y) (zipWith f xs ys)
+
+--
+structure BitVector (width : Width) where
+  (bits : LengthIndexedList Bool width)
+  deriving Repr, DecidableEq
+
+def BitVectorFun {width : Width} := Fin width.val → Bool
+
+def BitVectorFun.fromList {width : Width} (l : LengthIndexedList Bool width.val) : @BitVectorFun width :=
+  fun i =>
+
+def nextSignificantBit (val : Nat) (b : Bool) := 2 * val + if (b = true) then 1 else 0
+
+-- We could instead just define this directly as Fin (2^width)
+def RawBitVectorVal {w : Width} (x : LengthIndexedList Bool w) : Nat :=
+  x.foldl nextSignificantBit 0
+
+-- #eval RawBitVectorVal (LengthIndexedList.fromList [false,true]) -- 1
+-- #eval RawBitVectorVal (LengthIndexedList.fromList [true,false,false,false]) -- 8
+-- #eval RawBitVectorVal $ LengthIndexedList.fromList [true,true,false,false,false] -- 24
+-- #eval RawBitVectorVal $ LengthIndexedList.fromList [true,false,false,false, true] -- 17
+#eval RawBitVectorVal $ (LengthIndexedList.fromList [true,false,false,false]).cons true -- 24
+
+theorem nextSignificantBitTrue {val : Nat} : nextSignificantBit val true = 2 * val + 1 := by
+  simp [nextSignificantBit]
+
+theorem nextSignificantBitFalse {val : Nat} : nextSignificantBit val false = 2 * val := by
+  simp [nextSignificantBit]
+
+theorem RawBitVectorFalse {w : Width} {x : LengthIndexedList Bool w} :
+  (LengthIndexedList.cons false x).foldl nextSignificantBit 0 = x.foldl nextSignificantBit 0 := by simp[LengthIndexedList.foldl, nextSignificantBitFalse]
+
+def testExample := fun n => [true,false,true].foldl nextSignificantBit n
+#eval [0,1,2,3,4,5].map testExample
+theorem foldNextSignificantBit {w : Nat} {x : LengthIndexedList Bool w} {v : Nat} :
+  x.foldl nextSignificantBit n = 2^w + x.foldl nextSignificantBit 0 := by
+    induction x with
+      | nil => simp[LengthIndexedList.foldl, nextSignificantBit]
+      | cons b bs ih => simp
+
+theorem RawBitVectorValNil : RawBitVectorVal (LengthIndexedList.nil) = 0 := by
+  simp [RawBitVectorVal]
+
+-- Appends to the most significant bit
+theorem RawBitVectorValConsTrue {w : Width} (x : LengthIndexedList Bool w) (b : Bool) :
+    RawBitVectorVal (x.cons true) =  2 * RawBitVectorVal x + 1 := by
+      simp [RawBitVectorVal, LengthIndexedList.foldl]
+      rw [LengthIndexedList.foldl]
+
+
+
+theorem RawBitVectorValGrowth {w : Width} (x : LengthIndexedList Bool (w - 1)) : ∀ b : Bool, RawBitVectorVal (x.cons b) ≤ 2 * RawBitVectorVal x := by
+  intro b; cases b with
+   | false => simp [RawBitVectorVal]; rw [Nat.two_mul]
+              apply Nat.le_add_left
+   | true => simp [RawBitVectorVal]; unfold LengthIndexedList.foldl <;> cases w
+
+
+theorem BitVector.valRawLT {w : Width} (x : LengthIndexedList Bool w) : RawBitVectorVal x < 2^w := by
+  simp [RawBitVectorVal]
+  induction x with
+  | nil => simp
+  | cons b bs ih => cases b with
+    | false => rw [Nat.pow_succ,Nat.mul_two]
+               apply Nat.lt_add_right
+               simp [LengthIndexedList.foldl] at *
+               exact ih
+    | true => simp  [LengthIndexedList.foldl] at *
+
+
+def BitVector.unsigned {w : Width} (x : BitVector w) : Fin (2^w) :=
 instance : Goedel BaseType where
 toType := fun
   | .bitvec w => BitVector w
@@ -92,20 +191,23 @@ def eval : ∀ (o : Op), Goedel.toType (argUserType o) → (Goedel.toType (rgnDo
     (fun w => uncurry $ @BitVector.ashr w)
     arg
 
-def foo : True := default
-
 instance : SSA.TypedUserSemantics Op BaseType where
-argUserType := argUserType
-rgnDom := rgnDom
-rgnCod := rgnCod
-outUserType := outUserType
-eval := eval
+  argUserType := argUserType
+  rgnDom := rgnDom
+  rgnCod := rgnCod
+  outUserType := outUserType
+  eval := eval
 
 /-
-Optimization: InstCombineShift: 239
-%Op0 = shl %X, C
-%r = lshr %Op0, C
+Optimization: InstCombineShift: 279
+  %Op0 = lshr %X, C
+  %r = shl %Op0, C
 =>
-%r = and %X, (-1 u>> C)
+  %r = and %X, (-1 << C)
 -/
+
+theorem InstCombineShift239_base : ∀ w : Width, ∀ x C : BitVector w,
+  BitVector.lshr (BitVector.shl x C) C = BitVector.and x (BitVector.shl (BitVector.mk ⟨-1, sorry⟩ C) :=
+
+
 end InstCombine
