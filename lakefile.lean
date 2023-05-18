1c55ce20670565cda4e9a995b0c758ce9ee6278e
Andres Goens <andres.goens@ed.ac.uk>
2023-05-18 10:20:41 +0100

    chore: bump to nightly 2023-05-18

--- lakefile.lean
+++ lakefile.lean
@@ -13,7 +13,7 @@ lean_lib «alive» {
 
 -- NOTE: this must be 'm'mathlib, as indicated from:
 --  https://github.com/leanprover-community/mathlib4#using-mathlib4-as-a-dependency
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "eff4459"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "968d650"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
   require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "e888e9c"
