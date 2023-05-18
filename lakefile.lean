f7e76c4b3fb524080265bf0c72e40f90a8fac9bb
Siddharth Bhat <siddu.druid@gmail.com>
2023-05-18 02:01:58 +0100

    chore: bump up doc-gen4

--- lakefile.lean
+++ lakefile.lean
@@ -16,5 +16,5 @@ lean_lib «alive» {
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "eff4459"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-  require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "b9421b9"
+  require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "e888e9c"
 
