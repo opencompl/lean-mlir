5a66fa9de2d307a561f2a56a211f3c3a2bd0b2e8
Andres Goens <andres.goens@ed.ac.uk>
2023-06-01 14:15:26 +0100

    Add some pointers to an encryption scheme

--- SSA/Projects/FullyHomomorphicEncryption/Basic.lean
+++ SSA/Projects/FullyHomomorphicEncryption/Basic.lean
@@ -4,13 +4,38 @@ import SSA.Core.WellTypedFramework
 
 open Polynomial -- for R[X] notation
 
-variable (q : Nat) [Fact (q > 1)] (n : Nat)
-abbrev R := ZMod q
+-- Basics from Junfeng Fan and Frederik Vercauteren, Somewhat Practical Fully Homomorphic Encryption
+-- https://eprint.iacr.org/2012/144
+
+variable (q t : Nat) [Fact (q > 1)] (n : Nat)
 
 -- TODO: can we make this computable?
 -- failed to compile definition, consider marking it as 'noncomputable' because it depends on 'Polynomial.add'', and it does not have executable code
-noncomputable def f : (R q)[X] := X^n + 1
-abbrev R' := (@R q)[X] ⧸ (Ideal.span {f q n})
+
+-- Question: Can we make something like d := 2^n work as a macro?
+noncomputable def f : (ZMod q)[X] := X^(2^n) + 1
+abbrev R := (ZMod q)[X] ⧸ (Ideal.span {f q n})
+
+-- Coefficients of `a : R' q n` are `a\_i : Zmod q`.
+-- TODO: get this from mathlib
+def R.coeff (a : R q n) (i : Nat) : ZMod q := sorry
+
+
+
+-- TODO: get infinity norm from mathlib
+--
+--
+
+namespace FV
+namespace SH
+
+def Delta : Nat := sorry -- (q.toFloat / t.toFloat).floor
+def encrypt (pk : R q n) (m : Nat) : R q n := sorry -- ([p0 ·u+e1 +∆·m] ,[p1 ·u+e2])
+def decrypt (sk : R q n) (ct : R q n) : Nat := sorry -- [ (t·[c0 +c1 ·s]q / q).ceil ]t
+def add (ct1 ct2 : R q n) : R q n := sorry -- ([ct1 [0] + ct2 [0]]q , [ct1 [1] + ct2 [1]]q ) .
+
+end SH
+end FV
 
 inductive BaseType
   | nat : BaseType
@@ -21,7 +46,7 @@ instance : Inhabited BaseType := ⟨BaseType.nat⟩
 instance : Goedel BaseType where
 toType := fun
   | .nat => Nat
-  | .poly q n => (R' q n)
+  | .poly q n => (R q n)
 
 abbrev UserType := SSA.UserType BaseType
 
@@ -30,7 +55,7 @@ inductive Op
 | add : Op
 | add_poly (q : Nat) (n : Nat) : Op
 | const : Nat → Op 
-| const_poly : R' q n → Op
+| const_poly : R q n → Op
 | encrypt : Op
 | decrypt : Op
 --deriving Repr, DecidableEq
@@ -58,7 +83,7 @@ def rgnDom : Op q n → UserType := fun _ => .unit
 @[simp]
 def rgnCod : Op q n → UserType := fun _ => .unit
 
-variable (a b : R' q n)
+variable (a b : R q n)
 
 @[simp]
 noncomputable def eval (o : Op q n)
@@ -67,7 +92,7 @@ noncomputable def eval (o : Op q n)
   Goedel.toType (outUserType q n o) :=
     match o with
     | Op.add => (fun args : Nat × Nat => args.1 + args.2) arg
-    | Op.add_poly q n => (fun args : (R' q n) × (R' q n) => args.1 + args.2) arg
+    | Op.add_poly q n => (fun args : (R q n) × (R q n) => args.1 + args.2) arg
     | Op.const n => n
     | Op.const_poly a => a
     | Op.encrypt => sorry
