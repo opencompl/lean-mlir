0863ccd048bf04fd514c67ec4911a618356c22e2
Tobias Grosser <tobias@grosser.es>
2023-06-20 14:03:14 +0100

    Update mathlib to a recent version

--- lakefile.lean
+++ lakefile.lean
@@ -34,7 +34,7 @@ lean_lib SSA {
 
 -- NOTE: this must be 'm'mathlib, as indicated from:
 --  https://github.com/leanprover-community/mathlib4#using-mathlib4-as-a-dependency
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "e50b7a3"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "2094440"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
   require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "e888e9c"
