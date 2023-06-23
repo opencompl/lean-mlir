5fb2fbc22289d531af61a2c0e078a41135e12409
Andres Goens <andres.goens@ed.ac.uk>
2023-06-23 13:10:23 +0100

    update alive statements and generation scripts

--- SSA/Projects/InstCombine/extract_alive_statements.sh
+++ SSA/Projects/InstCombine/extract_alive_statements.sh
@@ -5,7 +5,7 @@ INST_COMBINE_PREFIX="SSA/Projects/InstCombine"
 OUTPUT_FILE="InstCombineAliveStatements.lean"
 THEOREMS_TEMP_FILE="theorems_temp_file.tmp"
 LEAN_DEV_ROOT="../../../"
-PREAMBLE="import SSA.Projects.InstCombine.InstCombineBase"
+PREAMBLE="import SSA.Projects.InstCombine.InstCombineBase\nimport SSA.Projects.InstCombine.Tactic"
 
 cd "$LEAN_DEV_ROOT" || exit
 echo "$PREAMBLE" > "$INST_COMBINE_PREFIX/$OUTPUT_FILE"
@@ -14,12 +14,12 @@ rg "theorem" $INST_COMBINE_PREFIX/$ALIVE_FILE | gsed 's/:.*/:/' | gsed 's/alive_
 
 #sed's R command will read one line at a time from a file and insert it
 gsed -i "/_THEOREM/R $THEOREMS_TEMP_FILE" "$INST_COMBINE_PREFIX/$OUTPUT_FILE"
-gsed -i "s/INSERT_THEOREM/:=sorry\n/" "$INST_COMBINE_PREFIX/$OUTPUT_FILE" #add sorries
+gsed -i "s/INSERT_THEOREM/:= alive_auto\n      try sorry\n/" "$INST_COMBINE_PREFIX/$OUTPUT_FILE" #add sorries
 gsed -i "2d" "$INST_COMBINE_PREFIX/$OUTPUT_FILE" #remove first instance
 echo "\n:=sorry" >> "$INST_COMBINE_PREFIX/$OUTPUT_FILE"  #add last one
 
 #now replace print_goal_as_error's with recovered theorem statements
-gsed -i -e "s/theorem /     try apply /" -e "s/://" "$THEOREMS_TEMP_FILE"
+gsed -i -e "s/theorem /     apply /" -e "s/://" "$THEOREMS_TEMP_FILE"
 gsed -i "/print_goal_as_error/R $THEOREMS_TEMP_FILE" "$INST_COMBINE_PREFIX/$ALIVE_FILE"
 gsed -i "/print_goal_as_error/d" "$INST_COMBINE_PREFIX/$ALIVE_FILE"
 gsed -i "/import SSA.Projects.InstCombine.InstCombineBase/a import SSA.Projects.InstCombine.${OUTPUT_FILE%.*}" "$INST_COMBINE_PREFIX/$ALIVE_FILE"
