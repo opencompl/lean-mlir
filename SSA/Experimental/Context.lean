346ac4424cd1c7fedeb563d2adbe4f5eab93e92f
Alex Keizer <alex@keizer.dev>
2023-07-13 14:23:16 +0100

    wip

--- SSA/Experimental/Context.lean
+++ SSA/Experimental/Context.lean
@@ -1,4 +1,5 @@
-
+import Mathlib.Data.List.Indexes
+import Mathlib.Data.Fin.Basic
 
 /-- A very simple type universe. -/
 inductive Ty
@@ -40,6 +41,66 @@ def last (Γ : Ctxt) (t : Ty) : Ctxt.Var (Ctxt.snoc Γ t) t :=
 
 def emptyElim {α : Sort _} {t : Ty} : Ctxt.Var ∅ t → α :=
   fun ⟨_, h⟩ => by contradiction
+
+/-- Take a variable in a context `Γ` and get the corresponding variable
+in context `Γ.snoc t`. This is marked as a coercion. -/
+@[coe]
+def toSnoc {Γ : Ctxt} {t t' : Ty} (var : Ctxt.Var Γ t) : Ctxt.Var (Ctxt.snoc Γ t') t  :=
+  ⟨var.1+1, by cases var; simp_all⟩
+  
+
+-- axiom Ctxt.append : Ctxt → Ctxt → Ctxt
+
+-- axiom Ctxt.append_empty (Γ : Ctxt) : Ctxt.append Γ ∅ = Γ
+
+-- axiom Ctxt.append_snoc (Γ Γ' : Ctxt) (t : Ty) : 
+--   Ctxt.append Γ (Ctxt.snoc Γ' t) = (Γ.append Γ').snoc t
+
+-- axiom Ctxt.Var.inl {Γ Γ' : Ctxt} {t : Ty} (v : Var Γ t) : 
+--   Var (Ctxt.append Γ Γ') t
+
+-- axiom Ctxt.Var.inr {Γ Γ' : Ctxt} {t : Ty} (v : Var Γ' t) :
+--   Var (Ctxt.append Γ Γ') t
+
+/-- This is an induction principle that case splits on whether or not a variable 
+is the last variable in a context. -/
+@[elab_as_elim]
+def casesOn 
+    {motive : (Γ : Ctxt) → (t t' : Ty) → Ctxt.Var (Γ.snoc t') t → Sort _}
+    {Γ : Ctxt} {t t' : Ty} (v : (Γ.snoc t').Var t)
+    (base : {t t' : Ty} → 
+        {Γ : Ctxt} → (v : Γ.Var t) → motive Γ t t' v.toSnoc)
+    (last : {Γ : Ctxt} → {t : Ty} → motive Γ t t (Ctxt.Var.last _ _)) :
+      motive Γ t t' v :=
+  match v with
+    | ⟨0, h⟩ => 
+        cast (by cases h; simp only [Var.last]) <| @last Γ t
+    | ⟨i+1, h⟩ =>
+        base ⟨i, h⟩
+
+/-- `Ctxt.Var.casesOn` behaves in the expected way when applied to the last variable -/
+@[simp]
+def casesOn_last
+    {motive : (Γ : Ctxt) → (t t' : Ty) → Ctxt.Var (Γ.snoc t') t → Sort _}
+    {Γ : Ctxt} {t : Ty}
+    (base : {t t' : Ty} → 
+        {Γ : Ctxt} → (v : Γ.Var t) → motive Γ t t' v.toSnoc)
+    (last : {Γ : Ctxt} → {t : Ty} → motive Γ t t (Ctxt.Var.last _ _)) :
+    Ctxt.Var.casesOn (motive := motive)
+        (Ctxt.Var.last Γ t) base last = last :=
+  rfl
+
+/-- `Ctxt.Var.casesOn` behaves in the expected way when applied to a previous variable,
+that is not the last one. -/
+@[simp]
+def casesOn_toSnoc 
+    {motive : (Γ : Ctxt) → (t t' : Ty) → Ctxt.Var (Γ.snoc t') t → Sort _}
+    {Γ : Ctxt} {t t' : Ty} (v : Γ.Var t)
+    (base : {t t' : Ty} → 
+        {Γ : Ctxt} → (v : Γ.Var t) → motive Γ t t' v.toSnoc)
+    (last : {Γ : Ctxt} → {t : Ty} → motive Γ t t (Ctxt.Var.last _ _)) :
+      Ctxt.Var.casesOn (motive := motive) (Ctxt.Var.toSnoc (t' := t') v) base last = base v :=
+  rfl
 
 end Var
 
