ff62ec224f180d914d37893c4238ad016e0f5890
Alex Keizer <alex@keizer.dev>
2023-07-31 18:12:25 +0100

    TSSA Hoas

--- SSA/Experimental/Hoas.lean
+++ SSA/Experimental/Hoas.lean
@@ -1,114 +1,56 @@
 import SSA.Core.WellTypedFramework
-import SSA.Experimental.Context
 
 namespace SSA
 
-/-- A very simple intrinsically typed expression. -/
-inductive IExpr : Ctxt → Ty → Type
-  | add (a b : Γ.Var .nat) : IExpr Γ .nat
-  /-- Nat literals. -/
-  | nat (n : Nat) : IExpr Γ .nat
-
-/-- A very simple intrinsically typed program: a sequence of let bindings. -/
-inductive ICom : Ctxt →  Ty → Type where
-  | ret {Γ : Ctxt} : Γ.Var t → ICom Γ t
-  | lete (e : IExpr Γ α) (body : ICom (Γ.snoc α) β) : ICom Γ β
 
 
-class HasToSnoc (ε : Ctxt → Ty → Type) where
-  toSnoc {Γ : Ctxt} {t u : Ty} : ε Γ t → ε (Γ.snoc u) t 
+structure HOASTypes (β : Type) where
+  (expr stmt var : Context β → UserType β → Type)
+  (region : Context β → UserType β → UserType β → Type)
 
 
-class HOAS (expr stmt var : Ctxt → Ty → Type) where
-  [toSnocExpr : HasToSnoc expr]
-  [toSnocStmt : HasToSnoc stmt]
-  [toSnocVar : HasToSnoc var]
+open OperationTypes UserType in
+class HOAS (Op : Type) {β : Type} [Goedel β] [OperationTypes Op β] (h : HOASTypes β) where
+  varToSnoc {Γ} {t u : UserType β} {v : Var} : h.var Γ t → h.var (Γ.snoc v u) t 
   /-- let-binding -/
-  assign {Γ : Ctxt} {T : Ty} (rhs : expr Γ T) 
-      (rest : var (Γ.snoc T) T → stmt (Γ.snoc T) T')
-      : stmt Γ T'
+  assign {Γ : Context β} {T : UserType β} 
+      (lhs : Var) (rhs : h.expr Γ T) 
+      (rest : h.var (Γ.snoc lhs T) T → h.stmt (Γ.snoc lhs T) T')
+      : h.stmt Γ T'
   /-- above; ret v -/
-  ret (v : var Γ T) : stmt Γ T
-  add (a b : var Γ .nat) : expr Γ .nat
-  nat (n : Nat) : expr Γ .nat 
+  ret (v : h.var Γ T) : h.stmt Γ T
+  /-- build a unit value -/
+  unit : h.expr Γ .unit
+  /-- (fst, snd) -/
+  pair (fst : h.var Γ T₁) (snd : h.var Γ T₂) : h.expr Γ (.pair T₁ T₂)
+  /-- (fst, snd, third) -/
+  triple (fst : h.var Γ T₁) (snd : h.var Γ T₂) (third : h.var Γ T₃) : h.expr Γ (.triple T₁ T₂ T₃)
+  /-- op (arg) { rgn } rgn is an argument to the operation -/
+  op (o : Op) (arg : h.var Γ (argUserType o)) (rgn : h.region Context.empty (rgnDom o) (rgnCod o)) :
+      h.expr Γ (outUserType o)
+  /- fun arg => body -/
+  rgn (arg : Var) {dom cod : UserType β} 
+      (body : h.var (Γ.snoc arg dom) dom → h.stmt (Γ.snoc arg dom) cod) : h.region Γ dom cod
+  /- no function / non-existence of region. -/
+  rgn0 : h.region Γ .unit .unit
+  /- a region variable. --/
+  rgnvar (v : h.var Γ (.region T₁ T₂)) : h.region Γ T₁ T₂
+  /-- a variable. -/
+  var (v : h.var Γ T) : h.expr Γ T
 
+variable (Op : Type) {β : Type} [Goedel β] [OperationTypes Op β]
 
+def HOASExpr (Γ : Context β) (t : UserType β) : Type _ := 
+  ∀ h, [HOAS Op h] → h.expr Γ t
 
-def HOASExpr (Γ : Ctxt) (t : Ty) : Type 1 := 
-  ∀ expr stmt var, [HOAS expr stmt var] → expr Γ t
+def HOASStmt (Γ : Context β) (t : UserType β) : Type 1 := 
+  ∀ h, [HOAS Op h] → h.stmt Γ t
 
-def HOASCom (Γ : Ctxt) (t : Ty) : Type 1 := 
-  ∀ expr stmt var, [HOAS expr stmt var] → stmt Γ t
+def HOASVar (Γ : Context β) (t : UserType β) : Type 1 := 
+  ∀ h, [HOAS Op h] → h.var Γ t
 
-def HOASVar (Γ : Ctxt) (t : Ty) : Type 1 := 
-  ∀ expr stmt var, [HOAS expr stmt var] → var Γ t
-
-
-
-
-instance : HasToSnoc (Ctxt.Var) where
-  toSnoc v := v.toSnoc
-
-def IExpr.changeVars (varsMap : Γ.hom Γ') : 
-    (e : IExpr Γ ty) → IExpr Γ' ty
-  | .nat n => .nat n
-  | .add a b => .add (varsMap a) (varsMap b)
-
-instance : HasToSnoc (IExpr) := ⟨IExpr.changeVars Ctxt.Var.toSnoc⟩
-
-def ICom.changeVars (varsMap : Γ.hom Γ') : ICom Γ ty → ICom Γ' ty
-  | .ret e => .ret (varsMap e)
-  | .lete e body => .lete (e.changeVars varsMap) 
-      (body.changeVars (fun t v => varsMap.toSnoc v))
-
-instance : HasToSnoc (ICom) := ⟨ICom.changeVars Ctxt.Var.toSnoc⟩
-
-
-instance : HOAS IExpr ICom Ctxt.Var where
-  assign rhs rest := ICom.lete rhs (rest <| .last ..)
-  ret := ICom.ret
-  add := IExpr.add
-  nat := IExpr.nat
-
-
-
-def ICom.fromHOAS (com : HOASCom Γ t) : ICom Γ t :=
-  com IExpr ICom Ctxt.Var
-
-def IExpr.fromHOAS (expr : HOASExpr Γ t) : IExpr Γ t :=
-  expr IExpr ICom Ctxt.Var
-
-def Var.fromHOAS (v : HOASVar Γ t) : Γ.Var t :=
-  v IExpr ICom Ctxt.Var
-
-
-abbrev HOASVar.Map (Γ : Ctxt) : Type 1 :=
-  ⦃t : Ty⦄ → Γ.Var t → HOASVar Γ t
-
-
-def IExpr.toHOASAux (expr stmt var) [HOAS expr stmt var] (map : ⦃t : Ty⦄ → Γ.Var t → var Γ t) : 
-    IExpr Γ t → expr Γ t
-  | .add a b => HOAS.add stmt (map a) (map b)
-  | .nat n => HOAS.nat stmt var n
-
-def ICom.toHOASAux (expr stmt var) [inst : HOAS expr stmt var] (map : ⦃t : Ty⦄ → Γ.Var t → var Γ t) : 
-    ICom Γ t → stmt Γ t
-  | .lete e body => HOAS.assign (var:=var)
-      (e.toHOASAux expr stmt var map) 
-      fun v => body.toHOASAux expr stmt var (fun _ w => by
-        cases w using Ctxt.Var.casesOn with
-        | last => exact v
-        | base w => exact inst.toSnocVar.toSnoc <| map w
-      )
-  | .ret v => HOAS.ret expr <| map v
-
-
-
-def IExpr.toHOAS (e : IExpr ∅ t) : HOASExpr ∅ t :=
-  fun expr stmt var _ => e.toHOASAux expr stmt var (fun _ v => v.emptyElim)
-
-def ICom.toHOAS (c : ICom ∅ t) : HOASCom ∅ t :=
-  fun expr stmt var _ => c.toHOASAux expr stmt var (fun _ v => v.emptyElim)
+def HOASRegion (Γ : Context β) (t₁ t₂ : UserType β) : Type 1 := 
+  ∀ h, [HOAS Op h] → h.region Γ t₁ t₂
 
 
 
@@ -117,20 +59,94 @@ def ICom.toHOAS (c : ICom ∅ t) : HOASCom ∅ t :=
 
 
 
-namespace Eval
 
-def Eval : Ctxt → Ty → Type := fun _ => Ty.toType
 
-instance : HasToSnoc Eval where
-  toSnoc e := e
 
-instance : HOAS Eval Eval Eval where
-  assign e rest := rest e
-  ret v := v
-  add (a b : Nat) := a + b
-  nat n := n
+def TSSA.EXPR (Γ : Context β) (t : UserType β) : Type :=
+  TSSA Op Γ (.EXPR t)
 
-end Eval
+def TSSA.STMT (Γ : Context β) (t : UserType β) : Type :=
+  TSSA Op Γ (.STMT t)
+
+def TSSA.REGION (Γ : Context β) (t₁ t₂ : UserType β) : Type :=
+  TSSA Op Γ (.REGION t₁ t₂)
+
+
+
+instance : HOAS Op ⟨TSSA.EXPR Op, TSSA.STMT Op, Context.Var, TSSA.REGION Op⟩ where
+  varToSnoc := Context.Var.prev
+  assign lhs rhs rest := TSSA.assign lhs rhs (rest .last)
+  ret := TSSA.ret
+  unit := TSSA.unit
+  pair := TSSA.pair
+  triple := TSSA.triple
+  op := TSSA.op
+  rgn arg _ _ body := TSSA.rgn arg (body .last)
+  rgn0 := TSSA.rgn0
+  rgnvar := TSSA.rgnvar
+  var := TSSA.var
+
+
+def TSSAIndex.toHOASType (Γ : Context β) : TSSAIndex β → Type _
+  | .STMT t => HOASStmt Op Γ t
+  | .EXPR t => HOASExpr Op Γ t
+  | .REGION t₁ t₂ => HOASRegion Op Γ t₁ t₂
+
+def TSSA.fromHOASExpr (expr : HOASExpr Op Γ t) : TSSA Op Γ (.EXPR t) :=
+  expr ⟨TSSA.EXPR Op, TSSA.STMT Op, Context.Var, TSSA.REGION Op⟩
+
+def TSSA.fromHOASStmt (stmt : HOASStmt Op Γ t) : TSSA Op Γ (.STMT t) :=
+  stmt ⟨TSSA.EXPR Op, TSSA.STMT Op, Context.Var, TSSA.REGION Op⟩
+
+def TSSA.fromHOASRegion (stmt : HOASRegion Op Γ t₁ t₂) : TSSA Op Γ (.REGION t₁ t₂) :=
+  stmt ⟨TSSA.EXPR Op, TSSA.STMT Op, Context.Var, TSSA.REGION Op⟩
+
+def Context.Var.fromHOAS (v : HOASVar Op Γ t) : Γ.Var t :=
+  v ⟨TSSA.EXPR Op, TSSA.STMT Op, Context.Var, TSSA.REGION Op⟩
+
+
+def HOASTypes.fromIndex (h : HOASTypes β) (Γ : Context β) : TSSAIndex β → Type 
+  | .STMT t => h.stmt Γ t
+  | .EXPR t => h.expr Γ t
+  | .REGION t₁ t₂ => h.region Γ t₁ t₂
+
+open HOAS in
+def TSSA.toHOASAux (h) [hoas : HOAS Op h] {Γ : Context β} {i : TSSAIndex β}
+    (map : ⦃t : UserType β⦄ → Γ.Var t → h.var Γ t) : 
+    TSSA Op Γ i → h.fromIndex Γ i
+  | .assign lhs rhs rest => HOAS.assign Op lhs (rhs.toHOASAux h map) 
+      (fun v => rest.toHOASAux h <| mapping_snoc map v)
+  | .ret v => HOAS.ret Op <| map v
+  | .unit => HOAS.unit Op
+  | .pair a b => HOAS.pair Op (map a) (map b)
+  | .triple a b c => HOAS.triple Op (map a) (map b) (map c)
+  | .op o arg region => HOAS.op o (map arg) (region.toHOASAux h fun _ v => v.emptyElim)
+  | .var v => HOAS.var Op (map v)
+  | .rgnvar v => HOAS.rgnvar Op (map v)
+  | .rgn0 => HOAS.rgn0 Op
+  | .rgn arg body => HOAS.rgn Op arg (fun v => body.toHOASAux h <| mapping_snoc map v)
+where
+  mapping_snoc {var} {u} (map : ⦃t : UserType β⦄ → Γ.Var t → h.var Γ t) 
+      (newVal : h.var (Γ.snoc var u) u) :
+      ⦃t : UserType β⦄ → (Γ.snoc var u).Var t → h.var (Γ.snoc var u) t :=
+    fun _ w => by cases w with
+      | last => exact newVal
+      | prev w => exact hoas.varToSnoc <| map w
+    
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
   
