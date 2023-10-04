75260e46e9af2f9f75540b4011963ca3be2befc8
Alex Keizer <alex@keizer.dev>
2023-10-04 12:24:02 +0100

    Instantiate width meta-vars before reducing the expression in EDSL macro

--- SSA/Projects/InstCombine/LLVM/EDSL.lean
+++ SSA/Projects/InstCombine/LLVM/EDSL.lean
@@ -4,28 +4,22 @@ import SSA.Projects.InstCombine.LLVM.Transform
 
 open Qq Lean Meta Elab.Term
 
-#check MLIR.AST.mkCom
-#check MLIR.AST.mkComInstantiate
-
-#check Syntax.TSepArray.getElems
-
 open MLIR.AST InstCombine in
 elab "[mlir_icom (" mvars:ident,* ")| " reg:mlir_region "]" : term => do
   let ast_stx ← `([mlir_region| $reg])
   let φ : Nat := mvars.getElems.size
   let ast ← elabTermEnsuringTypeQ ast_stx q(Region $φ)
-  let com := q(mkComInstantiate $ast)
+  let mvalues ← `(⟨[$mvars,*], by rfl⟩)
+  let mvalues : Q(Vector Nat $φ) ← elabTermEnsuringType mvalues q(Vector Nat $φ)
+  let com := q(mkComInstantiate $ast |>.map (· $mvalues))
   synthesizeSyntheticMVarsNoPostponing
-  let com : Q(ExceptM (Vector Nat $φ → Σ (Γ' : Ctxt Ty) (ty : InstCombine.Ty), Com Γ' ty)) ← 
+  let com : Q(ExceptM (Σ (Γ' : Ctxt Ty) (ty : InstCombine.Ty), Com Γ' ty)) ← 
     withTheReader Core.Context (fun ctx => { ctx with options := ctx.options.setBool `smartUnfolding false }) do
       withTransparency (mode := TransparencyMode.all) <| 
         return ←reduce com
   trace[Meta] com
   match com with
-    | ~q(Except.ok $comOk)    => 
-        let mvalues ← `(⟨[$mvars,*], by rfl⟩)
-        let mvalues : Q(Vector Nat $φ) ← elabTermEnsuringType mvalues q(Vector Nat $φ)
-        return q($comOk $mvalues)
+    | ~q(Except.ok $comOk)    => return comOk
     | ~q(Except.error $err) => do
         let err ← unsafe evalExpr TransformError q(TransformError) err
         throwError "Translation failed with error:\n\t{repr err}"
