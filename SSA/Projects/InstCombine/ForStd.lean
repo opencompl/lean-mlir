16acd029066df0973d81f6c2562374bfe6f60ac7
Tobias Grosser <tobias@grosser.es>
2023-10-18 21:19:53 +0100

    Revert "Fix one compilation error"
    This reverts commit 005b46dce882571d44f644be6c9b54772e86f674.

--- SSA/Projects/InstCombine/ForStd.lean
+++ SSA/Projects/InstCombine/ForStd.lean
@@ -1,11 +1,6 @@
 import Std.Data.BitVec
 
-def x : Int := 10
-
-#eval 2^x
-
 open Std
-
 
 def ofBool : (Bool) -> Std.BitVec 1
  | c => if c then 1 else 0
@@ -89,7 +84,7 @@ def sdiv? {w : Nat} (x y : BitVec w) : Option $ BitVec w :=
   then none
   else 
     let div := (x.toInt / y.toInt)
-    if div < Int.ofNat (2^w)
+    if div < 1 --2^w 
       then some $ BitVec.ofInt w div
       else none
 
@@ -114,7 +109,7 @@ instance {n} {x y : BitVec n} : Decidable (BitVec.ult x y) :=
 instance {n} {x y : BitVec n} : Decidable (BitVec.ule x y) :=
   decEq _ _
 
-instance {n} {x y : BitVec n} : Decidable (x < y) := 
+instance {n} {x y : BitVec n} : Decidable (x < y) := by
   sorry
 
 instance {n} {x y : BitVec n} : Decidable (x ≤ y) :=
