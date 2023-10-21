34f670070e13d22108845988e65c6f356e7368c9
Siddharth Bhat <siddu.druid@gmail.com>
2023-10-21 23:21:18 +0100

    wip: checkpoint to pull in DCE

--- SSA/Projects/CSE/CSE.lean
+++ SSA/Projects/CSE/CSE.lean
@@ -21,23 +21,23 @@ structure State (Op : Type) [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] (Γ
 def State.canonicalizeExpr_ 
  [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
  {Γ : Ctxt Ty} (s : State Op Γ) (e : IExpr Op Γ α) : { e' : IExpr Op Γ α // e.denote = e'.denote } :=
-  sorry
+  match e with
+  | .mk op ty_eq args regArgs => sorry
 
-/-- add a variable with a given expression into the CSE map -/
-def State.assignVar
+/-- assign an expression to a snoc'd variable with a given expression into the CSE map -/
+def State.assignSnocVar
  [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
- {Γ : Ctxt Ty} (s : State Op Γ)
- (v : Γ.Var α) (e : IExpr Op Γ α) : State Op Γ := sorry
-
-/-- lookup an expression in the state and return a corresponding CSE'd variable for it -/
-def State.cseExpr
- [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
- {Γ : Ctxt Ty} (s : State Op Γ)
- (e : IExpr Op Γ α) (e : IExpr Op Γ α) : Option ({ v' : Γ.Var α // ∀ (V : Γ.Valuation), V v' = e.denote V }) := sorry
-
+ {Γ : Ctxt Ty} (s : State Op Γ) (e : IExpr Op Γ α) : State Op (Γ.snoc α) := sorry
 
 def _root_.Ctxt.Valuation.pullback [Goedel Ty] {Γ Δ : Ctxt Ty} (hom : Ctxt.Hom Γ Δ) (V : Δ.Valuation) : Γ.Valuation :=
   fun _tΓ vΓ => V (hom vΓ)
+
+@[simp]
+theorem Ctxt.Valuation.pullback_id [Goedel Ty] {Γ : Ctxt Ty} (V : Γ.Valuation) :
+  V.pullback Ctxt.Hom.id = V := by
+    funext _t _v
+    simp[Ctxt.Valuation.pullback]
+
 /-- return value of the CSE pass. -/
 structure CSERet [OpSignature Op Ty] [Goedel Ty] [OpDenote Op Ty] (com: ICom Op Γ α) where
   Δ : Ctxt Ty
@@ -45,10 +45,45 @@ structure CSERet [OpSignature Op Ty] [Goedel Ty] [OpDenote Op Ty] (com: ICom Op 
   com' : ICom Op Δ α
   hcom' : ∀ (VΓ: Ctxt.Valuation Γ), com.denote VΓ = com'.denote (VΓ.pullback hom) 
 
-def cse [OpSignature Op Ty] [Goedel Ty] [OpDenote Op Ty] {α : Ty} (com: ICom Op Γ α) (cseState : State Op Γ) : 
+/- CSE for HVector / ICom / IExpr. -/
+mutual 
+
+/-- lookup an expression in the state and return a corresponding CSE'd variable for it -/
+def State.cseExpr
+ [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
+ {Γ : Ctxt Ty} (s : State Op Γ)
+ (e : IExpr Op Γ α) : Option ({ v' : Γ.Var α // ∀ (V : Γ.Valuation), V v' = e.denote V }) := 
+    match e with
+  | .mk op ty_eq args regArgs => 
+      sorry
+
+def State.cseICom [OpSignature Op Ty] [Goedel Ty] [OpDenote Op Ty] {α : Ty}  (s : State Op Γ) (com: ICom Op Γ α) : 
   CSERet com := 
   match com with
-  | .ret v => sorry
-  | .lete (α := α) e body => sorry
+  | .ret v => {
+    Δ := Γ
+    hom := Ctxt.Hom.id
+    com' := .ret (s.var2var v).val
+    hcom' := by
+      let ⟨v', hv'⟩ := s.var2var v
+      intros VΓ
+      simp
+      simp[ICom.denote, hv']
+  }
+  | .lete (α := α) e body => 
+      match s.cseExpr e with
+      | .none => 
+        let ⟨e', he'⟩ := s.canonicalizeExpr_ e
+        let s' := s.assignSnocVar e'
+        let ret' := s'.cseICom body
+        {
+          Δ := Γ
+          hom := Ctxt.Hom.id
+          com' := .lete e' sorry -- need to pullback `body` along the morphism. 
+          hcom' := sorry
+        } 
+      | .some ⟨e', he'⟩ => sorry 
+
+end -- mutual. 
 
 end CSE
