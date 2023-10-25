09d2ab95102aeb20a109ee8b0910f522de20c3ef
Tobias Grosser <tobias@grosser.es>
2023-10-25 02:21:29 +0100

    fix indentation

--- SSA/Projects/InstCombine/Tactic.lean
+++ SSA/Projects/InstCombine/Tactic.lean
@@ -16,33 +16,33 @@ open Std (BitVec)
 macro "simp_alive_peephole" : tactic =>
   `(tactic|
       (
-      dsimp only [ICom.Refinement]
-      intros Γv
-      simp_peephole at Γv
-      /- note that we need the `HVector.toPair`, `HVector.toSingle` lemmas since it's used in `InstCombine.Op.denote`
-        We need `HVector.toTuple` since it's used in `MLIR.AST.mkOpExpr`. -/
-      try simp (config := {decide := false}) only [OpDenote.denote, InstCombine.Op.denote, HVector.toPair, pairMapM, BitVec.Refinement,
-        bind, Option.bind, pure,
-        DerivedContext.ofContext, DerivedContext.snoc, Ctxt.snoc,
-        MOp.instantiateCom, InstCombine.MTy.instantiate, ConcreteOrMVar.instantiate,
-        Vector.get, HVector.toSingle, HVector.toTuple, List.nthLe]
-      try intros v0
-      try intros v1
-      try intros v2
-      try intros v3
-      try intros v4
-      try intros v5
-      try cases' v0 with x0 <;> simp
-        <;> try cases' v1 with x1 <;> simp
-        <;> try cases' v2 with x2 <;> simp
-        <;> try cases' v3 with x3 <;> simp
-        <;> try cases' v4 with x4 <;> simp
-        <;> try cases' v5 with x5 <;> simp
-      try revert v5
-      try revert v4
-      try revert v3
-      try revert v2
-      try revert v1
-      try revert v0
+        dsimp only [ICom.Refinement]
+        intros Γv
+        simp_peephole at Γv
+        /- note that we need the `HVector.toPair`, `HVector.toSingle` lemmas since it's used in `InstCombine.Op.denote`
+          We need `HVector.toTuple` since it's used in `MLIR.AST.mkOpExpr`. -/
+        try simp (config := {decide := false}) only [OpDenote.denote, InstCombine.Op.denote, HVector.toPair, pairMapM, BitVec.Refinement,
+          bind, Option.bind, pure,
+          DerivedContext.ofContext, DerivedContext.snoc, Ctxt.snoc,
+          MOp.instantiateCom, InstCombine.MTy.instantiate, ConcreteOrMVar.instantiate,
+          Vector.get, HVector.toSingle, HVector.toTuple, List.nthLe]
+        try intros v0
+        try intros v1
+        try intros v2
+        try intros v3
+        try intros v4
+        try intros v5
+        try cases' v0 with x0 <;> simp
+          <;> try cases' v1 with x1 <;> simp
+          <;> try cases' v2 with x2 <;> simp
+          <;> try cases' v3 with x3 <;> simp
+          <;> try cases' v4 with x4 <;> simp
+          <;> try cases' v5 with x5 <;> simp
+        try revert v5
+        try revert v4
+        try revert v3
+        try revert v2
+        try revert v1
+        try revert v0
       )
    )
