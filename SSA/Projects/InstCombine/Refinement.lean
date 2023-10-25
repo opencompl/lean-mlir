5c34fcd40f5be4edcc4c814280db9928fd31eb07
Tobias Grosser <tobias@grosser.es>
2023-10-25 02:38:54 +0100

    Drop high prority refinement notation

--- SSA/Projects/InstCombine/Refinement.lean
+++ SSA/Projects/InstCombine/Refinement.lean
@@ -6,4 +6,4 @@ open MLIR AST
 abbrev ICom.Refinement (src tgt : Com (φ:=0) Γ t) (h : Goedel.toType t = Option α := by rfl) : Prop :=
   ∀ Γv, (h ▸ src.denote Γv) ⊑ (h ▸ tgt.denote Γv)
 
-infixr:90 (priority:=high) " ⊑ "  => ICom.Refinement
+infixr:90 " ⊑ "  => ICom.Refinement
