363a370eff7095b18ea298a74d516f9ce819864d
Siddharth Bhat <siddu.druid@gmail.com>
2023-10-27 20:58:31 +0100

    change proof path

--- SSA/Projects/CSE/CSE.lean
+++ SSA/Projects/CSE/CSE.lean
@@ -84,12 +84,21 @@ regionVector.decEq as bs => sizeOf as
 
 end DecEqICom
 
+
+namespace HomSemanticPreserving
+  variable [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
+  variable { Γstart Γ Γ' : Ctxt Ty} (lets : Lets Op Γstart Γ)
+  -- Not sure this is the right theorem.
+
+
+end HomSemanticPreserving
+
 namespace CSE
 
 /-- State stored by CSE pass. -/
 structure State (Op : Type) [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] (lets : Lets Op Γstart Γ) where
   /-- map variable to its canonical value -/
-  var2var : (v : Γ.Var α) → { v' : Γ.Var α // ∀ (V : Γ.Valuation), V v = V v' }
+  var2var : (v : Γ.Var α) → { v' : Γ.Var α // ∀ (V : Γstart.Valuation), (lets.denote V) v = (lets.denote V) v' }
   /-- map an IExpr to its canonical variable -/
   expr2cache : (α : Ty) → (e : IExpr Op Γ α) → Option ({ v : Γ.Var α // ∀ (V : Γstart.Valuation), (lets.denote V) v = e.denote (lets.denote V) })
 
@@ -98,26 +107,17 @@ def State.empty [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] (lets : Lets Op
   var2var := fun v => ⟨v, by intros V; rfl⟩
   expr2cache := fun α e => .none
 
-def State.snocExpr2Cache
+def State.snocNewExpr2Cache
  [Goedel Ty] [DecidableEq Ty] [DecidableEq Op] [OpSignature Op Ty] [OpDenote Op Ty]
  {Γ : Ctxt Ty} {α : Ty}
  {lets : Lets Op Γstart Γ}
- (s : State Op lets) (e : IExpr Op Γ α) : State Op (Lets.lete lets e) := {
-  var2var := fun v => by /- TODO: refactor to use term mode. -/
-    refine Ctxt.Var.casesOn v ?_ ?_
-    . intros t t' Δ v
-      constructor
-      intros V
-      rfl
-    . intros Δ t
-      constructor
-      intros V
-      rfl
+ (s : State Op lets) (e : IExpr Op Γ α) : State Op (Lets.lete lets e) :=
+ {
+  var2var := fun v => by
+    apply Subtype.mk
+    intros V
+    rfl
   expr2cache := fun β eneedle =>
-    /-
-    Since `eneedle` lives in `(Γ.snoc β)`, we need to try to corce it into `Γ`. To do this,
-    we use the tools built in DCE to try to delete the variable in `eneedle`.
-    -/
     let eneedleΓ? := DCE.IExpr.deleteVar? (DEL := Deleted.deleteSnoc Γ α) (eneedle)
     match eneedleΓ? with
     | .none => .none -- this expression actually uses β in some nontrivial way, we're fucked,
@@ -144,24 +144,100 @@ def State.snocExpr2Cache
             | .isFalse _neq => .none -- s.expr2cache β eneedleΓ /- different expression, query cache. -/
  }
 
+/- denoting a `lete` is the same as `snoc`ing the denotation of `e` onto the old valuation `V`. -/
+@[simp]
+theorem Lets.denote_lete [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
+  {Γstart Γ : Ctxt Ty}
+  {lets : Lets Op Γstart Γ}
+  (e : IExpr Op Γ α)
+  (V : Ctxt.Valuation Γstart) :
+  Lets.denote (Lets.lete lets e) V = (Ctxt.Valuation.snoc (Lets.denote lets V) (IExpr.denote e (Lets.denote lets V))) := by
+  simp[Lets.denote, eq_rec_constant]
+  funext t v
+  cases v using Ctxt.Var.casesOn <;> simp
+  done
+
+/- Remap the last variable in a context, to get a new context without the last variable -/
+def _root_.Ctxt.Hom.remapLast [Goedel Ty]  {α : Ty} (Γ : Ctxt Ty) (var : Γ.Var α) :
+  Ctxt.Hom (Γ.snoc α) Γ := fun ty' var' => by
+    cases var' using Ctxt.Var.casesOn
+    case toSnoc var' => exact var'
+    case last => exact var
+
+
+section RemapVar
+  variable [Goedel Ty] [DecidableEq Ty] [DecidableEq Op] [OpSignature Op Ty] [OpDenote Op Ty]
+  variable  {Γstart Γ Γ' : Ctxt Ty} {α : Ty}
+  variable (lets : Lets Op Γstart Γ)
+  variable (hom : Ctxt.Hom Γ' Γ)
+  variable (vold : Γ.Var α) (vnew : Γ'.Var α)
+  variable (VNEW: ∀ (Vstart : Ctxt.Valuation Γstart), (lets.denote Vstart) vold = ((lets.denote Vstart).hom hom) vnew)
+
+
+  def VarRemapVar (w' : Γ'.Var β) :
+  { w : Γ.Var β // ∀ (Vstart : Ctxt.Valuation Γstart), (lets.denote Vstart) w =
+    ((lets.denote Vstart).hom hom) w' } := sorry
+  def VarRemapIExpr (e' : IExpr Op Γ' β) :
+  { e : IExpr Op Γ β  // ∀ (Vstart : Ctxt.Valuation Γstart),
+    e.denote (lets.denote Vstart) =  e'.denote ((lets.denote Vstart).hom hom) } := sorry
+end RemapVar
+
+
 /-
-/-- assign an expression to a snoc'd variable with a given expression into the CSE map -/
-def State.addExpr2Cache
- [Goedel Ty] [DecidableEq Ty] [DecidableEq Op] [OpSignature Op Ty] [OpDenote Op Ty]
- {Γstart Γ : Ctxt Ty} {α : Ty}
- {lets : Lets Op Γstart Γ}
- (s : State Op lets) (v : Γ.Var α) (e : { e : IExpr Op Γ α // ∀ (V : Γstart.Valuation), e.denote (lets.denote V)  = (lets.denote V) v }) : State Op lets := {
-  s with
-  expr2cache := fun β eneedle =>
-    if hβ : α = β
-    then
-      match he : IExpr.decEq (hβ ▸  eneedle) e.val with
-      | .isTrue exprEq => /- same expression, return the variable. -/
-          .some ⟨hβ ▸ v, by subst hβ; simp at exprEq; subst exprEq; simp at he; simp[e.property]⟩
-      | .isFalse _neq => s.expr2cache β eneedle /- different expression, query cache. -/
-    else s.expr2cache β eneedle
- }
+e: IExpr Op Γ α
+body: ICom Op (Ctxt.snoc Γ α) α✝
+e': IExpr Op Γ α
+he': IExpr.denote e' = IExpr.denote e
+v'?: Option { v' // ∀ (V : Ctxt.Valuation Γstart), Lets.denote lets V v' = IExpr.denote e (Lets.denote lets V) }
+s': State Op (Lets.lete lets e') := snocNewExpr2Cache s e'
 -/
+
+def State.snocOldExpr2Cache
+ [Goedel Ty] [DecidableEq Ty] [DecidableEq Op] [OpSignature Op Ty] [OpDenote Op Ty]
+ {Γ : Ctxt Ty} {α : Ty}
+ {lets : Lets Op Γstart Γ}
+ (s : State Op lets) (enew : IExpr Op Γ α) (eold : IExpr Op Γ α) (henew :
+    ∀ (V : Γstart.Valuation), enew.denote (lets.denote V) = eold.denote (lets.denote V))
+  (v : Γ.Var α) (hv : ∀ (V : Γstart.Valuation), eold.denote (lets.denote V) = lets.denote V v) : State Op (Lets.lete lets enew) := {
+    var2var := fun v => by
+      cases v using Ctxt.Var.casesOn
+      case toSnoc v => -- old variable, look up 'var2var'
+        let ⟨v', hv'⟩ := s.var2var v
+        apply (Subtype.mk v'.toSnoc)
+        intros V
+        simp[Lets.denote_lete]
+        rw[hv']
+
+      case last => -- new variable, return the CSE'd variable.
+        apply (Subtype.mk v.toSnoc)
+        intros V
+        simp[Lets.denote_lete]
+        rw[← hv]
+        rw[henew]
+    expr2cache := fun β eneedle =>
+      let homRemap := Ctxt.Hom.remapLast Γ v
+      let ⟨eneedle', heneedle'⟩ := VarRemapIExpr (lets) homRemap eneedle
+      match s.expr2cache β eneedle' with
+      | .none => .none
+      | .some ⟨e', he'⟩ =>
+        .some ⟨e', by {
+          intros V
+          simp
+          rw[he']
+          rw[heneedle']
+          congr
+          funext ty var
+          cases var using Ctxt.Var.casesOn
+          case e_Γv.h.h.toSnoc v =>
+            simp[Ctxt.Valuation.hom, Ctxt.Hom.remapLast]
+            done
+          case e_Γv.h.h.last =>
+            simp[Ctxt.Valuation.hom, Ctxt.Hom.remapLast]
+            rw[henew]
+            rw[hv]
+            done
+        }⟩
+}
 
 @[simp]
 theorem Ctxt.Valuation.pullback_id [Goedel Ty] {Γ : Ctxt Ty} (V : Γ.Valuation) :
@@ -169,20 +245,13 @@ theorem Ctxt.Valuation.pullback_id [Goedel Ty] {Γ : Ctxt Ty} (V : Γ.Valuation)
     funext _t _v
     simp[Ctxt.Valuation.hom]
 
-/-- return value of the CSE pass. -/
-structure CSERet [OpSignature Op Ty] [Goedel Ty] [OpDenote Op Ty] (lets : Lets Op Γstart Γ) (com: ICom Op Γ α) where
-  Δ : Ctxt Ty
-  hom : Ctxt.Hom Δ Γ
-  com' : ICom Op Δ α
-  hcom' : ∀ (V: Ctxt.Valuation Γstart), com.denote (lets.denote V) = com'.denote ((lets.denote V).hom hom)
-
 /-- Replace the variables in `as` with new variables that have the same valuation -/
 def State.cseArgList
  [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
   {Γstart Γ : Ctxt Ty} {lets : Lets Op Γstart Γ} (s : State Op lets)
   {ts : List Ty}
   (as : HVector (Ctxt.Var Γ) <| ts) :
-  { as' : HVector (Ctxt.Var Γ) <| ts // ∀ (V : Γ.Valuation), as.map V.eval = as'.map V.eval  } :=
+  { as' : HVector (Ctxt.Var Γ) <| ts // ∀ (V : Γstart.Valuation), as.map (lets.denote V).eval = as'.map (lets.denote V).eval  } :=
   match as with
   | .nil => ⟨.nil, by
       simp[HVector.map]
@@ -216,142 +285,6 @@ instance [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] : Inhabited (
   default := fun _s com => ⟨com, by intros V; rfl⟩
 
 
-/-- Default instance for `partial def` to compile. -/
-instance [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] : Inhabited (
-  {lets : Lets Op Γstart Γ} →
-  State Op lets →
-  (com: ICom Op Γ α) →  CSERet lets com) where
-  default := fun _s com => CSERet.mk Γ Ctxt.Hom.id com (by intros V; rfl)
-instance [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty] : Inhabited (
-  {lets : Lets Op Γstart Γ} →
-  State Op lets →
- (e : IExpr Op Γ α) → {e' : IExpr Op Γ α // e'.denote = e.denote } × Option ({ v' : Γ.Var α // ∀ (V : Γ.Valuation), V v' = e.denote V })) where
- default := fun _s e => (⟨e, rfl⟩, .none)
-
-
-/- denoting a `lete` is the same as `snoc`ing the denotation of `e` onto the old valuation `V`. -/
-@[simp]
-theorem Lets.denote_lete [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
-  {Γstart Γ : Ctxt Ty}
-  {lets : Lets Op Γstart Γ}
-  (e : IExpr Op Γ α)
-  (V : Ctxt.Valuation Γstart) :
-  Lets.denote (Lets.lete lets e) V = (Ctxt.Valuation.snoc (Lets.denote lets V) (IExpr.denote e (Lets.denote lets V))) := by
-  simp[Lets.denote, eq_rec_constant]
-  funext t v
-  cases v using Ctxt.Var.casesOn <;> simp
-  done
-
-
-
--- given an variable `v` that equals `e` in `(lets VΓstart)`, replace a variable `v'` in the context
--- given by `(e (lets VΓstart))`, which uses `v` instead of the variable is the new one added by `e`,
--- and uses the non-shadowing variable from the old context `(lets VΓstart)` sotherwise.
-def varSubstVar [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
-  {Γstart Γ : Ctxt Ty}
-  {lets : Lets Op Γstart Γ}
-  (s : State Op lets)
-  (replacee : IExpr Op Γ α)
-  (replacev : Γ.Var α)
-  (REPLACEV : ∀ (V : Γstart.Valuation), replacee.denote (lets.denote V) = (lets.denote V) replacev)
-  (findv : (Γ.snoc α).Var β) :
-  { findv' : Γ.Var β //
-    ∀ (V : Γstart.Valuation), ((Lets.lete lets replacee).denote V) findv = (lets.denote V) findv' } := by
-    cases findv using Ctxt.Var.casesOn <;> try simp
-    case toSnoc findv_unsnoc =>
-      apply (Subtype.mk findv_unsnoc)
-      intros V
-      rfl
-    case last =>
-      apply (Subtype.mk replacev)
-      exact REPLACEV
-
-/-- Try to delete the variable from the argument list.
-  Succeeds if variable does not occur in the argument list.
-  Fails otherwise. -/
-def arglistSubstVar [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
-  {Γstart Γ : Ctxt Ty}
-  {lets : Lets Op Γstart Γ}
-  (s : State Op lets)
-  (replacee : IExpr Op Γ α)
-  (replacev : Γ.Var α)
-  (REPLACEV : ∀ (V : Γstart.Valuation), replacee.denote (lets.denote V) = (lets.denote V) replacev)
-  (findv : (Γ.snoc α).Var α)
-  (as : HVector (Ctxt.Var (Γ.snoc α)) <| ts) :
-    { as' : HVector (Ctxt.Var Γ) <| ts // ∀ (Vstart : Γstart.Valuation),
-      as.map (fun (t : Ty) (a : (Γ.snoc α).Var t) => (Lets.lete lets replacee).denote Vstart a) =
-      as'.map (fun (t : Ty) (a : Γ.Var t) => lets.denote Vstart a)} :=
-  match as with
-  | .nil => ⟨.nil, by simp[HVector.map]; done⟩
-  | .cons (a := α) a as =>
-    let ⟨a', ha'⟩ := varSubstVar s replacee replacev REPLACEV a
-    let ⟨as', has'⟩ := arglistSubstVar s replacee replacev REPLACEV findv as
-    ⟨.cons a' as', by
-      intros V
-      simp at ha' has' ⊢
-      simp[HVector.map]
-      constructor
-      . apply ha'
-      . apply has'
-      done
-    ⟩
-
-mutual
-
-def IExpr.substVar [DecidableEq Ty] [DecidableEq Op] [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
-  {Γstart Γ : Ctxt Ty}
-  {lets : Lets Op Γstart Γ}
-  (s : State Op lets)
-  (replacee : IExpr Op Γ α)
-  (replacev : Γ.Var α)
-  (REPLACEV : ∀ (V : Γstart.Valuation), replacee.denote (lets.denote V) = (lets.denote V) replacev)
-  (findv : (Γ.snoc α).Var α)
-  (body : IExpr Op (Γ.snoc α) β) :
-  { body' : IExpr Op Γ β //
-    ∀ (V : Γstart.Valuation), body.denote ((Lets.lete lets replacee).denote V)  = body'.denote (lets.denote V) } :=
-  match body with
-  | .mk op ty_eq args regArgs =>
-    let ⟨args', hargs'⟩ := arglistSubstVar s replacee replacev REPLACEV findv args
-    ⟨.mk op ty_eq args' regArgs, by
-      subst ty_eq
-      simp[IExpr.denote, hargs']
-      intros V
-      congr 1
-      simp[Lets.denote_lete] at hargs'
-      rw[hargs']
-      done
-    ⟩
-
-/- replace the snoc'd variable of an ICom with a different variable of the same denotation-/
-def ICom.substVar [DecidableEq Ty] [DecidableEq Op] [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
-  {Γstart Γ : Ctxt Ty}
-  {lets : Lets Op Γstart Γ}
-  (s : State Op lets)
-  (replacee : IExpr Op Γ α)
-  (replacev : Γ.Var α)
-  (REPLACEV : ∀ (V : Γstart.Valuation), replacee.denote (lets.denote V) = (lets.denote V) replacev)
-  (findv : (Γ.snoc α).Var α)
-  (body : ICom Op (Γ.snoc α) β) (v : Γ.Var α) :
-  { body' : ICom Op Γ β //
-    ∀ (V : Γstart.Valuation), body.denote ((Lets.lete lets replacee).denote V)  = body'.denote (lets.denote V) } :=
-  match body with
-  | .ret (t := γ) v =>
-      let ⟨v', hv'⟩ :=  varSubstVar s replacee replacev REPLACEV v
-      ⟨.ret v', by
-        intros V
-        apply hv'
-        done⟩
-  | .lete e body' =>
-    let ⟨e', he'⟩ := IExpr.substVar s replacee replacev REPLACEV findv e
-    -- fucked x(
-    -- need to create new state?
-    let lets' := Lets.lete lets e
-    let ⟨body'', hbody''⟩ := ICom.substVar s replacee replacev REPLACEV findv body'
-    _
-
-end
-
-
 /- CSE for HVector / ICom / IExpr. -/
 mutual
 variable [DecidableEq Ty] [DecidableEq Op] [Goedel Ty] [OpSignature Op Ty] [OpDenote Op Ty]
@@ -373,7 +306,7 @@ unsafe def State.cseRegionArgList
   | .cons r rs =>
     -- Need to create a fresh state to CSE the region.
     let s' := State.empty Lets.nil
-    let ⟨r', hr'⟩ :=  s'.cseICom' r
+    let ⟨r', hr'⟩ :=  s'.cseICom r
     let ⟨rs', hrs'⟩ := s.cseRegionArgList rs
     ⟨.cons r' rs', by
           simp[HVector.denote]
@@ -390,7 +323,8 @@ unsafe def State.cseExpr
  {lets : Lets Op Γstart Γ}
  (s : State Op lets)
  (e : IExpr Op Γ α) :
- {e' : IExpr Op Γ α // e'.denote = e.denote } × Option ({ v' : Γ.Var α // ∀ (V : Γstart.Valuation), (lets.denote V) v' = e.denote (lets.denote V) }) :=
+ {e' : IExpr Op Γ α //
+  ∀ (V : Γstart.Valuation), e'.denote (lets.denote V) = e.denote (lets.denote V) } × Option ({ v' : Γ.Var α // ∀ (V : Γstart.Valuation), (lets.denote V) v' = e.denote (lets.denote V) }) :=
   match E : e with
   | .mk op ty_eq args regArgs =>
       let ⟨args', hargs'⟩ := s.cseArgList args
@@ -398,9 +332,8 @@ unsafe def State.cseExpr
       let ⟨regArgs', hregArgs'⟩ := regArgs'
       let e' : IExpr Op Γ α  := .mk op ty_eq args' regArgs'
       ⟨⟨e', by {
-        funext V
+        intros V
         simp[E, hargs', IExpr.denote_unfold]
-
         congr 1
         simp[Ctxt.Valuation.eval] at hargs'
         rw[hargs']
@@ -423,66 +356,41 @@ unsafe def State.cseICom {α : Ty}
   {lets : Lets Op Γstart Γ}
   (s : State Op lets)
   (com: ICom Op Γ α) :
-  CSERet lets com :=
+  { com' : ICom Op Γ α // ∀ (V: Ctxt.Valuation Γstart), com.denote (lets.denote V) = com'.denote (lets.denote V) } :=
   match com with
-  | .ret v => {
-    Δ := Γ
-    hom := Ctxt.Hom.id
-    com' := .ret (s.var2var v).val
-    hcom' := by
+  | .ret v => ⟨.ret (s.var2var v).val, by
       let ⟨v', hv'⟩ := s.var2var v
       intros VΓ
       simp
-      simp[ICom.denote, hv']
-  }
+      simp[ICom.denote, hv']⟩
   | .lete (α := α) e body =>
       let ⟨⟨e', he'⟩, v'?⟩ := s.cseExpr e
       match v'? with
       | .none => /- no variable to replace. -/
-        let s' := s.snocExpr2Cache (e := e') /- add this expression into the cache for the latest variable. -/
-        let ⟨body', hbody'⟩ := s'.cseICom' body
-        {
-          Δ := Γ
-          hom := Ctxt.Hom.id
-          com' := .lete e' body' -- need to pullback `body` along the morphism.
-          hcom' := by {
+        let s' := s.snocNewExpr2Cache (e := e') /- add this expression into the cache for the latest variable. -/
+        let ⟨body', hbody'⟩ := s'.cseICom body
+        ⟨.lete e' body',  by
             intros VΓ
             simp[ICom.denote]
-            rw[hbody']
+            simp[Lets.denote_lete] at hbody' ⊢
+            rw[← hbody']
             rw[he']
-            done
-          }
-        }
+            done⟩
       | .some ⟨v', hv'⟩ =>
-        let ⟨body', hbody'⟩ := ICom.substVar s e body v' (by
-          intros V
-          rw[hv']
-          done
-        )
-        {
-          Δ := Γ
-          hom := Ctxt.Hom.id
-          com' := body'
-          hcom' := by {
+        let s' := s.snocOldExpr2Cache (enew := e') (eold := e) (henew := by { intros V; rw[he'] })
+          (v := v') (hv := by {intros V; rw[hv'] }) -- add this expression into the cache for the latest variable.
+        let ⟨body', hbody'⟩ := s'.cseICom body
+        ⟨.lete e body' -- we still keep the `e` for now. In the next version, we will delete the `e`
+        , by
             intros V
             simp[ICom.denote]
-            rw[← hbody']
-            simp
+            simp[Lets.denote_lete] at hbody' ⊢
+            specialize (hbody' V)
+            specialize (he' V)
+            rw[he'] at hbody'
+            apply hbody'
             done
-          }
-        }
+        ⟩
 
-/-- A variant of cseICom that exposes the same context `Γ`, instead of witnessing the change in context. This
-  uses the context morphism in `CSERet` to adapt the original context `Γ` to the new context `Δ`. -/
-unsafe def State.cseICom'
-  {lets : Lets Op Γstart Γ}
-  (s : State Op lets)
-  (com: ICom Op Γ α) :
-  { com' : ICom Op Γ α // ∀ (V: Ctxt.Valuation Γ), com.denote V = com'.denote V } :=
-  let ⟨Δ, hom, com', hcom'⟩ := s.cseICom com
-  ⟨com'.changeVars hom, by
-    intros V
-    rw[hcom']
-    simp[Ctxt.Valuation.hom]⟩
 end -- mutual.
 end CSE
