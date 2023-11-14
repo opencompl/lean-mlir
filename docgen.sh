ef8e95e7752ff38da7c856ac0ec117ba049f05bb
Siddharth Bhat <siddu.druid@gmail.com>
2023-11-14 16:41:09 +0000

    change docgen.sh to ignore failed doc build

--- docgen.sh
+++ docgen.sh
@@ -2,4 +2,4 @@ set -o xtrace
 set -e
 lake -Kenv=dev update
 lake exe cache get # load mathlib from cache
-lake -Kenv=dev build SSA:docs # Alive depends on SSA.
+lake -Kenv=dev build SSA:docs || true # Alive depends on SSA.
