379e58bcd466c0a51dab017d6f79829de5f0fbd7
Siddharth Bhat <siddu.druid@gmail.com>
2023-11-14 19:42:48 +0000

    fix lake invocation with -R
    https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/Listing.20Lake.20facets.3F/near/402026611

--- docgen.sh
+++ docgen.sh
@@ -1,6 +1,6 @@
 set -o xtrace
 set -e
-lake -Kenv=dev update
-lake exe cache get # load mathlib from cache
+lake -R -Kenv=dev update
+lake -R exe cache get # load mathlib from cache
 # make a phony build/doc if doc-gen fails.
-lake -Kenv=dev build SSA:docs || mkdir -p build/doc # Alive depends on SSA.
+lake -R -Kenv=dev build SSA:docs || mkdir -p build/doc # Alive depends on SSA.
