214f9d3128edceeac94e7e59ab262d84b3bb32b0
Siddharth Bhat <siddu.druid@gmail.com>
2023-11-16 02:55:37 +0000

    show example of 'rewritePeephole' with fuel

--- SSA/Projects/PaperExamples/PaperExamples.lean
+++ SSA/Projects/PaperExamples/PaperExamples.lean
@@ -102,7 +102,7 @@ def mlir2simple (reg : MLIR.AST.Region 0) :
     MLIR.AST.ExceptM Op (Σ (Γ : Ctxt Ty) (ty : Ty), Com Op Γ ty) := MLIR.AST.mkCom reg
 
 open Qq MLIR AST Lean Elab Term Meta in
-elab "[toy_icom| " reg:mlir_region "]" : term => do
+elab "[simple_com| " reg:mlir_region "]" : term => do
   let ast_stx ← `([mlir_region| $reg])
   let ast ← elabTermEnsuringTypeQ ast_stx q(Region 0)
   let mvalues ← `(⟨[], by rfl⟩)
@@ -130,9 +130,22 @@ elab "[toy_icom| " reg:mlir_region "]" : term => do
 end MLIR2Simple
 
 open MLIR AST MLIR2Simple in
+def eg₀ : Com Op (Ctxt.ofList []) .int :=
+  [simple_com| {
+    %c2= "const"() {value = 2} : () -> i32
+    %c4 = "const"() {value = 4} : () -> i32
+    %c6 = "add"(%c2, %c4) : (i32, i32) -> i32
+    %c8 = "add"(%c6, %c2) : (i32, i32) -> i32
+    "return"(%c8) : (i32) -> ()
+  }]
+
+def eg₀val := Com.denote eg₀ Ctxt.Valuation.nil
+#eval eg₀val -- 0x00000008#32
+
+open MLIR AST MLIR2Simple in
 /-- x + 0 -/
 def lhs : Com Op (Ctxt.ofList [.int]) .int :=
-  [toy_icom| {
+  [simple_com| {
     ^bb0(%x : i32):
       %c0 = "const" () { value = 0 : i32 } : () -> i32
       %out = "add" (%x, %c0) : (i32, i32) -> i32
@@ -142,7 +155,7 @@ def lhs : Com Op (Ctxt.ofList [.int]) .int :=
 open MLIR AST MLIR2Simple in
 /-- x -/
 def rhs : Com Op (Ctxt.ofList [.int]) .int :=
-  [toy_icom| {
+  [simple_com| {
     ^bb0(%x : i32):
       "return" (%x) : (i32) -> (i32)
   }]
@@ -168,10 +181,9 @@ def p1 : PeepholeRewrite Op [.int] .int :=
       sorry -- TODO: import ring instance from other file.
     }
 
-def ex1' : Com Op  (Ctxt.ofList [.int]) .int := rewritePeepholeAt p1 1 lhs
+def ex1_rewritePeepholeAt : Com Op  (Ctxt.ofList [.int]) .int := rewritePeepholeAt p1 1 lhs
 
-
-theorem EX1' : ex1' = (
+theorem hex1_rewritePeephole : ex1_rewritePeepholeAt = (
   -- %c0 = 0
   Com.lete (cst 0) <|
   -- %out_dead = %x + %c0
@@ -179,6 +191,18 @@ theorem EX1' : ex1' = (
   -- ret %c0
   Com.ret ⟨2, by simp [Ctxt.snoc]⟩)
   := by rfl
+
+def ex1_rewritePeephole : Com Op  (Ctxt.ofList [.int]) .int := rewritePeephole (fuel := 100) p1 lhs
+
+theorem Hex1_rewritePeephole : ex1_rewritePeephole = (
+  -- %c0 = 0
+  Com.lete (cst 0) <|
+  -- %out_dead = %x + %c0
+  Com.lete (add ⟨1, by simp [Ctxt.snoc]⟩ ⟨0, by simp [Ctxt.snoc]⟩ ) <| -- %out = %x + %c0
+  -- ret %c0
+  Com.ret ⟨2, by simp [Ctxt.snoc]⟩)
+  := by rfl
+
 
 end ToyNoRegion
 
