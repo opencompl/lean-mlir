5c8d0f8a3d13000e1176aceb51122005f54ee395
Siddharth Bhat <siddu.druid@gmail.com>
2023-12-08 10:32:55 +0530

    chore: nuke dead code

--- SSA/Projects/InstCombine/LLVM/Transform.lean
+++ SSA/Projects/InstCombine/LLVM/Transform.lean
@@ -27,9 +27,9 @@ section Monads
   errors.
 -/
 
-abbrev ExceptM  (Op) [OpSignature Op Ty] := Except (TransformError Ty)
-abbrev BuilderM (Op) [OpSignature Op Ty] := StateT NameMapping (ExceptM Op)
-abbrev ReaderM  (Op) [OpSignature Op Ty] := ReaderT NameMapping (ExceptM Op)
+abbrev ExceptM  Op [OpSignature Op Ty] := Except (TransformError Ty)
+abbrev BuilderM Op [OpSignature Op Ty] := StateT NameMapping (ExceptM Op)
+abbrev ReaderM  Op [OpSignature Op Ty] := ReaderT NameMapping (ExceptM Op)
 
 instance {Op : Type} [OpSignature Op Ty] : MonadLift (ReaderM Op) (BuilderM Op) where
   monadLift x := do (ReaderT.run x (←get) : ExceptM ..)
@@ -37,7 +37,7 @@ instance {Op : Type} [OpSignature Op Ty] : MonadLift (ReaderM Op) (BuilderM Op) 
 instance {Op : Type} [OpSignature Op Ty] : MonadLift (ExceptM Op) (ReaderM Op) where
   monadLift x := do return ←x
 
-def BuilderM.runWithNewMapping {Op : Type} [OpSignature Op Ty] (k : BuilderM Op α) : ExceptM Op α :=
+def BuilderM.runWithEmptyMapping {Op : Type} [OpSignature Op Ty] (k : BuilderM Op α) : ExceptM Op α :=
   Prod.fst <$> StateT.run k []
 
 end Monads
@@ -107,214 +107,9 @@ def BuilderM.isErr {α : Type} (x : BuilderM Op α) : Bool :=
   | Except.ok _ => true
   | Except.error _ => false
 
-#check Ctxt.Var
-/-
-def mkUnaryOp {Γ : Ctxt Ty} {ty : Ty} (op : MOp φ)
-  (e : Ctxt.Var Γ ty) : ExceptM Op <| Expr Op Γ ty :=
- match ty with
- | .bitvec w =>
-   match op with
-   -- Can't use a single arm, Lean won't write the rhs accordingly
-    | .neg w' => if h : w = w'
-      then return ⟨
-        .neg w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e) .nil,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .not w' => if h : w = w'
-      then return ⟨
-        .not w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e) .nil,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .copy w' => if h : w = w'
-      then return ⟨
-        .copy w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e) .nil,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | _ => throw <| .unsupportedUnaryOp
-
-def mkBinOp {Γ : Ctxt Ty} {ty : Ty} (op : MOp φ)
-    (e₁ e₂ : Ctxt.Var Γ ty) : ExceptM Op <| Expr Op Γ ty :=
- match ty with
- | .bitvec w =>
-   match op with
-   -- Can't use a single arm, Lean won't write the rhs accordingly
-    | .add w' => if h : w = w'
-      then return ⟨
-        .add w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .and w' => if h : w = w'
-      then return ⟨
-        .and w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .or w' => if h : w = w'
-      then return ⟨
-        .or w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .xor w' => if h : w = w'
-      then return ⟨
-        .xor w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .shl w' => if h : w = w'
-      then return ⟨
-        .shl w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .lshr w' => if h : w = w'
-      then return ⟨
-        .lshr w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | .ashr w' => if h : w = w'
-      then return ⟨
-        .ashr w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-     | .urem w' => if h : w = w'
-      then return ⟨
-        .urem w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-     | .srem w' => if h : w = w'
-      then return ⟨
-        .srem w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-     | .mul w' => if h : w = w'
-      then return ⟨
-        .mul w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-      | .sub w' => if h : w = w'
-      then return ⟨
-        .sub w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-      | .sdiv w' => if h : w = w'
-      then return ⟨
-        .sdiv w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-      | .udiv w' => if  h : w = w'
-      then return ⟨
-        .udiv w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-    | op => throw <| .unsupportedBinaryOp s!"unsupported binary operation {op}"
-
-def mkIcmp {Γ : Ctxt _} {ty : Ty} (op : MOp φ)
-    (e₁ e₂ : Ctxt.Var Γ ty) : ExceptM Op <| Expr Op Γ (.bitvec 1) :=
-  match ty with
-  | .bitvec w =>
-    match op with
-      | .icmp p w' => if  h : w = w'
-      then return ⟨
-        .icmp p w',
-        by simp [OpSignature.outTy, signature, h],
-        .cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-        .nil
-      ⟩
-      else throw <| .widthError w w'
-      | _ => throw <| .unsupportedOp "unsupported icmp operation"
-
-def mkSelect {Γ : Ctxt Ty} {ty : Ty} (op : MOp φ)
-    (c : Ctxt.Var Γ (.bitvec 1)) (e₁ e₂ : Ctxt.Var Γ ty) :
-    ExceptM Op <| Expr Op Γ ty :=
-  match ty with
-  | .bitvec w =>
-    match op with
-        | .select w' => if  h : w = w'
-        then return ⟨
-          .select w',
-          by simp [OpSignature.outTy, signature, h],
-          .cons c <|.cons (h ▸ e₁) <| .cons (h ▸ e₂) .nil ,
-          .nil
-        ⟩
-        else throw <| .widthError w w'
-        | _ => throw <| .unsupportedOp "Unsupported select operation"
-
-def mkOpExpr {Γ : Ctxt Ty} (op : MOp φ)
-    (arg : HVector (fun t => Ctxt.Var Γ t) (OpSignature.sig op)) :
-    ExceptM Op <| Expr Op Γ (OpSignature.outTy op) :=
-  match op with
-  | .and _ | .or _ | .xor _ | .shl _ | .lshr _ | .ashr _
-  | .add _ | .mul _ | .sub _ | .udiv _ | .sdiv _
-  | .srem _ | .urem _  =>
-    let (e₁, e₂) := arg.toTuple
-    mkBinOp op e₁ e₂
-  | .icmp _ _ =>
-    let (e₁, e₂) := arg.toTuple
-    mkIcmp op e₁ e₂
-  | .not _ | .neg _ | .copy _ =>
-    mkUnaryOp op arg.head
-  | .select _ =>
-    let (c, e₁, e₂) := arg.toTuple
-    mkSelect op c e₁ e₂
-  | .const .. => throw <| .unsupportedOp  "Tried to build Op expression from constant"
-
-def MLIRType.mkTy : MLIRType φ → ExceptM Op Ty
-  | MLIRType.int Signedness.Signless w => do
-    return .bitvec w
-  | _ => throw .unsupportedType -- "Unsupported type"
--/
-
 def TypedSSAVal.mkTy [TransformTy Op Ty φ] : TypedSSAVal φ → ExceptM Op Ty
   | (.SSAVal _, ty) => TransformTy.mkTy ty
-/-
-def mkVal (ty : InstCombine.Ty) : Int → BitVec ty.width
-  | val => BitVec.ofInt ty.width val
--/
+
 /-- Translate a `TypedSSAVal` (a name with an expected type), to a variable in the context.
     This expects the name to have already been declared before -/
 def TypedSSAVal.mkVal [instTransformTy : TransformTy Op Ty φ] (Γ : Ctxt Ty) : TypedSSAVal φ →
@@ -343,105 +138,6 @@ def TypedSSAVal.newVal [instTransformTy : TransformTy Op Ty φ] (Γ : Ctxt Ty) :
     let ty ← instTransformTy.mkTy tyStx
     let ⟨Γ, var⟩ ← addValToMapping Γ valStx ty
     return ⟨Γ, ty, var⟩
-
-/-
-def mkExpr (Γ : Ctxt Ty) (opStx : MLIR.AST.Op φ) : ReaderM Op (Σ ty, Expr Op Γ ty) := do
-  match opStx.args with
-  | v₁Stx::v₂Stx::v₃Stx::[] =>
-      let ⟨.bitvec w₁, v₁⟩ ← TypedSSAVal.mkVal Γ v₁Stx
-      let ⟨.bitvec w₂, v₂⟩ ← TypedSSAVal.mkVal Γ v₂Stx
-      let ⟨.bitvec w₃, v₃⟩ ← TypedSSAVal.mkVal Γ v₃Stx
-      match opStx.name with
-      | "llvm.select" =>
-        if hw1 : w₁ = 1 then
-          if hw23 : w₂  = w₃ then
-            let selectOp ← mkSelect (MOp.select w₂) (hw1 ▸ v₁) v₂ (hw23 ▸ v₃)
-            return ⟨.bitvec w₂, selectOp⟩
-          else
-            throw <| .widthError w₁ w₂ -- s!"mismatched types {ty₁} ≠ {ty₂} in binary op"
-        else throw <| .unsupportedOp s!"expected select condtion to have width 1, found width '{w₁}'"
-      | op => throw <| .unsupportedOp s!"Unsuported ternary operation or invalid arguments '{op}'"
-  | v₁Stx::v₂Stx::[] =>
-    let ⟨.bitvec w₁, v₁⟩ ← TypedSSAVal.mkVal Γ v₁Stx
-    let ⟨.bitvec w₂, v₂⟩ ← TypedSSAVal.mkVal Γ v₂Stx
-    -- let ty₁ := ty₁.instantiave
-    let op ← match opStx.name with
-      | "llvm.and"    => pure (MOp.and w₁)
-      | "llvm.or"     => pure (MOp.or w₁)
-      | "llvm.xor"    => pure (MOp.xor w₁)
-      | "llvm.shl"    => pure (MOp.shl w₁)
-      | "llvm.lshr"   => pure (MOp.lshr w₁)
-      | "llvm.ashr"   => pure (MOp.ashr w₁)
-      | "llvm.urem"   => pure (MOp.urem w₁)
-      | "llvm.srem"   => pure (MOp.srem w₁)
-      | "llvm.add"    => pure (MOp.add w₁)
-      | "llvm.mul"    => pure (MOp.mul w₁)
-      | "llvm.sub"    => pure (MOp.sub w₁)
-      | "llvm.sdiv"   => pure (MOp.sdiv w₁)
-      | "llvm.udiv"   => pure (MOp.udiv w₁)
-      | "llvm.icmp.eq" => pure (MOp.icmp LLVM.IntPredicate.eq w₁)
-      | "llvm.icmp.ne" => pure (MOp.icmp LLVM.IntPredicate.ne w₁)
-      | "llvm.icmp.ugt" => pure (MOp.icmp LLVM.IntPredicate.ugt w₁)
-      | "llvm.icmp.uge" => pure (MOp.icmp LLVM.IntPredicate.uge w₁)
-      | "llvm.icmp.ult" => pure (MOp.icmp LLVM.IntPredicate.ult w₁)
-      | "llvm.icmp.ule" => pure (MOp.icmp LLVM.IntPredicate.ule w₁)
-      | "llvm.icmp.sgt" => pure (MOp.icmp LLVM.IntPredicate.sgt w₁)
-      | "llvm.icmp.sge" => pure (MOp.icmp LLVM.IntPredicate.sge w₁)
-      | "llvm.icmp.slt" => pure (MOp.icmp LLVM.IntPredicate.slt w₁)
-      | "llvm.icmp.sle" => pure (MOp.icmp LLVM.IntPredicate.sle w₁)
-      | opstr => throw <| .unsupportedOp s!"Unsuported binary operation or invalid arguments '{opstr}'"
-    match op with
-    | .icmp .. =>
-      if hty : w₁ = w₂ then
-        let icmpOp ← mkIcmp op v₁ (hty ▸ v₂)
-        return ⟨.bitvec 1, icmpOp⟩
-      else
-        throw <| .widthError w₁ w₂ -- s!"mismatched types {ty₁} ≠ {ty₂} in binary op"
-    | _ =>
-      if hty : w₁ = w₂ then
-        let binOp ← mkBinOp op v₁ (hty ▸ v₂)
-        return ⟨.bitvec w₁, binOp⟩
-      else
-        throw <| .widthError w₁ w₂ -- s!"mismatched types {ty₁} ≠ {ty₂} in binary op"
-  | vStx::[] =>
-    let ⟨.bitvec w, v⟩ ← TypedSSAVal.mkVal Γ vStx
-    let op ← match opStx.name with
-        | "llvm.not" => pure <| MOp.not w
-        | "llvm.neg" => pure <| MOp.neg w
-        | "llvm.copy" => pure <| MOp.copy w
-        | _ => throw <| .generic s!"Unknown (unary) operation syntax {opStx.name}"
-    let op ← mkUnaryOp op v
-    return ⟨.bitvec w, op⟩
-  | [] =>
-    if opStx.name ==  "llvm.mlir.constant"
-    then do
-    let some att := opStx.attrs.getAttr "value"
-      | throw <| .generic "tried to resolve constant without 'value' attribute"
-    match att with
-      | .int val ty =>
-          let opTy@(MTy.bitvec w) ← MLIRType.mkTy ty -- ty.mkTy
-          return ⟨opTy, ⟨
-            MOp.const w val,
-            by simp [OpSignature.outTy, signature, *],
-            HVector.nil,
-            HVector.nil
-          ⟩⟩
-      | _ => throw <| .generic "invalid constant attribute"
-    else
-      throw <| .generic s!"invalid (0-ary) expression {opStx.name}"
-  | _ => throw <| .generic s!"unsupported expression (with unsupported arity) {opStx.name}"
--/
-
-/-
-def mkReturn (Γ : Ctxt Ty) (opStx : MLIR.AST.Op φ) : ReaderM Op (Σ ty, Com Op Γ ty) :=
-  if opStx.name == "llvm.return"
-  then match opStx.args with
-  | vStx::[] => do
-    let ⟨ty, v⟩ ← TypedSSAVal.mkVal Γ vStx
-    return ⟨ty, _root_.Com.ret v⟩
-  | _ => throw <| .generic s!"Ill-formed return statement (wrong arity, expected 1, got {opStx.args.length})"
-  else throw <| .generic s!"Tried to build return out of non-return statement {opStx.name}"
--/
 
 /-- Given a list of `TypedSSAVal`s, treat each as a binder and declare a new variable with the
     given name and type -/
@@ -472,59 +168,9 @@ def mkCom [TransformTy Op Ty φ] [TransformExpr Op Ty φ] [TransformReturn Op Ty
   (reg : MLIR.AST.Region φ) : ExceptM Op  (Σ (Γ : Ctxt Ty) (ty : Ty), Com Op Γ ty) :=
   match reg.ops with
   | [] => throw <| .generic "Ill-formed region (empty)"
-  | coms => BuilderM.runWithNewMapping <| do
+  | coms => BuilderM.runWithEmptyMapping <| do
     let Γ ← declareBindings ∅ reg.args
     let com ← mkComHelper Γ coms
     return ⟨Γ, com⟩
 
-/-!
-  ## Instantiation
-  Finally, we show how to instantiate a family of programs to a concrete program
--/
-
-/-
-def _root_.InstCombine.MTy.instantiate (vals : Vector Nat φ) : Ty → InstCombine.Ty
-  | .bitvec w => .bitvec <| .concrete <| w.instantiate vals
-
-def _root_.InstCombine.MOp.instantiate (vals : Vector Nat φ) : MOp φ → InstCombine.Op
-  | .and w => .and (w.instantiate vals)
-  | .or w => .or (w.instantiate vals)
-  | .not w => .not (w.instantiate vals)
-  | .xor w => .xor (w.instantiate vals)
-  | .shl w => .shl (w.instantiate vals)
-  | .lshr w => .lshr (w.instantiate vals)
-  | .ashr w => .ashr (w.instantiate vals)
-  | .urem w => .urem (w.instantiate vals)
-  | .srem w => .srem (w.instantiate vals)
-  | .select w => .select (w.instantiate vals)
-  | .add w => .add (w.instantiate vals)
-  | .mul w => .mul (w.instantiate vals)
-  | .sub w => .sub (w.instantiate vals)
-  | .neg w => .neg (w.instantiate vals)
-  | .copy w => .copy (w.instantiate vals)
-  | .sdiv w => .sdiv (w.instantiate vals)
-  | .udiv w => .udiv (w.instantiate vals)
-  | .icmp c w => .icmp c (w.instantiate vals)
-  | .const w val => .const (w.instantiate vals) val
-
-def Ctxt.instantiate (vals : Vector Nat φ) (Γ : Ctxt Ty) : Ctxt InstCombine.Ty :=
-  Γ.map (MTy.instantiate vals)
-
-def MOp.instantiateCom (vals : Vector Nat φ) : DialectMorphism Op (InstCombine.Op) where
-  mapOp := MOp.instantiate vals
-  mapTy := MTy.instantiate vals
-  preserves_signature op := by
-    simp only [MTy.instantiate, MOp.instantiate, ConcreteOrMVar.instantiate, (· <$> ·), signature,
-      InstCombine.MOp.sig, InstCombine.MOp.outTy, Function.comp_apply, List.map, Signature.mk.injEq,
-      true_and]
-    cases op <;> simp only [List.map, and_self, List.cons.injEq]
-
-
-open InstCombine (Op Ty) in
-def mkComInstantiate (reg : Region φ) :
-    ExceptM Op (Vector Nat φ → Σ (Γ : Ctxt InstCombine.Ty) (ty : InstCombine.Ty), Com InstCombine.Op Γ ty) := do
-  let ⟨Γ, ty, com⟩ ← mkCom reg
-  return fun vals =>
-    ⟨Γ.instantiate vals, ty.instantiate vals, com.map (MOp.instantiateCom vals)⟩
--/
 end MLIR.AST
