d68bd8e6ecf2b5fafeb6e759b7cb70f1136a4d6e
Andrés Goens <Andrs Goens>
2024-01-13 09:33:40 +0100

    parse mty expressions

--- SSA/Projects/InstCombine/LLVM/Signature.lean
+++ SSA/Projects/InstCombine/LLVM/Signature.lean
@@ -14,14 +14,69 @@ instance : ToString CliType where
  | .varw => "w"
  | .width (n : Nat) => s!"{n}"
 
-
-
 opaque k : Nat
 opaque m : Nat
 opaque e : Expr
 
+
+def concrete? (e : Expr) : Option (Nat → ConcreteOrMVar ℕ 0) := do
+  guard <| e.isAppOfArity ``ConcreteOrMVar.concrete 3
+  -- It should be possible to ensure the array accesses from the guard above
+  let args := e.getAppArgs
+
+  -- first argument sholud be Nat
+  let mvarsExpr ← args[0]?
+  guard (mvarsExpr.isConstOf `Nat)
+
+  -- second argument sholud be 0
+  let mvarsExpr ← args[1]?
+  guard (mvarsExpr.isNatLit)
+  guard (mvarsExpr.natLit! = 0)
+
+  -- third argument is value, either literal or bound variable
+  let widthExpr ← args[2]?
+  if widthExpr.isBVar then
+    some <| fun w => .concrete w
+  else if widthExpr.isNatLit then
+    some <| fun _ => .concrete (widthExpr.natLit!)
+  else
+    none
+
+def concrete!  (e : Expr) : Nat → ConcreteOrMVar ℕ 0 := concrete? e |>.get!
+
+def mty? (e : Expr) : Option (Nat → InstCombine.MTy 0) := do
+  guard <| e.isAppOfArity ``InstCombine.MTy.bitvec 2
+  -- It should be possible to ensure the array accesses from the guard above
+  let args := e.getAppArgs
+
+  -- first argument sholud be 0, otherwise it still has metavars
+  let mvarsExpr ← args[0]?
+  guard (mvarsExpr.isNatLit)
+  guard (mvarsExpr.natLit! = 0)
+
+  -- second argument should be a concrete value, extract it
+  let widthExpr ← args[1]?
+  let width ← concrete? widthExpr
+  pure <| fun w => .bitvec <| width w
+
+def mty! (e : Expr) : Nat → InstCombine.MTy 0 := mty? e |>.get!
+
+/-
+
+
+def concreteOrMVarToCliType (e : Expr) : MetaM CliType := do
+  match e with
+  | .app (.const ``ConcreteOrMVar.concrete _) kexp => do
+    match kexp : Q(Nat) with
+      | ~q($k) => pure <| CliType.width k
+      | _ => throw <| Exception.error default "Unexpected expression (ill-formed signature): {e}"
+    pure <| CliType.width 42
+  | _ => throw <| Exception.error default "Unexpected expression (ill-formed signature): {e}"
+-/
+
 -- pattern match on an `e` to get a list of lean expressions
-def ExprToList (e : Expr) : MetaM (List Lean.Expr) := do --sorry
+def exprToList (e : Expr) : MetaM (List Lean.Expr) := do --sorry
+ logInfo m!"exprToList {e}"
  return []
 
 
@@ -51,8 +106,10 @@ def printSignature (ty0 : Expr) := do
        let llvmArgTys := args[3]!
        let llvmRetTy := args[4]!
        logInfo m!"argTys: {llvmArgTys}"
-       logInfo m!"retTy: {llvmRetTy}"
-       let llvmArgTys : List CliType ← liftM <| (← ExprToList llvmArgTys).mapM ExprToCliType
+       let (_,llvmArgs) := llvmArgTys.listLit?.get!
+       logInfo m!"argTys: {llvmArgs.map mty! |>.map (fun f : Nat → InstCombine.MTy 0 => f 42)}"
+       logInfo m!"retTy.mty!: {mty! llvmRetTy 42}"
+       let llvmArgTys : List CliType ← liftM <| (← exprToList llvmArgTys).mapM ExprToCliType
        logInfo m!"argTys (as Cli type): {llvmArgTys}"
        let llvmRetTy ← ExprToCliType llvmRetTy
        logInfo m!"retTy (as Cli type): {llvmRetTy}"
