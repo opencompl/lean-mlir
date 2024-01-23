1956f95a51a976e6aa41f2c4936da76e877e7353
Alex Keizer <alex@keizer.dev>
2024-01-23 15:27:34 +0000

    feat: `change_mlir_context` tactic

--- SSA/Core/Tactic.lean
+++ SSA/Core/Tactic.lean
@@ -1,8 +1,38 @@
 import SSA.Core.Framework
+import Qq
 
 namespace SSA
 
 open Ctxt (Var Valuation DerivedCtxt)
+
+section
+
+open Lean Meta Elab.Tactic Qq
+
+/-- Given a `V : Valuation Γ`, fully reduce the context `Γ` in the type of `V` -/
+elab "change_mlir_context " V:ident : tactic => do
+  let V : Name := V.getId
+  withMainContext do
+    let ctx ← getLCtx
+    let Vdecl : LocalDecl ← match ctx.findFromUserName? V with
+      | some decl => pure decl
+      | none => throwError f!"Failed to find variable `{V}` in the local context"
+
+    -- Assert that the type of `V` is `Ctxt.Valuation ?Γ`
+    let Ty ← mkFreshExprMVarQ q(Type)
+    let Γ  ← mkFreshExprMVarQ q(Ctxt $Ty)
+    let G  ← mkFreshExprMVarQ q(Goedel $Ty)
+    let _  ← assertDefEqQ Vdecl.type q(@Ctxt.Valuation $Ty $G $Γ)
+
+    -- Reduce the context `Γ`
+    let Γr ← reduce Γ
+    let Γr : Q(Ctxt $Ty) := Γr
+
+    let goal ← getMainGoal
+    let newGoal ← goal.changeLocalDecl Vdecl.fvarId q(Valuation $Γr)
+    replaceMainGoal [newGoal]
+
+end
 
 /--
 `simp_peephole [t1, t2, ... tn]` at Γ simplifies the evaluation of the context Γ,
@@ -11,6 +41,7 @@ leaving behind a bare Lean level proposition to be proven.
 macro "simp_peephole" "[" ts: Lean.Parser.Tactic.simpLemma,* "]" "at" ll:ident : tactic =>
   `(tactic|
       (
+      change_mlir_context $ll
       try simp (config := {decide := false}) only [
         Int.ofNat_eq_coe, Nat.cast_zero, DerivedCtxt.snoc, DerivedCtxt.ofCtxt,
         DerivedCtxt.ofCtxt_empty, Valuation.snoc_last,
