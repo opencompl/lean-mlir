fe1b1c4ab89a2c959cca9ba9a2320a0be558c87d
Andrés Goens <Andrs Goens>
2024-01-26 14:36:34 +0100

    Instances for some tuples that are generated from numbers

--- SSA/Projects/InstCombine/LLVM/CLITests.lean
+++ SSA/Projects/InstCombine/LLVM/CLITests.lean
@@ -5,20 +5,18 @@ import Cli
 
 open Lean
 
-/-- Parse a triple -/
-instance [A : Cli.ParseableType α] [B : Cli.ParseableType β] [C : Cli.ParseableType γ] :
-  Cli.ParseableType (α × β × γ) where
-    name :=  s!"({A.name} × {B.name} × {C.name})"
+/-- Parse a tuple -/
+instance instParseableTuple [A : Cli.ParseableType α] [B : Cli.ParseableType β] :
+  Cli.ParseableType (α × β) where
+    name :=  s!"({A.name} × {B.name})"
     parse? str := do
       let str := str.trim.splitOn ","
       match str with
-      | [a, b, c] => do
+      | [a, b] => do
         let a ← A.parse? a.trim
         let b ← B.parse? b.trim
-        let c ← C.parse? c.trim
-        return (a, b, c)
+        return (a, b)
       | _ => .none
-
 
 structure CliTest where
   name : Name
@@ -28,20 +26,46 @@ structure CliTest where
   code : MLIR.AST.Com context ty
   signature : CliSignature
 
-/-- unify this with CliTest -/
-structure Test where
-  name : String
-  params : Type
-  paramsParseable : Cli.ParseableType params
-  testFn : params → IO Bool
+-- We add a special case for 1 because Nat × Unit ≠ Nat
+private def natParams : Nat → Type
+  | 0 => Unit
+  | 1 => Nat
+  | n + 1 => Nat × (natParams n)
 
-def Test.ofFn (name : String) ⦃params : Type⦄ [Cli.ParseableType params]
-  (testFn : params → IO Bool) : Test where
-  name := name
-  params := _
-  paramsParseable := inferInstance
-  testFn := testFn
+theorem natParamsTup : ∀ n : Nat, n > 0 → natParams (n + 1) = (Nat × natParams n) := by
+  intro n
+  induction n <;> simp [natParams]
 
+def instParseableNatParams {n : Nat} : Cli.ParseableType (natParams n) where
+  name := s!"{n} nat(s)."
+  parse? str :=
+  match n with
+    | 0 =>
+      let inst : Cli.ParseableType Unit := inferInstance
+      inst.parse? str
+    | 1 =>
+      let inst : Cli.ParseableType Nat := inferInstance
+      inst.parse? str
+    -- this feels unnecessarily complicated
+    | (n + 1) + 1 =>
+      let inst1 : Cli.ParseableType Nat := inferInstance
+      let instn : Cli.ParseableType (natParams (n + 1)) := @instParseableNatParams (n + 1)
+      let instTup : Cli.ParseableType (Nat × (natParams <| n + 1)) := @instParseableTuple Nat (natParams (n + 1)) inst1 instn
+      let hn1gt0 : (n + 1) > 0 := by
+        simp_all only [gt_iff_lt, add_pos_iff, or_true] -- aesop?
+      let hn1eq := natParamsTup (n + 1) hn1gt0
+      hn1eq ▸ instTup.parse? str
+
+instance {n : Nat} : Cli.ParseableType (natParams n) := instParseableNatParams
+
+def CliTest.params : CliTest → Type
+| test => natParams test.mvars
+
+def CliTest.paramsParseable (test : CliTest) : Cli.ParseableType (test.params) :=
+  instParseableNatParams
+
+def CliTest.testFn (test : CliTest) : test.params → IO Bool :=
+  fun _ => pure true -- placeholder
 
 -- Define an attribute to add up all LLVM tests
 -- https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/.E2.9C.94.20Stateful.2FAggregating.20Macros.3F/near/301067121
