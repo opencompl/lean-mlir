d7843b193607ab6b5e1b61238f8ef2736defb829
Tobias Grosser <tobias@grosser.es>
2024-04-16 18:51:40 +0200

    Drop two experiments that are unrelated to this PR

--- SSA/Experimental/Bugred/irreducible_def_1.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.Tactic.IrreducibleDef
-
-namespace Irred
-irreducible_def foo (x : Nat) : Int := x
-
-def bar (x : Nat) : Int := foo x
-end Irred
--- SSA/Experimental/Bugred/irreducible_def_2.lean
+++
@@ -1,1 +0,0 @@
-import IrreducibleDef1
