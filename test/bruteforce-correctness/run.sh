e2a465980fbae18d242b008a6cab0fc2609f8f53
Andrés Goens <andres@goens.org>
2024-04-18 14:28:48 +0200

    fix two more bash files

--- test/bruteforce-correctness/run.sh
+++ test/bruteforce-correctness/run.sh
@@ -1,7 +1,6 @@
-/-
-Released under Apache 2.0 license as described in the file LICENSE.
--/
 #!/usr/bin/env bash
+# Released under Apache 2.0 license as described in the file LICENSE.
+
 set -e
 set -o xtrace
 rm generated* || true # don't error if no files are removed
