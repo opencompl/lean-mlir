d264d196d036b9ab6188390fa2138cc741f3c3b9
Tobias Grosser <tobias@grosser.es>
2024-04-25 09:11:53 +0100

    feat: do not leak the type definition in alive (#257)
    This change ensures that simp_alive_ssa does not unfold the type
    definition of LLVM bitvectors, such that our semantics do not leak
    before we unfold them.

--- SSA/Projects/InstCombine/LLVM/Enumerator.lean
+++ SSA/Projects/InstCombine/LLVM/Enumerator.lean
@@ -152,8 +152,8 @@ def concreteCliTestRows (test : ConcreteCliTest) : IO <| Array Row := do
         let ty : InstCombine.MTy 0 := test.ty
         match hty : ty with
           | .bitvec (.concrete w) =>
-              let h : TyDenote.toType ty = Option (Std.BitVec w) := by simp [hty, TyDenote.toType]
-              let retv' : Option (Std.BitVec w) := h ▸ retv
+              let h : TyDenote.toType ty = Option (BitVec w) := by simp [hty, TyDenote.toType, LLVM.IntW]
+              let retv' : Option (BitVec w) := h ▸ retv
               let retv := BitVec.outputToString retv'
               let row : Row := {
                 opName := s!"{testNameToRowName test.name.toString}",
