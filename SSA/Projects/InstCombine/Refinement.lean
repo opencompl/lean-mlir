7e62e10a526b51db42605ff834dfd1ad4a3e4d39
Tobias Grosser <tobias@grosser.es>
2024-05-06 20:48:04 +0100

    chore: limit inclusion of AliveStatements
    Some of our BitVec lemmas depend on mathlib. Make this explicit
    and keep Refinement and Tactic independent of AliveStatements.
    
    This will facilitate the inclusion of Refinement in AliveStatements
    (which currently seems to break).

--- SSA/Projects/InstCombine/Refinement.lean
+++ SSA/Projects/InstCombine/Refinement.lean
@@ -2,7 +2,6 @@
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 import SSA.Projects.InstCombine.Base
-import SSA.Projects.InstCombine.AliveStatements
 
 open InstCombine (LLVM)
 
--- SSA/Projects/InstCombine/Tactic.lean
+++ SSA/Projects/InstCombine/Tactic.lean
@@ -3,7 +3,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 -/
 import SSA.Projects.InstCombine.LLVM.EDSL
 import SSA.Projects.InstCombine.LLVM.SimpSet
-import SSA.Projects.InstCombine.AliveStatements
 import SSA.Projects.InstCombine.Refinement
 import SSA.Projects.InstCombine.ForStd
 import Mathlib.Tactic
