ea226f34ed1acba053eac2c6bdeb1d90684e020e
Siddharth <siddu.druid@gmail.com>
2024-05-08 13:31:11 +0100

    feat: implement 'opt' entrypoint to parse MLIR from files. (#102) (#282)
    This adds an entry point to parse MLIR code and execute it via our
    denotational semantics.
    
    Co-authored-by: Andrés Goens <andres@goens.org>

---
+++ SSA/Projects/InstCombine/LLVM/Parser.lean
@@ -0,0 +1,15 @@
+import SSA.Core.MLIRSyntax.Parser
+import SSA.Projects.InstCombine.LLVM.EDSL
+import Init.Data.Repr
+
+open MLIR AST InstCombine
+def regionTransform (region : Region 0) : Except ParseError
+  (Σ (Γ' : Context) (eff : EffectKind) (ty : Ty), Com LLVM Γ' eff ty) :=
+    let res := mkCom (d:= LLVM) region
+    match res with
+      | Except.error e => Except.error s!"Error:\n{reprStr e}"
+      | Except.ok res => Except.ok res
+
+def parseComFromFile (fileName : String) :
+ IO (Option (Σ (Γ' : Context) (eff : EffectKind) (ty : InstCombine.Ty), Com LLVM Γ' eff ty)) := do
+ parseRegionFromFile fileName regionTransform
