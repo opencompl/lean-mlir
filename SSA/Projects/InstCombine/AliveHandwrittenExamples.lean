e3d6ee3a107d26f084919f857f7c2dc694c6b39c
Tobias Grosser <tobias@grosser.es>
2024-05-23 08:35:46 +0100

    WIP

--- SSA/Projects/InstCombine/AliveHandwrittenExamples.lean
+++ SSA/Projects/InstCombine/AliveHandwrittenExamples.lean
@@ -7,6 +7,7 @@ import SSA.Projects.InstCombine.Refinement
 import SSA.Projects.InstCombine.Tactic
 import SSA.Projects.InstCombine.Base
 import SSA.Projects.InstCombine.ForStd
+import SSA.Projects.InstCombine.ComWrappers
 import SSA.Core.ErasedContext
 
 open MLIR AST
@@ -28,20 +29,45 @@ precondition: true
 %r = udiv %X, %Y
 
 -/
-def alive_DivRemOfSelect_src (w : Nat) :=
-  [alive_icom (w)| {
+def alive_DivRemOfSelect_src :=
+  [alive_icom ()| {
   ^bb0():
-    %c0 = "llvm.mlir.constant" () { value = 0 : _ } :() -> (_)
-    %c1 = "llvm.mlir.constant" () { value = 0 : _ } :() -> (_)
-    "llvm.return" (%c0) : (_) -> ()
+    %c0 = "llvm.mlir.constant" () { value = 0 : i16 } :() -> (i16)
+    "llvm.return" (%c0) : (i16) -> ()
   }]
+
+open ComWrappers
+
+def aliveDivRemOfSelect_comWrapper :
+    Com InstCombine.LLVM [] .pure (InstCombine.Ty.bitvec 16) :=
+  .lete (const 16 0  ) <|
+  .ret ⟨0, by simp [Ctxt.snoc]⟩
+
+#check alive_DivRemOfSelect_src
+#check aliveDivRemOfSelect_comWrapper
+
+lemma src_eq_comwrapper :
+  alive_DivRemOfSelect_src
+  =
+  aliveDivRemOfSelect_comWrapper := by
+  unfold alive_DivRemOfSelect_src
+  unfold aliveDivRemOfSelect_comWrapper
+  simp only [Com.changeDialect_ret, Com.changeDialect_lete, Expr.changeDialect,
+    (HVector.changeDialect_nil), InstcombineTransformDialect.MOp.instantiateCom]
+  dsimp only [DialectMorphism.mapOp_mk, DialectMorphism.mapTy_mk,
+    InstcombineTransformDialect.MOp.instantiateCom,
+    InstcombineTransformDialect.instantiateMOp,
+    InstcombineTransformDialect.instantiateMTy,
+    HVector.changeDialect_nil, HVector.map']
+  unfold ComWrappers.const
+  simp_alive_meta
+  rfl
 
 def alive_DivRemOfSelect_tgt (w : Nat) :=
   [alive_icom (w)| {
   ^bb0():
     %c0 = "llvm.mlir.constant" () { value = 0 : _ } :() -> (_)
-    %c1 = "llvm.mlir.constant" () { value = 0 : _ } :() -> (_)
-    "llvm.return" (%c1) : (_) -> ()
+    "llvm.return" (%c0) : (_) -> ()
   }]
 
 @[simp]
@@ -58,18 +84,18 @@ BitVec.toNat (BitVec.ofInt w 0) = 0 := by
 
 def test {w : ℕ}
   (Γv : @Ctxt.Valuation InstCombine.LLVM.Ty InstCombine.instTyDenoteTy []) :
-  @Ctxt.Valuation.snoc InstCombine.LLVM.Ty InstCombine.instTyDenoteTy
+  (@Ctxt.Valuation.snoc InstCombine.LLVM.Ty InstCombine.instTyDenoteTy
   (@List.nil InstCombine.Ty : List InstCombine.Ty)
   (InstCombine.Ty.bitvec w)
   Γv
-  (@LLVM.const? w 0) =
-  @Ctxt.Valuation.snoc InstCombine.LLVM.Ty InstCombine.instTyDenoteTy
+  (@LLVM.const? w 0)) =
+  (@Ctxt.Valuation.snoc InstCombine.LLVM.Ty InstCombine.instTyDenoteTy
   (@List.nil InstCombine.Ty : List InstCombine.Ty)
   (InstCombine.Ty.bitvec w)
   Γv
-  (@LLVM.const? w 0) := rfl
+  (@LLVM.const? w 0)) := rfl
 
---set_option pp.proofs true in
+set_option pp.proofs true in
 theorem alive_DivRemOfSelect (w : Nat) :
     alive_DivRemOfSelect_src w ⊑ alive_DivRemOfSelect_tgt w := by
   unfold alive_DivRemOfSelect_src alive_DivRemOfSelect_tgt
@@ -87,6 +113,64 @@ theorem alive_DivRemOfSelect (w : Nat) :
   dsimp only [Ctxt.map_cons, ConcreteOrMVar.instantiate_mvar_zero'', Ctxt.get?, Var.zero_eq_last,
     List.map_cons, List.map_nil]
   dsimp (config := {failIfUnchanged := false }) only [ConcreteOrMVar.instantiate]
+  simp_alive_meta
+          /- access the valuation -/
+  intros Γv
+  unfold MOp.const
+
+
+  unfold InstcombineTransformDialect.instantiateMOp
+  unfold InstcombineTransformDialect.instantiateMTy
+
+
+  simp (config := {failIfUnchanged := false}) only [
+        Com.denote, Expr.denote,
+        Ctxt.Valuation.snoc_last, Ctxt.map,
+        DialectDenote.denote,
+        /- Effect massaging -/
+        EffectKind.liftEffect_rfl,
+        InstCombine.Op.denote                      | ConcreteOrMVar.concrete w => w
+
+      -- `simp` might close trivial goals, so we use `only_goal` to ensure we only run
+      -- more tactics when we still have goals to solve, to avoid 'no goals to be solved' errors.
+      only_goal
+        simp (config := {failIfUnchanged := false}) only [Ctxt.Var.toSnoc, Ctxt.Var.last]
+        repeat (generalize_or_fail at $Γv)
+        try clear $Γv
+      )
+  simp_peephole [InstCombine.Op.denote] at Γv
+  simp_alive_ssa
+  simp (config := {failIfUnchanged := false}) only [
+        Int.ofNat_eq_coe, Nat.cast_zero, DerivedCtxt.snoc, DerivedCtxt.ofCtxt,
+        DerivedCtxt.ofCtxt_empty, Valuation.snoc_last,
+        Com.denote, Expr.denote, Var.zero_eq_last, Var.succ_eq_toSnoc,
+        Ctxt.empty, Ctxt.empty_eq, Ctxt.snoc, Ctxt.Valuation.nil, Ctxt.Valuation.snoc_last, Ctxt.map,
+        Ctxt.Valuation.snoc_eval, Ctxt.ofList, Ctxt.Valuation.snoc_toSnoc,
+        HVector.map, HVector.getN, HVector.get, HVector.toSingle, HVector.toPair, HVector.toTuple,
+        DialectDenote.denote, Expr.op_mk, Expr.args_mk,
+        DialectMorphism.mapOp, DialectMorphism.mapTy, List.map, Ctxt.snoc, List.map,
+        Function.comp, Valuation.ofPair, Valuation.ofHVector, Function.uncurry,
+        List.length_singleton, Fin.zero_eta, List.map_eq_map, List.map_cons, List.map_nil,
+        bind_assoc, pairBind,
+        /- `castPureToEff` -/
+        Com.letPure, Expr.denote_castPureToEff,
+        /- Unfold denotation -/
+        Com.denote_lete, Com.denote_ret, Expr.denote_unfold, HVector.denote,
+        /- Effect massaging -/
+        EffectKind.toMonad_pure, EffectKind.toMonad_impure,
+        EffectKind.liftEffect_rfl,
+        Id.pure_eq, Id.bind_eq, id_eq,
+        (HVector.denote_cons), (HVector.denote_nil), InstCombine.Op.denote, *]
+  simp (config := {failIfUnchanged := false}) only [
+            InstCombine.Op.denote, HVector.getN, HVector.get
+          ]
+
+
+
+
+
+
+  simp_peephole
   simp_alive_ssa
   simp only [zero_add]
   simp only [Var.succ_eq_toSnoc]
