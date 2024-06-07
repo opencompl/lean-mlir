030a9f9705be1fbf1817410e3598f932aa3f5b01
Tobias Grosser <tobias@grosser.es>
2024-06-07 07:31:05 +0100

    chore: remove update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-
-(cd SSA/Projects/InstCombine/; sed -e 's/EDSL/EDSLPretty/' -i Alive*.lean)
-(cd SSA/Projects/InstCombine/; sed -e 's/= "\(llvm..*\)" (\(.*\),\(.*\)) : (\(.*\),.*) -> (.*)/= \1 \2, \3 : \4/' -i Alive*.lean)
-(cd SSA/Projects/InstCombine/; sed -e 's/= "\(llvm..*\)" (\(.*\)) : (\(.*\)) -> (.*)/= \1 \2 : \3/' -i Alive*.lean)
-(cd SSA/Projects/InstCombine/; sed -e 's/"\(llvm.return\)" (\(.*\)) : (\(.*\)) -> (.*)/\1 \2 : \3/' -i Alive*.lean)
-(cd SSA/Projects/InstCombine/; sed -e 's/= "\(llvm.mlir.constant\)" () { value = \(.*\) : \(.*\) } :() -> (\(.*\))/= \1 \2 : \3/' -i Alive*.lean)
