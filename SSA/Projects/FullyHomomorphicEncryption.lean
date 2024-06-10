8243acd2cd4c9d1e1eb032cbe4eb1c0f29ed80dc
Siddharth Bhat <siddu.druid@gmail.com>
2024-06-10 16:11:10 +0100

    chore: move FHE rewrites into file called PaperRewrites

--- SSA/Projects/FullyHomomorphicEncryption.lean
+++ SSA/Projects/FullyHomomorphicEncryption.lean
@@ -2,7 +2,7 @@
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 import SSA.Projects.FullyHomomorphicEncryption.Basic
-import SSA.Projects.FullyHomomorphicEncryption.Rewrites
+import SSA.Projects.FullyHomomorphicEncryption.PaperExamples
 import SSA.Projects.FullyHomomorphicEncryption.Statements
 import SSA.Projects.FullyHomomorphicEncryption.Syntax
 import SSA.Projects.FullyHomomorphicEncryption.PrettySyntax
