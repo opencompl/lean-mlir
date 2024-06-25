c311a286ab125ca1aa765a024dd322dd86ca8afc
Siddharth Bhat <siddu.druid@gmail.com>
2024-06-25 13:21:23 +0100

    chore: address another comment

--- SSA/Projects/InstCombine/PaperExamples.lean
+++ SSA/Projects/InstCombine/PaperExamples.lean
@@ -15,70 +15,6 @@ import Lean
 namespace AlivePaperExamples
 open BitVec
 
-theorem iunfoldr_getLsb' {f : Fin w → α → α × Bool} (state : Nat → α)
-    (ind : ∀(i : Fin w), (f i (state i.val)).fst = state (i.val+1)) :
-  (∀ i : Fin w, getLsb (iunfoldr f (state 0)).snd i.val = (f i (state i.val)).snd)
-  ∧ (iunfoldr f (state 0)).fst = state w := by
-  unfold iunfoldr
-  simp
-  apply Fin.hIterate_elim
-        (fun j (p : α × BitVec j) => (hj : j ≤ w) →
-         (∀ i : Fin j,  getLsb p.snd i.val = (f ⟨i.val, Nat.lt_of_lt_of_le i.isLt hj⟩ (state i.val)).snd)
-          ∧ p.fst = state j)
-  case hj => simp
-  case init =>
-    intro
-    apply And.intro
-    · intro i
-      have := Fin.size_pos i
-      contradiction
-    · rfl
-  case step =>
-    intro j ⟨s, v⟩ ih hj
-    apply And.intro
-    case left =>
-      intro i
-      simp only [getLsb_cons]
-      have hj2 : j.val ≤ w := by simp
-      cases (Nat.lt_or_eq_of_le (Nat.lt_succ.mp i.isLt)) with
-      | inl h3 => simp [if_neg, (Nat.ne_of_lt h3)]
-                  exact (ih hj2).1 ⟨i.val, h3⟩
-      | inr h3 => simp [h3, if_pos]
-                  cases (Nat.eq_zero_or_pos j.val) with
-                  | inl hj3 => congr
-                               rw [← (ih hj2).2]
-                  | inr hj3 => congr
-                               exact (ih hj2).2
-    case right =>
-      simp
-      have hj2 : j.val ≤ w := by simp
-      rw [← ind j, ← (ih hj2).2]
-
-
-theorem iunfoldr_getLsb {f : Fin w → α → α × Bool} (state : Nat → α) (i : Fin w)
-    (ind : ∀(i : Fin w), (f i (state i.val)).fst = state (i.val+1)) :
-  getLsb (iunfoldr f (state 0)).snd i.val = (f i (state i.val)).snd := by
-  exact (iunfoldr_getLsb' state ind).1 i
-
--- /--
--- Correctness theorem for `iunfoldr`.
--- -/
--- theorem iunfoldr_replace
---     {f : Fin w → α → α × Bool} (state : Nat → α) (value : BitVec w) (a : α)
---     (init : state 0 = a)
---     (step : ∀(i : Fin w), f i (state i.val) = (state (i.val+1), value.getLsb i.val)) :
---     iunfoldr f a = (state w, value) := by
---   simp [iunfoldr.eq_test state value a init step]
-
--- theorem iunfoldr_replace_snd
---   {f : Fin w → α → α × Bool} (state : Nat → α) (value : BitVec w) (a : α)
---     (init : state 0 = a)
---     (step : ∀(i : Fin w), f i (state i.val) = (state (i.val+1), value.getLsb i.val)) :
---     (iunfoldr f a).snd = value := by
---   simp [iunfoldr.eq_test state value a init step]
-
-
-
 -- Example proof of shift + mul, this is one of the hardest alive examples.
 -- (alive_simplifyMulDivRem290)
 theorem shift_mul:
@@ -127,7 +63,6 @@ theorem xor_sub :
 #guard_msgs in #print axioms xor_sub
 
 /-- Example with nontrivial mixture of arithmetic and bitwise ops. -/
-
 theorem BitVec_AddSub_1309_stmt {A B : BitVec w} : (B &&& A) + (B ||| A) = B + A := by
   rw [add_eq_adc, add_eq_adc, adc_spec B A]
   unfold adc
