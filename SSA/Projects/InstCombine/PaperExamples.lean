dd63f8ffe675570c4942c8585dfd5edaf6fc977c
Siddharth <siddu.druid@gmail.com>
2024-06-25 13:31:46 +0100

    feat: Add AddSub example that tobi likes into PaperExamples. (#411)

--- SSA/Projects/InstCombine/PaperExamples.lean
+++ SSA/Projects/InstCombine/PaperExamples.lean
@@ -6,10 +6,14 @@ import SSA.Projects.InstCombine.Refinement
 import SSA.Projects.InstCombine.Tactic
 import SSA.Projects.InstCombine.TacticAuto
 import SSA.Projects.InstCombine.Base
+import Init.Data.BitVec.Bitblast
+import Init.Data.BitVec.Lemmas
+import Init.Data.BitVec.Folds
 import Lean
 
 
 namespace AlivePaperExamples
+open BitVec
 
 -- Example proof of shift + mul, this is one of the hardest alive examples.
 -- (alive_simplifyMulDivRem290)
@@ -58,5 +62,50 @@ theorem xor_sub :
 /-- info: 'AlivePaperExamples.xor_sub' depends on axioms: [propext, Classical.choice, Quot.sound] -/
 #guard_msgs in #print axioms xor_sub
 
+/-- Example with nontrivial mixture of arithmetic and bitwise ops. -/
+theorem BitVec_AddSub_1309_stmt {A B : BitVec w} : (B &&& A) + (B ||| A) = B + A := by
+  rw [add_eq_adc, add_eq_adc, adc_spec B A]
+  unfold adc
+  rw [iunfoldr_replace (fun i => carry i B A false)]
+  · simp [carry]; omega
+  · intro i
+    simp only [adcb, getLsb_and, getLsb_or, ofBool_false, ofNat_eq_ofNat, zeroExtend_zero,
+      BitVec.add_zero, Prod.mk.injEq]
+    constructor
+    · rw [carry_succ]
+      cases A.getLsb i
+      <;> cases B.getLsb i
+      <;> cases carry i B A false
+      <;> rfl
 
-end AlivePaperExamples
+    · rw [getLsb_add (by omega)]
+      cases A.getLsb i
+      <;> cases B.getLsb i
+      <;> cases carry i B A false
+      <;> rfl
+
+open BitVec in
+theorem bitvec_AddSub_1309 :
+  [llvm (w)| {
+    ^bb0(%X : _, %Y : _):
+      %v1 = llvm.and %X, %Y
+      %v2 = llvm.or %X, %Y
+      %v3 = llvm.add %v1, %v2
+      llvm.return %v3
+  }] ⊑ [llvm (w)| {
+    ^bb0(%X : _, %Y : _):
+      %v3 = llvm.add %X, %Y
+      llvm.return %v3
+  }] := by
+    simp_alive_peephole
+    simp_alive_undef
+    simp_alive_ops
+    intros A B
+    rcases A with rfl | A  <;> (try (simp [Option.bind, Bind.bind]; done)) <;>
+    rcases B with rfl | B  <;> (try (simp [Option.bind, Bind.bind]; done))
+    simp [BitVec_AddSub_1309_stmt]
+
+/--
+info: 'AlivePaperExamples.bitvec_AddSub_1309' depends on axioms: [propext, Classical.choice, Quot.sound]
+-/
+#guard_msgs in #print axioms bitvec_AddSub_1309
