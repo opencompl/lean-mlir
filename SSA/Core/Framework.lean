f8f17273aa9384ffb2007beb5b38c1426bace76f
Tobias Grosser <tobias@grosser.es>
2024-07-06 09:20:25 +0100

    Drop effects to debug for FRO

--- SSA/Core/Framework.lean
+++ SSA/Core/Framework.lean
@@ -36,43 +36,31 @@ end
 
 class DialectDenote (d : Dialect) [TyDenote d.Ty] [DialectSignature d] where
   denote : (op : d.Op) → HVector toType (DialectSignature.sig op) →
-    (HVector (fun t : Ctxt d.Ty × d.Ty => t.1.Valuation → EffectKind.impure.toMonad d.m (toType t.2))
+    (HVector (fun t : Ctxt d.Ty × d.Ty => t.1.Valuation → toType t.2)
             (DialectSignature.regSig op)) →
-    ((DialectSignature.effectKind op).toMonad d.m (toType <| DialectSignature.outTy op))
+    (toType <| DialectSignature.outTy op)
 
-/- # Datastructures -/
 section DataStructures
 
 variable (d : Dialect) [DialectSignature d]
 
 mutual
-/-- An intrinsically typed expression whose effect is *at most* EffectKind -/
-inductive Expr : (Γ : Ctxt d.Ty) → (eff : EffectKind) → (ty : d.Ty) → Type :=
+inductive Expr : (Γ : Ctxt d.Ty) → (ty : d.Ty) → Type :=
   | mk {Γ} {ty} (op : d.Op)
     (ty_eq : ty = DialectSignature.outTy op)
-    (eff_le : DialectSignature.effectKind op ≤ eff)
     (args : HVector (Var Γ) <| DialectSignature.sig op)
-    /- For now, assume that regions are impure.
-       We keep it this way to minimize the total amount of disruption in our definitions.
-       We shall change this once the rest of the file goes through. -/
-    (regArgs : HVector (fun t : Ctxt d.Ty × d.Ty => Com t.1 .impure t.2)
-      (DialectSignature.regSig op)) : Expr Γ eff ty
+    (regArgs : HVector (fun t : Ctxt d.Ty × d.Ty => Com t.1 t.2)
+      (DialectSignature.regSig op)) : Expr Γ ty
 
-/-- A very simple intrinsically typed program: a sequence of let bindings.
-Note that the `EffectKind` is uniform: if a `Com` is `pure`, then the expression and its body are pure,
-and if a `Com` is `impure`, then both the expression and the body are impure!
--/
-inductive Com : Ctxt d.Ty → EffectKind → d.Ty → Type where
-  | ret {eff : EffectKind} (v : Var Γ t) : Com Γ eff t
-  | var (e : Expr Γ eff α) (body : Com (Γ.snoc α) eff β) : Com Γ eff β
+inductive Com : Ctxt d.Ty → d.Ty → Type where
+  | ret (v : Var Γ t) : Com Γ t
+  | var (e : Expr Γ α) (body : Com (Γ.snoc α) β) : Com Γ β
 end
 
-/-- `Lets d Γ_in Γ_out` is a sequence of lets which are well-formed under context `Γ_out` and result in
-    context `Γ_in`-/
-inductive Lets (Γ_in : Ctxt d.Ty) (eff : EffectKind) :
+inductive Lets (Γ_in : Ctxt d.Ty) :
     (Γ_out : Ctxt d.Ty) → Type where
-  | nil : Lets Γ_in eff Γ_in
-  | var (body : Lets Γ_in eff Γ_out) (e : Expr d Γ_out eff t) : Lets Γ_in eff (Γ_out.snoc t)
+  | nil : Lets Γ_in Γ_in
+  | var (body : Lets Γ_in Γ_out) (e : Expr d Γ_out t) : Lets Γ_in (Γ_out.snoc t)
 
 end DataStructures
 
@@ -83,33 +71,33 @@ end DataStructures
 variable {d : Dialect} [DialectSignature d]
 
 @[elab_as_elim]
-def Com.recAux' {motive : ∀ {Γ eff t}, Com d Γ eff t → Sort u}
-    (ret : ∀ {Γ t} {eff : EffectKind}, (v : Var Γ t) → motive (eff := eff) (Com.ret v))
-    (var : ∀ {Γ} {t u : d.Ty} {eff},
-      (e : Expr d Γ eff t) → (body : Com d (Ctxt.snoc Γ t) eff u) →
+def Com.recAux' {motive : ∀ {Γ t}, Com d Γ t → Sort u}
+    (ret : ∀ {Γ t}, (v : Var Γ t) → motive (Com.ret v))
+    (var : ∀ {Γ} {t u : d.Ty},
+      (e : Expr d Γ t) → (body : Com d (Ctxt.snoc Γ t) u) →
         motive body → motive (Com.var e body)) :
-    ∀ {Γ eff t}, (com : Com d Γ eff t) → motive com
-  | _, _, _, Com.ret v => ret v
-  | _, _, _, Com.var e body => var e body (Com.recAux' ret var body)
+    ∀ {Γ t}, (com : Com d Γ t) → motive com
+  | _, _, Com.ret v => ret v
+  | _, _, Com.var e body => var e body (Com.recAux' ret var body)
 
 @[implemented_by Com.recAux', elab_as_elim, induction_eliminator]
 -- ^^^^ `Com.rec` is noncomputable, so have a computable version as well
 --      See `Com.recAux'_eq` for a theorem that states these definitions are equal
-def Com.rec' {motive : ∀ {Γ eff t}, Com d Γ eff t → Sort u}
-    (ret : ∀ {Γ t} {eff : EffectKind}, (v : Var Γ t) → motive (eff := eff) (Com.ret v))
-    (var : ∀ {Γ} {t u : d.Ty} {eff},
-      (e : Expr d Γ eff t) → (body : Com d (Ctxt.snoc Γ t) eff u) →
+def Com.rec' {motive : ∀ {Γ t}, Com d Γ t → Sort u}
+    (ret : ∀ {Γ t}, (v : Var Γ t) → motive (Com.ret v))
+    (var : ∀ {Γ} {t u : d.Ty},
+      (e : Expr d Γ t) → (body : Com d (Ctxt.snoc Γ t) u) →
         motive body → motive (Com.var e body)) :
-    ∀ {Γ eff t}, (com : Com d Γ eff t) → motive com :=
+    ∀ {Γ t}, (com : Com d Γ t) → motive com :=
   /- HACK: the obvious definition of `rec'` using the match compiler does not have the
      def-eqs we expect. Thus, we directly use the recursion principle. -/
-  Com.rec (motive_1 := fun _ _ _ _ => PUnit) (motive_2 := fun _ _ _ c => motive c)
-    (motive_3 := fun _ _ => PUnit) (fun _ _ _ _ _ _ => ⟨⟩) -- `Expr.mk` case
+  Com.rec (motive_1 := fun _  _ _ => PUnit) (motive_2 := fun  _ _ c => motive c)
+    (motive_3 := fun _ _ => PUnit) (fun _  _ _ _ _ => ⟨⟩) -- `Expr.mk` case
     (ret) -- `Com.ret` case
     (fun e body _ r => var e body r) -- `Com.var` case
     ⟨⟩ (fun _ _ _ _ => ⟨⟩)
 
-@[simp] lemma Com.rec'_var (e : Expr d Γ eff t) (body : Com d _ _ u)
+@[simp] lemma Com.rec'_var (e : Expr d Γ t) (body : Com d  _ u)
     {motive} {ret var} :
     (Com.var e body).rec' (motive:=motive) ret var
     = var e body (body.rec' ret var) :=
@@ -121,7 +109,7 @@ def Com.rec' {motive : ∀ {Γ eff t}, Com d Γ eff t → Sort u}
 
 /-- The `outContext` of a program is a context which includes variables for all let-bindings
 of the program. That is, it is the context under which the return value is typed -/
-def Com.outContext {Γ} : Com d Γ eff t → Ctxt d.Ty :=
+def Com.outContext {Γ} : Com d Γ t → Ctxt d.Ty :=
   Com.rec' (motive := fun _ => Ctxt d.Ty)
     (@fun Γ _ _ _ => Γ) -- `Com.ret` case
     (fun _ _ r => r) -- `Com.var` case
@@ -134,17 +122,17 @@ variable [TyDenote d.Ty] [DialectDenote d] [DecidableEq d.Ty] [Monad d.m] [Lawfu
 
 mutual
 
-def HVector.denote : {l : List (Ctxt d.Ty × d.Ty)} → (T : HVector (fun t => Com d t.1 .impure t.2) l) →
-    HVector (fun t => t.1.Valuation → EffectKind.impure.toMonad d.m (toType t.2)) l
+def HVector.denote : {l : List (Ctxt d.Ty × d.Ty)} → (T : HVector (fun t => Com d t.1 t.2) l) →
+    HVector (fun t => t.1.Valuation → toType t.2) l
   | _, .nil => HVector.nil
   | _, .cons v vs => HVector.cons (v.denote) (HVector.denote vs)
 
-def Expr.denote {ty : d.Ty} (e : Expr d Γ eff ty) (Γv : Valuation Γ) : eff.toMonad d.m (toType ty) :=
+def Expr.denote {ty : d.Ty} (e : Expr d Γ ty) (Γv : Valuation Γ) : toType ty :=
   match e with
-  | ⟨op, Eq.refl _, heff, args, regArgs⟩ =>
+  | ⟨op, heff, args, regArgs⟩ =>
     EffectKind.liftEffect heff <| DialectDenote.denote op (args.map (fun _ v => Γv v)) regArgs.denote
 
-def Com.denote : Com d Γ eff ty → (Γv : Valuation Γ) → eff.toMonad d.m (toType ty)
+def Com.denote : Com d Γ ty → (Γv : Valuation Γ) → toType ty
   | .ret e, Γv => pure (Γv e)
   | .var e body, Γv =>
      match eff with
@@ -152,8 +140,8 @@ def Com.denote : Com d Γ eff ty → (Γv : Valuation Γ) → eff.toMonad d.m (t
      | .impure => e.denote Γv >>= fun x => body.denote (Γv.snoc x)
 end
 
-def Com.denoteLets : (com : Com d Γ eff ty) → (Γv : Valuation Γ) →
-    eff.toMonad d.m (com.outContext.Valuation)
+def Com.denoteLets : (com : Com d Γ ty) → (Γv : Valuation Γ) →
+    com.outContext.Valuation
   | .ret _, V => pure V
   | .var e body, V =>
       e.denote V >>= fun Ve =>
@@ -161,9 +149,9 @@ def Com.denoteLets : (com : Com d Γ eff ty) → (Γv : Valuation Γ) →
       return V.cast (by simp [Com.outContext])
 
 def Lets.denote [DialectSignature d] [DialectDenote d]
-    (lets : Lets d Γ₁ eff Γ₂) (Γ₁'v : Valuation Γ₁) : (eff.toMonad d.m <| Valuation Γ₂) :=
+    (lets : Lets d Γ₁ Γ₂) (Γ₁'v : Valuation Γ₁) : Valuation Γ₂ :=
   match lets with
-  | .nil => return Γ₁'v
+  | .nil => Γ₁'v
   | .var lets' e =>
       lets'.denote Γ₁'v >>= fun Γ₂'v =>
       e.denote Γ₂'v >>= fun v =>
