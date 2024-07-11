e969a81369d8d22ecefda4a696227a64dc91ffbb
Tobias Grosser <tobias@grosser.es>
2024-07-11 10:47:14 +0100

    feat: implement core regalloc loop

--- SSA/Projects/Regalloc/RegallocSingleBB.lean
+++ SSA/Projects/Regalloc/RegallocSingleBB.lean
@@ -1,6 +1,7 @@
 -- Register allocation for a single basic block program.
 import SSA.Core.Framework
 import Lean.Data.HashMap
+import Mathlib.Init.Function
 
 namespace Pure
 
@@ -55,7 +56,7 @@ instance : DialectDenote dialect where
  def Reg := Nat
  deriving DecidableEq, Repr, Inhabited, BEq
 
- abbrev RegisterFile : Type := Reg → Int
+abbrev RegisterFile : Type := Reg → Int
 def RegisterFile.get (R : RegisterFile) (r : Reg) : Int := R r
  def RegisterFile.set (R : RegisterFile) (r : Reg) (v : Int) : RegisterFile :=
   fun r' => if r = r' then v else R r'
@@ -115,17 +116,25 @@ instance : DialectDenote dialect where
 
 /-- A mapping of variables in a context to a register. -/
 structure RegisterMap (Γ : Ctxt Pure.Ty) where
-  toFun : Γ.Var Pure.Ty.int → RegAlloc.Reg -- I actually want a partial function here.
-  live : List RegAlloc.Reg -- TODO: move to using HashMap once available for HashSet.
-  -- hlive : ∀ r ∈ live, ∃ v, toFun v = r -- every live register is mapped to a variable.
-  -- hinjective : Injective toFun
+  toFun : Γ.Var Pure.Ty.int → Option RegAlloc.Reg
+  dead : List RegAlloc.Reg -- list of dead registers
+
+/-- A register is free if no variable maps to it. -/
+def RegisterMap.registerDead (f : RegisterMap Γ) (r : RegAlloc.Reg) : Prop :=
+  ∀ (v : Γ.Var Pure.Ty.int), r ∉ f.toFun v
+
+structure LawfulRegisterMap (Γ : Ctxt Pure.Ty) extends RegisterMap Γ where
+  /-- every register in the set of dead registers is in fact dead. -/
+  hdead : ∀ r ∈ dead, toRegisterMap.registerDead r
+  /-- our mapping from variables to registers is injective. -/
+  hinj : Function.Injective toRegisterMap.toFun
 
  /--
  A correspondence between variables 'v ∈ Γ' and registers in the register file.
  This correspondence is witnessed by 'f'.
  -/
  def correspondence {Γ : Ctxt Pure.Ty} (V : Γ.Valuation) (f : RegisterMap Γ) (R : RegAlloc.RegisterFile) : Prop :=
-  ∀ (v : Γ.Var Pure.Ty.int), R (f.toFun v) = V v
+  ∀ (v : Γ.Var Pure.Ty.int), ∃ r ∈ f.toFun v, R r = V v
 
 /-- All register files correspond to the start of the program. -/
 theorem correspondence_nil (R : RegAlloc.RegisterFile) :
@@ -134,17 +143,66 @@ theorem correspondence_nil (R : RegAlloc.RegisterFile) :
   exact v.emptyElim
 
 
+structure AllocateDeadRegisterResult (fold : RegisterMap Γ) where
+  r : RegAlloc.Reg
+  f : RegisterMap Γ
+  -- hr : f.registerDead r
+
+def RegisterMap.allocateDeadRegister {Γ : Ctxt Pure.Ty} (f : RegisterMap Γ) : Option (AllocateDeadRegisterResult f) :=
+  match hf : f.dead with
+  | [] => none
+  | r :: rs =>
+    .some {
+      r := r,
+      f := {
+        toFun := f.toFun,
+        dead := rs,
+      },
+    }
+
+
+
 /--
-For a sequence of lets [v1 := e1, ..., vn := en, vn+1 := en+1], clear the register assigned to 'vn+1' and return it.
+Consume the allocate dead register result,
+and use this for the variable 'v'.
 -/
-def RegisterMap.lookupAndDeleteLast {Γ : Ctxt Pure.Ty} {t : Pure.Ty} (f : RegisterMap <| Γ.snoc t) : RegAlloc.Reg × RegisterMap Γ :=
-  let vlast := Ctxt.Var.last Γ t
-  let r := f.toFun vlast
-  (r, {
-    toFun := fun v => f.toFun (v.toSnoc),
-    live := f.live.erase (f.toFun <| Ctxt.Var.last Γ t) -- state that this register is now killed.
+def AllocateDeadRegisterResult.consume {Γ : Ctxt Pure.Ty}
+    {fold : RegisterMap Γ}
+    (f : AllocateDeadRegisterResult fold) {v : Γ.Var t} (hv : fold.toFun v = .none) : RegAlloc.Reg × RegisterMap Γ :=
+  (f.r, {
+    toFun := fun v' => if v' = v then .some f.r else fold.toFun v'
+    dead := f.f.dead
   })
 
+def RegisterMap.lookupOrInsert {Γ : Ctxt Pure.Ty} (f : RegisterMap Γ) (v : Γ.Var int) : Option (RegAlloc.Reg × RegisterMap Γ) :=
+  match hv : f.toFun v with
+  | .some r => (r, f)
+  | .none =>
+      match f.allocateDeadRegister with
+      | .none => .none
+      | .some result =>
+        some (result.consume hv)
+
+/-- Lookup an argument in a register map, where the variable is defined in the register map. -/
+def RegisterMap.lookupOrInsertArg {Γ : Ctxt Pure.Ty} (f : RegisterMap <| Γ.snoc t) (v : Γ.Var int) :
+  Option (RegAlloc.Reg × RegisterMap (Γ.snoc t)) :=
+  match hv : f.toFun v with
+  | .some r => (r, f)
+  | .none =>
+      match f.allocateDeadRegister with
+      | .none => .none
+      | .some result =>
+        some (result.consume hv)
+
+def RegisterMap.lookupOrInsertResult {Γ : Ctxt Pure.Ty} (f : RegisterMap (Γ.snoc t)) : Option (RegAlloc.Reg × RegisterMap (Γ.snoc t)) :=
+  f.lookupOrInsert (Ctxt.Var.last Γ t)
+
+/-- Delete the last register from the register map. -/
+def RegisterMap.deleteLast {Γ : Ctxt Pure.Ty} (f : RegisterMap (Γ.snoc t)) : RegisterMap Γ :=
+  let toFun := fun v => f.toFun v.toSnoc
+  match f.toFun (Ctxt.Var.last Γ t) with
+  | .none => { toFun := toFun, dead := f.dead }
+  | .some r =>  { toFun := toFun, dead := f.dead.erase r }
 
 /--
 Convert a pure context to an impure context, where all SSA variables are now side effecting, and thus
@@ -160,12 +218,16 @@ def doRegAllocCtx_cons : doRegAllocCtx (Γ.snoc s) = (doRegAllocCtx Γ).snoc Reg
 
 def doRegAllocExpr (f : RegisterMap (Γ.snoc s))
   (e : Expr Pure.dialect Γ EffectKind.pure .int) :
-  Expr RegAlloc.dialect (doRegAllocCtx Γ) EffectKind.impure .unit × RegisterMap Γ :=
-  match e.op with
-  | .const i =>
-      let (r, f) := f.lookupAndDeleteLast
-      (Expr.mk (RegAlloc.Op.const i r) rfl (by simp) .nil .nil, f)
-  | .increment => sorry
+  Option (Expr RegAlloc.dialect (doRegAllocCtx Γ) EffectKind.impure .unit × RegisterMap Γ) :=
+  match e with
+  | Expr.mk (.const i) .. => do
+      let (rout, f) ← f.lookupOrInsertResult
+      some (Expr.mk (RegAlloc.Op.const i rout) rfl (by simp) .nil .nil, f.deleteLast)
+  | Expr.mk .increment rfl _heff args .. => do
+      let arg := args.getN 0
+      let (rout, f) ← f.lookupOrInsertResult
+      let (r₁, f) ← f.lookupOrInsertArg arg
+      some (Expr.mk (RegAlloc.Op.increment rout r₁) rfl (by simp) .nil .nil, f.deleteLast)
 
 /-- TODO: we will get stuck in showing that 'nregs > 0' when we decrement it when a variable is defined (ie, dies in reverse order).
 This might have us actually need to compute liveness anyway to prove correctness.
