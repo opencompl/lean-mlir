e3cf7a233543a126cab49655035bba268385fd43
Tobias Grosser <tobias@grosser.es>
2024-07-12 14:19:27 +0100

    Try to fix things

--- SSA/Projects/LeanMlirCommon/LeanMlirCommon.lean
+++ SSA/Projects/LeanMlirCommon/LeanMlirCommon.lean
@@ -4,4 +4,4 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 -- Core
 -- ====
-import SSA.Projects.LeanMlirCommon.Simplytyped.Substitution
+import SSA.Projects.LeanMlirCommon.SimplyTyped.Substitution
