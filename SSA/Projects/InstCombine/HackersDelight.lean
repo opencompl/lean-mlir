037ed2e349f83140623489e29b02a9e9a8831bf5
Agnieszka <agnieszka.k.palasz@gmail.com>
2024-07-12 11:49:30 +0100

    corrected

--- SSA/Projects/InstCombine/HackersDelight.lean
+++ SSA/Projects/InstCombine/HackersDelight.lean
@@ -4,11 +4,12 @@ import Init.Data.BitVec.Bitblast
 import Mathlib.Data.BitVec
 
 namespace HackersDelight
-end HackersDelight
 
-#check BitVec.neg_eq_not_add
+namespace Ch1
 
 theorem neg_eq_not_add_one {x : BitVec w} : - x = ~~~ x + 1 := by
   apply BitVec.neg_eq_not_add
 
-#check BitVec.neg
+end Ch1
+
+end HackersDelight
