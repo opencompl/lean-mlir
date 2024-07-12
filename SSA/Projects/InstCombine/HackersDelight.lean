e1a7c4ff2d53fa1f6131f62f8cf0fec76f20a733
Agnieszka <agnieszka.k.palasz@gmail.com>
2024-07-12 11:45:31 +0100

    first theorem

--- SSA/Projects/InstCombine/HackersDelight.lean
+++ SSA/Projects/InstCombine/HackersDelight.lean
@@ -1,1 +1,14 @@
-def bestInteger : Int := 3
+import Init.Data.BitVec.Basic
+import Init.Data.BitVec.Lemmas
+import Init.Data.BitVec.Bitblast
+import Mathlib.Data.BitVec
+
+namespace HackersDelight
+end HackersDelight
+
+#check BitVec.neg_eq_not_add
+
+theorem neg_eq_not_add_one {x : BitVec w} : - x = ~~~ x + 1 := by
+  apply BitVec.neg_eq_not_add
+
+#check BitVec.neg
