14fed268d2b85770ed563f2a895978cec80f5180
Atticus Kuhn <atticusmkuhn@gmail.com>
2024-07-16 10:57:42 +0100

    formatted with `black`

--- SSA/Projects/InstCombine/scripts/mlir-tool.py
+++ SSA/Projects/InstCombine/scripts/mlir-tool.py
@@ -21,8 +21,9 @@ import os
 import io
 from pathlib import Path
 from xdsl.printer import Printer
+
 # Initialize the MLIR context and register the LLVM dialect
-ctx = MLContext(allow_unregistered = True)
+ctx = MLContext(allow_unregistered=True)
 ctx.load_dialect(LLVM)
 ctx.load_dialect(Builtin)
 
@@ -46,67 +47,87 @@ allowed_names = {
     "llvm.add",
     "llvm.mul",
     "llvm.sub",
-    "llvm.sdiv"
+    "llvm.sdiv",
 }
-allowed_unregistered = set() #{
-    # "llvm.icmp"
+allowed_unregistered = set()  # {
+
+
+# "llvm.icmp"
 # }
 def allowed(op):
-        return hasattr(op, "sym_name") or (op.name== "builtin.unregistered" and op.op_name.data in allowed_unregistered)  or (op.name in allowed_names)
+    return (
+        hasattr(op, "sym_name")
+        or (
+            op.name == "builtin.unregistered"
+            and op.op_name.data in allowed_unregistered
+        )
+        or (op.name in allowed_names)
+    )
+
+
 def show(block):
-        output = io.StringIO()
-        p = Printer(stream = output)
-        block.print(p)
-        contents = output.getvalue()
-        output.close()
-        return contents
+    output = io.StringIO()
+    p = Printer(stream=output)
+    block.print(p)
+    contents = output.getvalue()
+    output.close()
+    return contents
+
+
 def showr(region):
-        output = io.StringIO()
-        p = Printer(stream = output)
-        p.print_region(region)
-        contents = output.getvalue()
-        output.close()
-        return contents
+    output = io.StringIO()
+    p = Printer(stream=output)
+    p.print_region(region)
+    contents = output.getvalue()
+    output.close()
+    return contents
+
+
 def size(func):
-        s = 0
-        for _ in func.walk():
-                s +=1
-        return s
+    s = 0
+    for _ in func.walk():
+        s += 1
+    return s
+
+
 for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        print(filename)
-        stem = "g" + filename.split(".")[0].replace("-", "h")
-        output = ""
-        with open(os.path.join("../vcombined-mlir", filename), "r") as f1:
-                with open(os.path.join("../vbefore-mlir", filename), "r") as f2:
-                        data = f1.read()
-                        data2 = f2.read()
-                        parser = Parser(ctx, data)
-                        try:
-                                module = parser.parse_module()
-                                parser2 = Parser(ctx, data2)
-                                module2 = parser2.parse_module()
-                                funcs = [
-                                        func
-                                        for func
-                                        in module.walk()
-                                        if isinstance(func, FuncOp)
-                                        and all(allowed(o) for o in func.walk())
-                                        and size(func) > 1
-                                ]
-                                for func in funcs:
-                                        try:
-                                                other = next(f for f in module2.walk() if isinstance(f, FuncOp) and f.sym_name == func.sym_name)
-                                                if all(allowed(o) for o in other.walk()):
-                                                        s1 = showr(func.body)
-                                                        s2 = showr(other.body)
-                                                        name = func.sym_name.data
-                                                        if s1 == s2:
-                                                            continue
-                                                        if "vector" in (s1 + s2):
-                                                                continue
-                                                        print(f"-----{filename}.{func.sym_name}-----")
-                                                        o1 = f"""
+    filename = os.fsdecode(file)
+    print(filename)
+    stem = "g" + filename.split(".")[0].replace("-", "h")
+    output = ""
+    with open(os.path.join("../vcombined-mlir", filename), "r") as f1:
+        with open(os.path.join("../vbefore-mlir", filename), "r") as f2:
+            data = f1.read()
+            data2 = f2.read()
+            parser = Parser(ctx, data)
+            try:
+                module = parser.parse_module()
+                parser2 = Parser(ctx, data2)
+                module2 = parser2.parse_module()
+                funcs = [
+                    func
+                    for func in module.walk()
+                    if isinstance(func, FuncOp)
+                    and all(allowed(o) for o in func.walk())
+                    and size(func) > 1
+                ]
+                for func in funcs:
+                    try:
+                        other = next(
+                            f
+                            for f in module2.walk()
+                            if isinstance(f, FuncOp) and f.sym_name == func.sym_name
+                        )
+                        if all(allowed(o) for o in other.walk()):
+                            s1 = showr(func.body)
+                            s2 = showr(other.body)
+                            name = func.sym_name.data
+                            if s1 == s2:
+                                continue
+                            if "vector" in (s1 + s2):
+                                continue
+                            print(f"-----{filename}.{func.sym_name}-----")
+                            o1 = f"""
 def {name}_before := [llvm|
 {s2}
 ]
@@ -123,11 +144,20 @@ theorem {name}_proof : {name}_before ⊑ {name}_after := by
   ---BEGIN {name}
   all_goals (try extract_goal ; sorry)
   ---END {name}\n\n\n"""
-                                                        print(o1)
-                                                        write_file = os.path.join("../lean-mlir" , "SSA", "Projects", "InstCombine", "tests", "LLVM", f"{stem}.lean")
-                                                        with open(write_file, "a+") as f3:
-                                                                if os.stat(write_file).st_size == 0:
-                                                                       f3.write("""
+                            print(o1)
+                            write_file = os.path.join(
+                                "../lean-mlir",
+                                "SSA",
+                                "Projects",
+                                "InstCombine",
+                                "tests",
+                                "LLVM",
+                                f"{stem}.lean",
+                            )
+                            with open(write_file, "a+") as f3:
+                                if os.stat(write_file).st_size == 0:
+                                    f3.write(
+                                        """
 import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 import SSA.Projects.InstCombine.TacticAuto
 import SSA.Projects.InstCombine.LLVM.Semantics
@@ -141,10 +171,13 @@ open Ctxt (Var)
 set_option linter.deprecated false
 set_option linter.unreachableTactic false
 set_option linter.unusedTactic false
-                                                                       """)
-                                                                f3.write(o1)
+                                                                       """
+                                    )
+                                f3.write(o1)
 
-                                        except StopIteration as e:
-                                            print(f"Cannot find a function named {func.sym_name} in file {filename}")
-                        except ParseError as e:
-                               print("failed to parse the file")
+                    except StopIteration as e:
+                        print(
+                            f"Cannot find a function named {func.sym_name} in file {filename}"
+                        )
+            except ParseError as e:
+                print("failed to parse the file")
--- SSA/Projects/InstCombine/scripts/types.py
+++ SSA/Projects/InstCombine/scripts/types.py
@@ -6,20 +6,23 @@ import subprocess
 
 def get_lines(msg):
     # Define the regex pattern to match error messages with line numbers
-    pattern = re.compile(r'info: .+?:(\d+):\d+: (.+?)(?=warning)', flags=re.DOTALL)
+    pattern = re.compile(r"info: .+?:(\d+):\d+: (.+?)(?=warning)", flags=re.DOTALL)
     # Find all matches in the log
     matches = pattern.findall(msg)
-    lines = [(int(l),m) for (l,m) in matches if "no goals to be solved" not in m]
+    lines = [(int(l), m) for (l, m) in matches if "no goals to be solved" not in m]
     # Replace this with your actual implementation
     return lines
     # return [(1, "New message for line 1"), (3, "New message for line 3")]
+
 
 def process_file(file_path):
     # Run the `lake build` command and capture the output
     module_name = file_path[2:-5].replace("/", ".")
     proof_name = file_path[:-5] + "_proof"
     stem_name = file_path.split("/")[-1][:-5]
-    result = subprocess.run(['lake', 'build', module_name], capture_output=True, text=True)
+    result = subprocess.run(
+        ["lake", "build", module_name], capture_output=True, text=True
+    )
     msg = result.stdout
     print(f"msg = {msg}")
 
@@ -27,43 +30,44 @@ def process_file(file_path):
     lines_to_replace = get_lines(msg)
 
     # Read the file content
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
 
     # # Replace the specified lines
 
-    named = [
-        (l,lines[l - 2][11:-1],m)
-        for (l,m)
-        in lines_to_replace
-    ]
-    for l, n,m in named:
+    named = [(l, lines[l - 2][11:-1], m) for (l, m) in lines_to_replace]
+    for l, n, m in named:
         if 0 <= l - 1 < len(lines):
-            lines[l - 1] = f"  apply {n}_thm" + '\n'
+            lines[l - 1] = f"  apply {n}_thm" + "\n"
     lines[0] = f"import SSA.Projects.InstCombine.tests.LLVM.{stem_name}_proof\n"
     # Write the modified content back to the file
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.writelines(lines)
 
     # Append the messages to the end of the file
-    with open(proof_name + ".lean", 'a') as file:
-        file.write("""
+    with open(proof_name + ".lean", "a") as file:
+        file.write(
+            """
 open Std (BitVec)
-""")
+"""
+        )
         for _, n, m in named:
             print(f"m = {m}")
             print(f"n = {n}")
             thm = m.replace("extracted_1", n + "_thm")
             print(thm)
-            file.write(thm + '\n')
+            file.write(thm + "\n")
+
+
 def main():
-    directory = './SSA/Projects/InstCombine/test/LLVM'
+    directory = "./SSA/Projects/InstCombine/test/LLVM"
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.lean'):  # Assuming the files have a .lean extension
+            if file.endswith(".lean"):  # Assuming the files have a .lean extension
                 file_path = os.path.join(root, file)
                 print(file_path)
                 process_file(file_path)
+
 
 if __name__ == "__main__":
     main()
