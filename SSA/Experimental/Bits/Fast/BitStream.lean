ec7c3b61aa99d5368c5e3af9227f0e68cb6e70d6
Atticus Kuhn <atticusmkuhn@atticsmkuhn.com>
2024-08-15 16:15:30 +0100

    make william and alex's changes

--- SSA/Experimental/Bits/Fast/BitStream.lean
+++ SSA/Experimental/Bits/Fast/BitStream.lean
@@ -1,6 +1,7 @@
 import Mathlib.Tactic.NormNum
 
 import Mathlib.Logic.Function.Iterate
+import SSA.Experimental.Bits.Fast.ForLean
 -- TODO: upstream the following section
 section UpStream
 
@@ -377,37 +378,18 @@ local infix:20 " ≈ʷ " => EqualUpTo w
 theorem ofBitVec_sub : ofBitVec (x - y) ≈ʷ (ofBitVec x) - (ofBitVec y)  := by
   sorry
 
-theorem Nat.two_mod_mod (x : Nat) (w : Nat) (_ : 0 < w) : x % 2 ^ w % 2 = x % 2 := by
-  have y : 2 ^ 1 ∣ 2 ^ w := Nat.pow_dvd_pow 2 (by omega)
-  rw [pow_one] at y
-  exact Nat.mod_mod_of_dvd x y
-
-theorem Nat.parity_and (n m : Nat) :
-    2 ≤ n % 2 + m % 2 ↔ n % 2 = 1 ∧ m % 2 = 1 := by
-  omega
-
-theorem Nat.add_odd_iff_neq (n m : Nat) :
-    (n + m) % 2 = 1 ↔ (n % 2 = 1) ≠ (m % 2 = 1) := by
-  cases Nat.mod_two_eq_zero_or_one n
-  <;> cases Nat.mod_two_eq_zero_or_one m
-  <;> (
-    rename_i mparity nparity
-    simp [mparity, nparity, Nat.add_mod]
-  )
-
-theorem xor_decide (p q : Prop) [dp : Decidable p] [Decidable  q] :
-    (decide p).xor (decide q) = decide (p ≠ q) := by
-  cases dp
-  <;> (rename_i pt ; simp [pt])
+@[simp]
+theorem ofBitVec_getLsb (n : Nat) (h  : n < w) : ofBitVec x n = x.getLsb n := by
+  simp [ofBitVec, h]
 
 theorem ofBitVec_add : ofBitVec (x + y) ≈ʷ (ofBitVec x) + (ofBitVec y)  := by
   intros n a
   have add_lemma : ⟨BitVec.carry (n + 1) x y false , (x + y).getLsb n⟩ = (ofBitVec x).addAux (ofBitVec y) n := by
     induction' n with n ih
-    · simp [addAux, BitVec.adcb, ofBitVec,a, BitVec.getLsb, Nat.two_mod_mod, BitVec.carry, ← Bool.decide_and, decide_eq_decide, xor_decide, Nat.parity_and x.toNat y.toNat, Nat.add_odd_iff_neq x.toNat y.toNat]
-    · simp [addAux, ← ih (by omega), BitVec.adcb, ofBitVec, a, BitVec.carry_succ (n + 1) x y false, BitVec.getLsb_add a x y]
-  simp only [HAdd.hAdd, Add.add, BitStream.add, ← add_lemma, ofBitVec, a]
-  simp
+    · simp [addAux, BitVec.adcb, a, BitVec.getLsb, BitVec.carry, ← Bool.decide_and,
+        Bool.xor_decide, Nat.parity_and x.toNat y.toNat, Nat.add_odd_iff_neq x.toNat y.toNat]
+    · simp [addAux, ← ih (by omega), BitVec.adcb, a, BitVec.carry_succ (n + 1) x y false, BitVec.getLsb_add a x y]
+  simp [HAdd.hAdd, Add.add, BitStream.add, ← add_lemma, a, -BitVec.add_eq, -Nat.add_eq, -Nat.add_def]
 
 @[refl]
 theorem equal_up_to_refl : a ≈ʷ a := by
