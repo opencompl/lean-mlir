644d3f4f0b477b9219fb5dc2544861e95b763d51
Atticus Kuhn <atticusmkuhn@atticsmkuhn.com>
2024-08-15 16:39:44 +0100

    rename theorem

--- SSA/Experimental/Bits/Fast/BitStream.lean
+++ SSA/Experimental/Bits/Fast/BitStream.lean
@@ -387,7 +387,7 @@ theorem ofBitVec_add : ofBitVec (x + y) ≈ʷ (ofBitVec x) + (ofBitVec y) := by
   have add_lemma : ⟨BitVec.carry (n + 1) x y false , (x + y).getLsb n⟩ = (ofBitVec x).addAux (ofBitVec y) n := by
     induction' n with n ih
     · simp [addAux, BitVec.adcb, a, BitVec.getLsb, BitVec.carry, ← Bool.decide_and,
-        Bool.xor_decide, Nat.parity_and, Nat.add_odd_iff_neq]
+        Bool.xor_decide, Nat.two_le_add_iff_odd_and_odd, Nat.add_odd_iff_neq]
     · simp [addAux, ← ih (by omega), BitVec.adcb, a, BitVec.carry_succ, BitVec.getLsb_add]
   simp [HAdd.hAdd, Add.add, BitStream.add, ← add_lemma, a, -BitVec.add_eq, -Nat.add_eq, -Nat.add_def]
 
