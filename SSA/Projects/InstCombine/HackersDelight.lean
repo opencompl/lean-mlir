8e46c38ee3f838bf56daa6b3854d1b499dc3cfc3
Tobias Grosser <tobias@grosser.es>
2024-08-17 06:34:21 +0100

    chore: update to 2024-08-16 (#541)
    As we upstreamed a few of BitVec theorems to Lean earlier, these can now
    be deleted from our internal ForLean branch.
    
    We also have to disable the doc-gen4 build, which is currently
    incompatible with the latest lean nightly. As this is resolved, we will
    enable it again.
    
    Finally, there is currently one Hacker's Delight proof that is timing
    out. We comment out the relevant tactic and preserve the timeout
    information in a comment. We will revisit this as we further analyze the
    Hacker's Delight theorems.

--- SSA/Projects/InstCombine/HackersDelight.lean
+++ SSA/Projects/InstCombine/HackersDelight.lean
@@ -198,7 +198,7 @@ theorem neq_iff_neg_sub_abs :
 
 theorem lt_iff_sub_xor_xor_and_sub_xor :
     (x <ₛ y) ↔ ((x - y) ^^^ ((x ^^^ y) &&& ((x - y) ^^^ x))).getMsb 0 := by
-  try alive_auto
+  -- try alive_auto -- this leads to a heartbeat timeout
   all_goals sorry
 
 theorem slt_iff_and_not_or_not_xor_and_sub :
