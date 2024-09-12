0f3fc4bd0a06e034ee2535dbfdaaf756daae925d
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2024-09-12 13:29:31 +0100

    chore: add theorems for n`bv_InstCombineShift__279` (#610)

--- SSA/Projects/InstCombine/ForLean.lean
+++ SSA/Projects/InstCombine/ForLean.lean
@@ -569,6 +569,26 @@ theorem xor_allOnes_sshiftRight_xor_allOnes {a b : BitVec w} :
     rw [ih]
     rw [BitVec.sshiftRight_add]
 
+@[simp]
+theorem shiftLeft_shiftRight {x : BitVec w} {n : Nat}:
+  x >>> n <<< n = x &&& BitVec.allOnes w <<< n := by
+  induction n generalizing x
+  case zero =>
+    ext; simp
+  case succ n ih =>
+    rw [BitVec.shiftLeft_add, Nat.add_comm, BitVec.shiftRight_add, ih,
+       Nat.add_comm, BitVec.shiftLeft_add, BitVec.shiftLeft_and_distrib]
+    ext i
+    simp only [getLsbD_and, getLsbD_shiftLeft, Fin.is_lt, decide_True, Nat.lt_one_iff,
+      Bool.true_and, getLsbD_ushiftRight, getLsbD_allOnes]
+    rw [Nat.add_comm]
+    by_cases hw : w = 0
+    · simp [hw]
+    · by_cases h : i.val = 0
+      · simp [h]
+      · rw [Nat.sub_add_cancel (by omega)]
+        simp [h]
+
 end BitVec
 
 namespace Bool
