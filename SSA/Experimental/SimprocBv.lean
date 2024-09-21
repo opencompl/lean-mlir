c9ed9cf0a97d1c0be341c8b19307acc15c9ab8ba
Tobias Grosser <tobias@grosser.es>
2024-09-22 00:02:23 +0100

    Further cleanup
    - Make the simpproc actually work
    
      Just drop the earlier variants. It seems multiple simp-procs for the same pattern cause conflicts
    
    - Catch exception in case of omega failing

--- SSA/Experimental/SimprocBv.lean
+++ SSA/Experimental/SimprocBv.lean
@@ -6,98 +6,6 @@ Authors: Siddharth Bhat, Tobias Groser
 import Lean
 open Lean Meta Elab Simp
 
-/-! # V0 -/
-
-/-- We need this to be a `post` procedure, so that we can e.g.
-simplify `((2 % 16 + 4 % 16) % 16)` to collapse leaf-first:
-[((2 % 16 + 4 % 16) % 16)]
--> ([(2 % 16 + 4 % 16)] % 16)
--> (([2 % 16] + 4 % 16) % 16)
--> (([2] + 4 % 16) % 16)
--> ((2 + [4 % 16]) % 16)
--> ((2 + [4]) % 16)
--> [((2 + 4) % 16)]
--> [(6 % 16)]
-
--/
-@[inline] def reduceModEqOfLtV0 (e : Expr) : SimpM Step := do
-  match_expr e with
-  | _ => do
-     trace[debug] "modEqOfLt: '{toString e}' | {e}"
-     return .done { expr := e  : Result }
-simproc↑ reduce_mod_eq_of_lt_v0 (_ % _) := fun e => reduceModEqOfLtV0 e
-
--- set_option trace.Debug.Meta.Tactic.simp true in
-set_option trace.debug true in
-example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
-  simp only [reduce_mod_eq_of_lt_v0]
-  sorry
-
-/-! # V1 -/
-
-@[inline] def reduceModEqOfLtV1 (e : Expr) : SimpM Step := do
-  match_expr e with
-  | HMod.hMod _nat _nat _nat  _inst x n =>
-     trace[debug] "modEqOfLt: '{x}' % '{n}'"
-     return .done { expr := e  : Result }
-  | _ => do
-     trace[debug] "no match: '{toString e}'"
-     return .done { expr := e  : Result }
-simproc↑ reduce_mod_eq_of_lt_v1 (_ % _) := fun e => reduceModEqOfLtV1 e
-
--- set_option trace.Debug.Meta.Tactic.simp true in
-set_option trace.debug true in
-example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
-  simp only [reduce_mod_eq_of_lt_v1]
-  sorry
-
-/-! # V2 -/
-
-@[inline] def reduceModEqOfLtV2 (e : Expr) : SimpM Step := do
-  match_expr e with
-  | HMod.hMod xTy nTy outTy  _inst x n =>
-     let natTy := mkConst ``Nat
-     -- x must be a Nat
-     if xTy != natTy then
-       trace[debug] "modEqOfLt: xTy:'{xTy}' != Nat"
-       return .done { expr := e }
-     if nTy != natTy then
-       trace[debug] "modEqOfLt: nTy:'{nTy}' != Nat"
-       return .done { expr := e }
-     if outTy != natTy then
-       trace[debug] "modEqOfLt: outTy:'{outTy}' != Nat"
-       return .done { expr := e }
-     trace[debug] "modEqOfLt: '{x}' % '{n}'"
-     return .done { expr := e  : Result }
-  | _ => do
-     trace[debug] "no match: '{toString e}'"
-     return .done { expr := e  : Result }
-simproc↑ reduce_mod_eq_of_lt_v2 (_ % _) := fun e => reduceModEqOfLtV2 e
-
-set_option trace.debug true in
-example (x : BitVec w) : (x.toNat : Int) % 2^w = x.toNat + 100:= by
-  simp only [reduce_mod_eq_of_lt_v2]
-  sorry
-
--- set_option trace.Debug.Meta.Tactic.simp true in
-set_option trace.debug true in
-example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
-  simp only [reduce_mod_eq_of_lt_v2]
-  sorry
-
-/-! # V3 -/
-
--- /-- info: Nat.mod_eq_of_lt {a b : Nat} (h : a < b) : a % b = a -/
-set_option pp.all true in
-/-
-⊢ ∀ {a b : Nat},
-  @LT.lt.{0} Nat instLTNat a b → @Eq.{1} Nat (@HMod.hMod.{0, 0, 0} Nat Nat Nat (@instHMod.{0} Nat Nat.instMod) a b) a
--/
-#check Nat.mod_eq_of_lt
-
-/-- info: Lean.Meta.mkAppM (constName : Name) (xs : Array Expr) : MetaM Expr -/
-#guard_msgs in #check mkAppM
-
 @[inline] def reduceModEqOfLtV3 (e : Expr) : SimpM Step := do
   match_expr e with
   | HMod.hMod xTy nTy outTy  _inst x n =>
@@ -128,53 +36,48 @@ set_option pp.all true in
      trace[debug] "ltProofMVar: {ltProofMVar}"
      let some g ← ltProofMVar.falseOrByContra
        | return .done { expr := e }
-     g.withContext (do
-        let hyps := (← getLocalHyps).toList
-        Lean.Elab.Tactic.Omega.omega hyps g {})
-     let eqProof ← mkAppM ``Nat.mod_eq_of_lt #[ltProof]
-     trace[debug] "proof: {eqProof}"
-     return .done { expr := x, proof? := eqProof : Result }
+     try
+       g.withContext (do
+          let hyps := (← getLocalHyps).toList
+          Lean.Elab.Tactic.Omega.omega hyps g {})
+       let eqProof ← mkAppM ``Nat.mod_eq_of_lt #[ltProof]
+       trace[debug] "proof: {eqProof}"
+       return .done { expr := x, proof? := eqProof : Result }
+     catch ex =>
+       return .done { expr := e }
   | _ => do
      trace[debug] "no match: '{toString e}'"
      return .done { expr := e  : Result }
 
 simproc↑ reduce_mod_eq_of_lt_v3 (_ % _) := fun e => reduceModEqOfLtV3 e
 
-attribute [simp] reduce_mod_eq_of_lt_v3
-
-#check Lean.Meta.Omega.OmegaConfig
-#check Lean.Elab.Tactic.Omega.OmegaM
-#check Lean.Elab.Tactic.Omega.omega
-#check Lean.Elab.Tactic.Omega.omegaTactic
--- Lean/Elab/Tactic/Omega/OmegaM.lean
-
-
 -- set_option trace.Debug.Meta.Tactic.simp true in
 set_option trace.debug true in
 theorem eg₁ (x : BitVec w) : x.toNat % 2^w = x.toNat + 0:= by
-  simp only [reduce_mod_eq_of_lt_v3]
-  rfl
+  simp
 
-/-- info: 'eg₁' depends on axioms: [propext, Quot.sound] -/
+/-- info: 'eg₁' depends on axioms: [propext] -/
 #guard_msgs in #print axioms eg₁
 
 theorem eg₂ (x y : BitVec w)  (h : x.toNat + y.toNat < 2^w) :
   (x + y).toNat = x.toNat + y.toNat := by
   simp
-  -- simp [-BitVec.toNat_add, reduce_mod_eq_of_lt_v3]
-  simp only [-BitVec.toNat_add, reduce_mod_eq_of_lt_v3]
-  -- simp only [BitVec.toNat_add, reduce_mod_eq_of_lt_v3]
-
 
 /-- info: 'eg₂' depends on axioms: [propext, Quot.sound] -/
 #guard_msgs in #print axioms eg₂
 
-theorem eg₃ (x y z : BitVec w)
+theorem eg₃ (x y : BitVec w) :
+  (x + y).toNat = (x.toNat + y.toNat) % 2 ^ w := by
+  simp
+
+/-- info: 'eg₂' depends on axioms: [propext, Quot.sound] -/
+#guard_msgs in #print axioms eg₂
+
+theorem eg₄ (x y z : BitVec w)
   (h₂ : y.toNat + z.toNat < 2^w)
   (h : x.toNat * (y.toNat + z.toNat) < 2^w) :
   (x * (y + z)).toNat = x.toNat * (y.toNat + z.toNat) := by
   simp
-  simp only [BitVec.toNat_add, reduce_mod_eq_of_lt_v3]
 
-/-- info: 'eg₃' depends on axioms: [propext, Quot.sound] -/
-#guard_msgs in #print axioms eg₃
+/-- info: 'eg₄' depends on axioms: [propext, Quot.sound] -/
+#guard_msgs in #print axioms eg₄
