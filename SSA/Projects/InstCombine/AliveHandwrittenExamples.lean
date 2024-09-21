17784799e6cac4bbd7e7dcb6f036a7b698ff6f9f
Tobias Grosser <tobias@grosser.es>
2024-09-21 17:13:39 +0100

    chore: simplify handwritten example (#643)

--- SSA/Projects/InstCombine/AliveHandwrittenExamples.lean
+++ SSA/Projects/InstCombine/AliveHandwrittenExamples.lean
@@ -2,19 +2,11 @@
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 import SSA.Projects.InstCombine.LLVM.PrettyEDSL
-import SSA.Projects.InstCombine.Refinement
-import SSA.Projects.InstCombine.Tactic
 import SSA.Projects.InstCombine.TacticAuto
-import SSA.Projects.InstCombine.Base
-import SSA.Projects.InstCombine.ForStd
-import SSA.Core.ErasedContext
 
 open MLIR AST
-open Ctxt (Var DerivedCtxt)
-open InstCombine (MOp)
 
 namespace AliveHandwritten
-
 
 /-
 Name: SimplifyDivRemOfSelect
@@ -49,23 +41,7 @@ BitVec.toNat (BitVec.ofInt w 0) = 0 := by
 theorem alive_DivRemOfSelect (w : Nat) :
     alive_DivRemOfSelect_src w ⊑ alive_DivRemOfSelect_tgt w := by
   unfold alive_DivRemOfSelect_src alive_DivRemOfSelect_tgt
-  simp_alive_meta
-  simp_alive_ssa
-  simp_alive_undef
-  simp [simp_llvm]
-  intro y c x
-  cases c
-  -- | select condition is itself `none`, nothing more to be done. propagate the `none`.
-  case none => cases x <;> cases y <;> simp
-  case some cond =>
-     obtain ⟨vcond, hcond⟩ := cond
-     obtain (h | h) : vcond = 1 ∨ vcond = 0 := by
-       norm_num at hcond
-       rcases vcond with zero | vcond <;> simp;
-       rcases vcond with zero | vcond <;> simp;
-       linarith
-     · subst h
-       simp
-     · subst h; simp
+  simp_alive_peephole
+  alive_auto
 
 end AliveHandwritten
