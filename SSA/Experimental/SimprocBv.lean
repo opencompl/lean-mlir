229d702f31cf0b36a13373282a0e7cb83d6fe0be
Siddharth Bhat <siddu.druid@gmail.com>
2024-09-20 21:56:09 -0500

    chore: add scaffolding for simproc for bv stuff to simplify with Nat.mod_eq_of_lt

---
+++ SSA/Experimental/SimprocBv.lean
@@ -0,0 +1,121 @@
+import Lean
+open Lean Meta Elab Simp
+
+/-! # V0 -/
+
+/-- We need this to be a `post` procedure, so that we can e.g.
+simplify `((2 % 16 + 4 % 16) % 16)` to collapse leaf-first: 
+[((2 % 16 + 4 % 16) % 16)]
+-> ([(2 % 16 + 4 % 16)] % 16)
+-> (([2 % 16] + 4 % 16) % 16)
+-> (([2] + 4 % 16) % 16)
+-> ((2 + [4 % 16]) % 16)
+-> ((2 + [4]) % 16)
+-> [((2 + 4) % 16)]
+-> [(6 % 16)]
+
+-/
+@[inline] def reduceModEqOfLtV0 (e : Expr) : SimpM Step := do
+  match_expr e with 
+  | _ => do
+     trace[debug] "modEqOfLt: '{toString e}'"
+     return .done { expr := e  : Result }
+simproc↑ reduce_mod_eq_of_lt_v0 (_ % _) := fun e => reduceModEqOfLtV0 e
+
+-- set_option trace.Debug.Meta.Tactic.simp true in
+set_option trace.debug true in
+example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v0]
+  sorry
+
+/-! # V1 -/
+
+@[inline] def reduceModEqOfLtV1 (e : Expr) : SimpM Step := do
+  match_expr e with 
+  | HMod.hMod _nat _nat _nat  _inst x n => 
+     trace[debug] "modEqOfLt: '{x}' % '{n}'"
+     return .done { expr := e  : Result }
+  | _ => do
+     trace[debug] "no match: '{toString e}'"
+     return .done { expr := e  : Result }
+simproc↑ reduce_mod_eq_of_lt_v1 (_ % _) := fun e => reduceModEqOfLtV1 e
+
+-- set_option trace.Debug.Meta.Tactic.simp true in
+set_option trace.debug true in
+example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v1]
+  sorry
+
+/-! # V2 -/
+
+@[inline] def reduceModEqOfLtV2 (e : Expr) : SimpM Step := do
+  match_expr e with 
+  | HMod.hMod xTy nTy outTy  _inst x n => 
+     let natTy := mkConst ``Nat
+     -- x must be a Nat
+     if ! (← isDefEq xTy natTy) then 
+       trace[debug] "modEqOfLt: xTy:'{xTy}' != Nat"
+       return .done { expr := e }
+     if ! (← isDefEq nTy natTy) then 
+       trace[debug] "modEqOfLt: nTy:'{nTy}' != Nat"
+       return .done { expr := e }
+     if ! (← isDefEq outTy natTy) then 
+       trace[debug] "modEqOfLt: outTy:'{outTy}' != Nat"
+       return .done { expr := e }
+     trace[debug] "modEqOfLt: '{x}' % '{n}'"
+     return .done { expr := e  : Result }
+  | _ => do
+     trace[debug] "no match: '{toString e}'"
+     return .done { expr := e  : Result }
+simproc↑ reduce_mod_eq_of_lt_v2 (_ % _) := fun e => reduceModEqOfLtV2 e
+
+set_option trace.debug true in
+example (x : BitVec w) : (x.toNat : Int) % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v2]
+  sorry
+
+-- set_option trace.Debug.Meta.Tactic.simp true in
+set_option trace.debug true in
+example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v2]
+  sorry
+
+/-! # V3 -/
+
+/-- info: Nat.mod_eq_of_lt {a b : Nat} (h : a < b) : a % b = a -/
+#guard_msgs in #check Nat.mod_eq_of_lt
+/-- info: Lean.Meta.mkAppM (constName : Name) (xs : Array Expr) : MetaM Expr -/
+#guard_msgs in #check mkAppM
+
+@[inline] def reduceModEqOfLtV2 (e : Expr) : SimpM Step := do
+  match_expr e with 
+  | HMod.hMod xTy nTy outTy  _inst x n => 
+     let natTy := mkConst ``Nat
+     -- x must be a Nat
+     if ! (← isDefEq xTy natTy) then 
+       trace[debug] "modEqOfLt: xTy:'{xTy}' != Nat"
+       return .done { expr := e }
+     if ! (← isDefEq nTy natTy) then 
+       trace[debug] "modEqOfLt: nTy:'{nTy}' != Nat"
+       return .done { expr := e }
+     if ! (← isDefEq outTy natTy) then 
+       trace[debug] "modEqOfLt: outTy:'{outTy}' != Nat"
+       return .done { expr := e }
+     trace[debug] "modEqOfLt: '{x}' % '{n}'"
+     let proof ← mkAppM ``Nat.mod_eq_of_lt 
+     return .done { expr := e  : Result }
+  | _ => do
+     trace[debug] "no match: '{toString e}'"
+     return .done { expr := e  : Result }
+simproc↑ reduce_mod_eq_of_lt_v2 (_ % _) := fun e => reduceModEqOfLtV2 e
+
+set_option trace.debug true in
+example (x : BitVec w) : (x.toNat : Int) % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v2]
+  sorry
+
+-- set_option trace.Debug.Meta.Tactic.simp true in
+set_option trace.debug true in
+example (x : BitVec w) : x.toNat % 2^w = x.toNat + 100:= by
+  simp only [reduce_mod_eq_of_lt_v2]
+  sorry
