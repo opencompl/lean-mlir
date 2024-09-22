7c5c9b9450bc7aaf7ac5935ffddb4a57e4b17c11
Tobias Grosser <tobias@grosser.es>
2024-09-22 13:15:34 +0100

    Update SSA/Experimental/SimprocBv.lean

--- SSA/Experimental/SimprocBv.lean
+++ SSA/Experimental/SimprocBv.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Siddharth Bhat, Tobias Groser
+Authors: Siddharth Bhat, Tobias Grosser
 -/
 import Lean
 open Lean Meta Elab Simp
