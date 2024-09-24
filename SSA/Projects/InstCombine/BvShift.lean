104f4856ca4701cc2fd5e80231bfe990ae363b8d
luisacicolini <luisacicolini@gmail.com>
2024-09-24 12:57:33 +0100

    fixed comments

--- SSA/Projects/InstCombine/BvShift.lean
+++ SSA/Projects/InstCombine/BvShift.lean
@@ -6,31 +6,31 @@ import SSA.Projects.InstCombine.TacticAuto
 import SSA.Projects.InstCombine.ForLean
 
 @[simp]
-theorem shiftRight_and_or_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+theorem shiftRight_and_or_shiftLeft_distrib {x y z : BitVec w} {n : Nat} :
     (x >>> n &&& y ||| z) <<< n = x &&& y <<< n ||| z <<< n := by
   bv_auto
 
 @[simp]
-theorem or_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+theorem or_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat} :
     (z ||| x >>> n &&& y) <<< n = z <<< n ||| x &&& y <<< n := by
   bv_auto
 
 @[simp]
-theorem shiftRight_and_xor_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+theorem shiftRight_and_xor_shiftLeft_distrib {x y z : BitVec w} {n : Nat} :
     (x >>> n &&& y ^^^ z) <<< n = x &&& y <<< n ^^^ z <<< n := by
   bv_auto
 
 @[simp]
-theorem xor_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+theorem xor_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat} :
     (z ^^^ x >>> n &&& y) <<< n = z <<< n ^^^ x &&& y <<< n := by
   bv_auto
 
 @[simp]
-theorem and_shiftLeft_allOnes {x y : BitVec w} (n : Nat):
-  x &&& BitVec.allOnes w <<< n &&& y <<< n = x &&& y <<< n := by
+theorem and_shiftLeft_allOnes {x y : BitVec w} {n : Nat} :
+    x &&& BitVec.allOnes w <<< n &&& y <<< n = x &&& y <<< n := by
   bv_auto
 
 @[simp]
-theorem or_shiftLeft_allOnes {x y : BitVec w} (n : Nat):
-  x ||| BitVec.allOnes w <<< n &&& y <<< n = x ||| y <<< n := by
+theorem or_shiftLeft_allOnes {x y : BitVec w} {n : Nat} :
+    x ||| BitVec.allOnes w <<< n &&& y <<< n = x ||| y <<< n := by
   bv_auto
