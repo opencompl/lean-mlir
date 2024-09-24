deb91919dd639e2cc7a3803f43f36131eed4d552
luisacicolini <luisacicolini@gmail.com>
2024-09-24 11:13:43 +0100

    theorems that benefit from new tactic

---
+++ SSA/Projects/InstCombine/BvShift.lean
@@ -0,0 +1,26 @@
+/-
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
+import SSA.Projects.InstCombine.TacticAuto
+import SSA.Projects.InstCombine.ForLean
+
+@[simp]
+theorem shiftRight_and_or_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+    (x >>> n &&& y ||| z) <<< n = x &&& y <<< n ||| z <<< n := by
+  bv_distrib
+
+@[simp]
+theorem or_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+    (z ||| x >>> n &&& y) <<< n = z <<< n ||| x &&& y <<< n := by
+  bv_distrib
+
+@[simp]
+theorem shiftRight_and_xor_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+    (x >>> n &&& y ^^^ z) <<< n = x &&& y <<< n ^^^ z <<< n := by
+  bv_distrib
+
+@[simp]
+theorem xor_shiftRight_and_shiftLeft_distrib {x y z : BitVec w} {n : Nat}:
+    (z ^^^ x >>> n &&& y) <<< n = z <<< n ^^^ x &&& y <<< n := by
+  bv_distrib
