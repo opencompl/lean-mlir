65c657bf758eef554e76c81a27d087a45d082c87
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2024-09-26 18:26:43 +0100

    fixed ci

--- SSA/Projects/CIRCT/DC/DCExample.lean
+++ SSA/Projects/CIRCT/DC/DCExample.lean
@@ -12,19 +12,17 @@ namespace CIRCTStream
 open MLIR AST in
 
 unseal String.splitOnAux in
-def SourceEg1 := [DC_com| {
+def SourceEg := [DC_com| {
   ^entry():
     %src = "DC.source" () : () -> (!TokenStream)
     "return" (%src) : (!TokenStream) -> ()
   }]
 
-#check SourceEg1
-#eval SourceEg1
-#reduce SourceEg1
-#check SourceEg1.denote
-#print axioms SourceEg1
+#check SourceEg
+#eval SourceEg
+#reduce SourceEg
+#check SourceEg.denote
+#print axioms SourceEg
 
-
--- is this in the opposite order or am I misunderstanding? if yes: why?
 def test : DC.TokenStream :=
-  SourceEg1.denote (Ctxt.Valuation.nil)
+  SourceEg.denote (Ctxt.Valuation.nil)
