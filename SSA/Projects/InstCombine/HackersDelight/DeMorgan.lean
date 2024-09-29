1cf061d482de79d5ebb379858d93d4a7c8cd4988
Tobias Grosser <tobias@grosser.es>
2024-09-29 10:03:09 +0100

    Minimize diff

--- SSA/Projects/InstCombine/HackersDelight/DeMorgan.lean
+++ SSA/Projects/InstCombine/HackersDelight/DeMorgan.lean
@@ -8,43 +8,43 @@ namespace Ch2Basics
 
 namespace DeMorgansLawsExtended
 
-variable {x y z : BitVec 1000}
+variable {x y z : BitVec w}
 
 theorem not_and_eq_not_or_not :
     ~~~ (x &&& y) = ~~~ x ||| ~~~ y := by
-  bv_compare'
+  bv_auto
 
 theorem not_or_eq_not_and_not :
     ~~~ (x ||| y) = ~~~ x &&& ~~~ y := by
-  bv_compare'
+  bv_auto
 
 theorem not_add_one_eq_not_sub_one :
     ~~~ (x + 1) = ~~~ x - 1 := by
-  bv_compare'
+  bv_auto
 
 theorem not_sub_one_eq_not_add_one :
     ~~~ (x - 1) = ~~~ x + 1 := by
-  bv_compare'
+  bv_auto
 
 theorem not_neg_eq_sub_one :
     ~~~ (- x) = x - 1 := by
-  bv_compare'
+  bv_auto
 
 theorem not_xor_eq_not_xor :
     ~~~ (x ^^^ y) = ~~~ x ^^^ y := by
-  bv_compare'
+  bv_auto
 
 theorem q_not_xor :
     ~~~ (x ^^^ y) = ~~~ x ^^^ y := by
-  bv_compare'
+  bv_auto
 
 theorem not_add_eq_not_sub :
     ~~~ (x + y) = ~~~ x - y := by
-  bv_compare'
+  bv_auto
 
 theorem not_sub_eq_not_add :
     ~~~ (x - y) = ~~~ x + y := by
-  bv_compare'
+  bv_auto
 
 end DeMorgansLawsExtended
 
