d4e7c9e295249bd892060d924f62d6de7ca7411b
luisacicolini <lc985@cam.ac.uk>
2024-10-04 15:58:50 +0100

    pairing

--- SSA/Projects/CIRCT/Handshake-to-DC/ForkEquiv.lean
+++ SSA/Projects/CIRCT/Handshake-to-DC/ForkEquiv.lean
@@ -46,8 +46,6 @@ theorem fork_hs_dc_equiv_fst (x : DC.TokenStream):
     · intros i h
       cases h
 
-
-
 theorem fork_hs_dc_equiv_snd (x : DC.TokenStream):
     (DC.fork (x)).snd ~ (Handshake.fork (x)).snd := by
   simp [Bisim, DC.fork, Handshake.fork]
@@ -105,39 +103,91 @@ def test : DC.ValueStream Int × DC.ValueStream Int :=
 
 /- step 3: prove equivalence -/
 
--- theorem corec_of_corec₂ (streamInt : DC.ValueStream Int):
--- corec
---       ((corec₂ streamInt fun x =>
---             (match x 0 with
---               | some val => (x 0, some (), x.tail)
---               | none => (none, none, x.tail)).run).1,
---         (corec₂
---             (corec₂ streamInt fun x =>
---                 (match x 0 with
---                   | some val => (x 0, some (), x.tail)
---                   | none => (none, none, x.tail)).run).2
---             fun x => Id.run (x 0, x 0, tail x)).1)
---       fun x =>
---       match x.1 0, x.2 0 with
---       | some x₀, some val => (some x₀, tail x.1, tail x.2)
---       | some val, none => (none, x.1, tail x.2)
---       | none, some val => (none, tail x.1, x.2)
---       | none, none => (none, tail x.1, tail x.2)
-
 open Ctxt in
-theorem equiv_fork (streamInt : DC.ValueStream Int) :
-  (Handshake.fork streamInt) = DCFork.denote (Valuation.ofHVector (.cons streamInt <| .nil)) := by
+theorem equiv_fork_fst (streamInt : DC.ValueStream Int) :
+  (Handshake.fork streamInt).fst ~ (DCFork.denote (Valuation.ofHVector (.cons streamInt <| .nil))).fst := by
   simp [DCFork, Valuation.ofPair, Valuation.ofHVector]
-  simp_peephole
   unfold Handshake.fork
-  simp_peephole
   unfold DC.pack
-  simp_peephole
   unfold DC.unpack
-  simp_peephole
   unfold DC.fork
-
+  /-
+  streamInt : DC.ValueStream ℤ
+  ⊢ (corec₂ streamInt fun x => Id.run (x 0, x 0, x.tail)).1 ~
+    corec
+      ((corec₂ streamInt fun x =>
+            (match x 0 with
+              | some val => (x 0, some (), x.tail)
+              | none => (none, none, x.tail)).run).1,
+        (corec₂
+            (corec₂ streamInt fun x =>
+                (match x 0 with
+                  | some val => (x 0, some (), x.tail)
+                  | none => (none, none, x.tail)).run).2
+            fun x => Id.run (x 0, x 0, tail x)).1)
+      fun x =>
+      match x.1 0, x.2 0 with
+      | some x₀, some val => (some x₀, tail x.1, tail x.2)
+      | some val, none => (none, x.1, tail x.2)
+      | none, some val => (none, tail x.1, x.2)
+      | none, none => (none, tail x.1, tail x.2)
+  -/
+  simp_peephole
   sorry
+
+theorem stream_pair_1 (s : Stream α) (f : Stream α → Option α × Option α × Stream α):
+    (corec₂ s f).1 = corec s (fun x => let ⟨f1, _, f2⟩ := f x; (f1, f2)) := by sorry
+
+inductive concrete_bisim (x y : Stream α) : Prop
+
+-- pb: generate bisim relation defined over streams (a concrete one)
+-- needs to rely on cores
+
+def concrete_bisim' (f1 f2 : Stream α → (Option α × Stream α)) (s : Stream α) : Prop :=
+    match s 0 with
+    | some x => (f1 s).1 = some x ∧ -- ((f1 s).1 = some ∨ ∃ n : alwaysNoneUntil n ∧ (f1 s ).n = some
+      -- ) ∧  concrete_bisim' f1 f2 s.tail
+    | none => (f1 s).1 = none ∧ (f2)
+
+theorem corec₂_eq_corec_of_corec₂ (streamInt: DC.ValueStream Int) :
+    (corec₂ streamInt fun x => (x 0, x 0, x.tail)).1 ≈
+    corec
+      ((corec₂ streamInt fun (x : DC.ValueStream Int) =>
+            (match x 0 with
+              | some val => (x 0, some (), x.tail)
+              | none => (none, none, x.tail))).1,
+        (corec₂
+            (corec₂ streamInt fun x =>
+                (match x 0 with
+                  | some val => (x 0, some (), x.tail)
+                  | none => (none, none, x.tail))).2
+            fun x => Id.run (x 0, x 0, tail x)).1)
+      fun x =>
+      match x.1 0, x.2 0 with
+      | some x₀, some val => (some x₀, tail x.1, tail x.2)
+      | some val, none => (none, x.1, tail x.2)
+      | none, some val => (none, tail x.1, x.2)
+      | none, none => (none, tail x.1, tail x.2) := by
+  rw [stream_pair_1]
+  unfold HasEquiv.Equiv
+  unfold instHasEquivOfSetoid
+  simp
+  unfold Setoid.r
+  unfold StreamSetoid
+  simp
+  unfold Bisim
+  simp
+  exists Eq
+  sorry
+
+
+  -- apply corec_eq_corec_of (· ≈ ·)
+  -- · sorry
+  -- · sorry
+  -- · sorry
+
+
+
 
 
 
