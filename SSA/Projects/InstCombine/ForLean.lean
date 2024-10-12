9ee8299f10dca0c43d6fb31b9c4df362c8f3ba6b
luisacicolini <lc985@cam.ac.uk>
2024-10-12 19:24:23 +0100

    fixed build

--- SSA/Projects/InstCombine/ForLean.lean
+++ SSA/Projects/InstCombine/ForLean.lean
@@ -593,7 +593,8 @@ theorem getMsbD_add {i : Nat} {i_lt : i < w} {x y : BitVec w} :
 theorem getMsbD_sub {i : Nat} {i_lt : i < w} {x y : BitVec w} :
     (x - y).getMsbD i =
       (x.getMsbD i ^^ ((~~~y + 1).getMsbD i ^^ carry (w - 1 - i) x (~~~y + 1) false)) := by
-  simp [BitVec.sub_eq_add_neg, neg_eq_not_add, getMsbD_add]
+  rw [BitVec.sub_eq_add_neg, neg_eq_not_add, getMsbD_add]
+  omega
 
 theorem getMsbD_neg {i : Nat} {i_lt : i < w} {x : BitVec w} :
     getMsbD (~~~x) i = (getMsbD x i).not := by
