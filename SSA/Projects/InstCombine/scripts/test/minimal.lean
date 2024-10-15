9211d78e2b15379d50f888a9e2110e91dd5c23b4
Leon Frenot <leon.frenot@ens-lyon.fr>
2024-10-15 16:47:42 +0100

    Made a minimal test file for the `(kernel) deep recursion detected` error

---
+++ SSA/Projects/InstCombine/scripts/test/minimal.lean
@@ -0,0 +1,68 @@
+import SSA.Projects.InstCombine.LLVM.Semantics
+open LLVM
+open BitVec
+
+set_option linter.deprecated false
+set_option linter.unreachableTactic false
+set_option linter.unusedTactic false
+
+set_option pp.proofs true
+set_option pp.analyze true
+
+theorem minimal : ∀ (x : BitVec 32),
+  Refinement (α := BitVec 32)
+    ((some x).bind fun x =>
+      (if False then none else some (x.sshiftRight 1)).bind fun x' =>
+        some (x'.sshiftRight 1))
+    (some x) := by
+  intro x
+  simp only [Option.some_bind]
+
+  sorry
+
+-- Reducing/removing the if-then-else removes the error
+theorem minimal1 : ∀ (x : BitVec 32),
+  Refinement (α := BitVec 32)
+    ((some x).bind fun x =>
+      (if False then none else some (x.sshiftRight 1)).bind fun x' =>
+        some (x'.sshiftRight 1))
+    (some x) := by
+  intro x
+  simp only [Nat.reduceLeDiff, ↓reduceIte]
+  simp only [Option.some_bind]
+
+  sorry
+
+-- removing the operation on x removes the error
+theorem minimal2 : ∀ (x : BitVec 32),
+  Refinement (α := BitVec 32)
+    ((some x).bind fun x =>
+      (if False then none else some x).bind fun x' =>
+        some (x'.sshiftRight 1))
+    (some x) := by
+  intro x
+  simp only [Option.some_bind]
+
+  sorry
+
+-- removing the operation on x' removes the error
+theorem minimal3 : ∀ (x : BitVec 32),
+  Refinement (α := BitVec 32)
+    ((some x).bind fun x =>
+      (if False then none else some (x.sshiftRight 1)).bind fun x' =>
+        some (x'))
+    (some x) := by
+  intro x
+  simp only [Option.some_bind]
+
+  sorry
+
+-- removing the trivial bind removes the error (note: this is the state in `minimal` after the simp)
+theorem minimal4 : ∀ (x : BitVec 32),
+  Refinement (α := BitVec 32)
+    ((if False then none else some (x.sshiftRight 1)).bind fun x' => some (x'.sshiftRight 1))
+    (some x) := by
+  intro x
+  simp (config:={failIfUnchanged := false}) only [Option.some_bind]
+
+  sorry
