6e54110f1f19059ae90b720bc78dcd2db746a73b
luisacicolini <lc985@cam.ac.uk>
2024-10-24 07:13:30 -0700

    restored test

--- SSA/Projects/InstCombine/tests/proofs/g2004h02h23hShiftShiftOverflow_proof.lean
+++ SSA/Projects/InstCombine/tests/proofs/g2004h02h23hShiftShiftOverflow_proof.lean
@@ -7,3 +7,4 @@ section g2004h02h23hShiftShiftOverflow_proof
 theorem test_thm (x : BitVec 32) : (x.sshiftRight 17).sshiftRight 17 = x.sshiftRight 31 := sorry
 
 theorem test2_thm (x : BitVec 32) : x <<< 34 = 0#32 := sorry
+
