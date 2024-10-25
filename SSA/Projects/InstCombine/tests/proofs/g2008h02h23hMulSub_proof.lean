5986bd18dde5e89f41151b69be2f8385fb149025
Leon Frenot <leon.frenot@ens-lyon.fr>
2024-10-25 17:19:32 +0100

    Ran the scripts

--- SSA/Projects/InstCombine/tests/proofs/g2008h02h23hMulSub_proof.lean
+++
@@ -1,8 +0,0 @@
-
-import SSA.Projects.InstCombine.TacticAuto
-import SSA.Projects.InstCombine.LLVM.Semantics
-open BitVec
-
-section g2008h02h23hMulSub_proof
-theorem test_thm (x : BitVec 26) : x * 2885#26 - x * 2884#26 = x := sorry
-
