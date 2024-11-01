8f83c7f0a5763b0a7bd4eea1ec6b8a9021d0c4d9
Tobias Grosser <tobias@grosser.es>
2024-11-01 05:46:04 +0000

    wip|

---
+++ SSA/Projects/InstCombine/tests/proofs/gsubhorhandhxor_proof.lean
@@ -0,0 +1,31 @@
+
+import SSA.Projects.InstCombine.TacticAuto
+import SSA.Projects.InstCombine.LLVM.Semantics
+open BitVec
+open LLVM
+
+section gsubhorhandhxor_proof
+theorem sub_to_xor_thm (e e_1 : IntW 32) : sub (LLVM.or e_1 e) (LLVM.and e_1 e) ⊑ LLVM.xor e_1 e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem sub_to_xor_or_commuted_thm (e e_1 : IntW 32) : sub (LLVM.or e_1 e) (LLVM.and e e_1) ⊑ LLVM.xor e e_1 := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem sub_to_xor_and_commuted_thm (e e_1 : IntW 32) : sub (LLVM.or e_1 e) (LLVM.and e e_1) ⊑ LLVM.xor e e_1 := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
