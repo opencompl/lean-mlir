9b9bac5720b7c77ab158a7c0cb47df0af448e5c9
Tobias Grosser <tobias@grosser.es>
2024-10-31 22:20:11 -0500

    Update SSA/Projects/InstCombine/scripts/cfg.py

--- SSA/Projects/InstCombine/scripts/cfg.py
+++ SSA/Projects/InstCombine/scripts/cfg.py
@@ -8,8 +8,7 @@ import os
 from enum import Enum, auto
 
 # The path from lean-mlir to llvm-project
-llvm_path = "../llvm-project"
-
+llvm_path = "../llvm-project-main"
 if len(llvm_path) == 0:
     raise ValueError("You need to give the path to llvm in config.py")
 
