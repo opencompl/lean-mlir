f5236bec6c579cc7ae01a8dab1ab71a192f44d27
Tobias Grosser <tobias@grosser.es>
2024-11-01 05:55:25 +0000

    wip

---
+++ SSA/Projects/InstCombine/tests/proofs/gbinophofhdisplacedhshifts_proof.lean
@@ -0,0 +1,125 @@
+
+import SSA.Projects.InstCombine.TacticAuto
+import SSA.Projects.InstCombine.LLVM.Semantics
+open BitVec
+open LLVM
+
+section gbinophofhdisplacedhshifts_proof
+theorem shl_or_thm (e : IntW 8) :
+  LLVM.or (shl (const? 16) e) (shl (const? 3) (add e (const? 1))) ⊑ shl (const? 22) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem lshr_or_thm (e : IntW 8) :
+  LLVM.or (lshr (const? 16) e) (lshr (const? 3) (add e (const? 1))) ⊑ lshr (const? 17) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem ashr_or_thm (e : IntW 8) :
+  LLVM.or (ashr (const? (-64)) e) (ashr (const? (-128)) (add e (const? 1))) ⊑ ashr (const? (-64)) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_xor_thm (e : IntW 8) :
+  LLVM.xor (shl (const? 16) e) (shl (const? 3) (add e (const? 1))) ⊑ shl (const? 22) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem lshr_xor_thm (e : IntW 8) :
+  LLVM.xor (lshr (const? 16) e) (lshr (const? 3) (add e (const? 1))) ⊑ lshr (const? 17) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem ashr_xor_thm (e : IntW 8) :
+  LLVM.xor (ashr (const? (-128)) e) (ashr (const? (-64)) (add e (const? 1))) ⊑ lshr (const? 96) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_and_thm (e : IntW 8) :
+  LLVM.and (shl (const? 48) e) (shl (const? 8) (add e (const? 1))) ⊑ shl (const? 16) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem lshr_and_thm (e : IntW 8) :
+  LLVM.and (lshr (const? 48) e) (lshr (const? 64) (add e (const? 1))) ⊑ lshr (const? 32) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem ashr_and_thm (e : IntW 8) :
+  LLVM.and (ashr (const? (-64)) e) (ashr (const? (-128)) (add e (const? 1))) ⊑ ashr (const? (-64)) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_add_thm (e : IntW 8) :
+  add (shl (const? 16) e) (shl (const? 7) (add e (const? 1))) ⊑ shl (const? 30) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem lshr_add_fail_thm (e : IntW 8) :
+  add (lshr (const? 16) e) (lshr (const? 7) (add e (const? 1))) ⊑
+    add (lshr (const? 16) e) (lshr (const? 7) (add e (const? 1))) { «nsw» := true, «nuw» := true } := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_or_commuted_thm (e : IntW 8) :
+  LLVM.or (shl (const? 3) (add e (const? 1))) (shl (const? 16) e) ⊑ shl (const? 22) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_or_with_or_disjoint_instead_of_add_thm (e : IntW 8) :
+  LLVM.or (shl (const? 16) e) (shl (const? 3) (LLVM.or e (const? 1))) ⊑ shl (const? 22) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
---
+++ SSA/Projects/InstCombine/tests/proofs/gsethlowbitshmaskhcanonicalize_proof.lean
@@ -0,0 +1,193 @@
+
+import SSA.Projects.InstCombine.TacticAuto
+import SSA.Projects.InstCombine.LLVM.Semantics
+open BitVec
+open LLVM
+
+section gsethlowbitshmaskhcanonicalize_proof
+theorem shl_add_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? (-1)) ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_add_nsw_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? (-1)) { «nsw» := true, «nuw» := false } ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_add_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? (-1)) { «nsw» := false, «nuw» := true } ⊑ const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_add_nsw_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? (-1)) { «nsw» := true, «nuw» := true } ⊑ const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_add_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := false }) (const? (-1)) ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_add_nsw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := false }) (const? (-1)) { «nsw» := true, «nuw» := false } ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_add_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := false }) (const? (-1)) { «nsw» := false, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_add_nsw_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := false }) (const? (-1)) { «nsw» := true, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nuw_add_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? (-1)) ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nuw_add_nsw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? (-1)) { «nsw» := true, «nuw» := false } ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nuw_add_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? (-1)) { «nsw» := false, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nuw_add_nsw_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? (-1)) { «nsw» := true, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_nuw_add_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := true }) (const? (-1)) ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_nuw_add_nsw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := true }) (const? (-1)) { «nsw» := true, «nuw» := false } ⊑
+    LLVM.xor (shl (const? (-1)) e { «nsw» := true, «nuw» := false }) (const? (-1)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_nuw_add_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := true }) (const? (-1)) { «nsw» := false, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem shl_nsw_nuw_add_nsw_nuw_thm (e : IntW 32) :
+  add (shl (const? 1) e { «nsw» := true, «nuw» := true }) (const? (-1)) { «nsw» := true, «nuw» := true } ⊑
+    const? (-1) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem bad_add0_thm (e e_1 : IntW 32) :
+  add (shl (const? 1) e_1) e ⊑ add (shl (const? 1) e_1 { «nsw» := false, «nuw» := true }) e := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem bad_add1_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? 1) ⊑
+    add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? 1) { «nsw» := false, «nuw» := true } := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
+theorem bad_add2_thm (e : IntW 32) :
+  add (shl (const? 1) e) (const? (-2)) ⊑ add (shl (const? 1) e { «nsw» := false, «nuw» := true }) (const? (-2)) := by 
+    simp_alive_undef
+    simp_alive_ops
+    simp_alive_case_bash
+    try alive_auto
+    all_goals sorry
+
+
