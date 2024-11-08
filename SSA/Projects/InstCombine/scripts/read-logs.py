67721a614f757e5edf93421c7aa9085276eab5ac
lfrenot <leon.frenot@gmail.com>
2024-11-08 11:45:56 +0000

    chore: Cleaning Instcombine test scripts (#809)
    This introduces the following changes to the script files:
    
    - Added a skipped_funcs dict to solve
    https://github.com/opencompl/lean-mlir/issues/805
    - Separated the logs from test-gen and proof-gen, and moved the test
    cases accordingly
    - Removed some useless imports
    - Removed the paths in `proof-gen.py` which are already defined in
    `cfg.py`
    - Ran an automated formatter (python black)

--- SSA/Projects/InstCombine/scripts/read-logs.py
+++ SSA/Projects/InstCombine/scripts/read-logs.py
@@ -4,11 +4,13 @@ import re
 import subprocess
 from cfg import *
 
+
 def add_or_1(d, k):
     if k in d:
         d[k] += 1
     else:
         d[k] = 1
+
 
 def main():
     unsupported_func = dict()
@@ -16,20 +18,16 @@ def main():
     error_dict = dict()
     log_errors = []
     func_count = 0
-    build_error = 0
-    for log in os.listdir(log_path):
-        with open(f"{log_path}/{log}", "r") as l:
+    for log in os.listdir(llvm_log_path):
+        with open(f"{llvm_log_path}/{log}", "r") as l:
             func_success = False
             lines = l.readlines()
             if not (len(lines) > 0):
                 log_errors.append(log)
                 continue
-            elif not lines[0][0].isnumeric():
-                build_error += 1
-                continue
             for line in lines:
                 s_line = re.split('"|: |\n', line)
-                s_line = [s.strip() for s in s_line if s != '']
+                s_line = [s.strip() for s in s_line if s != ""]
                 if len(s_line) > 0:
                     msg = Msg(int(s_line[0]))
                     func = s_line[-1]
@@ -50,31 +48,31 @@ def main():
     llvm_test_count = len(os.listdir(llvm_test_path))
     translated_test_count = len(os.listdir(test_path))
     proof_count = len(os.listdir(proof_path)) >> 1
-    
+    build_error = len(os.listdir(proof_log_path))
     grep_process = f"grep -o theorem {proof_path}/*_proof.lean | wc -l"
-    
+
     theorem_count = subprocess.run(
-            grep_process,
-            shell=True,
-            capture_output=True,
-            encoding="utf-8"
+        grep_process, shell=True, capture_output=True, encoding="utf-8"
     ).stdout
-    
+
     print(f"Number of files in llvm's tests: {llvm_test_count}\n")
     print(f"Number of translated test files: {translated_test_count}\n")
     print(f"Number of successfully built test files: {proof_count}\n")
-    
+
     print(f"Empty logs: {log_errors}\n")
-    
+
     print(f"Number of builds that failed: {build_error}\n")
     print(f"Number of generated BitVector theorems: {theorem_count}\n")
     print(f"Occurrences of unsupported functions: {sum(unsupported_func.values())}")
-    for key, value in sorted(unsupported_func.items(), key=lambda x: x[1]): 
+    for key, value in sorted(unsupported_func.items(), key=lambda x: x[1]):
         print("{} : {}".format(key, value))
-    
-    print(f"Number of functions that couldn't be translated due to each error: {sum(error_dict.values())}")
-    for key, value in sorted(error_dict.items(), key=lambda x: x[1]): 
+
+    print(
+        f"Number of functions that couldn't be translated due to each error: {sum(error_dict.values())}"
+    )
+    for key, value in sorted(error_dict.items(), key=lambda x: x[1]):
         print("{} : {}".format(key, value))
-    
+
+
 if __name__ == "__main__":
-   main()
+    main()
