d1c1ca0bc39440652f1b44bc16458cbd684aaea6
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-11-11 16:47:06 +0000

    clearer variable name

--- SSA/Projects/InstCombine/LLVM/PrettyEDSLOverflow.lean
+++ SSA/Projects/InstCombine/LLVM/PrettyEDSLOverflow.lean
@@ -17,34 +17,34 @@ It gets translated to `<{overflowFlags = #llvm.overflow< flags >}>` in the gener
 declare_syntax_cat MLIR.Pretty.overflow_op
 declare_syntax_cat MLIR.Pretty.overflow_int_cast_op
 
-syntax overflow_flag := "nsw" <|> "nuw"
+syntax no_wrap_flag := "nsw" <|> "nuw"
 
-syntax (mlir_op_operand " = ")? MLIR.Pretty.overflow_op mlir_op_operand,* (" overflow<" overflow_flag,* "> ")?
+syntax (mlir_op_operand " = ")? MLIR.Pretty.overflow_op mlir_op_operand,* (" overflow<" no_wrap_flag,* "> ")?
   (" : " mlir_type)? : mlir_op
 macro_rules
-  | `(mlir_op| $[$resName =]? $name:MLIR.Pretty.overflow_op $xs,* $[overflow< $[$f],* >]? $[: $t]? ) => do
+  | `(mlir_op| $[$resName =]? $name:MLIR.Pretty.overflow_op $xs,* $[overflow< $[$noWrapFlags],* >]? $[: $t]? ) => do
     let some opName := extractOpName name.raw | Macro.throwUnsupported
     let t ← t.getDM `(mlir_type| _)
     let argTys : TSyntaxArray `mlir_type := xs.getElems.map (fun _ => t)
     let retTy : TSyntaxArray `mlir_type := match resName with
       | some _ => #[t]
       | none => #[]
-    let ovflags ← f.mapM fun f => f.mapM fun
-      | `(overflow_flag|nsw) =>
+    let ovflags ← noWrapFlags.mapM fun noWrapFlag => noWrapFlag.mapM fun
+      | `(no_wrap_flag|nsw) =>
         `(mlir_attr_val|$(mkIdent `nsw):ident)
-      | `(overflow_flag|nuw) =>
+      | `(no_wrap_flag|nuw) =>
         `(mlir_attr_val|$(mkIdent `nuw):ident)
       | _ => Macro.throwUnsupported
     `([mlir_op| $[$resName =]? $opName ($xs,*) $[<{overflowFlags = #llvm.overflow< $ovflags,* >}>]? : ($argTys,*) -> ($retTy:mlir_type,*) ])
 
-syntax mlir_op_operand " = " MLIR.Pretty.overflow_int_cast_op mlir_op_operand (" overflow<" overflow_flag,* "> ")? " : " mlir_type " to " mlir_type : mlir_op
+syntax mlir_op_operand " = " MLIR.Pretty.overflow_int_cast_op mlir_op_operand (" overflow<" no_wrap_flag,* "> ")? " : " mlir_type " to " mlir_type : mlir_op
 macro_rules
-  | `(mlir_op| $resName:mlir_op_operand = $name:MLIR.Pretty.overflow_int_cast_op $x $[overflow< $[$f],* >]? : $t to $t' ) => do
+  | `(mlir_op| $resName:mlir_op_operand = $name:MLIR.Pretty.overflow_int_cast_op $x $[overflow< $[$noWrapFlags],* >]? : $t to $t' ) => do
     let some opName := extractOpName name.raw | Macro.throwUnsupported
-    let ovflags ← f.mapM fun f => f.mapM fun
-      | `(overflow_flag|nsw) =>
+    let ovflags ← noWrapFlags.mapM fun noWrapFlag => noWrapFlag.mapM fun
+      | `(no_wrap_flag|nsw) =>
         `(mlir_attr_val|$(mkIdent `nsw):ident)
-      | `(overflow_flag|nuw) =>
+      | `(no_wrap_flag|nuw) =>
         `(mlir_attr_val|$(mkIdent `nuw):ident)
       | _ => Macro.throwUnsupported
     `([mlir_op| $resName:mlir_op_operand = $opName ($x) $[<{overflowFlags = #llvm.overflow< $ovflags,* >}>]? : ($t) -> $t' ])
