0133238563774510c96d46636a35a6be90ff2b86
Siddharth <siddu.druid@gmail.com>
2024-11-14 06:29:25 +0000

    feat: collect data from hacker's delight in symbolic mode, and add data to paper (#842)
    This uses the previously built `tac_bench` infra to create a CSV file of
    all the runs, and writes it down into
    
    https://github.com/opencompl/paper-lean-bitvectors/commit/9458bd73452c6bf8ad6c250cb141afbb14899af3
    
    Regrettably, I don't know of any easy way to get the theorem name, and
    for whatever reason, the `logInfo` does not seem to prompt lean into
    printing the filename / line number. If we want, we can consider using
    something like `logWarning` and even more hackery, but I'm totally
    unsure this is worth it.

--- bv-evaluation/compare-leansat-vs-bitwuzla-hdel-sym.py
+++ bv-evaluation/compare-leansat-vs-bitwuzla-hdel-sym.py
@@ -23,11 +23,13 @@ reps = 1
 bvw = "w"
 for file in os.listdir(benchmark_dir_list):
     print(file)
+    subprocess.Popen(f'git checkout -- '+benchmark_dir_list+file, shell=True).wait()
     subprocess.Popen('sed -i -E \'s/variable \\{x y z : BitVec .+\\}/variable \\{x y z : BitVec '+str(bvw)+'\\}/g\' '+benchmark_dir_list+file, shell=True).wait()
     # replace any sorrys with bv_compare'
-    TACTIC = 'tac_bench [ "auto" : bv_auto, "ring" : ring ]'
+    TACTIC = 'tac_bench [ "auto" : (bv_auto; done), "ring" : (ring; done) ]'
     subprocess.Popen(f'sed -i -E \'s@all_goals sorry@{TACTIC}@g\' '+benchmark_dir_list+file, shell=True).wait()
     for r in range(reps):
         print(f'cd .. && lake lean '+benchmark_dir+file+' 2>&1 |" tee '+results_dir+file.split(".")[0]+'_'+str(bvw)+'_'+'r'+str(r)+'.txt')
         subprocess.Popen(f'cd .. && lake lean '+benchmark_dir+file+' 2>&1 | tee '+results_dir+file.split(".")[0]+'_'+str(bvw)+'_'+'r'+str(r)+'.txt', shell=True).wait()
     subprocess.Popen(f'sed -i -E \'s@{TACTIC}@all_goals sorry@g\' '+benchmark_dir_list+file, shell=True).wait()
+    subprocess.Popen(f'git checkout -- '+benchmark_dir_list+file, shell=True).wait()
