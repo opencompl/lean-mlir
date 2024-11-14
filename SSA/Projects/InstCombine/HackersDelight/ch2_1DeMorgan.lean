197c14925fb472951c78d4137912e5c64d7c0653
Tobias Grosser <tobias@grosser.es>
2024-11-14 06:53:45 +0000

    Set all goals

--- SSA/Projects/InstCombine/HackersDelight/ch2_1DeMorgan.lean
+++ SSA/Projects/InstCombine/HackersDelight/ch2_1DeMorgan.lean
@@ -12,40 +12,40 @@ variable {x y z : BitVec 64}
 
 theorem not_and_eq_not_or_not :
     ~~~ (x &&& y) = ~~~ x ||| ~~~ y := by
-  bv_bench
+  all_goals sorry
 
 theorem not_or_eq_not_and_not :
     ~~~ (x ||| y) = ~~~ x &&& ~~~ y := by
-  bv_bench
+  all_goals sorry
 
 theorem not_add_one_eq_not_sub_one :
     ~~~ (x + 1) = ~~~ x - 1 := by
-  -- bv_bench -- maximal recursion depth has been reached
+  -- all_goals sorry -- maximal recursion depth has been reached
   sorry
 
 theorem not_sub_one_eq_not_add_one :
     ~~~ (x - 1) = ~~~ x + 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem not_neg_eq_sub_one :
     ~~~ (- x) = x - 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem not_xor_eq_not_xor :
     ~~~ (x ^^^ y) = ~~~ x ^^^ y := by
-  bv_bench
+  all_goals sorry
 
 theorem q_not_xor :
     ~~~ (x ^^^ y) = ~~~ x ^^^ y := by
-  bv_bench
+  all_goals sorry
 
 theorem not_add_eq_not_sub :
     ~~~ (x + y) = ~~~ x - y := by
-  bv_bench
+  all_goals sorry
 
 theorem not_sub_eq_not_add :
     ~~~ (x - y) = ~~~ x + y := by
-  bv_bench
+  all_goals sorry
 
 end DeMorgansLawsExtended
 
--- SSA/Projects/InstCombine/HackersDelight/ch2_2AdditionAndLogicalOps.lean
+++ SSA/Projects/InstCombine/HackersDelight/ch2_2AdditionAndLogicalOps.lean
@@ -12,91 +12,91 @@ variable {x y z : BitVec 64}
 
 theorem neg_eq_not_add_one :
     -x = ~~~ x + 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem neg_eq_neg_not_one :
     -x = ~~~ (x - 1) := by
-  bv_bench
+  all_goals sorry
 
 theorem not_eq_neg_sub_one :
     ~~~ x = - x - 1:= by
-  bv_bench
+  all_goals sorry
 
 theorem neg_not_eq_add_one :
     - ~~~ x = x + 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem not_neg_eq_sub_one :
     ~~~ (-x) = x - 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem add_eq_sub_not_sub_one :
     x + y = x - ~~~ y - 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem add_eq_xor_add_mul_and :
     x + y = (x ^^^ y) + 2 * (x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem add_eq_or_add_and :
     x + y = (x ||| y) + (x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem add_eq_mul_or_neg_xor :
     x + y = 2 * (x ||| y) - (x ^^^ y) := by
-  bv_bench
+  all_goals sorry
 
 theorem sub_eq_add_not_add_one :
     x - y = x + ~~~ y + 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem sub_eq_xor_sub_mul_not_and :
     x - y = (x ^^^ y) - 2 * (~~~ x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem sub_eq_and_not_sub_not_and :
     x - y = (x &&& ~~~ y) - (~~~ x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem sub_eq_mul_and_not_sub_xor :
     x - y = 2 * (x &&& ~~~ y) - (x ^^^ y) := by
-  bv_bench
+  all_goals sorry
 
 theorem xor_eq_or_sub_and :
     x ^^^ y = (x ||| y) - (x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem and_not_eq_or_sub:
     x &&& ~~~ y = (x ||| y) - y := by
-  bv_bench
+  all_goals sorry
 
 theorem and_not_eq_sub_add :
     x &&& ~~~ y = x - (x &&& y) := by
-  bv_bench
+  all_goals sorry
 
 theorem not_sub_eq_sub_sub_one :
     ~~~ (x - y) = y - x - 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem not_sub_eq_not_add :
     ~~~ (x - y) = ~~~x + y := by
-  bv_bench
+  all_goals sorry
 
 theorem not_xor_eq_and_sub_or_sub_one :
     ~~~ (x ^^^ y) = (x &&& y) - (x ||| y) - 1 := by
-  bv_bench
+  all_goals sorry
 
 theorem not_xor_eq_and_add_not_or :
     ~~~ (x ^^^ y) = (x &&& y) + ~~~ (x ||| y) := by
-  bv_bench
+  all_goals sorry
 
 theorem or_eq_and_not_add :
     x ||| y = (x &&& ~~~ y) + y := by
-  bv_bench
+  all_goals sorry
 
 theorem and_eq_not_or_sub_not :
     x &&& y = (~~~ x ||| y) - ~~~ x := by
-  bv_bench
+  all_goals sorry
 
 end AdditionCombinedWithLogicalOperations
 
