2faca6e269a204e0292764a873b94b217fb9ccd5
Siddharth Bhat <siddu.druid@gmail.com>
2024-11-14 04:03:34 +0000

    feat: refactor to make calls atomic by taking a list of TacBench items

--- SSA/Core/Tactic/TacBench.lean
+++ SSA/Core/Tactic/TacBench.lean
@@ -19,7 +19,8 @@ open Lean.Meta
 Run `tac_bench <name> <tacticSeq>` to run a sequence of tactics whose runtime is benchmarked.
 This does not affect the current goal state, and thus allow multiple `tac_bench` statements to be run in sequence.
 -/
-syntax (name := tacBench) "tac_bench" str tacticSeq : tactic
+syntax tacBenchItem := str &":" tacticSeq
+syntax (name := tacBench) "tac_bench" "["(tacBenchItem),*"]" : tactic
 
 
 def setTraceOptions (opt : Options) : Options := opt
@@ -33,52 +34,68 @@ def withFreshTraceState {α : Type} (x : TacticM α) : TacticM α := do
     resetTraceState
     try x finally setTraceState traces
 
-inductive Result
-| ok (time : Float)
-| err (time : Float) (e : Exception)
-
 def Nat.deltaInMs (now past : Nat) : Float := (Float.ofNat <| now - past) / 1000000.0
 
-def hermeticRun (g : MVarId) (tac : Syntax) : TacticM Result := g.withContext do
+structure Item where
+ name : String
+ tac : Syntax
+
+
+inductive Result
+| ok (item : Item) (time : Float)
+| err (item : Item) (time : Float) (e : Exception)
+
+
+def Result.toMessageData : Result → MessageData
+| .ok item timeMs => m!"TACBENCH {item.name} PASS, TIME_ELAPSED {timeMs} ms, "
+| .err item timeMs e => m!"TACBENCH {item.name} FAIL, TIME_ELAPSED {timeMs} ms, {indentD e.toMessageData}"
+
+instance : ToMessageData Result where
+  toMessageData := Result.toMessageData
+
+
+def hermeticRun (g : MVarId) (item : Item) : TacticM Result := g.withContext do
   let t1 ← IO.monoNanosNow
   try
     -- TODO: think if we need this, I'm just stealing from Henrik at this point.
     -- We can configure more options here to enable/disable tracing as needed.
     withOptions setTraceOptions <| withoutModifyingEnv <| withoutModifyingState <| withFreshTraceState do
-      evalTactic tac
+      evalTactic item.tac
       let t2 ← IO.monoNanosNow
-      return .ok (Nat.deltaInMs t2 t1)
+      return .ok item (Nat.deltaInMs t2 t1)
   catch e =>
     let t2 ← IO.monoNanosNow
-    return .err (Nat.deltaInMs t2 t1) e
+    return .err item (Nat.deltaInMs t2 t1) e
 
+
+
+def parseTacBenchItem : TSyntax `tacBenchItem → TacticM Item
+| `(tacBenchItem| $name:str : $tac:tacticSeq) => 
+     return { name := name.getString, tac := tac : Item }
+| _ => throwUnsupportedSyntax
 
 
 @[tactic tacBench]
 def evalTacBench : Tactic := fun
-| `(tactic| tac_bench $name:str $tac:tactic) => do
+| `(tactic| tac_bench [$tacBenchItems:tacBenchItem,*]) => do
     let g ← getMainGoal
-    match ← hermeticRun g tac with
-    | .ok tms =>
-      logInfo m!"TACBENCH {name} PASS, TIME_ELAPSED {tms} ms, "
-    | .err tms e =>
-      logInfo m!"TACBENCH {name} FAIL, TIME_ELAPSED {tms} ms, {indentD e.toMessageData}"
-
+    let items ← tacBenchItems.getElems.mapM parseTacBenchItem
+    -- logInfo m!{← hermeticRun g tac}
+    let mut msg := m!""
+    for item in items do
+      let out ← hermeticRun g item
+      msg := msg ++ m!"\n" ++ out.toMessageData
+    logInfo msg
 | _ => throwUnsupportedSyntax
 
 
 section Examples
 theorem eg1 : 1 = 1 := by
-  tac_bench "rfl" rfl
-  tac_bench "wrong" (rw [Nat.add_comm])
-  tac_bench "success" simp
-  tac_bench "done" done
-  tac_bench "sorry" sorry
+  tac_bench ["rfl" : rfl, "wrong" : (rw [Nat.add_comm]), "success" : simp, "done" : done, "sorry" : sorry]
   sorry
 
 theorem eg2 (x y : BitVec 8) : x * y = y * x := by
-  tac_bench "bv_decide" bv_decide
-  tac_bench "ac_nf" ac_nf
+  tac_bench ["bv_decide" :  bv_decide, "ac_nf" : ac_nf]
   sorry
 
 end Examples
