99b0dcce53094072de212ad4a23ee7ed0cdc2089
luisacicolini <lc985@cam.ac.uk>
2024-11-14 23:46:19 +0000

    hdel: fix all_goals

--- bv-evaluation/compare-leansat-vs-bitwuzla-hdel.py
+++ bv-evaluation/compare-leansat-vs-bitwuzla-hdel.py
@@ -27,8 +27,8 @@ for file in os.listdir(benchmark_dir_list):
     for bvw in bv_width:
         subprocess.Popen('sed -i -E \'s/variable \\{x y z : BitVec .+\\}/variable \\{x y z : BitVec '+str(bvw)+'\\}/g\' '+benchmark_dir_list+file, shell=True).wait()
         # replace any sorrys with bv_compare'
-        subprocess.Popen('sed -i -E \'s,all_goals sorry,bv_compare\'\\\'\',g\' '+benchmark_dir_list+file, shell=True).wait()
+        subprocess.Popen('sed -i -E \'s,sorry,bv_compare\'\\\'\',g\' '+benchmark_dir_list+file, shell=True).wait()
         for r in range(reps):
             print(f'cd .. && lake lean '+benchmark_dir+file+' 2>&1 > '+results_dir+file.split(".")[0]+'_'+str(bvw)+'_'+'r'+str(r)+'.txt')
             subprocess.Popen(f'cd .. && lake lean '+benchmark_dir+file+' 2>&1 > '+results_dir+file.split(".")[0]+'_'+str(bvw)+'_'+'r'+str(r)+'.txt', shell=True).wait()
-        subprocess.Popen('sed -i -E \'s,bv_compare\'\\\'\',all_goals sorry,g\' '+benchmark_dir_list+file, shell=True).wait()
+        subprocess.Popen('sed -i -E \'s,bv_compare\'\\\'\',sorry,g\' '+benchmark_dir_list+file, shell=True).wait()
