ad8874727096b5e668a7f023b224f3a3bd05d919
luisacicolini <lc985@cam.ac.uk>
2024-12-09 18:46:12 +0100

    first round of polishing but the plot of the lost theorem thickens

--- SSA/Projects/CIRCT/Handshake-to-DC/ForkData.lean
+++ SSA/Projects/CIRCT/Handshake-to-DC/ForkData.lean
@@ -8,14 +8,16 @@ namespace CIRCTStream
 namespace Stream.Bisim
 
 theorem corec₂_eq_tok (x : DC.TokenStream):
-  (corec₂ x fun x => Id.run (x 0, x 0, tail x)) = (corec₂ x fun x => Id.run (x 0, x 0, x.tail)) := by
+  (corec₂ x fun x => (x 0, x 0, tail x)) = (corec₂ x fun x => Id.run (x 0, x 0, x.tail)) := by
   apply corec₂_eq_corec₂_of
   rotate_left 2
   · exact Eq.refl _
   · intro b₁ b₂ h
     simp [h]
+    and_intros <;> rfl
   · intro b₁ b₂ h
     simp [h]
+    and_intros <;> rfl
 
 theorem corec₂_eq_val (x : Stream α):
   (corec₂ x fun x => Id.run (x 0, x 0, tail x)) = (corec₂ x fun x => Id.run (x 0, x 0, x.tail)) := by
@@ -157,79 +159,12 @@ open Ctxt in
 theorem equiv_fork_fst (streamInt : DC.ValueStream Int) :
   (Handshake.fork streamInt).fst ~ (DCFork.denote (Valuation.ofHVector (.cons streamInt <| .nil))).fst := by
   simp [DCFork, Valuation.ofPair, Valuation.ofHVector]
-  -- unfold Handshake.fork DC.fork
   simp_peephole
   unfold Bisim
   refine ⟨Eq, ?_, EqIsBisim⟩
   rw [DC.fork_eq]
   simp
 
-
-  unfold DC.pack DC.unpack DC.fork
-  rw [corec₂_corec1]
-  unfold corec
-  simp
-  unfold Stream'.corec Stream'.map Stream'.get Stream'.iterate
-  rw [corec₂_corec1]
-  and_intros
-  · apply corec_eq_corec_of (R := map_to_unit_pair)
-    · intros a b hm
-      and_intros
-      all_goals
-        unfold CIRCTStream.Stream.tail
-        rcases hm with ⟨ heq, hmap ⟩; subst_vars
-        rcases b with ⟨ b1, b2 ⟩; dsimp only at *
-        rcases hb1 : b1 0 with _ | val <;> cases hb2 : b2 0
-          <;> first | rfl | simp [Id.run, Stream'.map_tail, hmap]
-        rw [Stream'.ext_iff] at hmap; specialize hmap 0
-        simp_all [Stream'.get, Stream'.map]
-    · unfold map_to_unit_pair
-      unfold Id.run
-      have h (s' : Stream Int) :
-         ((match
-            (match s' 0 with
-              | some val => (s' 0, some (), s'.tail)
-              | none => (none, none, s'.tail)) with
-          | (a, fst, b) => (a, b)) : Option Int × Stream Int) =
-          ((s' 0, s'.tail)) := by
-          cases h : s' 0 <;> simp_all [Id.run]
-      simp;
-      rw [corec₂_corec1, corec₂_corec2]
-      dsimp [TyDenote.toType, MLIR2DC.Ty2, MLIR2DC.instTyDenoteTy2, MLIR2DC.Ty2.int] at *;
-      simp [h]
-      unfold DC.unpack.match_1 equiv_fork_fst.match_1 at *
-      unfold corec₂_corec1.match_1 at *
-      rw [h]
-
-      and_intros
-      · simp; rw [corec₂_corec1]
-        have h : ((fun s' => match
-            Id.run (match s' 0 with
-              | some val => (s' 0, some (), s'.tail)
-              | none => (none, none, s'.tail)) with
-          | (a, fst, b) => (a, b)) : Stream Int → Option Int × Stream Int) =
-          (fun s' => (s' 0, s'.tail)) := by
-          ext s' <;> cases h : s' 0 <;> simp_all [Id.run]
-        dsimp [TyDenote.toType, MLIR2DC.Ty2, MLIR2DC.instTyDenoteTy2, MLIR2DC.Ty2.int] at *;
-        unfold DC.unpack.match_1 equiv_fork_fst.match_1 at *
-        rw [h]
-        unfold corec
-        rw [Stream'.ext_iff]; intro n
-        clear h
-        rw [Stream'.corec_def]; simp
-        unfold Stream Stream.tail at *
-        have {α} : ((fun x => x.tail) : Stream' α → Stream' α) = Stream'.tail := by rfl
-        rw [this]; unfold Stream'.get
-        rw [tail_iterate']
-      · simp; rw [corec₂_corec2, corec₂_corec1]
-        unfold Id.run
-        simp
-        funext i
-        simp
-
-
-        sorry
-  · apply EqIsBisim
 
 open Ctxt in
 theorem equiv_fork_fst2 (streamInt : DC.ValueStream Int) :
@@ -253,7 +188,7 @@ theorem equiv_fork_fst2 (streamInt : DC.ValueStream Int) :
         rcases hb1 : b1 0 with _ | val <;> cases hb2 : b2 0
           <;> first | rfl | simp [Id.run, Stream'.map_tail, hmap]
         rw [Stream'.ext_iff] at hmap; specialize hmap 0
-        simp_all [Stream'.get, Stream'.map]
+        simp_all [Stream'.get, Stream'.map, Option.map_none']
     · unfold map_to_unit_pair
       and_intros
       · simp; rw [corec₂_corec1]
@@ -264,18 +199,18 @@ theorem equiv_fork_fst2 (streamInt : DC.ValueStream Int) :
           | (a, fst, b) => (a, b)) : Stream Int → Option Int × Stream Int) =
           (fun s' => (s' 0, s'.tail)) := by
           ext s' <;> cases h : s' 0 <;> simp_all [Id.run]
-        dsimp [TyDenote.toType, MLIR2DC.Ty2, MLIR2DC.instTyDenoteTy2, MLIR2DC.Ty2.int] at *;
+        dsimp only [MLIR2DC.instTyDenoteTy2, TyDenote.toType] at *;
         unfold DC.unpack.match_1 equiv_fork_fst2.match_1 at *
         rw [h]
         unfold corec
         rw [Stream'.ext_iff]; intro n
         clear h
-        rw [Stream'.corec_def]; simp
+        rw [Stream'.corec_def]; simp only [Stream'.get_map]
         unfold Stream Stream.tail at *
         have {α} : ((fun x => x.tail) : Stream' α → Stream' α) = Stream'.tail := by rfl
         rw [this]; unfold Stream'.get
         rw [tail_iterate']
-      · simp; rw [corec₂_corec1, corec₂_corec2]
+      · simp only; rw [corec₂_corec1, corec₂_corec2]
         dsimp [Id.run]
         have h : ((fun s' => match
             Id.run (match s' 0 with
@@ -284,12 +219,8 @@ theorem equiv_fork_fst2 (streamInt : DC.ValueStream Int) :
           | (fst, a, b) => (a, b)) : Stream Int → Option Unit × Stream Int) =
           (fun s' => (Option.map (fun _ => ()) (s' 0), s'.tail)) := by
           ext s' <;> cases h : s' 0 <;> simp_all [Id.run]
-        unfold CIRCTStream.DC.unpack.match_1 CIRCTStream.Stream.Bisim.corec₂_corec1.match_1 CIRCTStream.Stream.Bisim.equiv_fork_fst2.match_2 CIRCTStream.Stream.Bisim.equiv_fork_fst2.match_1 at *
-        dsimp [Id.run, TyDenote.toType] at *
-        set_option pp.explicit true in trace_state
-        rw [h]
-        clear h
-
+        dsimp only [MLIR2DC.instTyDenoteTy2, TyDenote.toType] at *;
+        unfold DC.unpack.match_1 equiv_fork_fst2.match_1 at *
         sorry
   · apply EqIsBisim
 
@@ -297,74 +228,9 @@ theorem stream_pair_1 (s : Stream α) (f : Stream α → Option α × Option α 
     (corec₂ s f).1 = corec s (fun x => let ⟨f1, _, f2⟩ := f x; (f1, f2)) := by
   rfl
 
-
-inductive concrete_bisim (x y : Stream α) : Prop
-
--- pb: generate bisim relation defined over streams (a concrete one)
--- needs to rely on cores
-
 def IsBisim' (R' : Stream α → Stream α → Prop) : Prop :=
   ∀ a b, R' a b → ∃ n m,
     R' (a.drop (n+1)) (b.drop (m+1))
     ∧ a.get n = b.get m
     ∧ (∀ i < n, a.get i = none)
     ∧ (∀ j < m, b.get j = none)
-
-theorem corec₂_eq_corec_of_corec₂ (streamInt: DC.ValueStream Int) :
-    (corec₂ streamInt fun x => (x 0, x 0, x.tail)).1 ≈
-    corec
-      ((corec₂ streamInt fun (x : DC.ValueStream Int) =>
-            (match x 0 with
-              | some val => (x 0, some (), x.tail)
-              | none => (none, none, x.tail))).1,
-        (corec₂
-            (corec₂ streamInt fun x =>
-                (match x 0 with
-                  | some val => (x 0, some (), x.tail)
-                  | none => (none, none, x.tail))).2
-            fun x => Id.run (x 0, x 0, tail x)).1)
-      fun x =>
-      match x.1 0, x.2 0 with
-      | some x₀, some val => (some x₀, tail x.1, tail x.2)
-      | some val, none => (none, x.1, tail x.2)
-      | none, some val => (none, tail x.1, x.2)
-      | none, none => (none, tail x.1, tail x.2) := by
-  rw [stream_pair_1]
-  unfold HasEquiv.Equiv
-  unfold instHasEquivOfSetoid
-  simp
-  unfold Setoid.r
-  unfold StreamSetoid
-  simp
-  unfold Bisim
-  simp
-  exists Eq
-  and_intros
-  · sorry
-  · sorry
-
-
-
-
-
-
-
-
--- theorem determinate :
---   Set.Subsingleton (nondeterminify2 (fun s1 s2 => BranchEg1.denote (Ctxt.Valuation.ofPair s1 s2)) (s1', s2')) := by
---   intro x Hx y  Hy
---   simp [Stream.nondeterminify2, Stream.StreamWithoutNones.hasStream] at *
---   rcases Hx with ⟨ x1Stream, x1, x2Stream, x2, rfl ⟩
---   rcases Hy with ⟨ y1Stream, y1, y2Stream, y2, rfl ⟩
---   apply Quotient.sound
---   -- simp [BranchEg1]
---   -- simp [Stream.Bisim, Stream.IsBisim]
---   subst s2'; subst s1'
---   have y1' := Quotient.exact y1
---   have y2' := Quotient.exact y2
---   clear y1; clear y2
---   trans x1Stream
---   apply (equiv_arg1 _ _).symm
---   trans y1Stream
---   · assumption
---   · apply equiv_arg1
