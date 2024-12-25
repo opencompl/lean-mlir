dda72b76e6b9c2aeaac74b23de8a6f214fc36ce8
Tobias Grosser <tobias@grosser.es>
2024-12-25 13:39:21 +0100

    improve naming

--- SSA/Core/HVector.lean
+++ SSA/Core/HVector.lean
@@ -116,9 +116,8 @@ theorem eq_of_type_eq_nil {A : α → Type*} {l : List α}
 syntax "[" withoutPosition(term,*) "]ₕ"  : term
 
 @[simp]
-theorem cons_get_zero {A : α → Type*} {tt : α} {as : List α} {adf : A tt}
-  {afs: HVector A as} :
-   (HVector.cons adf afs).get (@OfNat.ofNat (Fin (as.length + 1)) 0 Fin.instOfNat) = adf := by
+theorem cons_get_zero {A : α → Type*} {a: α} {as : List α} {e : A a} {vec : HVector A as} :
+   (HVector.cons e vec).get (@OfNat.ofNat (Fin (as.length + 1)) 0 Fin.instOfNat) = e := by
   rfl
 
 -- Copied from core for List
