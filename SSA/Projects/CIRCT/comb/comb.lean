8e2b64313320446ac4b38f6bd9903619c2c78ead
luisacicolini <lc985@cam.ac.uk>
2025-01-07 12:19:35 +0100

    chore: denotation of operations. need to reason about bitvec sizes

--- SSA/Projects/CIRCT/comb/comb.lean
+++ SSA/Projects/CIRCT/comb/comb.lean
@@ -253,8 +253,6 @@ def mkTy : MLIR.AST.MLIRType φ → MLIR.AST.ExceptM Comb Comb.Ty
 instance instTransformTy : MLIR.AST.TransformTy Comb 0 where
   mkTy := mkTy
 
-
-
 def add {Γ : Ctxt _} (a : Γ.Var (.bv w)) (b : Γ.Var (.bv w)) : Expr (Comb) Γ .pure (.bv w) :=
   Expr.mk
     (op := .and w)
@@ -414,332 +412,110 @@ def xor {Γ : Ctxt _} (a : Γ.Var (.bv w)) (b : Γ.Var (.bv w)) : Expr (Comb) Γ
 def mkExpr (Γ : Ctxt _) (opStx : MLIR.AST.Op 0) :
     MLIR.AST.ReaderM (Comb) (Σ eff ty, Expr (Comb) Γ eff ty) := do
   match opStx.name with
-  | op@"Comb.source" =>
-    if opStx.args.length > 0 then
-      throw <| .generic s!"expected one operand for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-    else
-      return ⟨_, .tokenstream, source⟩
-  | op@"Comb.sink" | op@"Comb.unpack" | op@"Comb.fork" | op@"Comb.branch" | op@"Comb.fst" | op@"Comb.snd" | op@"Comb.fstVal" | op@"Comb.sndVal" =>
+  | op@"Comb.parity"  =>
     match opStx.args with
     | v₁Stx::[] =>
       let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
       match ty₁, op with
-      | .tokenstream2, "Comb.fst" => return ⟨_, .tokenstream, fst v₁⟩
-      | .tokenstream2, "Comb.snd"  => return ⟨_, .tokenstream, snd v₁⟩
-      | .valuetokenstream r, "Comb.fstVal" => return ⟨_, .valuestream r, fstVal v₁⟩
-      | .valuetokenstream r, "Comb.sndVal"  => return ⟨_, .tokenstream, sndVal v₁⟩
-      | .tokenstream, "Comb.sink" => return ⟨_, .tokenstream, sink v₁⟩
-      | .valuestream r, "Comb.unpack"  => return ⟨_, .valuetokenstream r, unpack v₁⟩
-      | .tokenstream, "Comb.fork"  => return ⟨_, .tokenstream2, fork v₁⟩
-      | .valuestream .bool, "Comb.branch"  => return ⟨_, .tokenstream2, branch v₁⟩
+      | .bv w, "Comb.parity" => return ⟨_, .bv w, parity v₁⟩
       | _, _ => throw <| .generic s!"type mismatch"
     | _ => throw <| .generic s!"expected one operand for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-  | op@"Comb.merge" | op@"Comb.join" | op@"Comb.pack" | op@"Comb.pair"  =>
+  | op@"Comb.add" | op@"Comb.and" | op@"Comb.concat" | op@"Comb.divs" | op@"Comb.divu" | op@"Comb.extract" | op@"Comb.mods" | op@"Comb.modu" | op@"Comb.mul"
+  | op@"Comb.or" | op@"Comb.replicate" | op@"Comb.shl" | op@"Comb.shrs" | op@"Comb.shru" | op@"Comb.sub" | op@"Comb.xor"   =>
     match opStx.args with
     | v₁Stx::v₂Stx::[] =>
       let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
       let ⟨ty₂, v₂⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₂Stx
       match ty₁, ty₂, op with
-      | .tokenstream, .tokenstream, "Comb.merge" => return ⟨_, .valuestream .bool, merge v₁ v₂⟩
-      | .valuestream r, .valuestream r', "Comb.pair" =>
-        if h: r = r' then return ⟨_, .valuestream2 r, pair v₁ (by subst r ; exact v₂)⟩
-        else throw <| .generic s!"type mismatch"
-      | .tokenstream, .tokenstream, "Comb.join"  => return ⟨_, .tokenstream, join v₁ v₂⟩
-      | .valuestream r, .tokenstream, "Comb.pack"  => return ⟨_, .valuestream r, pack v₁ v₂⟩
-      | _, _, _ => throw <| .generic s!"type mismatch"
+      /- more checks need to be added here to ensure the consistency of operations and bitvec sizes -/
+      | .bv w₁, .bv w₂, "Comb.add" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, add v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.and" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, add v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.concat" => return ⟨_, .bv (w₁ + w₂), concat v₁ v₂⟩
+      | .bv w₁, .bv w₂, "Comb.divs" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, divs v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.divu" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, divu v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w, .nat, "Comb.extract" => sorry -- i need a way to have n here! beyond the type. ideal: return ⟨_, .bv (w - n), extract v₁ n⟩
+      | .bv w₁, .bv w₂, "Comb.mods" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, mods v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.modu" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, modu v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.mul" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, mul v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.or" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, or v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      /- TODO: conditions on shifts can be relaxed. reason about how. -/
+      | .bv w₁, .bv w₂, "Comb.shl" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, shl v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.shrs" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, shrs v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.shru" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, shru v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.sub" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, sub v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, "Comb.xor" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w₁, xor v₁ v₂⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | _, _, _=> throw <| .generic s!"type mismatch"
     | _ => throw <| .generic s!"expected two operands for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-  | op@"Comb.select" =>
+  | op@"Comb.icmp" | op@"Comb.mux" =>
     match opStx.args with
     | v₁Stx::v₂Stx::v₃Stx::[] =>
       let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
       let ⟨ty₂, v₂⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₂Stx
       let ⟨ty₃, v₃⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₃Stx
       match ty₁, ty₂, ty₃, op with
-      | .tokenstream, .tokenstream, .valuestream .bool, "Comb.select" => return ⟨_, .tokenstream, select v₁ v₂ v₃⟩
-      | _, _, _, _=> throw <| .generic s!"type mismatch"
-    | _ => throw <| .generic s!"expected three operands for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-  | _ => throw <| .unsupportedOp s!"unsupported operation {repr opStx}"
-
-instance : MLIR.AST.TransformExpr (Comb) 0 where
-  mkExpr := mkExpr
-
-def mkReturn (Γ : Ctxt _) (opStx : MLIR.AST.Op 0) : MLIR.AST.ReaderM (Comb)
-    (Σ eff ty, Com Comb Γ eff ty) :=
-  if opStx.name == "return"
-  then match opStx.args with
-  | vStx::[] => do
-    let ⟨ty, v⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ vStx
-    return ⟨.pure, ty, Com.ret v⟩
-  | _ => throw <| .generic s!"Ill-formed return statement (wrong arity, expected 1, got {opStx.args.length})"
-  else throw <| .generic s!"Tried to build return out of non-return statement {opStx.name}"
-
-instance : MLIR.AST.TransformReturn (Comb) 0 where
-  mkReturn := mkReturn
-
-open Qq MLIR AST Lean Elab Term Meta in
-elab "[Comb_com| " reg:mlir_region "]" : term => do
-  SSA.elabIntoCom reg q(Comb)
-
-end MLIR2Comb
-
-  | .divs
-  | .divu
-  | .extract
-  | .icmp
-  | .mods
-  | .modu
-  | .mul
-  | .mux
-  | .or
-  | .parity
-  | .replicate
-  | .shl
-  | .shrs
-  | .shru
-  | .sub
-  | .xor
-  | .fst => [Ty.tokenstream2]
-  | .fstVal t => [Ty.valuetokenstream t]
-  | .snd => [Ty.tokenstream2]
-  | .sndVal t => [Ty.valuetokenstream t]
-  | .pair t => [Ty.valuestream t, Ty.valuestream t]
-  | .merge => [Ty.tokenstream, Ty.tokenstream]
-  | .branch => [Ty.valuestream Ty2.bool]
-  | .fork => [Ty.tokenstream]
-  | .join => [Ty.tokenstream, Ty.tokenstream]
-  | .select => [Ty.tokenstream, Ty.tokenstream, Ty.valuestream Ty2.bool]
-  | .sink => [Ty.tokenstream]
-  | .source => []
-  | .pack t => [Ty.valuestream t, Ty.tokenstream]
-  | .unpack t => [Ty.valuestream t]
-
--- return type CONF
-@[simp, reducible]
-def Op.outTy : Op → Ty
-  | .fst => Ty.tokenstream
-  | .fstVal t => Ty.valuestream t
-  | .sndVal _ => Ty.tokenstream
-  | .snd => Ty.tokenstream
-  | .pair t => Ty.valuestream2 t
-  | .merge => Ty.valuestream Ty2.bool
-  | .branch => Ty.tokenstream2
-  | .fork => Ty.tokenstream2
-  | .join => Ty.tokenstream
-  | .select => Ty.tokenstream
-  | .sink => Ty.tokenstream
-  | .source => Ty.tokenstream
-  | .pack t => Ty.valuestream t
-  | .unpack t => Ty.valuetokenstream t
-
-@[simp, reducible]
-def Op.signature : Op → Signature (Ty) :=
-  fun o => {sig := Op.sig o, outTy := Op.outTy o, regSig := []}
-
-instance : DialectSignature Comb := ⟨Op.signature⟩
-
-@[simp]
-instance : DialectDenote (Comb) where
-    denote
-    | .fst, arg, _ => (arg.getN 0 (by simp [DialectSignature.sig, signature])).fst
-    | .snd, arg, _ => (arg.getN 0 (by simp [DialectSignature.sig, signature])).snd
-    | .fstVal _, arg, _ => (arg.getN 0 (by simp [DialectSignature.sig, signature])).fst
-    | .sndVal _, arg, _ => (arg.getN 0 (by simp [DialectSignature.sig, signature]) ).snd
-    | .pair _, arg, _ => (arg.getN 0 (by simp [DialectSignature.sig, signature]), arg.getN 1 (by simp [DialectSignature.sig, signature]))
-    | .unpack _, arg, _ => CIRCTStream.Comb.unpack (arg.getN 0 (by simp [DialectSignature.sig, signature]))
-    | .pack _, arg, _  => CIRCTStream.Comb.pack (arg.getN 0 (by simp [DialectSignature.sig, signature])) (arg.getN 1 (by simp [DialectSignature.sig, signature]))
-    | .branch, arg, _  => CIRCTStream.Comb.branch (arg.getN 0 (by simp [DialectSignature.sig, signature]))
-    | .fork, arg, _  => CIRCTStream.Comb.fork (arg.getN 0 (by simp [DialectSignature.sig, signature]))
-    | .join, arg, _  => CIRCTStream.Comb.join (arg.getN 0 (by simp [DialectSignature.sig, signature])) (arg.getN 1 (by simp [DialectSignature.sig, signature]))
-    | .merge, arg, _  => CIRCTStream.Comb.merge (arg.getN 0 (by simp [DialectSignature.sig, signature])) (arg.getN 1 (by simp [DialectSignature.sig, signature]))
-    | .select, arg, _  => CIRCTStream.Comb.select (arg.getN 0 (by simp [DialectSignature.sig, signature])) (arg.getN 1 (by simp [DialectSignature.sig, signature])) (arg.getN 2 (by simp [DialectSignature.sig, signature]))
-    | .sink, arg, _  => CIRCTStream.Comb.sink (arg.getN 0 (by simp [DialectSignature.sig, signature]))
-    | .source, _, _  => CIRCTStream.Comb.source
-
-end Dialect
-
-
-
-def mkTy2 : String → MLIR.AST.ExceptM (Comb) Ty2
-  | "Int" => return (.int)
-  | "Bool" => return (.bool)
-  | _ => throw .unsupportedType
-
-def mkTy : MLIR.AST.MLIRType φ → MLIR.AST.ExceptM Comb Comb.Ty
-  | MLIR.AST.MLIRType.undefined s => do
-    match s.splitOn "_" with
-    | ["TokenStream"] =>
-      return .tokenstream
-    | ["TokenStream2"] =>
-      return .tokenstream2
-    | ["ValueStream", r] =>
-      return .valuestream (← mkTy2 r)
-    | ["ValueStream2", r] =>
-      return .valuestream2 (← mkTy2 r)
-    | ["ValueTokenStream", r] =>
-      return .valuetokenstream (← mkTy2 r)
-    | _ => throw .unsupportedType
-  | _ => throw .unsupportedType
-
-instance instTransformTy : MLIR.AST.TransformTy Comb 0 where
-  mkTy := mkTy
-
-def source : Expr (Comb) Γ .pure (.tokenstream) :=
-  Expr.mk
-    (op := .source)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .nil)
-    (regArgs := .nil)
-
-def sink {Γ : Ctxt _} (a : Γ.Var (.tokenstream)) : Expr (Comb) Γ .pure (.tokenstream) :=
-  Expr.mk
-    (op := .sink)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def unpack {r} {Γ : Ctxt _} (a : Γ.Var (.valuestream r)) : Expr (Comb) Γ .pure (.valuetokenstream r) :=
-  Expr.mk
-    (op := .unpack r)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def pack {r} {Γ : Ctxt _} (a : Γ.Var (.valuestream r)) (b : Γ.Var (.tokenstream)) : Expr (Comb) Γ .pure (.valuestream r) :=
-  Expr.mk
-    (op := .pack r)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .cons b <| .nil)
-    (regArgs := .nil)
-
-def branch {Γ : Ctxt _} (a : Γ.Var (.valuestream .bool)) : Expr (Comb) Γ .pure (.tokenstream2) :=
-  Expr.mk
-    (op := .branch)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def fork (a : Γ.Var (.tokenstream)) : Expr (Comb) Γ .pure (.tokenstream2) :=
-  Expr.mk
-    (op := .fork)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def join {Γ : Ctxt _} (a b : Γ.Var (.tokenstream)) : Expr (Comb) Γ .pure (.tokenstream) :=
-  Expr.mk
-    (op := .join)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .cons b <| .nil)
-    (regArgs := .nil)
-
-def merge {Γ : Ctxt _} (a b : Γ.Var (.tokenstream)) : Expr (Comb) Γ .pure (.valuestream .bool) :=
-  Expr.mk
-    (op := .merge)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .cons b <| .nil)
-    (regArgs := .nil)
-
-def select {Γ : Ctxt _} (a b : Γ.Var (.tokenstream)) (c : Γ.Var (.valuestream .bool)) : Expr (Comb) Γ .pure (.tokenstream) :=
-  Expr.mk
-    (op := .select)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .cons b <| .cons c <| .nil)
-    (regArgs := .nil)
-
-def fst {Γ : Ctxt _} (a : Γ.Var (.tokenstream2)) : Expr (Comb) Γ .pure (.tokenstream)  :=
-  Expr.mk
-    (op := .fst)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def fstVal {r} {Γ : Ctxt _} (a : Γ.Var (.valuetokenstream r))  : Expr (Comb) Γ .pure (.valuestream r)  :=
-  Expr.mk
-    (op := .fstVal r)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def sndVal {r} {Γ : Ctxt _} (a : Γ.Var (.valuetokenstream r))  : Expr (Comb) Γ .pure (.tokenstream)  :=
-  Expr.mk
-    (op := .sndVal r)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def snd {Γ : Ctxt _} (a : Γ.Var (.tokenstream2)) : Expr (Comb) Γ .pure (.tokenstream)  :=
-  Expr.mk
-    (op := .snd)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .nil)
-    (regArgs := .nil)
-
-def pair {r} {Γ : Ctxt _} (a b: Γ.Var (.valuestream r)) : Expr (Comb) Γ .pure (.valuestream2 r)  :=
-  Expr.mk
-    (op := .pair r)
-    (ty_eq := rfl)
-    (eff_le := by constructor)
-    (args := .cons a <| .cons b <| .nil)
-    (regArgs := .nil)
-
-def mkExpr (Γ : Ctxt _) (opStx : MLIR.AST.Op 0) :
-    MLIR.AST.ReaderM (Comb) (Σ eff ty, Expr (Comb) Γ eff ty) := do
-  match opStx.name with
-  | op@"Comb.source" =>
-    if opStx.args.length > 0 then
-      throw <| .generic s!"expected one operand for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-    else
-      return ⟨_, .tokenstream, source⟩
-  | op@"Comb.sink" | op@"Comb.unpack" | op@"Comb.fork" | op@"Comb.branch" | op@"Comb.fst" | op@"Comb.snd" | op@"Comb.fstVal" | op@"Comb.sndVal" =>
-    match opStx.args with
-    | v₁Stx::[] =>
-      let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
-      match ty₁, op with
-      | .tokenstream2, "Comb.fst" => return ⟨_, .tokenstream, fst v₁⟩
-      | .tokenstream2, "Comb.snd"  => return ⟨_, .tokenstream, snd v₁⟩
-      | .valuetokenstream r, "Comb.fstVal" => return ⟨_, .valuestream r, fstVal v₁⟩
-      | .valuetokenstream r, "Comb.sndVal"  => return ⟨_, .tokenstream, sndVal v₁⟩
-      | .tokenstream, "Comb.sink" => return ⟨_, .tokenstream, sink v₁⟩
-      | .valuestream r, "Comb.unpack"  => return ⟨_, .valuetokenstream r, unpack v₁⟩
-      | .tokenstream, "Comb.fork"  => return ⟨_, .tokenstream2, fork v₁⟩
-      | .valuestream .bool, "Comb.branch"  => return ⟨_, .tokenstream2, branch v₁⟩
-      | _, _ => throw <| .generic s!"type mismatch"
-    | _ => throw <| .generic s!"expected one operand for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-  | op@"Comb.merge" | op@"Comb.join" | op@"Comb.pack" | op@"Comb.pair"  =>
-    match opStx.args with
-    | v₁Stx::v₂Stx::[] =>
-      let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
-      let ⟨ty₂, v₂⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₂Stx
-      match ty₁, ty₂, op with
-      | .tokenstream, .tokenstream, "Comb.merge" => return ⟨_, .valuestream .bool, merge v₁ v₂⟩
-      | .valuestream r, .valuestream r', "Comb.pair" =>
-        if h: r = r' then return ⟨_, .valuestream2 r, pair v₁ (by subst r ; exact v₂)⟩
-        else throw <| .generic s!"type mismatch"
-      | .tokenstream, .tokenstream, "Comb.join"  => return ⟨_, .tokenstream, join v₁ v₂⟩
-      | .valuestream r, .tokenstream, "Comb.pack"  => return ⟨_, .valuestream r, pack v₁ v₂⟩
-      | _, _, _ => throw <| .generic s!"type mismatch"
-    | _ => throw <| .generic s!"expected two operands for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
-  | op@"Comb.select" =>
-    match opStx.args with
-    | v₁Stx::v₂Stx::v₃Stx::[] =>
-      let ⟨ty₁, v₁⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₁Stx
-      let ⟨ty₂, v₂⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₂Stx
-      let ⟨ty₃, v₃⟩ ← MLIR.AST.TypedSSAVal.mkVal Γ v₃Stx
-      match ty₁, ty₂, ty₃, op with
-      | .tokenstream, .tokenstream, .valuestream .bool, "Comb.select" => return ⟨_, .tokenstream, select v₁ v₂ v₃⟩
+      | .bv w₁, .bv w₂, .nat, "Comb.icmp" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w, icmp v₁ v₂ v₃⟩
+        else
+          throw <| .generic s!"type mismatch"
+      | .bv w₁, .bv w₂, .bool, "Comb.mux" =>
+        .bv w₁, .bv w₂, .nat, "Comb.icmp" =>
+        if h : w₁ = w₂ then
+          return ⟨_, .bv w, mux v₁ v₂ v₃⟩
+        else
+          throw <| .generic s!"type mismatch"
       | _, _, _, _=> throw <| .generic s!"type mismatch"
     | _ => throw <| .generic s!"expected three operands for `monomial`, found #'{opStx.args.length}' in '{repr opStx.args}'"
   | _ => throw <| .unsupportedOp s!"unsupported operation {repr opStx}"
