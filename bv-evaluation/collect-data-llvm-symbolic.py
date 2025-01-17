078d98dc8f95b36924308fc768ed589d889f8043
luisacicolini <lc985@cam.ac.uk>
2025-01-17 16:38:13 +0000

    chore: update docker file

--- bv-evaluation/collect-data-llvm-symbolic.py
+++ bv-evaluation/collect-data-llvm-symbolic.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os 
 import re
 
-paper_directory = 'for-paper/'
+paper_directory = ''
 benchmark_dir = "../SSA/Projects/InstCombine/tests/proofs/"
 res_dir = "results/InstCombineSymbolic/"
 raw_data_dir = paper_directory + 'raw-data/InstCombineSymbolic/'
--- bv-evaluation/run-tests.sh
+++ bv-evaluation/run-tests.sh
@@ -1,18 +1,20 @@
+#!/usr/bin/bash
+
 # run experiments and collect data
 
-python3 compare-leansat-vs-bitwuzla-llvm.py
-python3 collect-leansat-vs-bitwuzla-llvm.py
+python3 compare-leansat-vs-bitwuzla-llvm.py -j=256
+python3 collect-data-llvm.py
 
-python3 compare-leansat-vs-bitwuzla-llvm-symbolic.py
-python3 collect-leansat-vs-bitwuzla-llvm-symbolic.py
+python3 compare-leansat-vs-bitwuzla-llvm-symbolic.py -j=256
+python3 collect-data-llvm-symbolic.py
 
 python3 compare-leansat-vs-bitwuzla-hdel.py
-python3 collect-leansat-vs-bitwuzla-hdel.py
+python3 collect-data-hdel.py
 
 python3 compare-leansat-vs-bitwuzla-hdel-symbolic.py
-python3 collect-leansat-vs-bitwuzla-hdel-symbolic.py
+python3 collect-data-hdel-symbolic.py
 
 python3 compare-leansat-vs-bitwuzla-alive.py
-python3 collect-leansat-vs-bitwuzla-alive.py
+python3 collect-data-alive.py
 
-cd plots && python3 plot.py
+source venv/bin/activate && cd plots && python3 plot.py all
