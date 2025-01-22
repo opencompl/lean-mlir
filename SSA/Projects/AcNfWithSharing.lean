2f3bbf16199bb37415a5ddcb201c64cafb9db654
Alex Keizer <alex@keizer.dev>
2025-01-22 13:36:10 +0000

    refactor: extract `canonicalizeWithSharing` method

--- SSA/Projects/AcNfWithSharing.lean
+++ SSA/Projects/AcNfWithSharing.lean
@@ -91,45 +91,49 @@ def proveEqualityByAC (u : Level) (ty : Expr) (x y : Expr) : MetaM Expr := do
   AC.rewriteUnnormalizedRefl goal -- invoke `ac_rfl`
   instantiateMVars proof
 
-simproc acNormalizeEqWithSharing (@Eq (BitVec _) (_ + _) (_ + _)) := fun e => do
-  let_expr Eq _ x y := e          | return .continue
-
-  let w ← mkFreshExprMVar (mkConst ``Nat [])          -- `w` is a metavar
-  let instAdd := mkApp (mkConst ``BitVec.instAdd) w   -- instAdd is `@BitVec.instAdd ?w`
-  let bv := mkApp (mkConst ``BitVec) w                -- bv is `BitVec ?w`
-  let instHAdd := mkApp2 (mkConst ``instHAdd [0]) bv instAdd -- instHAdd is `instHAdd.{0} $bv $instAdd`
-
-  let hAdd := mkApp4 (mkConst ``HAdd.hAdd) bv bv bv instHAdd
-
+/--
+Given expressions `lhs, rhs : $ty` and `op : $ty → $ty → $ty`, where `ty : Sort $u`,
+canonicalize any top-level applications of the given associative and commutative operation `op` on
+both the `lhs` and the `rhs` in a way that pro
+-/
+def canonicalizeWithSharing (u : Level) (ty op lhs rhs : Expr) : SimpM Simp.Step := do
   VarStateT.run' <| do
-    /- Now we have access to the `VarState` state, and can modify it!
-        Note that VarStateT.run' gives an initial state as the empty hashmap -/
-    let xCoe ← computeCoefficients hAdd x
-    -- `let x ← mx` for some `mx : VarStateT`, means:
-    -- `let (x, newState) := mx (currentState)`
+    let lCoe ← computeCoefficients op lhs
+    let rCoe ← computeCoefficients op rhs
+    -- FIXME: we should identify any neutral/identity elements of the operation
+    --        (e.g., 0 for addition, or 1 for multiplication), and remove the
+    --        corresponding coefficient
 
-    let yCoe ← computeCoefficients hAdd y
-
-    let ⟨commonCoe, xCoe, yCoe⟩ := SharedCoefficients.compute xCoe yCoe
+    let ⟨commonCoe, xCoe, yCoe⟩ := SharedCoefficients.compute lCoe rCoe
 
     let commonExpr : Expr ← commonCoe.toExpr
-    let xNew : Expr ← xCoe.toExpr
-    let xNew := mkApp2 hAdd commonExpr xNew
-    let yNew : Expr ← yCoe.toExpr
-    let yNew := mkApp2 hAdd commonExpr yNew
+    let lNew : Expr ← xCoe.toExpr
+    let lNew := mkApp2 op commonExpr lNew
+    let rNew : Expr ← yCoe.toExpr
+    let rNew := mkApp2 op commonExpr rNew
 
-    let xEq : Expr /- of type `$x = $xNew` -/ ← proveEqualityByAC 1 bv x y
-    let yEq : Expr /- of type `$y = $yNew` -/ ← proveEqualityByAC 1 bv x y
+    let lEq : Expr /- of type `$lhs = $lNew` -/ ← proveEqualityByAC 1 ty lhs lNew
+    let rEq : Expr /- of type `$rhs = $rNew` -/ ← proveEqualityByAC 1 ty rhs rNew
 
-    let expr : Expr /- `$xNew = $yNew` -/ :=
-      -- @Eq (BitVec ?w) _ _
-      mkApp3 (.const ``Eq [1]) bv xNew yNew
+    let expr : Expr /- `$xNew = $yNew` -/ := -- @Eq (BitVec ?w) _ _
+      mkApp3 (.const ``Eq [u]) ty lNew rNew
 
     let proof : Expr /- of type `($x = $y) = ($xNew = $yNew)` -/ :=
-      mkAppN (mkConst ``Grind.eq_congr [1])
-        #[bv, x, y, xNew, yNew, xEq, yEq]
+      mkAppN (mkConst ``Grind.eq_congr [u])
+        #[ty, lhs, rhs, lNew, rNew, lEq, rEq]
 
     return Simp.Step.continue <| some {
       expr := expr
       proof? := some proof
     }
+
+simproc acNormalizeEqWithSharing (@Eq (BitVec _) (_ + _) (_ + _)) := fun e => do
+  let_expr Eq _ lhs rhs := e          | return .continue
+
+  let w ← mkFreshExprMVar (mkConst ``Nat [])          -- `w` is a metavar
+  let instAdd := mkApp (mkConst ``BitVec.instAdd) w   -- instAdd is `@BitVec.instAdd ?w`
+  let bv := mkApp (mkConst ``BitVec) w                -- bv is `BitVec ?w`
+  let instHAdd := mkApp2 (mkConst ``instHAdd [0]) bv instAdd -- instHAdd is `instHAdd.{0} $bv $instAdd`
+  let op := mkApp4 (mkConst ``HAdd.hAdd) bv bv bv instHAdd
+
+  canonicalizeWithSharing 1 bv op lhs rhs
