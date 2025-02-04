e237e09a2cf33370966121e008c92e9c305eafb7
Siddharth Bhat <siddu.druid@gmail.com>
2025-02-04 09:58:08 +0000

    feat: run.sh

--- SSA/Projects/InstCombine/scripts/run.sh
+++ SSA/Projects/InstCombine/scripts/run.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
+set -e
+set -o xtrace
 cd ../../../..
 
-python3 SA/Projects/InstCombine/scripts/test-gen.py
-
-python3 SSA/Projects/InstCombine/scripts/proof-gen.py
-
-python3 SSA/Projects/InstCombine/scripts/read-logs.py
+python3 SSA/Projects/InstCombine/scripts/test-gen.py
+# python3 SSA/Projects/InstCombine/scripts/proof-gen.py
+# python3 SSA/Projects/InstCombine/scripts/read-logs.py
