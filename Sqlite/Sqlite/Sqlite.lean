dd20668fe0b983aba99501c2205bb2a7c1570c1c
Siddharth Bhat <siddu.druid@gmail.com>
2025-02-04 09:27:19 +0000

    chore: add sqlite solver

--- Sqlite/Sqlite/Sqlite.lean
+++ Sqlite/Sqlite/Sqlite.lean
@@ -1,3 +1,4 @@
+-- https://chatgpt.com/share/67a1dba9-8030-8009-a92f-9d9638c8ae56
 @[extern "my_add"]
 opaque myAdd : UInt32 → UInt32 → UInt32
 
--- Sqlite/c/ffi.c
+++ Sqlite/c/ffi.c
@@ -1,4 +1,13 @@
 #include <lean/lean.h>
+#include "sqlite3.h"
+
+extern sqlite3 *lean_to_sqlite3(uint64_t ptr) {
+  return (sqlite3 *)ptr;
+}
+
+extern uint64_t sqlite3_to_lean(sqlite3 *ptr) {
+  return (uint64_t)ptr;
+}
 
 extern uint32_t my_add(uint32_t a, uint32_t b) {
     return a + b;
@@ -7,4 +16,27 @@ extern uint32_t my_add(uint32_t a, uint32_t b) {
 extern lean_obj_res my_lean_fun() {
     return lean_io_result_mk_ok(lean_box(0));
 }
+
+// int sqlite3_open(const char *filename, sqlite3 **ppDb);
+
+// int sqlite3_close(sqlite3*);
+void sqlite3_close_ffi(uint64_t ptr) {
+  sqlite3_close(lean_to_sqlite3(ptr));
+}
+
+// int sqlite3_prepare_v2(sqlite3 *db, const char *zSql, int nByte, sqlite3_stmt **ppStmt, const char **pzTail);
+// 
+// int sqlite3_bind_text(sqlite3_stmt*, int, const char*, int n, void(*)(void*));
+// 
+// int sqlite3_bind_int(sqlite3_stmt*, int, int);
+// 
+// int sqlite3_step(sqlite3_stmt*);
+// 
+// int sqlite3_finalize(sqlite3_stmt *pStmt);
+// 
+// int sqlite3_exec(sqlite3*, const char *sql, int (*callback)(void*,int,char**,char**), void *, char **errmsg);
+// 
+// const char *sqlite3_errmsg(sqlite3*);
+// 
+// void sqlite3_free(void*);
 
