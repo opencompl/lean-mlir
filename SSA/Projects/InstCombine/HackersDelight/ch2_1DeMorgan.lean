db47002043e4c8b52f13fc3bf384b22750e896cf
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-03-10 09:36:12 +0000

    Undo experimentation code

--- SSA/Projects/InstCombine/HackersDelight/ch2_1DeMorgan.lean
+++ SSA/Projects/InstCombine/HackersDelight/ch2_1DeMorgan.lean
@@ -11,8 +11,8 @@ namespace DeMorgansLawsExtended
 variable {x y z : BitVec 64}
 
 theorem not_and_eq_not_or_not :
-   (x &&& y) = ~~~ x ||| ~~~ y := by
-bv_decide
+    ~~~ (x &&& y) = ~~~ x ||| ~~~ y := by
+  all_goals sorry
 
 theorem not_or_eq_not_and_not :
     ~~~ (x ||| y) = ~~~ x &&& ~~~ y := by
