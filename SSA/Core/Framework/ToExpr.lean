d606f085a941b5638c00dd9b624065970668908c
Alex Keizer <alex@keizer.dev>
2025-03-11 13:48:23 +0000

    feat: generalize `Com.toExprM` to arbitrary "meta morphisms"

--- SSA/Core/Framework/ToExpr.lean
+++ SSA/Core/Framework/ToExpr.lean
@@ -15,11 +15,51 @@ open Qq
 variable {d : Dialect} [DialectSignature d]
   [Lean.ToExpr d.Ty] [Lean.ToExpr d.Op] [DialectToExpr d]
 
-private def effLeToExpr : {eff eff' : EffectKind} → (h : eff ≤ eff') → Q($eff ≤ $eff')
+def effLeToExpr : {eff eff' : EffectKind} → (h : eff ≤ eff') → Q($eff ≤ $eff')
   | .impure, .impure, _ => q(EffectKind.le_refl .impure)
   | .pure, .pure, _     => q(EffectKind.le_refl .pure)
   | .pure, .impure, _   => q(EffectKind.pure_le .impure)
 
+open DialectSignature in
+/--
+A dialect morphism represents a mapping from programs in the source dialect `d`
+into Lean *expressions* representing programs in the target dialect `d'`.
+
+`d` and `d'` could be the same dialect, in which case the morphism represents a
+straightforward `toExpr` transform (see also `DialectMorphism.id`).
+
+Alternatively, `d` could be a meta dialect with, e.g., embedded Lean expressions
+(i.e., anti-quotations). The morphism abstractions allows us to transparently
+emit such embedded anti-quotations when transforming a meta-time object into
+a Lean expression.
+-/
+structure DialectMetaMorphism (d : Dialect) [DialectSignature d] (d' : Q(Dialect)) where
+  /-- Map a source Operation into an expression of a target Operation -/
+  mapOp : d.Op → Q(($d').Op)
+  /-- Map a source Type into an expression of a target Type -/
+  mapTy : d.Ty → Q(($d').Ty)
+  /-- Transport a proof that `effectKind op ≤ eff` to
+      an expression of a proof that `effectKind (mapOp op) ≤ eff`.
+
+      NOTE: the default implementation assumes that `mapOp` preserves the
+            effectKind of mapped operations.
+  -/
+  mapEffLe {op : d.Op} {eff : EffectKind} (h : effectKind op ≤ eff) : Lean.Expr :=
+    effLeToExpr h
+
+open DialectToExpr (toExprDialect)
+/-- The trivial identity morphism, which maps programs in dialect `d` to
+*expressions* of programs in dialect `d`.
+
+That is, this morphism represents a straightforward `toExpr` transformation. -/
+def DialectMetaMorphism.id (d : Dialect) [DialectSignature d]
+    [ToExpr d.Op] [ToExpr d.Ty] [DialectToExpr d] :
+    DialectMetaMorphism d (toExprDialect d) where
+  mapOp := toExpr
+  mapTy := toExpr
+
+
+variable {d'} (f : DialectMetaMorphism d d') in
 mutual
 
 partial def Com.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
@@ -44,18 +84,19 @@ partial def Expr.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
     (sig : Q(DialectSignature $dE))
     : Expr d Γ eff ty → Lean.Expr :=
   let ΓE : Q(Ctxt ($dE).Ty) := Lean.toExpr Γ
-  let tyE : Q(($dE).Ty) := Lean.toExpr ty
+  let tyE : Q(($dE).Ty) := f.mapTy ty
   let effE : Q(EffectKind) := Lean.toExpr eff
   fun
   | ⟨op, _ty_eq, eff_le, args, regArgs⟩ =>
     let Ty := mkApp (mkConst ``Dialect.Ty) dE
+    let op := f.mapOp op
     let ty_eq : Lean.Expr := mkApp2 (.const ``rfl [1]) Ty tyE
-    let eff_le := effLeToExpr eff_le
+    let eff_le := f.mapEffLe eff_le
 
     let regArgs := Regions.toExprAux dE sig regArgs
 
     mkAppN (mkConst ``Expr.mk) #[
-      dE, sig, effE, ΓE, tyE, toExpr op, ty_eq, eff_le, toExpr args, regArgs
+      dE, sig, effE, ΓE, tyE, op, ty_eq, eff_le, toExpr args, regArgs
     ]
 
 partial def Regions.toExprAux {regSig : List (Ctxt d.Ty × d.Ty)}
@@ -80,14 +121,31 @@ end
 
 variable {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
 
-/-- Build a (Lean) `Expr` to represent a LeanMLIR program -/
-def Com.toExprM (com : Com d Γ eff ty) : MetaM Lean.Expr := do
-  let dE := d.toExpr
-  let sig ← synthInstanceQ q(DialectSignature $dE)
-  return com.toExprAux dE sig
+/-!
+## MetaMap helper
+-/
+section Map
 
-/-- Build a (Lean) `Expr` to represent a LeanMLIR expression -/
-def Expr.toExprM (expr : Expr d Γ eff ty) : MetaM Lean.Expr := do
+/-- Map a meta morphism over a LeanMLIR program, to obtain a (Lean) `Expr` representing
+the mapped program. -/
+def Com.metaMap (com : Com d Γ eff ty) (f : DialectMetaMorphism d d') : MetaM Lean.Expr := do
   let dE := d.toExpr
   let sig ← synthInstanceQ q(DialectSignature $dE)
-  return expr.toExprAux dE sig
+  return com.toExprAux f dE sig
+
+/-- Map a meta morphism over a LeanMLIR expression, to obtain a (Lean) `Expr`
+representing the mapped expression. -/
+def Expr.metaMap (expr : Expr d Γ eff ty) (f : DialectMetaMorphism d d') : MetaM Lean.Expr := do
+  let dE := d.toExpr
+  let sig ← synthInstanceQ q(DialectSignature $dE)
+  return expr.toExprAux f dE sig
+
+/-!
+## ToExpr
+-/
+
+/-- Build a (Lean) `Expr` to represent a LeanMLIR program. -/
+def Com.toExprM (com : Com d Γ eff ty) : MetaM Lean.Expr := com.metaMap (.id _)
+
+/-- Build a (Lean) `Expr` to represent a LeanMLIR expression. -/
+def Expr.toExprM (expr : Expr d Γ eff ty) : MetaM Lean.Expr := expr.metaMap (.id _)
