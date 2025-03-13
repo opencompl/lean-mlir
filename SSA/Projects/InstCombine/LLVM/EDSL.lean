36196f3e963ea1df79c598e370eae6292e76d463
Alex Keizer <alex@keizer.dev>
2025-03-13 11:19:48 +0000

    feat: add test to assert LLVM elaborator gives the correct context

--- SSA/Projects/InstCombine/LLVM/EDSL.lean
+++ SSA/Projects/InstCombine/LLVM/EDSL.lean
@@ -325,3 +325,18 @@ macro "deftest" name:ident " := " test_reg:mlir_region : command => do
   `(@[reducible, llvmTest $name] def $(name) : ConcreteCliTest :=
        let code := [llvm()| $test_reg]
        { name := $(quote name.getId), ty := code.ty, context := code.ctxt, code := code, })
+
+
+section Test
+open InstCombine Ty
+
+/-- Assert that the elaborator respects variable ordering correctly -/
+private def variable_order1 : Com LLVM [bitvec 2, bitvec 1]  .pure (bitvec 1) := [llvm()| {
+    ^bb0(%arg1: i1, %arg2 : i2):
+      "llvm.return"(%arg1) : (i1) -> ()
+  }]
+/-- Assert that the elaborator respects variable ordering correctly -/
+private def variable_order2 : Com LLVM [bitvec 2, bitvec 1] .pure (bitvec 2) := [llvm()| {
+    ^bb0(%arg1: i1, %arg2 : i2):
+      "llvm.return"(%arg2) : (i2) -> ()
+  }]
