4c4dc19dec2013066aa7a85752bfd67e265c5ad3
Alex Keizer <alex@keizer.dev>
2025-03-13 12:47:30 +0000

    feat: generalize Com.toExpr to a `metaMap` function that takes in an arbitrary `Op -> Expr` and `Ty -> Expr` mapping
    This helps us define meta-variable instantiation

--- SSA/Core/Framework/ToExpr.lean
+++ SSA/Core/Framework/ToExpr.lean
@@ -9,7 +9,7 @@ import Qq
 Define a `ToExpr` instance for core datastructures
 
 -/
-open Lean (ToExpr toExpr MetaM mkAppN mkConst mkApp mkApp2 mkApp6)
+open Lean (ToExpr toExpr MetaM mkAppN mkConst mkApp mkApp2 mkApp3 mkApp4 mkApp6)
 open Qq
 
 variable {d : Dialect} [DialectSignature d]
@@ -58,32 +58,63 @@ def DialectMetaMorphism.id (d : Dialect) [DialectSignature d]
   mapOp := toExpr
   mapTy := toExpr
 
+/-- Obtain an Lean expression of a list, from a list of Lean expressions. -/
+protected def List.foldToExpr {α : Q(Type)} : List Q($α) → Q(List $α) :=
+  List.foldr (fun a as => q($a :: $as)) q([])
+/-- Obtain an Lean expression of a product, from a product of Lean expressions. -/
+protected def Prod.foldToExpr {α β : Q(Type)} : Q($α) × Q($β) → Q($α × $β) :=
+  Function.uncurry <| mkApp4 (.const ``Prod [1,1]) α β
 
-variable {d'} (f : DialectMetaMorphism d d') in
+namespace DialectMetaMorphism
+
+variable {d'} (f : DialectMetaMorphism d d')
+
+def mapList (Γ : List d.Ty) : Q(List $(d').Ty) := (f.mapTy <$> Γ).foldToExpr
+def mapCtxt : (Γ : Ctxt d.Ty) → Q(Ctxt $(d').Ty) := f.mapList
+
+def mapVar {Γ : Ctxt d.Ty} (v : Γ.Var ty) : Q(Ctxt.Var $(f.mapCtxt Γ) $(f.mapTy ty)) :=
+  let Ty := q(($d').Ty)
+  let Γ := f.mapCtxt Γ
+  let ty := f.mapTy ty
+  let i := toExpr v.1
+  Ctxt.mkVar Ty Γ ty i none
+
+def mapArgs {Γ : Ctxt d.Ty} {argSig} (args : HVector (Γ.Var) argSig) :
+    Lean.Expr :=
+  let Ty : Q(Type) := q(($d').Ty)
+  let Γ := f.mapCtxt Γ
+  let A : Q($Ty → Type) := q(($Γ).Var)
+  let nil : (ts : Q(List $Ty)) × Q(HVector $A $ts) := ⟨q([]), q(.nil)⟩
+  let res := (args.foldr · nil) <| fun t v ⟨ts, xs⟩ =>
+    let t := f.mapTy t
+    let v := Ctxt.mkVar Ty Γ t (toExpr v.1) none
+    ⟨q($t :: $ts), q($v ::ₕ $xs)⟩
+  res.snd
+
+end DialectMetaMorphism
+
+variable {d'} (f : DialectMetaMorphism d d')
+  (dE : Q(Dialect)) (sig : Q(DialectSignature $dE)) in
 mutual
 
 partial def Com.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
-    (dE : Q(Dialect) := d.toExpr)
-    (sig : Q(DialectSignature $dE))
     (com : Com d Γ eff ty) : Lean.Expr :=
-  let ΓE : Q(Ctxt ($dE).Ty) := Lean.toExpr Γ
-  let tyE : Q(($dE).Ty) := Lean.toExpr ty
+  let ΓE : Q(Ctxt ($dE).Ty) := f.mapCtxt Γ
+  let tyE : Q(($dE).Ty) := f.mapTy ty
   let effE : Q(EffectKind) := Lean.toExpr eff
   match com with
   | .ret v =>
-    let v := toExpr v
+    let v := f.mapVar v
     mkAppN (mkConst ``Com.ret) #[dE, sig, ΓE, tyE, effE, v]
   | .var (α := eTy) e body =>
-    let eTyE := toExpr eTy
-    let e := e.toExprAux dE sig
-    let body := body.toExprAux dE sig
+    let eTyE := f.mapTy eTy
+    let e := e.toExprAux
+    let body := body.toExprAux
     mkAppN (mkConst ``Com.var) #[dE, sig, ΓE, effE, eTyE, tyE, e, body]
 
-partial def Expr.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
-    (dE : Q(Dialect) := d.toExpr)
-    (sig : Q(DialectSignature $dE))
-    : Expr d Γ eff ty → Lean.Expr :=
-  let ΓE : Q(Ctxt ($dE).Ty) := Lean.toExpr Γ
+partial def Expr.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty} :
+    Expr d Γ eff ty → Lean.Expr :=
+  let ΓE : Q(Ctxt ($dE).Ty) := f.mapCtxt Γ
   let tyE : Q(($dE).Ty) := f.mapTy ty
   let effE : Q(EffectKind) := Lean.toExpr eff
   fun
@@ -93,15 +124,14 @@ partial def Expr.toExprAux {Γ : Ctxt d.Ty} {eff : EffectKind} {ty : d.Ty}
     let ty_eq : Lean.Expr := mkApp2 (.const ``rfl [1]) Ty tyE
     let eff_le := f.mapEffLe eff_le
 
-    let regArgs := Regions.toExprAux dE sig regArgs
+    let args := f.mapArgs args
+    let regArgs := Regions.toExprAux regArgs
 
     mkAppN (mkConst ``Expr.mk) #[
-      dE, sig, effE, ΓE, tyE, op, ty_eq, eff_le, toExpr args, regArgs
+      dE, sig, effE, ΓE, tyE, op, ty_eq, eff_le, args, regArgs
     ]
 
 partial def Regions.toExprAux {regSig : List (Ctxt d.Ty × d.Ty)}
-    (dE : Q(Dialect) := d.toExpr)
-    (sig : Q(DialectSignature $dE))
     (regs : HVector (fun (t : _ × _) => Com d t.1 EffectKind.impure t.2) regSig) :
     Lean.Expr :=
   let α := q(Ctxt ($dE).Ty × ($dE).Ty)
@@ -111,10 +141,12 @@ partial def Regions.toExprAux {regSig : List (Ctxt d.Ty × d.Ty)}
   | [], .nil =>
     mkApp2 (.const ``HVector.nil [0,0]) α A
   | a::as, .cons x xs =>
-    let a := Lean.toExpr a
-    let as := Lean.toExpr as
-    let x := x.toExprAux dE sig
-    let xs := Regions.toExprAux dE sig xs
+    let pToExpr (a) := Prod.foldToExpr <| a.map f.mapCtxt f.mapTy
+    let a := pToExpr a
+    let as := (as.map pToExpr).foldToExpr
+
+    let x := x.toExprAux
+    let xs := Regions.toExprAux xs
     mkApp6 (.const ``HVector.cons [0,0]) α A as a x xs
 
 end
@@ -129,16 +161,14 @@ section Map
 /-- Map a meta morphism over a LeanMLIR program, to obtain a (Lean) `Expr` representing
 the mapped program. -/
 def Com.metaMap (com : Com d Γ eff ty) (f : DialectMetaMorphism d d') : MetaM Lean.Expr := do
-  let dE := d.toExpr
-  let sig ← synthInstanceQ q(DialectSignature $dE)
-  return com.toExprAux f dE sig
+  let sig ← synthInstanceQ q(DialectSignature $d')
+  return com.toExprAux f d' sig
 
 /-- Map a meta morphism over a LeanMLIR expression, to obtain a (Lean) `Expr`
 representing the mapped expression. -/
 def Expr.metaMap (expr : Expr d Γ eff ty) (f : DialectMetaMorphism d d') : MetaM Lean.Expr := do
-  let dE := d.toExpr
-  let sig ← synthInstanceQ q(DialectSignature $dE)
-  return expr.toExprAux f dE sig
+  let sig ← synthInstanceQ q(DialectSignature $d')
+  return expr.toExprAux f d' sig
 
 /-!
 ## ToExpr
