506014d38899dca619918d26e16898f5edfaaeb2
Tobias Grosser <tobias@grosser.es>
2025-03-15 09:47:27 +0000

    Fix

--- SSA/Projects/Scf/ScfFunctor.lean
+++ SSA/Projects/Scf/ScfFunctor.lean
@@ -1,7 +1,6 @@
 /-
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
-import Mathlib.Algebra.GroupPower.IterateHom
 import Mathlib.Logic.Function.Iterate
 import Mathlib.Tactic.Linarith
 import SSA.Core.Framework
