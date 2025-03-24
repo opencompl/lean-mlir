02347a232984ce61d4469ae82f1f8b014d2cfef3
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-03-24 15:04:16 +0000

    Reconstruct counterexamples

--- SSA/Experimental/Bits/Fast/ExistsForAll.lean
+++ SSA/Experimental/Bits/Fast/ExistsForAll.lean
@@ -7,6 +7,7 @@ import Lean.Meta.ForEachExpr
 import Lean.Meta.Tactic.Simp.BuiltinSimprocs.BitVec
 import Lean
 
+open Lean
 open Std.Sat
 open Std.Tactic.BVDecide
 
@@ -28,6 +29,47 @@ theorem verifyCert_correct : ∀ cnf cert, verifyCert cnf cert = true → cnf.Un
     assumption
   . contradiction
 
+
+def reconstructCounterExample' (var2Cnf : Std.HashMap BVBit Nat) (assignment : Array (Bool × Nat))
+    (aigSize : Nat)  :
+    Std.HashMap Nat BVExpr.PackedBitVec := Id.run do
+  let mut sparseMap : Std.HashMap Nat (RBMap Nat Bool Ord.compare) := {}
+
+  for (bitVar, cnfVar) in var2Cnf.toArray do
+    /-
+    The setup of the variables in CNF is as follows:
+    1. One auxiliary variable for each node in the AIG
+    2. The actual BitVec bitwise variables
+    Hence we access the assignment array offset by the AIG size to obtain the value for a BitVec bit.
+    We assume that a variable can be found at its index as CaDiCal prints them in order.
+
+    Note that cadical will report an assignment for all literals up to the maximum literal from the
+    CNF. So even if variable or AIG bits below the maximum literal did not occur in the CNF they
+    will still occur in the assignment that cadical reports.
+
+    There is one crucial thing to consider in addition: If the highest literal that ended up in the
+    CNF does not represent the highest variable bit not all variable bits show up in the assignment.
+    For this situation we do the same as cadical for literals that did not show up in the CNF:
+    set them to true.
+    -/
+    let idx := cnfVar + aigSize
+    let varSet := if h : idx < assignment.size then assignment[idx].fst else true
+    let mut bitMap := sparseMap.getD bitVar.var {}
+    bitMap := bitMap.insert bitVar.idx varSet
+    sparseMap := sparseMap.insert bitVar.var bitMap
+
+  let mut finalMap := Std.HashMap.empty
+  for (bitVecVar, bitMap) in sparseMap.toArray do
+    let mut value : Nat := 0
+    let mut currentBit := 0
+    for (bitIdx, bitValue) in bitMap.toList do
+      assert! bitIdx == currentBit
+      if bitValue then
+        value := value ||| (1 <<< currentBit)
+      currentBit := currentBit + 1
+    finalMap := finalMap.insert bitVecVar ⟨BitVec.ofNat currentBit value⟩
+  return finalMap
+
 /--
 Verify that `cert` is an UNSAT proof for the SAT problem obtained by bitblasting `bv`.
 -/
@@ -46,12 +88,7 @@ theorem unsat_of_verifyBVExpr_eq_true (bv : BVLogicalExpr) (c : String)
 
 open Lean Elab Std Sat AIG Tactic BVDecide Frontend
 
-
-def substitute (bvExpr: BVLogicalExpr) (subst: Std.HashMap Expr BVExpr.PackedBitVec) : BVLogicalExpr :=
-  --TODO
-  bvExpr
-
-def solver (bvExpr: BVLogicalExpr) : TermElabM (Option (Array (Bool × Nat))) := do
+def solver (bvExpr: BVLogicalExpr) : TermElabM (Option (Std.HashMap Nat BVExpr.PackedBitVec)) := do
     --TODO: Can this function return a model class like in Python?
     let cfg: BVDecideConfig := {timeout := 10}
     IO.FS.withTempFile fun _ lratFile => do
@@ -82,9 +119,30 @@ def solver (bvExpr: BVLogicalExpr) : TermElabM (Option (Array (Bool × Nat))) :=
         return none
       | .error assignment =>
         logInfo m! "SAT solver found a counter example."
-        -- let equations := reconstructCounterExample map assignment entry.aig.decls.size atomsAssignment --TODO: Extract counterexamples
-        return .some assignment
+        let equations := reconstructCounterExample' map assignment entry.aig.decls.size
+        return .some equations
 
+def bvExpr : BVLogicalExpr :=
+  let x := BVExpr.const (BitVec.ofNat 64 2)
+  let y := BVExpr.const (BitVec.ofNat 64 4)
+  let z := BVExpr.var 0
+  let sum := BVExpr.bin x BVBinOp.add y
+  BoolExpr.literal (BVPred.bin sum BVBinPred.eq z)
+
+
+syntax (name := testExFa) "test_exists_forall" : tactic
+@[tactic testExFa]
+def testExFaImpl : Tactic := fun _ => do
+  let res ← solver bvExpr
+  match res with
+    | none => pure ()
+    | some counterex =>
+        for (id, var) in counterex do
+          logInfo m! "Results: {id}={var.bv}"
+  pure ()
+
+theorem test : False := by
+  test_exists_forall
 
 
 structure Solver where
@@ -92,7 +150,6 @@ structure Solver where
   modelRef : IO.Ref (Std.HashMap Expr BVExpr.PackedBitVec)
 
 namespace Solver
-
 
 def new : IO Solver := do
   let constraints ← IO.mkRef ([] : List BVLogicalExpr)
@@ -116,6 +173,10 @@ def model (s : Solver) : IO (Std.HashMap Expr BVExpr.PackedBitVec) := do
   s.modelRef.get
 
 end Solver
+
+def substitute (bvExpr: BVLogicalExpr) (subst: Std.HashMap Expr BVExpr.PackedBitVec) : BVLogicalExpr :=
+  --TODO
+  bvExpr
 
 structure ExistsForAllConfig where
   expr : BVLogicalExpr
@@ -155,21 +216,3 @@ def existsForAll (cfg: ExistsForAllConfig) : IO (Option (Std.HashMap Expr BVExpr
     helper
 
   helper
-
-
-def bvExpr : BVLogicalExpr :=
-  let x := BVExpr.const (BitVec.ofNat 64 2)
-  let y := BVExpr.const (BitVec.ofNat 64 2)
-  let z := BVExpr.const (BitVec.ofNat 64 4)
-  let sum := BVExpr.bin x BVBinOp.add y
-  BoolExpr.literal (BVPred.bin sum BVBinPred.eq z)
-
-
-syntax (name := testExFa) "test_exists_forall" : tactic
-@[tactic testExFa]
-def testExFaImpl : Tactic := fun _ => do
-  let _ ← solver bvExpr
-  pure ()
-
-theorem test : False := by
-  test_exists_forall
