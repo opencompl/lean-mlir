736a207fb25be72b38322c02ac3da970bb380b49
Alex Keizer <alex@keizer.dev>
2025-03-24 16:19:49 +0000

    WIP: automatically infer the signature info from a defined `DialectSignature` instance.
    Note that this only works when the instance is defined in a particular way, so this still only works well if the signature is defined using `def_signature`, but it means we don't have to bother storing stuff in an environment extension

--- SSA/Core/Framework/Macro.lean
+++ SSA/Core/Framework/Macro.lean
@@ -77,9 +77,11 @@ instance : Coe (TSyntax ``Parser.matchAltsExpr) (TSyntax ``Lean.Parser.Term.matc
 end Parser
 
 
+
 /-!
 ## MetaSignature Datastructures and Environment Extension
 -/
+
 namespace Elab
 open Qq
 open LeanMLIR.Parser
@@ -293,8 +295,6 @@ def getDialectMetaSignatures (dialect : Expr) : MetaM MetaSignatureTree := do
         supported!"
   return sig
 
-
-
 /-
 TODO: Store the signature declared in `def_signature` somewhere, so that we can
 query it in `def_semantics`.
@@ -372,7 +372,11 @@ would not be great.
 
 -/
 
-/-! ## `def_signature` Elaboration  -/
+
+
+/-!
+## `def_signature` Elaboration
+-/
 
 /-
 TODO: automatically open the `Ty` namespace when elaborating the signature.
@@ -390,6 +394,18 @@ private def getMatchAlts (alts : TSyntax ``matchAltsSig) :
   let alts := alts.raw[0].getArgs
   alts.flatMap fun
     | ref@`(matchAltSig| | $[$patss,*]|* => $rhs) =>
+        patss.map fun patterns => {
+            ref := ref,
+            patterns := patterns.getElems,
+            rhs := rhs
+          }
+    | stx => #[⟨.missing, #[], ⟨.missing⟩⟩]
+
+private def getMatchAltsExpr (alts : TSyntax ``LeanMLIR.Parser.matchAltsExpr) :
+    Array (MatchAltView `term) :=
+  let alts := alts.raw[0].getArgs
+  alts.flatMap fun
+    | ref@`(matchAltExpr| | $[$patss,*]|* => $rhs) =>
         patss.map fun patterns => {
             ref := ref,
             patterns := patterns.getElems,
@@ -491,56 +507,210 @@ def _root_.Array.foldMapIdxM {m} [Monad m]
   StateT.run (s := init) <| as.mapIdxM fun n a s => f n s a
 
 open MetaSignatureTree (Key) in
-elab "def_signature" " for " dialect:term (" where ")? alts:matchAltsSig : command =>
+def elabDefSignatureFor (dialect : Term) (alts : TSyntax ``matchAltsSig) : CommandElabM Unit := do
   let msg := (return m!"{exceptEmoji ·} Defining operation signature for {dialect} dialect")
   withTraceNode `LeanMLIR.Elab msg (collapsed := false) <| do
     let alts := getMatchAlts alts
-    let (alts, (sigTree : MetaSignatureTree)) ← runTermElabM <| fun _ => do
-      let dialectExpr : Q(Dialect) ← elabTermEnsuringType dialect q(Dialect)
-      let Op := q(Dialect.Op $dialectExpr)
-      let Ty := q(Dialect.Ty $dialectExpr)
-      alts.foldMapIdxM (init := .empty) fun priority state view => do
+    let alts ← do
+      alts.mapIdxM fun priority view => do
         trace[LeanMLIR.Elab] m!"Parsing match alternative with\n\
           \tpatterns: {(view.patterns : Array _)}\n\
           \tsignature: {view.rhs}"
         -- HACK: The `(_ : Array _)` type ascription above is load-bearing.
         --       Without it, we get a type mismatch error.
 
-        let key : Key ← do
-          let op : Term := view.patterns[0]!
-          let op : Expr ← elabTermEnsuringType op Op
-          Key.ofExpr op
         let sigView ← parseSignature view.rhs
-        let signature ← MetaSignature.ofView Ty sigView priority
-        return ({view with
+        return {view with
                   rhs := ← sigView.toTerm
-                },
-                state.insert key signature)
+                }
+    -- let (alts, (sigTree : MetaSignatureTree)) ← runTermElabM <| fun _ => do
+    --   let dialectExpr : Q(Dialect) ← elabTermEnsuringType dialect q(Dialect)
+    --   let Op := q(Dialect.Op $dialectExpr)
+    --   let Ty := q(Dialect.Ty $dialectExpr)
+    --   alts.foldMapIdxM (init := .empty) fun priority state view => do
+    --     trace[LeanMLIR.Elab] m!"Parsing match alternative with\n\
+    --       \tpatterns: {(view.patterns : Array _)}\n\
+    --       \tsignature: {view.rhs}"
+    --     -- HACK: The `(_ : Array _)` type ascription above is load-bearing.
+    --     --       Without it, we get a type mismatch error.
+
+    --     -- let key : Key ← do
+    --     --   let op : Term := view.patterns[0]!
+    --     --   let op : Expr ← elabTermEnsuringType op Op
+    --     --   Key.ofExpr op
+    --     let sigView ← parseSignature view.rhs
+    --     return ({view with
+    --               rhs := ← sigView.toTerm
+    --             },
+    --             state.insert key signature)
 
     -- Define instance
     let matchAlts ← Elab.mkMatchAltsExpr alts
-    elabCommand <|← `(command|
+    let stx ← `(command|
       instance : DialectSignature $dialect where
         signature := fun op => match op with $matchAlts:matchAlts
     )
+    trace[LeanMLIR.Elab] "Elaborating:\n{stx}"
+    elabCommand stx
     -- Save signature
-    runTermElabM <| fun  _ => do
-      let dialectExpr ← elabTermEnsuringType dialect q(Dialect)
-      registerDialectMetaSignatures dialectExpr sigTree
+    -- runTermElabM <| fun  _ => do
+    --   let dialectExpr ← elabTermEnsuringType dialect q(Dialect)
+    --   registerDialectMetaSignatures dialectExpr sigTree
     return ()
 
-/-! ## `def_semantics`-/
+/--
+  `def_signature for FooDialect where ...` defines an instance of
+    `DialectSignature FooDialect` via an easier to read syntax.
+
+  `def_signature for FooDialect, BarDialect, ... where ...` defines signatures
+    for both `FooDialect` and `BarDialect`. Of course, the definition has to
+    constitute a valid signature for both dialects, so this is generally only
+    applicable if one dialect is a specialization or alias of the other.
+
+  For example, `PaperExamples.lean` defines a signature as follows:
+  ```lean
+  def_signature for SimpleReg
+    | .const _    => () → .int
+    | .add        => (.int, .int) → .int
+    | .iterate _  => { (.int) → .int } → (.int) -[.pure]-> .int
+  ```
+-/
+elab "def_signature" " for " dialects:term,* (" where ")? alts:matchAltsSig : command =>
+  for dialect in dialects.getElems do
+    elabDefSignatureFor dialect alts
+
+/-!
+## `def_semantics`
+-/
 open Parser (matchAltsExpr)
 open Qq
 
+structure MetaSignatureInfo where
+  numRegions : Nat
+  numArguments : Option Nat
+  cleanExpectedType : Term
+
+structure MetaSignatures where
+  map : Array (TermElabM Expr × MetaSignatureInfo)
+
+instance : ToMessageData MetaSignatureInfo where
+  toMessageData sig :=
+    m!"\{ numRegions := {sig.numRegions},\n  \
+         numArguments := {sig.numArguments},\n  \
+         cleanExpectedType := {sig.cleanExpectedType}}"
+
+def MetaSignature.fromInstance (dialect : Expr) : TermElabM MetaSignatures := do
+  let dialect : Q(Dialect) := dialect
+  let instType := q(DialectSignature $dialect)
+  let origInst ← synthInstance instType
+  trace[LeanMLIR.Elab] "Found instance: {origInst}"
+  let inst ← whnf origInst -- ensure we get the definition
+  let ⟨instMVars, _, inst⟩ ← lambdaMetaTelescope inst -- ignore any lambdas
+  let_expr DialectSignature.mk _d sig := inst
+    | throwError m!"\
+        Synthesised the following instance of {instType}:\n
+        \t{origInst}\n
+        Expected the definition to be an application of \
+        {Expr.const ``DialectSignature.mk []},found:\n\
+        \t{inst}"
+  /-
+  TODO: think about the corner case with a single trivial match arm:
+  ```
+  def_signature for MyCoolDialect where
+    | _ => () -> .int
+  ```
+
+  In this case, the elaborator notices that no match is necessary, hence the
+  following code will not work.
+  -/
+
+  let ⟨_, _, sig⟩ ← lambdaMetaTelescope sig -- ignore any lambdas
+
+  -- TODO: We ought to check here that `sig` is actually a `match _ with ...`
+  --       expression, and throw an error if not.
+
+  /- `match foo with ...` gets elaborated into an application of a
+      `$ident.match_$n` function: the following gets us that function. -/
+  let matchFun := sig.getAppFn
+  /- If the instance definition has variables, those get added to the match
+      definition as well. The following normalizes this, so that the type of
+      `matchFun` should be akin to:
+      ```
+        (motive : $Op → Type) → (op : $Op) → ...
+      ``` -/
+  let instArgs := origInst.getAppArgs ++ instMVars
+  let matchFun := mkAppN matchFun instArgs
+  let matchType ← inferType matchFun
+  trace[LeanMLIR.Elab] "Match function: {matchFun}"
+  forallTelescope matchType fun matchArgs _resultType => do
+    let matchArgs := matchArgs.drop 2 -- ignore `motive` and `op`
+    trace[LeanMLIR.Elab] "Match arguments:\n{matchArgs}"
+    let map ← matchArgs.mapM fun matchArg => do
+      let ty ← inferType matchArg
+      let pattern := do
+        let ⟨_, _, ty⟩ ← forallMetaTelescope ty
+        let (.app _motive ty) := ty
+          | throwError "Expected an application of a motive, found:\n\t{ty}"
+        pure ty
+      let signature ← `(term| _root_.Nat)
+      return (pattern, {
+        numRegions := 0
+        numArguments := some 0
+        cleanExpectedType := signature
+      })
+    return ⟨map⟩
+
+def MetaSignatures.find (self : MetaSignatures) (op : Expr) : TermElabM (Option MetaSignatureInfo) := do
+  let op ← instantiateMVars op
+  let msg := (pure m!"{exceptEmoji ·} Searching signature for {op}")
+  withTraceNode `LeanMLIR.Elab msg <| do
+    if op.isMVar then
+      return none
+
+    for entry in self.map do
+      let pattern ← entry.1
+      if ←isDefEq pattern op then
+        trace[LeanMLIR.Elab] "Found signature for {op}:\n\t{entry.2}"
+        return entry.2
+
+    logWarning "No signature found for operation {op}"
+    return none
+
+def MetaSignatures.findStx (self : MetaSignatures) (Op : Expr) (opPattern : Term) :
+    TermElabM (Option MetaSignatureInfo) :=
+  let msg := (pure m!"{exceptEmoji ·} Searching signature for {opPattern} (syntax)")
+  withTraceNode `LeanMLIR.Elab msg <| do
+    let patternVars ← getPatternVars opPattern
+    let patternVars := getPatternVarNames patternVars
+    trace[LeanMLIR.Elab] "pattern variables: {patternVars}"
+    let patternVars := patternVars.map fun name =>
+      (name, fun _ => (mkFreshExprMVar none : TermElabM _))
+    withLocalDeclsD patternVars <| fun _ => do
+      let expr ← elabTerm opPattern Op
+      self.find expr
 
 elab "def_semantics" " for " dialect:term (" where ")? alts:matchAltsExpr : command => do
-  let dialectExpr ← withRef dialect <| runTermElabM <| fun _ => do
+  let alts ← runTermElabM <| fun _ => do
     let dialectExpr : Q(Dialect) ← elabTermEnsuringType dialect q(Dialect)
-    let _ ← synthInstance q(TyDenote (Dialect.Ty $dialectExpr))
-    return dialectExpr
 
-  elabCommand <|← `(command|
+    -- Get signature info
+    let sigMap ← withRef dialect <|
+      MetaSignature.fromInstance dialectExpr
+    let findSig := sigMap.findStx q(Dialect.Op $dialectExpr)
+
+    -- Add function type annotations
+    let matchAlts := getMatchAltsExpr alts
+    let matchAlts ← matchAlts.mapM fun altView => withRef altView.ref <| do
+      let pattern := altView.patterns[0]!
+      match ← findSig pattern with
+      | none     => return altView
+      | some sig => do return { altView with
+          rhs := ←`( ($altView.rhs : $sig.cleanExpectedType) )
+        }
+    -- Re-assemble match alternatives
+    Elab.mkMatchAltsExpr matchAlts
+
+  elabCommand <|← `(command| -- Declare instance
     instance : DialectDenote $dialect where
-      signature := fun op => match op with $alts:matchAlts
+      denote := fun op => match op with $alts:matchAlts
   )
