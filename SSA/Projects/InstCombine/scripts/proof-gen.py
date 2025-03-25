8f3afa9024837490b920ba6158c46dbb7014a0ea
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-03-25 20:48:32 +0000

    chore: redundant change

--- SSA/Projects/InstCombine/scripts/proof-gen.py
+++ SSA/Projects/InstCombine/scripts/proof-gen.py
@@ -42,7 +42,6 @@ set_option linter.unreachableTactic false
 set_option maxHeartbeats 5000000
 set_option maxRecDepth 1000000
 set_option Elab.async false
-set_option Elab.async false
 
 section {stem}_proof
 """
