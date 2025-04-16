df562a7ae8233559287f0aec646e79e4c9081bac
Alex Keizer <alex@keizer.dev>
2025-04-16 18:14:44 +0100

    refactor: rename refinement class to (`H`)`Refinement`, rename prop to `IsRefinedBy`

--- SSA/Core/Framework/Refinement.lean
+++ SSA/Core/Framework/Refinement.lean
@@ -17,10 +17,10 @@ The notation typeclass for heterogenous refinement relations.
 This enables the notation `a ⊑ b`, where `a : α` and `b : β`.
 
 NOTE: This typeclass is not intended for dialect implementors. Please implement
-`DialectRefines` instead, from which appropriate `HIsRefinedBy` instances will
+`DialectRefines` instead, from which appropriate `HRefinement` instances will
 be inferred.
 -/
-class HIsRefinedBy (α β : Type) where
+class HRefinement (α β : Type) where
   /--
   We say that `a` is refined by `b`, written as `a ⊑ b`, when
   every observable behaviour of `b` is allowed by `a`.
@@ -28,37 +28,37 @@ class HIsRefinedBy (α β : Type) where
   Note that this notation is driven by a typeclass, thus the exact meaning
   is type-dependent.
   -/
-  isRefinedBy : α → β → Prop
+  IsRefinedBy : α → β → Prop
 
-@[inherit_doc] infix:50 " ⊑ "  => HIsRefinedBy.isRefinedBy
+@[inherit_doc] infix:50 " ⊑ "  => HRefinement.IsRefinedBy
 
 /--
-The homogenous version of `HIsRefinedBy`.
+The homogenous version of `HRefinement`.
 This enables the notation `a ⊑ b`, where `a, b : α`.
 
 NOTE: This typeclass is not intended for dialect implementors. Please implement
 `DialectRefines` instead.
 -/
-class IsRefinedBy (α : Type) where
-  isRefinedBy : α → α → Prop
+class Refinement (α : Type) where
+  IsRefinedBy : α → α → Prop
 
-instance [IsRefinedBy α] : HIsRefinedBy α α where
-  isRefinedBy := IsRefinedBy.isRefinedBy
+instance [Refinement α] : HRefinement α α where
+  IsRefinedBy := Refinement.IsRefinedBy
 
 /-
-TODO: Do we actually need `IsRefinedBy`?
-We don't expect users to derive instances of `HIsRefinedBy` themselves to begin
+TODO: Do we actually need `Refinement`?
+We don't expect users to derive instances of `HRefinement` themselves to begin
 with: we expect them to use `DialectRefines` (which might be heterogeneous or homogeneous).
 Then, we--the framework authors--write all the various notations derived from the
 `DialectRefines` instance.
 
-For now, we've used the homogeneous `IsRefinedBy` to define refinement of `Expr`s
+For now, we've used the homogeneous `Refinement` to define refinement of `Expr`s
 and `Coms` in a single dialect.
 
 However, in future we'll likely want to generalize this instance to refinement of
 `Com`s and `Expr`s accross two dialects (the definition of which could then be
 specialized to the case where `d₁ = d₂` to recover the homogenous version).
-It's unclear whether we'll still need `IsRefinedBy` then.
+It's unclear whether we'll still need `Refinement` then.
 
 Then again, the homogenous refinement relation will likely involve quantification
 over *two* valuations, plus an assumption of some form of refinement between
@@ -94,7 +94,7 @@ class DialectHRefines (d : Dialect) (d' : Dialect) [TyDenote d.Ty] [TyDenote d'.
   -/
   IsTypeCompatible : d.Ty → d'.Ty → Prop
   -- TODO: the following ought to be `IsRefinedBy` by the naming convention.
-  isRefinedBy {t u} : IsTypeCompatible t u → d.m ⟦t⟧ → d'.m ⟦u⟧ → Prop
+  IsRefinedBy {t u} : IsTypeCompatible t u → d.m ⟦t⟧ → d'.m ⟦u⟧ → Prop
 open DialectHRefines
 
 /-!
@@ -105,7 +105,7 @@ makes sense to ask whether `x` is refined by `y` (`x ⊑ y`) when `x` and `y` ar
 of the same type (i.e., `x, y : ⟦t⟧` for the same `t : d.Ty`; ignoring monadic
 effects for now). Under this assumption, it wouldn't make sense to ask whether
 `x` is refined by `y` when `y` is a different type, so we would simply define
-only instances of `IsRefinedBy ⟦t⟧`, making it impossible to even write
+only instances of `Refinement ⟦t⟧`, making it impossible to even write
 `x ⊑ y` in the latter case.
 
 Of course, there is no simple notion of "the same type" when working accross
@@ -153,21 +153,21 @@ variable {d d'} [TyDenote d.Ty] [TyDenote d'.Ty] [DialectHRefines d d']
 variable {t : d.Ty} {u : d'.Ty} [h : Fact (IsTypeCompatible t u)]
 
 /-- Refinement for monadic values -/
-instance instRefinementMonadic : HIsRefinedBy (d.m ⟦t⟧) (d'.m ⟦u⟧) where
-  isRefinedBy := DialectHRefines.isRefinedBy h.out
+instance instRefinementMonadic : HRefinement (d.m ⟦t⟧) (d'.m ⟦u⟧) where
+  IsRefinedBy := DialectHRefines.IsRefinedBy h.out
 
 variable [Pure d.m] [Pure d'.m]
 
 /-- Refinement for pure values -/
-instance instRefinementPure : HIsRefinedBy ⟦t⟧ ⟦u⟧ where
-  isRefinedBy x y := (pure x : d.m _) ⊑ (pure y : d'.m _)
+instance instRefinementPure : HRefinement ⟦t⟧ ⟦u⟧ where
+  IsRefinedBy x y := (pure x : d.m _) ⊑ (pure y : d'.m _)
 
 /-
 HACK: `EffectKind.toMonad` is reducible, which causes some issues.
       Namely, trying to synthesize
-        `HIsRefinedBy (EffectKind.pure.toMonad ..) _`
+        `HRefinement (EffectKind.pure.toMonad ..) _`
       will not find an instance where `eff` is a generic effect:
-        `HIsRefinedBy (eff.toMonad ..) _`
+        `HRefinement (eff.toMonad ..) _`
       Thus, we locally make `toMonad` irreducible.
 TODO: We should consider removing the reducible attribute globally, but I'm a
       bit scared that might break our proof automation (e.g. simp_peephole).
@@ -177,19 +177,19 @@ attribute [local semireducible] EffectKind.toMonad
 
 /-- Refinement for *potentially* monadic values -/
 instance instRefinementEffect {eff eff' : EffectKind} :
-    HIsRefinedBy (eff.toMonad d.m ⟦t⟧) (eff'.toMonad d'.m ⟦u⟧) where
-  isRefinedBy x y := eff.coe_toMonad x ⊑ eff'.coe_toMonad y
+    HRefinement (eff.toMonad d.m ⟦t⟧) (eff'.toMonad d'.m ⟦u⟧) where
+  IsRefinedBy x y := eff.coe_toMonad x ⊑ eff'.coe_toMonad y
 
 section Lemmas
 
 @[simp] theorem toMonad_pure_IsRefinedBy_toMonad_pure_iff
     {x : EffectKind.pure.toMonad d.m ⟦t⟧} {y : EffectKind.pure.toMonad d'.m ⟦u⟧} :
-    (x ⊑ y) ↔ (HIsRefinedBy.isRefinedBy (α := ⟦t⟧) (β := ⟦u⟧) x y) := by
+    (x ⊑ y) ↔ (HRefinement.IsRefinedBy (α := ⟦t⟧) (β := ⟦u⟧) x y) := by
   rfl
 
 @[simp] theorem toMonad_impure_IsRefinedBy_toMonad_impure_iff
     {x : EffectKind.impure.toMonad d.m ⟦t⟧} {y : EffectKind.impure.toMonad d'.m ⟦u⟧} :
-    (x ⊑ y) ↔ (HIsRefinedBy.isRefinedBy (α := d.m ⟦t⟧) (β := d'.m ⟦u⟧) x y) := by
+    (x ⊑ y) ↔ (HRefinement.IsRefinedBy (α := d.m ⟦t⟧) (β := d'.m ⟦u⟧) x y) := by
   rfl
 
 end Lemmas
