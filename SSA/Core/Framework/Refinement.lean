e635185f1ac2d1c3e654897f38a1ff8ef20313de
Alex Keizer <alex@keizer.dev>
2025-04-16 14:51:38 +0100

    chore: write some notes on implementation rationale/choices

--- SSA/Core/Framework/Refinement.lean
+++ SSA/Core/Framework/Refinement.lean
@@ -50,10 +50,23 @@ TODO: Do we actually need `IsRefinedBy`?
 We don't expect users to derive instances of `HIsRefinedBy` themselves to begin
 with: we expect them to use `DialectRefines` (which might be heterogeneous or homogeneous).
 Then, we--the framework authors--write all the various notations derived from the
-`DialectRefines` instance, but we do so generically and thus always heterogenously.
+`DialectRefines` instance.
 
-So far, I've not written a homogenous `IsRefinedBy` instance, and all the further instances
-I can think of right now will be heterogenous also.
+For now, we've used the homogeneous `IsRefinedBy` to define refinement of `Expr`s
+and `Coms` in a single dialect.
+
+However, in future we'll likely want to generalize this instance to refinement of
+`Com`s and `Expr`s accross two dialects (the definition of which could then be
+specialized to the case where `d₁ = d₂` to recover the homogenous version).
+It's unclear whether we'll still need `IsRefinedBy` then.
+
+Then again, the homogenous refinement relation will likely involve quantification
+over *two* valuations, plus an assumption of some form of refinement between
+those valuations. It's very possible that the specialization of this definition
+to the case where the dialects are the same is considerably less nice to work
+with than the current homogenous definition (and it's certainly unlikely to be
+logically equivalent, in general). So, we might want to have separate homo- and
+heterogenous instances (where the former should have a high-priority annotation).
 -/
 
 
@@ -83,6 +96,57 @@ class DialectHRefines (d : Dialect) (d' : Dialect) [TyDenote d.Ty] [TyDenote d'.
   -- TODO: the following ought to be `IsRefinedBy` by the naming convention.
   isRefinedBy {t u} : IsTypeCompatible t u → d.m ⟦t⟧ → d'.m ⟦u⟧ → Prop
 open DialectHRefines
+
+/-!
+**IsTypeCompatible Rationale**:
+
+The above definition has the base assumption that in the homogenous case, it only
+makes sense to ask whether `x` is refined by `y` (`x ⊑ y`) when `x` and `y` are
+of the same type (i.e., `x, y : ⟦t⟧` for the same `t : d.Ty`; ignoring monadic
+effects for now). Under this assumption, it wouldn't make sense to ask whether
+`x` is refined by `y` when `y` is a different type, so we would simply define
+only instances of `IsRefinedBy ⟦t⟧`, making it impossible to even write
+`x ⊑ y` in the latter case.
+
+Of course, there is no simple notion of "the same type" when working accross
+dialects, thus we have to make the refinement instance heterogenous. To still
+make it impossible to write `x ⊑ y` in "nonsensical" cases, we thus introduce
+the `IsTypeCompatible` relation.
+
+However, I now believe this rationale to be potentially flawed in two aspects.
+
+Firstly, it is not the case that we'd only want to compare values of the same type
+even within a single dialect. Consider a hypothetical LLVM+RiscV hybrid dialect
+used as an intermediary step in instruction lowering: this dialect would have both
+LLVM (potentially poisoned) generic-width bitvectors and Risc-V always-concrete,
+fixed-width bitvectors. To phrase instruction lowering as a peephole rewrite in
+the hybrid dialect, we'd have to be able to state the "correctness" of a rewrite
+where the original and rewritten types are not exactly the same.
+
+Of course, adding such capabilities to the rewriter does raise some more questions:
+we'd likely have to incorporate some form of casting from the new type to the original,
+to ensure the rewritten program is still well-typed. We might have to encode the
+meaning of such casts as part of the `DialectHRefines` typeclass, which *might*
+actually be easier with an explicit `IsTypeCompatible` relation, rather than
+implicitly encoding type compatibility as part of the refinement definition.
+It is exactly between compatible types that we should be allowed to implicitly
+insert casts (in either direction). Then again, we could always recover the
+compatibility relation as "types `t` and `u` are compatible if there exist some
+`x : ⟦t⟧` and `y : ⟦u⟧` s.t. `x ⊑ y`".
+
+Secondly, if we forget about any future musings of implicit casts, there is no
+harm in allowing `x ⊑ y` to be stated, but defined to be false, whenever `x` and
+`y` have compatible types. In fact, it would be somewhat simpler if we just bundle
+in the compatibility assumption. If we wanted to state some generic property in
+the current phrasing, we'd have to write:
+```
+∀ {t u} (h : IsTypeCompatible t u) (x : ⟦t⟧) (y : ⟦u⟧), x ⊑ y
+```
+In the bundled version, we could shorten that to simply:
+```
+∀ {t u} (x : ⟦t⟧) (y : ⟦u⟧), x ⊑ y
+```
+-/
 
 namespace DialectHRefines
 variable {d d'} [TyDenote d.Ty] [TyDenote d'.Ty] [DialectHRefines d d']
