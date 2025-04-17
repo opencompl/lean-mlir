ad9018b15a17b8be33655ae14cf7a40d74e4bad0
luisacicolini <luisacicolini@gmail.com>
2025-04-17 09:25:53 +0100

    chore: works and raised the correct error

--- SSA/Projects/CIRCT/Comb/CombExample.lean
+++ SSA/Projects/CIRCT/Comb/CombExample.lean
@@ -42,8 +42,8 @@ def test2 : BitVec 4 :=
 #eval test2
 
 def CombEg3 := [Comb_com| {
-  ^entry(%0: !BitVec_4):
-    %2 = "Comb.icmp_eq" (%0, %0) : (!BitVec_4, !BitVec_4, !IcmpPred) -> !Bool
+  ^entry(%0: !BitVec_4, %1 : !BitVec_4):
+    %2 = "Comb.icmp_slt" (%0, %1) : (!BitVec_4, !BitVec_4) -> !Bool
     "return" (%2) : (!Bool) -> ()
   }]
 
@@ -53,10 +53,28 @@ def CombEg3 := [Comb_com| {
 #check CombEg3.denote
 #print axioms CombEg3
 
-def bv1' : BitVec 4 := BitVec.ofNat 4 5 -- 0010
-def eqOp : CombOp.IcmpPredicate := CombOp.IcmpPredicate.ne
+def bv1' : BitVec 4 := BitVec.ofNat 4 1
+def bv2' : BitVec 4 := BitVec.ofNat 4 6
 
 def test3 : Bool :=
-  CombEg3.denote (Ctxt.Valuation.ofPair eqOp bv1')
+  CombEg3.denote (Ctxt.Valuation.ofPair bv2' bv1')
 
 #eval test3
+
+def CombEg4 := [Comb_com| {
+  ^entry(%0: !BitVec_4, %1 : !BitVec_4):
+    %2 = "Comb.icmp_eq" (%0, %1) : (!BitVec_4, !BitVec_4) -> !Bool
+    "return" (%2) : (!Bool) -> ()
+  }]
+
+#check CombEg4
+#eval CombEg4
+#reduce CombEg4
+#check CombEg4.denote
+#print axioms CombEg4
+
+def bv1'' : BitVec 4 := BitVec.ofNat 4 3
+def bv2'' : BitVec 4 := BitVec.ofNat 4 3
+
+def test4 : Bool :=
+  CombEg4.denote (Ctxt.Valuation.ofPair bv2'' bv1'')
