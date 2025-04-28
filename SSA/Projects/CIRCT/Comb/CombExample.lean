dbc21629e722d42f74594877d4d1437c40ddf4cd
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-04-28 14:22:44 +0100

    feat: add `comb` dialect (#1112)
    This PR adds the [`comb`
    dialect](https://circt.llvm.org/docs/Dialects/Comb/#combadd-circtcombaddop).
    `truthTable` operation and wildcard/cases SV semantics are missing, as
    they will require more infrastructure, e.g. establishing and defining
    4-value logic.
    
    I have no idea what's wrong with the `InstCombine` files, git seems to
    think that there's been some changes but they look identical to me (and
    I did `git restore` the path, no changes)
    
    ---------
    
    Co-authored-by: Yann Herklotz <git@yannherklotz.com>

---
+++ SSA/Projects/CIRCT/Comb/CombExample.lean
@@ -0,0 +1,120 @@
+import SSA.Projects.CIRCT.Comb.Comb
+import SSA.Projects.CIRCT.Stream.Stream
+import SSA.Projects.CIRCT.Stream.WeakBisim
+import SSA.Core.Tactic
+import SSA.Core.ErasedContext
+import SSA.Core.HVector
+import SSA.Core.EffectKind
+import SSA.Core.Util
+
+open MLIR AST in
+
+unseal String.splitOnAux in
+def CombEg1 := [Comb_com| {
+  ^entry(%0: !Bool):
+    "return" (%0) : (!Bool) -> ()
+  }]
+
+#check CombEg1
+#eval CombEg1
+#reduce CombEg1
+#check CombEg1.denote
+#print axioms CombEg1
+
+def CombEg2 := [Comb_com| {
+    ^entry(%0: i4, %1 : i4):
+      %2 = "Comb.add" (%0, %1) : (i4, i4) -> i4
+      "return" (%2) : (i4) -> ()
+    }]
+
+#print CombEg2
+#eval CombEg2
+#reduce CombEg2
+#check CombEg2
+#print axioms CombEg2
+
+def bv1 : BitVec 4 := BitVec.ofNat 4 5 -- 0010
+def bv2 : BitVec 4 := BitVec.ofNat 4 1 -- 0011
+
+def test2 : BitVec 4 :=
+  CombEg2.denote (Ctxt.Valuation.ofPair bv1 bv2)
+
+#eval test2
+
+def CombEg3 := [Comb_com| {
+  ^entry(%0: i4, %1 : i4):
+    %2 = "Comb.icmp_slt" (%0, %1) : (i4, i4) -> !Bool
+    "return" (%2) : (!Bool) -> ()
+  }]
+
+#check CombEg3
+#eval CombEg3
+#reduce CombEg3
+#check CombEg3.denote
+#print axioms CombEg3
+
+def bv1' : BitVec 4 := BitVec.ofNat 4 1
+def bv2' : BitVec 4 := BitVec.ofNat 4 6
+
+def test3 : Bool :=
+  CombEg3.denote (Ctxt.Valuation.ofPair bv2' bv1')
+
+#eval test3
+
+def CombEg4 := [Comb_com| {
+  ^entry(%0: i4, %1 : i4):
+    %2 = "Comb.icmp_eq" (%0, %1) : (i4, i4) -> !Bool
+    "return" (%2) : (!Bool) -> ()
+  }]
+
+#check CombEg4
+#eval CombEg4
+#reduce CombEg4
+#check CombEg4.denote
+#print axioms CombEg4
+
+def bv1'' : BitVec 4 := BitVec.ofNat 4 3
+def bv2'' : BitVec 4 := BitVec.ofNat 4 3
+
+def test4 : Bool :=
+  CombEg4.denote (Ctxt.Valuation.ofPair bv2'' bv1'')
+
+#eval test4
+
+def CombEg5 := [Comb_com| {
+  ^entry(%0: i1, %1: i1):
+    %2 = "Comb.replicate_3" (%0) : (i1) -> (i3)
+    "return" (%2) : (i3) -> ()
+}]
+
+#check CombEg5
+#eval CombEg5
+#reduce CombEg5
+#check CombEg5.denote
+#print axioms CombEg5
+
+def bv0 : BitVec 1 := BitVec.ofNat 1 1
+
+def test5 : BitVec 3 :=
+  CombEg5.denote (Ctxt.Valuation.ofPair bv0 bv0)
+
+#eval test5
+
+def CombEg6 := [Comb_com| {
+    ^entry (%0 : i2, %1 : i4, %2 : !Bool):
+      %3 = "Comb.mux" (%0, %1, %2) : (i2, i4, !Bool) -> !TypeSum_2_4
+      "return" (%3) : (!TypeSum_2_4) -> ()
+}]
+
+#check CombEg6
+#eval CombEg6
+#reduce CombEg6
+#check CombEg6.denote
+#print axioms CombEg6
+
+def bv6₁ : BitVec 2 := BitVec.ofNat 2 1
+def bv6₂ : BitVec 4 := BitVec.ofNat 4 3
+def bv6c : Bool := true
+
+def test6 : BitVec 2 ⊕ BitVec 4 :=
+  CombEg6.denote (Ctxt.Valuation.ofHVector (.cons bv6c <| .cons bv6₂ <| .cons bv6₁ <| .nil))
