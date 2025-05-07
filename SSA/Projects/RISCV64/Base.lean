2c4fbd514cf83c16845bec0ddff873fe90c6d1aa
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-07 22:20:53 +0100

    changed semantics to use def_denote

--- SSA/Projects/RISCV64/Base.lean
+++ SSA/Projects/RISCV64/Base.lean
@@ -1,7 +1,8 @@
 
 import SSA.Projects.RISCV64.Semantics
 import SSA.Core.Framework
-set_option maxHeartbeats 1000000000000000000
+import SSA.Core.Framework.Macro
+set_option maxHeartbeats 100000000
 
 open RV64Semantics
 
@@ -22,8 +23,9 @@ And operations out of selected RISC-V extension:
 
     - `Zicond` : extension for conditional operations https://github.com/riscvarchive/riscv-zicond/blob/main/zicondops.adoc
 -/
+set_option maxHeartbeats 1000000 in -- we need this because .... to do
 inductive Op
-|   li : (val : Int) → Op
+|  li : (val : Int) → Op
 |  lui (imm : BitVec 20)
 |  auipc (imm : BitVec 20)
 |  addi (imm : BitVec 12)
@@ -71,13 +73,13 @@ inductive Op
 | remwu
 | remu
 -- RISC-V `Zba` extension
-|add.uw
-|sh1add.uw
-|sh2add.uw
-|sh3add.uw
-|sh1add
-|sh2add
-|sh3add
+| add.uw
+| sh1add.uw
+| sh2add.uw
+| sh3add.uw
+| sh1add
+| sh2add
+| sh3add
 -- slli.uw missing
 -- part of the RISC-V `Zbb` & `Zbkb` extension
 | rol
@@ -250,7 +252,7 @@ def Op.outTy : Op  → Ty
   |.sraiw (shamt : BitVec 5) => Ty.bv
   |.slli (shamt : BitVec 6) => Ty.bv
   |.srli (shamt : BitVec 6) => Ty.bv
-  |srai (shamt : BitVec 6) => Ty.bv
+  |.srai (shamt : BitVec 6) => Ty.bv -- to do
   |.addw => Ty.bv
   |.subw => Ty.bv
   |.sllw => Ty.bv
@@ -319,15 +321,83 @@ abbrev RV64 : Dialect where
 
 instance : DialectSignature RV64 := ⟨Op.signature⟩
 
-/--
-## Dialect semantics
+def_denote for RV64 where
+  |.li imm =>  BitVec.ofInt 64 imm
+  |.addiw imm => λ rs1 =>  ADDIW_pure64 imm rs1
+  |.lui imm => λ rs1 => UTYPE_pure64_lui imm rs1
+  |.auipc imm => λ rs1 => UTYPE_pure64_AUIPC imm rs1
+  |.slliw shamt => λ rs1 => SHIFTIWOP_pure64_RISCV_SLLIW shamt rs1
+  |.srliw shamt => λ rs1 => SHIFTIWOP_pure64_RISCV_SRLIW shamt rs1
+  |.sraiw shamt => λ rs1 => SHIFTIWOP_pure64_RISCV_SRAIW shamt rs1
+  |.srli shamt => λ rs1 => SHIFTIOP_pure64_RISCV_SRLI shamt rs1
+  |.slli shamt => λ rs1 => SHIFTIOP_pure64_RISCV_SRLI shamt rs1
+  |.srai shamt => λ rs1 => SHIFTIOP_pure64_RISCV_SRAI shamt  rs1
+  |.addw => λ rs1 rs2 => RTYPEW_pure64_RISCV_SUBW rs2  rs1
+  |.subw => λ rs1 rs2 => RTYPEW_pure64_RISCV_SUBW rs2 rs1
+  |.sllw => λ rs1 rs2 => RTYPEW_pure64_RISCV_SLLW rs2 rs1
+  |.srlw => λ rs1 rs2 => RTYPEW_pure64_RISCV_SRLW rs2  rs1
+  |.sraw => λ rs1 rs2 => RTYPEW_pure64_RISCV_SRAW rs2 rs1
+  |.add => λ rs1 rs2 => RTYPE_pure64_RISCV_ADD rs2 rs1
+  |.slt => λ rs1 rs2 => RTYPE_pure64_RISCV_SLT rs2 rs1
+  |.sltu => λ rs1 rs2 => RTYPE_pure64_RISCV_SLTU rs2 rs1
+  |.and => λ rs1 rs2 => RTYPE_pure64_RISCV_AND rs2 rs1
+  |.or => λ rs1 rs2 => RTYPE_pure64_RISCV_OR rs2 rs1
+  |.xor => λ rs1 rs2 => RTYPE_pure64_RISCV_XOR rs2 rs1
+  |.sll => λ rs1 rs2 => RTYPE_pure64_RISCV_SLL rs2 rs1
+  |.srl => λ rs1 rs2 => RTYPE_pure64_RISCV_SRL rs2 rs1
+  |.sub => λ rs1 rs2 => RTYPE_pure64_RISCV_SUB rs2 rs1
+  |.sra => λ rs1 rs2 => RTYPE_pure64_RISCV_SRA rs2 rs1
+  |.remw => λ rs1 rs2 => REMW_pure64_signed rs2 rs1
+  |.remwu => λ rs1 rs2 => REMW_pure64_unsigned rs2 rs1
+  |.rem => λ rs1 rs2 => REM_pure64_signed rs2 rs1
+  |.remu => λ rs1 rs2 => REM_pure64_unsigned rs2 rs1
+  |.mulu => λ rs1 rs2 => MUL_pure64_fff rs2 rs1
+  |.mulhu => λ rs1 rs2 =>  MUL_pure64_tff rs2 rs1
+  |.mul => λ rs1 rs2 => MUL_pure64_ftt rs2 rs1
+  |.mulhsu => λ rs1 rs2 => MUL_pure64_ttf rs2 rs1
+  |.mulh => λ rs1 rs2 => MUL_pure64_ttt rs2 rs1
+  |.mulw => λ rs1 rs2 => MULW_pure64 rs2 rs1
+  |.div => λ rs1 rs2 => DIV_pure64_signed rs2 rs1
+  |.divu => λ rs1 rs2 => DIV_pure64_unsigned rs2 rs1
+  |.divw => λ rs1 rs2 => DIVW_pure64_signed rs2 rs1
+  |.divwu => λ rs1 rs2 => DIVW_pure64_unsigned rs2 rs1
+  |.addi imm => λ rs1 => ITYPE_pure64_RISCV_ADDI  imm rs1
+  |.slti imm => λ rs1 => ITYPE_pure64_RISCV_SLTI  imm rs1
+  |.sltiu imm => λ rs1 => ITYPE_pure64_RISCV_SLTIU  imm rs1
+  |.andi imm => λ rs1 => ITYPE_pure64_RISCV_ANDI  imm rs1
+  |.ori imm => λ rs1 => ITYPE_pure64_RISCV_ORI  imm rs1
+  |.xori imm => λ rs1 => ITYPE_pure64_RISCV_XORI  imm rs1
+  |.czero.eqz => λ rs1 rs2 => ZICOND_RTYPE_pure64_RISCV_CZERO_EQZ rs2 rs1
+  |.czero.nez => λ rs1 rs2 => ZICOND_RTYPE_pure64_RISCV_RISCV_CZERO_NEZ rs2 rs1
+  |.sext.b => λ rs1 => ZBB_EXTOP_pure64_RISCV_SEXTB rs1
+  |.sext.h => λ rs1 => ZBB_EXTOP_pure64_RISCV_SEXTH rs1
+  |.zext.h => λ rs1 => ZBB_EXTOP_pure64_RISCV_ZEXTH rs1
+  |.bclr => λ rs1 rs2 => ZBS_RTYPE_pure64_RISCV_BCLR rs2 rs1
+  |.bext => λ rs1 rs2=> ZBS_RTYPE_pure64_RISCV_BEXT rs2 rs1
+  |.binv => λ rs1 rs2 => ZBS_RTYPE_pure64_BINV rs2 rs1
+  |.bset => λ rs1 rs2=> ZBS_RTYPE_pure64_RISCV_BSET rs2 rs1
+  |.bclri shamt => λ rs1 => ZBS_IOP_pure64_RISCV_BCLRI shamt rs1
+  |.bexti shamt => λ rs1 => ZBS_IOP_pure64_RISCV_BEXTI shamt rs1
+  |.binvi shamt => λ rs1 => ZBS_IOP_pure64_RISCV_BINVI shamt rs1
+  |.bseti shamt  => λ rs1 => ZBS_IOP_pure64_RISCV_BSETI shamt rs1
+  |.rolw => λ rs1 rs2 => ZBB_RTYPEW_pure64_RISCV_ROLW rs2 rs1
+  |.rorw => λ rs1 rs2 => ZBB_RTYPEW_pure64_RISCV_RORW rs2 rs1
+  |.rol => λ rs1 rs2 => ZBB_RTYPE_pure64_RISCV_ROL rs2 rs1
+  |.ror => λ rs1 rs2 => ZBB_RTYPE_pure64_RISCV_ROR rs2 rs1
+  |.add.uw => λ rs1 rs2 => ZBA_RTYPEUW_pure64_RISCV_ADDUW rs2 rs1
+  |.sh1add.uw => λ rs1 rs2 => ZBA_RTYPEUW_pure64_RISCV_SH1ADDUW rs2 rs1
+  |.sh2add.uw => λ rs1 rs2 => ZBA_RTYPEUW_pure64_RISCV_SH2ADDUW rs2 rs1
+  |.sh3add.uw => λ rs1 rs2 => ZBA_RTYPEUW_pure64_RISCV_SH3ADDUW rs2 rs1
+  |.sh1add => λ rs1 rs2 => ZBA_RTYPE_pure64_RISCV_SH1ADD rs2 rs1
+  |.sh2add => λ rs1 rs2 => ZBA_RTYPE_pure64_RISCV_SH2ADD rs2 rs1
+  |.sh3add => λ rs1 rs2 => ZBA_RTYPE_pure64_RISCV_SH3ADD rs2 rs1
 
-We assign semantics defined in `RV64` to each operation. This defines the meaning of each operation in Lean.
 
-When writting RISC-V-like abstract MLIR ISA intutivly we write `op  arg1 arg2`. The `RISCV64` are defined to
-first process the second operand then the first. Therefore we first pass `.get 1` then `.get 0` into the
-functions that define our semantics.
--/
+
+
+
+
+/-
 @[simp, reducible]
 instance : DialectDenote (RV64) where
   denote
@@ -340,7 +410,7 @@ instance : DialectDenote (RV64) where
   |.sraiw shamt, regs, _  => SHIFTIWOP_pure64_RISCV_SRAIW shamt (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.slli shamt, regs, _  => SHIFTIOP_pure64_RISCV_SLLI shamt (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.srli shamt, regs, _  => SHIFTIOP_pure64_RISCV_SRLI shamt (regs.getN 0 (by simp [DialectSignature.sig, signature]))
-  |.srai shamt, regs, _  => SHIFTIOP_pure64_RISCV_SRAI shamt  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
+  |.srai shamt, regs, _  => subw shamt  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.addw, regs, _  =>  RTYPEW_pure64_RISCV_ADDW (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.subw, regs, _  => RTYPEW_pure64_RISCV_SUBW (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.sllw, regs, _  => RTYPEW_pure64_RISCV_SLLW (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
@@ -400,4 +470,5 @@ instance : DialectDenote (RV64) where
   |.sh1add, regs, _  =>  ZBA_RTYPE_pure64_RISCV_SH1ADD (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.sh2add, regs, _  =>  ZBA_RTYPE_pure64_RISCV_SH2ADD (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
   |.sh3add, regs, _  =>  ZBA_RTYPE_pure64_RISCV_SH3ADD (regs.getN 1 (by simp [DialectSignature.sig, signature]))  (regs.getN 0 (by simp [DialectSignature.sig, signature]))
+  -/
 end RISCV64
