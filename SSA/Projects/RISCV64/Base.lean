3a6893b76b5611d7deb914496df702d2d2956986
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-09 16:58:02 +0200

    added comment and links

--- SSA/Projects/RISCV64/Base.lean
+++ SSA/Projects/RISCV64/Base.lean
@@ -1,26 +1,28 @@
 import SSA.Projects.RISCV64.Semantics
 import SSA.Core.Framework
 set_option maxHeartbeats 1000000000000000000
-
+/- needed because are working with large inductive types in Lean.
+As all the inductive in this file are rather large this option was set for the whole file. -/
 open RV64Semantics
 
 namespace RISCV64
 /-! ## The `RISCV64` dialect -/
 
-/-!
-## Dialect operation definitions
-Each instruction operates on up to two registers. Any other attributes not
-passed via registers, e.g., flags and immediates are encoded as part of the op-code.
+/-! ## Dialect operation definitions -/
+/--
+`Op` models the RV64I base instruction set [1] plus selected RISC-V ISA extensions:
+`M` for standard integer division and multiplication [2],
+`B` for bit manipulation instructions (comprises instructions provided by the `Zba`, `Zbb`, and `Zbs` extensions) [3],
+and `Zicond` for conditional operations [4].
 
-We modell the RV64I base instruction set: https://github.com/riscv/riscv-isa-manual/blob/main/src/rv64.adoc,
-therefore allow for an 64-bit address space.
-Additionally, we modell operations out of selected RISC-V ISA extension:
-    - `M`: standart integer division and multiplication instruction extension
+We model RV64 as an SSA IR, meaning that instructions won't specify any registers
+(instead, they'll later receive SSA values).
+However, any other attributes (e.g., flags or immediate values) are still encoded as part of the operation.
 
-    - `B`: extension for bit manipulation (comprises instructions provided by the `Zba`, `Zbb`, and `Zbs` extensions)
-            here: https://github.com/riscv/riscv-isa-manual/blob/main/src/b-st-ext.adoc
-
-    - `Zicond` : extension for conditional operations https://github.com/riscvarchive/riscv-zicond/blob/main/zicondops.adoc
+[1] https://github.com/riscv/riscv-isa-manual/blob/main/src/rv64.adoc
+[2] https://github.com/riscv/riscv-isa-manual/blob/main/src/m-st-ext.adoc
+[3] https://github.com/riscv/riscv-isa-manual/blob/main/src/b-st-ext.adoc
+[4] https://github.com/riscvarchive/riscv-zicond/blob/main/zicondops.adoc
 -/
 inductive Op
   | li : (val : Int) → Op
