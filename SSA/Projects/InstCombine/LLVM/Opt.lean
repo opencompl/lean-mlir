104404941b9681dfff9f322637765dafe14fd1d0
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-14 08:03:11 +0100

    added comments to the opt tool

--- SSA/Projects/InstCombine/LLVM/Opt.lean
+++ SSA/Projects/InstCombine/LLVM/Opt.lean
@@ -1,18 +1,14 @@
 import SSA.Projects.InstCombine.LLVM.Parser
 import Cli
 
-def functionn {α : Type} (x : α) : α := x
-
 def runMainCmd (args : Cli.Parsed) : IO UInt32 := do
   let fileName := args.positionalArg! "file" |>.as! String
   let icom? ← parseComFromFile fileName
   match icom? with
     | none => return 1
-    | some (Sigma.mk _Γ icom) => do
-      let icom'' := icom.snd.snd
-      let icom_output := functionn (icom'')
-      IO.println s!"OK: parsed"
-      IO.println s!"{repr icom_output}"
+    | some (Sigma.mk _Γ ⟨_eff, ⟨_retTy, c⟩⟩) => do
+      IO.println s!"OK: parsed" -- to do: this OK statement can be removed if not wanted.
+      IO.println s!"{repr c}"
       return 0
 
 def mainCmd := `[Cli|
@@ -24,4 +20,3 @@ def mainCmd := `[Cli|
 
 def main (args : List String): IO UInt32 :=
   mainCmd.validate args
--- ((Γ' : Context) × (eff : EffectKind) × (ty : InstCombine.LLVM.Ty) × Com InstCombine.LLVM Γ' eff ty)
