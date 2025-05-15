f92d37892efc9def3ca22b997d22ee4a9a9b7ca0
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-05-15 13:51:57 +0100

    Add new tests

--- SSA/Experimental/Bits/Fast/GeneralizeTest.lean
+++ SSA/Experimental/Bits/Fast/GeneralizeTest.lean
@@ -4,14 +4,22 @@ set_option trace.profiler true
 set_option trace.profiler.threshold 1
 set_option trace.Generalize true
 
+variable {x y : BitVec 4}
+-- #generalize (x ^^^ -1#4 ||| y) &&& (x ^^^ y) = y &&& (x ^^^ -1#4) -- PASSED but trivial precondition
+
 variable {x y : BitVec 8}
 -- #generalize (0#8 - x ||| y) + y = (y ||| 0#8 - x) + y --- PASSED add_or_sub_comb_i8_negative_y_sub_thm
+-- #generalize (0#8 - x ||| y) + x = (y ||| 0#8 - x) + x -- PASSED add_or_sub_comb_i8_negative_y_or_thm
+-- #generalize (0#8 - x ^^^ x) + x = (x ^^^ 0#8 - x) + x -- PASSED add_or_sub_comb_i8_negative_xor_instead_or_thm
+-- #generalize (0#8 - x ||| x) + y = (x ||| 0#8 - x) + y -- PASSED  add_or_sub_comb_i8_negative_y_add_thm
 -- #generalize (x ^^^ -1#8 ||| 7#8) ^^^ 12#8 = x &&& BitVec.ofInt 8 (-8) ^^^ BitVec.ofInt 8 (-13) --  PASSED not_or_xor_thm
 -- #generalize (x ^^^ 33#8 ||| 7#8) ^^^ 12#8 = x &&& BitVec.ofInt 8 (-8) ^^^ 43#8 --- PASSED xor_or_xor_thm
 -- #generalize  x ^^^ 33#8 ||| 7#8 = x &&& BitVec.ofInt 8 (-8) ^^^ 39#8 --- PASSED xor_or2_thm
+-- #generalize x ^^^ 32#8 ||| 7#8 = x &&& BitVec.ofInt 8 (-8) ^^^ 39#8  ---PASSED
 -- #generalize (x <<< 3) <<< 4 = x <<< 7
 
--- #generalize x ^^^ 32#8 ||| 7#8 = x &&& BitVec.ofInt 8 (-8) ^^^ 39#8 --- xor_or_thm. Can't generate precondition
+
+
 -- #generalize (x ||| 33#8) ^^^ 12#8 ||| 7#8 = x &&& BitVec.ofInt 8 (-40) ^^^ 47#8 --- or_xor_or_thm. Can't generate precondition
 -- #generalize 28#8 >>> x <<< 3#8 ||| 7#8 = BitVec.ofInt 8 (-32) >>> x ||| 7#8 -- lshr_shl_demand1_thm. Can't generate precondition
 
@@ -22,19 +30,24 @@ variable {x y z: BitVec 32}
 -- #generalize ((x >>> 4#32 &&& 8#32) + y) <<< 4#32 = (x &&& 128#32) + y <<< 4#32 --#PASSED gshlhbo_proof/shl_add_and_lshr_thm
 -- #generalize (x + (y >>> 5#32 &&& 127#32)) <<< 5#32 = (y &&& 4064#32) + x <<< 5#32 --- PASSED gshlhbo_proof/lshr_add_and_shl_thm
 -- #generalize x <<< 6#32 <<< 28#32 = 0#32   -- PASSED but takes seven minutes to run due to model counting; shl_shl_thm
- #generalize x + (x ||| (0 - x )) = x &&& (x + BitVec.ofInt 32 (-1))
+-- #generalize x + (x ||| (0 - x )) = x &&& (x + BitVec.ofInt 32 (-1))
 -- #generalize (x + 5) - (y + 1)  =  x - y + 4
 -- #generalize (x + 5) + (y + 1)  =  x + y + 6
 -- #generalize (x <<< 3) <<< 4 = x <<< 7
 
 
--- Failing
 
+-- Failing
+#generalize (x * x ^^^ y * y) &&& (x * x ||| y * y ^^^ -1#32) = x * x &&& (y * y ^^^ -1#32) -- and_orn_xor_commute8_thm
 -- #generalize 8#32 - x &&& 7#32 = 0#32 - x &&& 7#32 -- Precondition synthesis failure g2008h07h08hSubAnd_proof#a_thm --Can't generate preconditions
 -- #generalize BitVec.sshiftRight' (x &&& ((BitVec.ofInt 32 (-1)) <<< (32 - y))) (BitVec.ofInt 32 32 - y) = BitVec.sshiftRight' x (BitVec.ofInt 32 32 - y) -- Precondition synthesis failure #41801
 
 -- #generalize ((x ^^^ 1234#32) >>> 8#32 ^^^ 1#32) + (x ^^^ 1234#32) = (x >>> 8#32 ^^^ 5#32) + (x ^^^ 1234#32) -- gxor2_proof/test5_thm -- Timeout
 
+variable {x y z: BitVec 64}
+ -- #generalize x * x + (x * x ||| 0#64 - x * x) = x * x + -1#64 &&& x * x -- Passing but trivial result add_or_sub_comb_i64_commuted4_thm
+
+
 
  variable {x y z: BitVec 232}
- #generalize x >>> 231#232 >>> 1#232 = 0#232 -- PASSED - lshr_lshr_thm
+--  #generalize x >>> 231#232 >>> 1#232 = 0#232 -- PASSED - lshr_lshr_thm
