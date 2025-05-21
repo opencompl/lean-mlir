ab3746c88b661e60b3bae72536a7303676f89bfd
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-21 09:29:36 +0100

    clean up simpprocs

--- SSA/Projects/LLVMRiscV/simpproc.lean
+++ SSA/Projects/LLVMRiscV/simpproc.lean
@@ -6,8 +6,10 @@ import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 import Lean
 
 open LLVMRiscV
-open RV64Semantics -- needed to use RISC-V semantics in simp tactic
-open InstCombine(LLVM) -- analog to RISC-V
+open RV64Semantics
+open InstCombine(LLVM)
+/- This file contains simplification procedures needed for the hybrid dialect.-/
+
 /-
 Disabled due to simproc implementation not being re-evaluated correctly
 on Lean version "4.20.0-nightly-2025-04-21" -/
@@ -20,9 +22,7 @@ private theorem valuation_var_last_eq.lemma {Ty : Type} [TyDenote Ty] {Γ : Ctxt
 
 open Lean Meta Elab in
 simproc [simp_denote] valuation_var_last_eq ((Ctxt.Valuation.snoc _ _) (Ctxt.Var.last _ _)) := fun e => do
-  -- logInfo m!"Matched (Valuation.snoc s x) (Ctxt.Var.last Γ t) with {e}"
   let (_f, xs) := e.getAppFnArgs
-  -- logInfo m!"f: {f}, xs: {xs}"
   let ty := xs[0]!
   let s := xs[4]!
   let x := xs[xs.size - 1 - 2]!
@@ -69,7 +69,6 @@ simproc [simp_denote] llvmArgsFromHybrid_cons_eq (llvmArgsFromHybrid (_) ) := fu
     | return .continue
   let_expr HVector.cons _α _f _as _a x xs := lhs
     | return .continue
-
   let ty : Q(LLVM.Ty) ← mkFreshExprMVar q(LLVM.Ty)
   let tys : Q(List LLVM.Ty) ← mkFreshExprMVar q(List LLVM.Ty)
   let expected : Expr := q($ty :: $tys)
@@ -86,7 +85,7 @@ simproc [simp_denote] llvmArgsFromHybrid_cons_eq (llvmArgsFromHybrid (_) ) := fu
 @[simp_denote]
 def riscvArgsFromHybrid_nil_eq :
   (riscvArgsFromHybrid HVector.nil) = HVector.nil := rfl
-
+@[simp_denote]
 def riscvArgsFromHybrid_cons_eq.lemma {ty  : RISCV64.RV64.Ty} {tys : List RISCV64.RV64.Ty}
     (x : TyDenote.toType (LLVMRiscV.Ty.riscv ty))
     (xs : HVector TyDenote.toType (tys.map LLVMRiscV.Ty.riscv)) :
@@ -95,7 +94,7 @@ def riscvArgsFromHybrid_cons_eq.lemma {ty  : RISCV64.RV64.Ty} {tys : List RISCV6
    := rfl
 
 open Lean Meta Elab in
-/-- Extract out the raw LLVM type. -/
+/-- Extracting out the raw LLVM type. -/
 def extractRiscvTy (x : Expr) : SimpM Expr := do
   let_expr Ty.riscv xRealTy := (← reduce x)
     | throwError m! "expected type of {x} to be `Ty.riscv _`, but got {x}"
@@ -111,7 +110,6 @@ simproc [simp_denote] riscvArgsFromHybrid_cons_eq (riscvArgsFromHybrid _) := fun
   let some (_, xsRealTys) := Expr.listLit? (← reduce as)
     | return .continue
   let xsRealTys ←  xsRealTys.mapM extractRiscvTy
-
   logInfo m!"found (llvmArgsFromHybrid (HVector.cons ({x} : {xRealTy}) ({xs} : {xsRealTys})"
   let llvmTypeType := mkApp (mkConst ``Dialect.Ty []) (mkConst ``RISCV64.RV64 [])
   let xsRealTys ← listExprToExprShallow (.some llvmTypeType) xsRealTys
@@ -159,81 +157,3 @@ def add_llvm_no_flags : Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 6
       %1 = llvm.add   %lhs, %rhs  : i64
       llvm.return %1 : i64
   }]
-  
-@[simp]
-theorem PoisonOr_eq_squb (a b : PoisonOr α) : PoisonOr.IsRefinedBy a b ↔ a ⊑ b := by rfl
-
-/- # Debug: single llvm op -/
-def single_bin_op_lhs := [LV| {
-  ^entry ():
-    %0 = llvm.mlir.constant (0) : i64
-    llvm.return  %0 : i64
-  }]
-def single_bin_op_rhs := [LV| {
-  ^entry ():
-    %0 = llvm.mlir.constant (0) : i64
-    llvm.return  %0 : i64
-  }]
-def single_llvm_bin_op : LLVMPeepholeRewriteRefine [] :=
-    {lhs:= single_bin_op_lhs , rhs:= single_bin_op_rhs
-      ,correct := by
-      unfold  single_bin_op_lhs single_bin_op_rhs
-      simp -- Replace with a general simplification tactic or another meaningful tactic
-  }
-
-/- # Debug: single riscv op -/
-def riscv_lhs := [LV| {
-  ^entry ():
-    %0 = li (1) : !i64
-    ret  %0 : !i64
-  }]
-def riscv_rhs := [LV| {
-  ^entry ():
-    %0 = li (0) : !i64
-    ret  %0 : !i64
-  }]
-
-/- # Debug: casts -/
-def cast_op_lhs := [LV| {
-  ^entry (%lhs: !i64):
-        %0 = "builtin.unrealized_conversion_cast"(%lhs) : (!i64) -> i64
-        --%1 = "builtin.unrealized_conversion_cast" (%0) : (i64) -> (!i64)
-      llvm.return  %0 : i64
-  }]
-
-def cast_op_rhs := [LV| {
-  ^entry (%lhs: !i64):
-        %0 = "builtin.unrealized_conversion_cast"(%lhs) : (!i64) -> i64
-        --%1 = "builtin.unrealized_conversion_cast" (%0) : (i64) -> (!i64)
-      llvm.return  %0 : i64
-  }]
-
--- Bellow are the examples where simppeephole does not apply as expected.
-
-attribute [simp_denote] MLIR.AST.Op.ParsedArgs.ofList
-
--- Here we get the motive not type correct error.
-def single_cast_op : LLVMPeepholeRewriteRefine [Ty.riscv (.bv)] :=
-  {lhs:= cast_op_lhs , rhs:= cast_op_rhs ,
-   correct := by
-    unfold cast_op_lhs cast_op_rhs
-    simp_peephole
-    simp only [MLIR.AST.Op.ParsedArgs.toHVector, MLIR.AST.Op.ParsedArgs.toHVector.go]
-    rw [List.reverse_cons]
-    sorry
-  }
-
-/- This example causes an expontially huge proof goal, it is inlining the wholeparsing code.
-simp peephole also does not apply.
--/
-def test1: LLVMPeepholeRewriteRefine [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-    {lhs:= add_riscv1 , rhs:= add_llvm_no_flags
-      ,correct := by
-      unfold  add_riscv1 add_llvm_no_flags
-      simp_peephole
-      simp only [MLIR.AST.Op.ParsedArgs.toHVector, MLIR.AST.Op.ParsedArgs.toHVector.go]
-      simp only  [castriscvToLLVM]
-      simp_peephole
-      simp only  [PoisonOr_eq_squb]
-      simp [PoisonOr.value_isRefinedBy_value]
-  }
