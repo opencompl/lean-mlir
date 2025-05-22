68c93596d69afabba18bd897a5ec527eeac0e617
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-22 11:19:23 +0100

    simp_riscv tactic

---
+++ SSA/Projects/LLVMRiscV/SimpRiscV.lean
@@ -0,0 +1,35 @@
+import SSA.Projects.LLVMRiscV.SimpRiscVAttr
+import SSA.Projects.InstCombine.ForLean
+import SSA.Projects.InstCombine.LLVM.EDSL
+import Lean.Meta.Tactic.Simp.SimpTheorems
+import Lean.Meta.Tactic.Simp.RegisterCommand
+import Lean.LabelAttribute
+import SSA.Projects.LLVMRiscV.LLVMAndRiscv
+
+open Lean
+open Lean.Elab.Tactic
+open RV64Semantics
+open LLVMRiscV
+
+/-!
+This file defines the `simp_riscv` tactic, which unfolds and simplifies the RISC-V semantic definitions.
+It also defines the `simp_riscv` attribute, used to tag semantic definition functions so that
+they are automatically simplified when `simp_riscv` is invoked.
+
+The purpose of this tactic is to reduce proof size when reasoning about instruction lowerings.
+-/
+
+attribute [simp_riscv]
+ ADDIW_pure64 UTYPE_pure64_lui UTYPE_pure64_AUIPC RTYPE_pure64_RISCV_ADD castriscvToLLVM
+ castLLVMToriscv RTYPE_pure64_RISCV_SRA_bv RTYPE_pure64_RISCV_SUB RTYPE_pure64_RISCV_AND
+ MUL_pure64_ftt RTYPE_pure64_RISCV_OR REM_pure64_signed_bv DIV_pure64_signed_bv
+ RTYPE_pure64_RISCV_SLL_bv RTYPE_pure64_RISCV_SRL_bv RTYPE_pure64_RISCV_SUB
+ DIV_pure64_unsigned_bv REM_pure64_unsigned_bv RTYPE_pure64_RISCV_XOR DIV_pure64_signed_bv
+
+syntax "simp_riscv" : tactic
+macro "simp_riscv" : tactic =>
+  `(tactic|(
+      simp (config := {failIfUnchanged := false}) only [
+          simp_riscv
+        ]
+    ))
