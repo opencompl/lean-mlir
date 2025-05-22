724acbd1ef522446a99a7b50af5eb1ce3abd0e6d
Alex Keizer <alex@keizer.dev>
2025-05-22 11:36:25 +0100

    feat: refinement for SLLVM

--- SSA/Projects/SLLVM/Dialect.lean
+++ SSA/Projects/SLLVM/Dialect.lean
@@ -1,6 +1,7 @@
 
 import SSA.Projects.SLLVM.Dialect.Base
 import SSA.Projects.SLLVM.Dialect.Refinement
+import SSA.Projects.SLLVM.Dialect.Parser
 
 import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 -- ^^ ensure we have the pretty EDSL macros available
--- SSA/Projects/SLLVM/Dialect/Refinement.lean
+++ SSA/Projects/SLLVM/Dialect/Refinement.lean
@@ -8,7 +8,26 @@ namespace LeanMLIR.SLLVM
 
 open InstCombine.LLVM.Ty (bitvec)
 
+scoped instance : Refinement (BitVec w) := .ofEq
+@[simp, simp_llvm_split] theorem bv_isRefinedBy_iff (x y : BitVec w) : x ⊑ y ↔ x = y := by rfl
+-- ^^ declare that for pure bitvectors, refinement is just equality
+
+instance (w) : Refinement (UBOr <| LLVM.IntW w) where
+  IsRefinedBy (x y : PoisonOr (PoisonOr _)) := x ⊑ y
+
+open HRefinement in
+@[simp, simp_denote]
+theorem isRefinedBy_iff (x y : UBOr <| LLVM.IntW w) :
+    x ⊑ y ↔ @IsRefinedBy (PoisonOr <| PoisonOr _) (PoisonOr <| PoisonOr _) _ x y := by
+  rfl
+
 instance : DialectHRefinement SLLVM SLLVM where
   IsRefinedBy := @fun
-    | bitvec v, bitvec w, (x : PoisonOr (LLVM.IntW v)), (y : PoisonOr (LLVM.IntW w)) =>
+    | bitvec v, bitvec w, (x : PoisonOr (PoisonOr _)), (y : PoisonOr (PoisonOr _)) =>
         ∃ h : v = w, x ⊑ h ▸ y
+
+@[simp, simp_denote]
+theorem dialect_isRefinedBy_iff_of_width_eq (x y : SLLVM.m ⟦bitvec w⟧) :
+    DialectHRefinement.IsRefinedBy x y
+    ↔ HRefinement.IsRefinedBy (α := UBOr <| LLVM.IntW w) (β := UBOr <| LLVM.IntW w) x y := by
+  simp [DialectHRefinement.IsRefinedBy]
