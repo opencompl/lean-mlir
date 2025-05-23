f03f9b106dba7c36080d53d0cc41634a36c97094
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-23 20:56:16 +0100

    improve layout

--- SSA/Projects/LLVMRiscV/Pipeline/srl.lean
+++ SSA/Projects/LLVMRiscV/Pipeline/srl.lean
@@ -1,19 +1,11 @@
 import SSA.Projects.LLVMRiscV.PeepholeRefine
-import SSA.Projects.LLVMRiscV.LLVMAndRiscv
-import SSA.Projects.InstCombine.Tactic
-import SSA.Projects.RISCV64.PrettyEDSL
-import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 import SSA.Projects.LLVMRiscV.simpproc
 import SSA.Projects.RISCV64.Tactic.SimpRiscV
 
-import Lean
-
 open LLVMRiscV
-open RV64Semantics
-open InstCombine(LLVM)
 
 /-
-Remove bitvec lemmas from the simp-set that simplify bitvector operations into toNat operations.
+Removing bitvec lemmas from the simp-set that simplify bitvector operations into toNat operations.
 -/
 attribute [-simp] BitVec.ushiftRight_eq' BitVec.shiftLeft_eq' BitVec.sshiftRight_eq'
 
@@ -21,25 +13,25 @@ attribute [-simp] BitVec.ushiftRight_eq' BitVec.shiftLeft_eq' BitVec.sshiftRight
 Logical right shift operation in LLVM: if exact flag is set,
 then returns poison if any non zero bit is shifted  -/
 
-def lshr_llvm_no_flag : Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
-    ^entry (%x: i64, %amount: i64 ):
-      %1 = llvm.lshr %x, %amount : i64
-      llvm.return %1 : i64
+def lshr_llvm_no_flag := [LV| {
+    ^entry (%x: i64, %amount: i64):
+    %1 = llvm.lshr %x, %amount : i64
+    llvm.return %1 : i64
   }]
 
 def srl_riscv := [LV| {
     ^entry (%x: i64, %amount: i64 ):
-      %base = "builtin.unrealized_conversion_cast"(%x) : (i64) -> !i64
-      %shamt = "builtin.unrealized_conversion_cast"(%amount) : (i64) -> !i64
-      %res = srl %base, %shamt : !i64
-      %y= "builtin.unrealized_conversion_cast" (%res) : (!i64) -> (i64)
-      llvm.return %y : i64
+    %base = "builtin.unrealized_conversion_cast" (%x) : (i64) -> (!i64)
+    %shamt = "builtin.unrealized_conversion_cast" (%amount) : (i64) -> (!i64)
+    %res = srl %base, %shamt : !i64
+    %y= "builtin.unrealized_conversion_cast" (%res) : (!i64) -> (i64)
+    llvm.return %y : i64
   }]
 
-def llvm_srl_lower_riscv : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := lshr_llvm_no_flag , rhs := srl_riscv ,
-    correct :=  by
+def llvm_srl_lower_riscv : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := lshr_llvm_no_flag
+  rhs := srl_riscv
+  correct :=  by
       unfold lshr_llvm_no_flag srl_riscv
       simp_peephole
       simp_alive_undef
@@ -55,17 +47,17 @@ def llvm_srl_lower_riscv : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), T
         simp at hff
         simp
         bv_decide
-    }
 
-def lshr_llvm_exact : Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
-    ^entry (%x: i64, %amount: i64 ):
-      %1 = llvm.lshr exact %x, %amount : i64
-      llvm.return %1 : i64 }]
+def lshr_llvm_exact := [LV| {
+    ^entry (%x: i64, %amount: i64):
+    %1 = llvm.lshr exact %x, %amount : i64
+    llvm.return %1 : i64
+  }]
 
-def llvm_srl_lower_riscv_exact : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := lshr_llvm_exact, rhs := srl_riscv,
-    correct :=  by
+def llvm_srl_lower_riscv_exact : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := lshr_llvm_exact
+  rhs := srl_riscv
+  correct := by
     unfold lshr_llvm_exact srl_riscv
     simp_peephole
     simp_alive_undef
@@ -83,4 +75,3 @@ def llvm_srl_lower_riscv_exact : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 
       case isFalse hf =>
         simp
         bv_decide
-  }
--- SSA/Projects/LLVMRiscV/Pipeline/sub.lean
+++ SSA/Projects/LLVMRiscV/Pipeline/sub.lean
@@ -1,40 +1,34 @@
 import SSA.Projects.LLVMRiscV.PeepholeRefine
-import SSA.Projects.LLVMRiscV.LLVMAndRiscv
-import SSA.Projects.InstCombine.Tactic
-import SSA.Projects.RISCV64.PrettyEDSL
-import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 import SSA.Projects.LLVMRiscV.simpproc
 import SSA.Projects.RISCV64.Tactic.SimpRiscV
 
 import Lean
 
 open LLVMRiscV
-open RV64Semantics
-open InstCombine(LLVM)
 
 /-
 Disabled due to simproc implementation not being re-evaluated correctly
 on Lean version "4.20.0-nightly-2025-04-21" -/
 set_option Elab.async true
 
-def llvm_sub_self: Com  LLVMPlusRiscV [.llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
+def llvm_sub_self := [LV| {
     ^entry (%x: i64 ):
-      %1 = llvm.sub  %x, %x : i64
-      llvm.return %1 : i64
+    %1 = llvm.sub %x, %x : i64
+    llvm.return %1 : i64
   }]
 
-def sub_riscv_self: Com  LLVMPlusRiscV [.llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
+def sub_riscv_self := [LV| {
     ^entry (%x: i64):
-      %0 = "builtin.unrealized_conversion_cast"(%x) : (i64) -> !i64
-      %2 = sub %0, %0 : !i64
-      %3 = "builtin.unrealized_conversion_cast"(%2) : (!i64) -> (i64)
-      llvm.return %3 : i64
+    %0 = "builtin.unrealized_conversion_cast"(%x) : (i64) -> (!i64)
+    %2 = sub %0, %0 : !i64
+    %3 = "builtin.unrealized_conversion_cast"(%2) : (!i64) -> (i64)
+    llvm.return %3 : i64
   }]
 
-def llvm_sub_lower_riscv_no_flag_self: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64)] :=
-  {lhs := llvm_sub_self , rhs := sub_riscv_self, correct := by
+def llvm_sub_lower_riscv_no_flag_self: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64)] where
+  lhs := llvm_sub_self
+  rhs := sub_riscv_self
+  correct := by
     unfold llvm_sub_self sub_riscv_self
     simp_peephole
     simp_riscv
@@ -43,47 +37,46 @@ def llvm_sub_lower_riscv_no_flag_self: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.b
     all_goals
     simp_alive_undef
     simp
-  }
 
-def llvm_sub: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64)) := [LV| {
-    ^entry (%x: i64, %y: i64 ):
-      %1 = llvm.sub  %x, %y : i64
-      llvm.return %1 : i64
+def llvm_sub := [LV| {
+    ^entry (%x: i64, %y: i64):
+    %1 = llvm.sub %x, %y : i64
+    llvm.return %1 : i64
   }]
 
-def sub_riscv: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64)) := [LV| {
+def sub_riscv := [LV| {
     ^entry (%x: i64, %y: i64 ):
-      %0 = "builtin.unrealized_conversion_cast"(%x) : (i64) -> !i64
-      %1 = "builtin.unrealized_conversion_cast"(%y) : (i64) -> !i64
-      %2 = sub %0, %1 : !i64
-      %3 = "builtin.unrealized_conversion_cast"(%2) : (!i64) -> (i64)
-      llvm.return %3 : i64
+    %0 = "builtin.unrealized_conversion_cast"(%x) : (i64) -> (!i64)
+    %1 = "builtin.unrealized_conversion_cast"(%y) : (i64) -> (!i64)
+    %2 = sub %0, %1 : !i64
+    %3 = "builtin.unrealized_conversion_cast"(%2) : (!i64) -> (i64)
+    llvm.return %3 : i64
   }]
 
-def llvm_sub_lower_riscv_no_flag: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := llvm_sub , rhs := sub_riscv, correct := by
-      unfold llvm_sub sub_riscv
-      simp_peephole
-      simp_riscv
-      simp_alive_undef
-      simp_alive_case_bash
-      simp_alive_split
-      simp
-      }
+def llvm_sub_lower_riscv_no_flag: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := llvm_sub
+  rhs := sub_riscv
+  correct := by
+    unfold llvm_sub sub_riscv
+    simp_peephole
+    simp_riscv
+    simp_alive_undef
+    simp_alive_case_bash
+    simp_alive_split
+    simp
 
 /-! # SUB, flags  -/
 
-def llvm_sub_nsw: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
+def llvm_sub_nsw := [LV| {
     ^entry (%x: i64, %y: i64 ):
-      %1 = llvm.sub  %x, %y overflow<nsw> : i64
-      llvm.return %1 : i64
+    %1 = llvm.sub  %x, %y overflow<nsw> : i64
+    llvm.return %1 : i64
   }]
 
-def llvm_sub_nsw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := llvm_sub_nsw , rhs := sub_riscv, correct := by
+def llvm_sub_nsw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := llvm_sub_nsw
+  rhs := sub_riscv
+  correct := by
      unfold llvm_sub_nsw sub_riscv
      simp_peephole
      simp_alive_undef
@@ -91,17 +84,17 @@ def llvm_sub_nsw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64)
      simp_alive_case_bash
      simp_alive_split
      simp
-  }
 
-def llvm_sub_nuw: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
+def llvm_sub_nuw := [LV| {
     ^entry (%x: i64, %y: i64 ):
-      %1 = llvm.sub  %x, %y overflow<nuw> : i64
-      llvm.return %1 : i64
+    %1 = llvm.sub %x, %y overflow<nuw> : i64
+    llvm.return %1 : i64
   }]
 
-def llvm_sub_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := llvm_sub_nuw , rhs := sub_riscv, correct := by
+def llvm_sub_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := llvm_sub_nuw
+  rhs := sub_riscv
+  correct := by
     unfold llvm_sub_nuw sub_riscv
     simp_peephole
     simp_riscv
@@ -109,16 +102,17 @@ def llvm_sub_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64)
     simp_alive_case_bash
     simp_alive_split
     simp
-    }
 
-def llvm_sub_nsw_nuw: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
-    .pure (.llvm (.bitvec 64))  := [LV| {
-    ^entry (%x: i64, %y: i64 ):
-      %1 = llvm.sub  %x, %y overflow<nsw, nuw> : i64
-      llvm.return %1 : i64
+def llvm_sub_nsw_nuw := [LV| {
+    ^entry (%x: i64, %y: i64):
+    %1 = llvm.sub  %x, %y overflow<nsw, nuw> : i64
+    llvm.return %1 : i64
   }]
-def llvm_sub_nsw_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
-  {lhs := llvm_sub_nsw_nuw, rhs := sub_riscv, correct := by
+
+def llvm_sub_nsw_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := llvm_sub_nsw_nuw
+  rhs := sub_riscv
+  correct := by
     unfold llvm_sub_nsw_nuw sub_riscv
     simp_peephole
     simp_riscv
@@ -126,4 +120,3 @@ def llvm_sub_nsw_nuw_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec
     simp_alive_case_bash
     simp_alive_split
     simp
-  }
