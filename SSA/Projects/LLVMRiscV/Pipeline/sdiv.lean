bd47abf5411ecbf7af8d85e934c65df52849e28d
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-05-23 10:41:59 +0100

    sdiv and shl: need to come up with a cleaner proof

---
+++ SSA/Projects/LLVMRiscV/Pipeline/sdiv.lean
@@ -0,0 +1,69 @@
+import SSA.Projects.LLVMRiscV.PeepholeRefine
+import SSA.Projects.LLVMRiscV.LLVMAndRiscv
+import SSA.Projects.InstCombine.Tactic
+import SSA.Projects.RISCV64.PrettyEDSL
+import SSA.Projects.InstCombine.LLVM.PrettyEDSL
+import SSA.Projects.LLVMRiscV.simpproc
+import SSA.Projects.RISCV64.Tactic.SimpRiscV
+
+import Lean
+
+open LLVMRiscV
+open RV64Semantics
+open InstCombine(LLVM)
+
+/-! # SDIV no exact   -/
+
+def sdiv_llvm_no_exact : Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
+    .pure (.llvm (.bitvec 64)) := [LV| {
+    ^entry (%x: i64, %y: i64 ):
+      %1 = llvm.sdiv  %x, %y : i64
+      llvm.return %1 : i64
+  }]
+
+def sdiv_riscv: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
+    .pure (.llvm (.bitvec 64))  := [LV| {
+    ^entry (%reg1: i64, %reg2: i64 ):
+      %0 = "builtin.unrealized_conversion_cast"(%reg1) : (i64) -> !i64
+      %1 = "builtin.unrealized_conversion_cast"(%reg2) : (i64) -> !i64
+      %2 = div %0, %1 : !i64
+      %3 = "builtin.unrealized_conversion_cast" (%2) : (!i64) -> (i64)
+      llvm.return %3 : i64
+  }]
+
+def llvm_sdiv_lower_riscv_no_flag: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64),
+  Ty.llvm (.bitvec 64)] :=
+  {lhs := sdiv_llvm_no_exact , rhs := sdiv_riscv, correct := by
+    unfold sdiv_llvm_no_exact sdiv_riscv
+    simp_peephole
+    simp_alive_undef
+    simp_riscv
+    simp_alive_ops
+    simp_alive_case_bash
+    intro x x'
+    by_cases onX2 :  x' = 0#64 <;>  simp [onX2]
+  }
+
+/-! # SDIV exact   -/
+def sdiv_llvm_exact : Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
+    .pure (.llvm (.bitvec 64))  := [LV| {
+    ^entry (%x: i64, %y: i64 ):
+      %1 = llvm.sdiv exact %x, %y : i64 -- value depends on wether to no overflow flag is present or not
+      llvm.return %1 : i64
+  }]
+
+def llvm_sdiv_lower_riscv_exact : LLVMPeepholeRewriteRefine 64  [Ty.llvm (.bitvec 64),
+  Ty.llvm (.bitvec 64)] :=
+  {lhs := sdiv_llvm_exact, rhs := sdiv_riscv, correct := by
+    unfold sdiv_llvm_exact sdiv_riscv
+    simp_peephole
+    simp_alive_undef
+    simp_riscv
+    simp_alive_ops
+    simp_alive_case_bash
+    intro x x'
+    by_cases onX2 :  x' = 0#64 <;> simp [onX2]
+    by_cases hx : x.smod x' = 0#64
+    simp [hx]
+    split<;> simp
+    }
