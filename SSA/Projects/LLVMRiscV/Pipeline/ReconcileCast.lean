3896f23ba22a26a0c06dda65f13ce4d9a089433f
kuhnsa <151550049+salinhkuhn@users.noreply.github.com>
2025-05-26 09:59:42 +0100

    Update SSA/Projects/LLVMRiscV/Pipeline/ReconcileCast.lean
    Co-authored-by: Tobias Grosser <tobias@grosser.es>

--- SSA/Projects/LLVMRiscV/Pipeline/ReconcileCast.lean
+++ SSA/Projects/LLVMRiscV/Pipeline/ReconcileCast.lean
@@ -8,7 +8,7 @@ open LLVMRiscV
 /-!
 ## Functionality
 This file contains rewrites for the Peephole Rewriter to eliminate unrealized conversion casts.
-The rewrites below are registered fold-away pairs of unrealized conversion casts.
+The rewrites below are registered to fold-away pairs of unrealized conversion casts.
 
 An `unrealized conversion cast` in MLIR is an operation inserted during the lowering
 from one dialect to another dialect to temporary guarantee compatible between type
