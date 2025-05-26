b08c526da6f56846efba2cb2bd87bb34664552ee
kuhnsa <151550049+salinhkuhn@users.noreply.github.com>
2025-05-26 23:01:26 +0100

    feat: add udiv, urem, xor instruction lowering (#1265)
    This PR dds the instruction lowering proofs for the LLVM 64-bit xor,
    udiv and urem instructions.
    
    ---------
    
    Co-authored-by: Sarah Kuhn <kuhnsa@student.ethz.ch>

---
+++ SSA/Projects/LLVMRiscV/Pipeline/urem.lean
@@ -0,0 +1,43 @@
+import SSA.Projects.LLVMRiscV.PeepholeRefine
+import SSA.Projects.LLVMRiscV.simpproc
+import SSA.Projects.RISCV64.Tactic.SimpRiscV
+
+open LLVMRiscV
+
+
+/-! # UREM  -/
+def llvm_urem: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
+    .pure (.llvm (.bitvec 64)) := [LV| {
+    ^entry (%x: i64, %y: i64 ):
+      %1 = llvm.urem  %x, %y : i64
+      llvm.return %1 : i64
+  }]
+
+def urem_riscv: Com  LLVMPlusRiscV [.llvm (.bitvec 64), .llvm (.bitvec 64)]
+    .pure (.llvm (.bitvec 64)) := [LV| {
+    ^entry (%reg1: i64, %reg2: i64):
+      %0 = "builtin.unrealized_conversion_cast"(%reg1) : (i64) -> (!i64)
+      %1 = "builtin.unrealized_conversion_cast"(%reg2) : (i64) -> (!i64)
+      %2 = remu %0, %1 : !i64
+      %3 = "builtin.unrealized_conversion_cast"(%2) : (!i64) -> (i64)
+      llvm.return %3 : i64
+  }]
+
+  def llvm_urem_lower_riscv: LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] :=
+  {lhs := llvm_urem, rhs := urem_riscv, correct := by
+    unfold llvm_urem urem_riscv
+    simp_peephole
+    simp_alive_undef
+    simp_riscv
+    simp_alive_ops
+    simp_alive_case_bash
+    intro x x'
+    split
+    case value.value.isTrue ht =>
+      simp
+    case value.value.isFalse hf =>
+      simp only [PoisonOr.toOption_getSome, BitVec.signExtend_eq, BitVec.ofNat_eq_ofNat,
+        BitVec.umod_eq, PoisonOr.value_isRefinedBy_value, InstCombine.bv_isRefinedBy_iff,
+        right_eq_ite_iff]
+      bv_omega
+  }
