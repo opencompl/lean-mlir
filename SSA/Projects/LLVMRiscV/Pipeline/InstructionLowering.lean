1922b9ac455af3204650a4657f441ff6e01b5ae4
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-06-03 22:58:24 +0100

    add const lowering to pipeline

--- SSA/Projects/LLVMRiscV/Pipeline/InstructionLowering.lean
+++ SSA/Projects/LLVMRiscV/Pipeline/InstructionLowering.lean
@@ -71,6 +71,8 @@ def rewritingPatterns1 :
 def reconcile_cast_pass : List (Σ Γ, Σ ty, PeepholeRewrite LLVMPlusRiscV Γ ty)
   := List.cons ⟨[Ty.riscv RISCV64.Ty.bv], (Ty.riscv RISCV64.Ty.bv), cast_eliminiation_riscv⟩ <| List.nil
 
+def const_match : List (Σ Γ, Σ ty, PeepholeRewrite LLVMPlusRiscV Γ ty)
+  := List.map (fun x => mkRewrite (LLVMToRiscvPeepholeRewriteRefine.toPeepholeUNSOUND x)) all_const_llvm_const_lower_riscv_li
 /-
 Pipeline structure:
  DCE (avoid lowering unnecessary instructions)
@@ -98,8 +100,10 @@ a maximal of 100 steps is performed. Currently we need to set this limit to avoi
  def selectionPipeFuelSafe {Γl : List LLVMPlusRiscV.Ty} (prog : Com LLVMPlusRiscV
     (Ctxt.ofList Γl) .pure (.llvm (.bitvec w))):=
   let rmInitialDeadCode :=  (DCE.dce' prog).val; -- First we eliminate the inital inefficenices in the code.
+  let loweredConst := multiRewritePeephole 100
+    const_match rmInitialDeadCode; -- Lower the instructions in the first array.
   let lowerPart1 := multiRewritePeephole 100
-    rewritingPatterns1  rmInitialDeadCode;
+    rewritingPatterns1  loweredConst;
   let lowerPart2 := multiRewritePeephole 100
     rewritingPatterns0 lowerPart1;
   let postLoweringDCE := (DCE.dce' lowerPart2).val;
