49038ef633fc0d8a14b63aa05a5aace97a13c2ee
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-06-04 21:03:24 +0100

    Clean up

--- SSA/Experimental/Bits/Fast/GeneralizerTests/42.lean
+++ SSA/Experimental/Bits/Fast/GeneralizerTests/42.lean
@@ -5,4 +5,4 @@ set_option trace.profiler.threshold 1
 set_option trace.Generalize true
 
 variable {x y : BitVec 32}
-#generalize (x <<< 10) <<< 14 = x <<< 24 --- #42
+#generalize (x <<< 10) <<< 14 = x <<< 24 --- #42 -- from Hydra paper
