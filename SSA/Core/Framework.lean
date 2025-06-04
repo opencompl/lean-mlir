3eb444ebc8f9beaf02f8308622ec29d76dd07552
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-06-04 16:22:38 +0100

    fix printing riscv

--- SSA/Core/Framework.lean
+++ SSA/Core/Framework.lean
@@ -275,6 +275,7 @@ end Repr
 
 /- # ToString instances for Com and Expr  -/
 section ToString
+
 variable {d} [DialectSignature d] [Repr d.Op] [Repr d.Ty] [ToString d.Ty] [ToString d.Op]
 
 /-- Format a list of formal arguments as `(%0 : t₀, %1 : t₁, ... %n : tₙ)` -/
