6dcb76a2acc9b35b1407da80bd7b88ae514d4407
Siddharth Bhat <siddu.druid@gmail.com>
2025-06-13 04:09:56 +0100

    chore: add IC3 references

---
+++ SSA/Experimental/Bits/Fast/IC3.lean
@@ -0,0 +1,11 @@
+/-#
+
+## IC3 Implementation for Automata Verification
+
+- Lectures notes from Prof. Dr. Roland Meyer, "Exercise: Implement IC3" [1]
+- Paper: Understanding IC3 [2] by Aaron R Bradley.
+
+[1] https://www.tu-braunschweig.de/fileadmin/Redaktionsgruppen/Institute_Fakultaet_1/TCS/Semantik/neu/sheet05.pdf
+[2] https://theory.stanford.edu/~arbrad/papers/Understanding_IC3.pdf
+-/
+
