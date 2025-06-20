20f916b98b3b4c22822e5e1065d4651885250773
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-06-20 11:24:08 +0100

    rm simp lowering attr

--- SSA/Projects/LLVMRiscV/Pipeline/tactics/SimpLoweringAttr.lean
+++
@@ -1,6 +0,0 @@
-/-
-Released under Apache 2.0 license as described in the file LICENSE.
--/
-import Lean
-/-! The `simp_lowering`attribute can be used to tag lemmmas such that -/
-register_simp_attr simp_lowering
