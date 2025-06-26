ce9a40168cf4ec9b535c4cd003479cdbfeee76f2
luisacicolini <luisacicolini@gmail.com>
2025-06-26 20:24:56 +0100

    chore: fix

--- bv-evaluation/collect-data-hdel.py
+++ bv-evaluation/collect-data-hdel.py
@@ -217,7 +217,7 @@ def compare_solvers_on_file(results: dict):
         "file_counter_bitwuzla_times_average": file_counter_bitwuzla_times_average,
         "": file_solved_bv_decide_times_average,
         "file_solved_bv_decide_rw_times_average": file_solved_bv_decide_rw_times_average,
-        "file_solved_bv_decide_rw_times_average": file_solved_bv_decide_bb_times_average,
+        "file_solved_bv_decide_bb_times_average": file_solved_bv_decide_bb_times_average,
         "file_solved_bv_decide_sat_times_average": file_solved_bv_decide_sat_times_average,
         "file_solved_bv_decide_lratt_times_average": file_solved_bv_decide_lratt_times_average,
         "file_solved_bv_decide_lratc_times_average": file_solved_bv_decide_lratc_times_average,
