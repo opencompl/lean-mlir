c5d5b81a01c395e7817c416895059290303d7e8e
luisacicolini <luisacicolini@gmail.com>
2025-06-26 14:41:39 +0100

    chore: refactor

--- bv-evaluation/collect-data-hdel.py
+++ bv-evaluation/collect-data-hdel.py
@@ -113,17 +113,17 @@ def compare_solvers_on_file (results : dict) :
         benchmark_errors.append(error)
 
     data = {
-        'file_solved_bitwuzla_times_average' = file_solved_bitwuzla_times_average
-        'file_counter_bitwuzla_times_average' = file_counter_bitwuzla_times_average
-        'file_solved_bv_decide_times_average' = file_solved_bv_decide_times_average
-        'file_solved_bv_decide_rw_times_average' = file_solved_bv_decide_rw_times_average
-        'file_solved_bv_decide_bb_times_average' = file_solved_bv_decide_bb_times_average
-        'file_solved_bv_decide_sat_times_average' = file_solved_bv_decide_sat_times_average
-        'file_solved_bv_decide_lratt_times_average' = file_solved_bv_decide_lratt_times_average
-        'file_solved_bv_decide_lratc_times_average' = file_solved_bv_decide_lratc_times_average
-        'file_counter_bv_decide_times_average' = file_counter_bv_decide_times_average
-        'file_counter_bv_decide_rw_times_average' = file_counter_bv_decide_rw_times_average
-        'file_counter_bv_decide_sat_times_average' = file_counter_bv_decide_sat_times_average
+        'file_solved_bitwuzla_times_average' : file_solved_bitwuzla_times_average
+        'file_counter_bitwuzla_times_average' : file_counter_bitwuzla_times_average
+        'file_solved_bv_decide_times_average' : file_solved_bv_decide_times_average
+        'file_solved_bv_decide_rw_times_average' : file_solved_bv_decide_rw_times_average
+        'file_solved_bv_decide_bb_times_average' : file_solved_bv_decide_bb_times_average
+        'file_solved_bv_decide_sat_times_average' : file_solved_bv_decide_sat_times_average
+        'file_solved_bv_decide_lratt_times_average' : file_solved_bv_decide_lratt_times_average
+        'file_solved_bv_decide_lratc_times_average' : file_solved_bv_decide_lratc_times_average
+        'file_counter_bv_decide_times_average' : file_counter_bv_decide_times_average
+        'file_counter_bv_decide_rw_times_average' : file_counter_bv_decide_rw_times_average
+        'file_counter_bv_decide_sat_times_average' : file_counter_bv_decide_sat_times_average
     }
     return data 
 
