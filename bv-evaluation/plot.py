c2f359614622a07b3ee1e08843bfd6856af5cdfd
luisacicolini <lc985@cam.ac.uk>
2025-06-27 18:09:06 +0200

    restore files

--- bv-evaluation/plot.py
+++ bv-evaluation/plot.py
@@ -250,12 +250,137 @@ def bv_decide_tot_stacked_perc(df, bm, type):
 
 
 def leanSAT_smtlib_unsat_stacked_perc(df: pd.DataFrame, bm, type):
+    if type == 'h_tot':
+        matplotlib.rcParams["figure.figsize"] = 14, 5
+    x = np.arange(len(df["benchmark"]))
+    width = 1.0
+    fig, ax = plt.subplots()
+    # df_sorted = df.sort_values(by="leanSAT")
+    # print(df_sorted)
+    df = df.apply(lambda x: x / 10**6 if x.name in
+      ['leanSAT-ld', 'leanSAT-rr', 'leanSAT-ac',
+       'leanSAT-af', 'leanSAT-ecs', 'leanSAT-bb',
+       'leanSAT-sat', 'leanSAT-lrat', 'leanSAT-kc'] else x)
+    tot_sum = (df["leanSAT-ld"] + df["leanSAT-rr"] + df["leanSAT-ac"] +
+               df["leanSAT-af"] + df["leanSAT-ecs"] + df["leanSAT-bb"] +
+               df["leanSAT-sat"] + df["leanSAT-lrat"] + df["leanSAT-kc"])
+    df['tot-sum'] = tot_sum
+    df_sorted = df.sort_values(by="tot-sum")
+    # print(df_sorted)
+    ax_right = ax.twinx()
+
+    bottom = np.zeros_like(df_sorted["leanSAT-ld"])
+    ax.bar(
+        x,
+        np.divide(df_sorted["leanSAT-ld"] + df_sorted["leanSAT-rr"] +
+                  df_sorted["leanSAT-ac"] + df_sorted["leanSAT-af"] +
+                  df_sorted["leanSAT-ecs"] + df_sorted["leanSAT-kc"], df_sorted['tot-sum'])*100,
+        width,
+        label="rewriting + kernel checking",
+        bottom=bottom,
+        color=dark_blue, 
+        edgecolor = dark_blue
+    )
+    bottom += np.divide(df_sorted["leanSAT-ld"] + df_sorted["leanSAT-rr"] +
+                        df_sorted["leanSAT-ac"] + df_sorted["leanSAT-af"] +
+                        df_sorted["leanSAT-ecs"] + df_sorted["leanSAT-kc"], df_sorted['tot-sum'])*100
+    ax.bar(
+        x,
+        np.divide(df_sorted["leanSAT-bb"] + df_sorted["leanSAT-sat"],
+                  df_sorted['tot-sum'])*100,
+        width,
+        label="bit-blasting and SAT solving",
+        bottom=bottom,
+        color=light_gray,
+        edgecolor=light_gray
+    )
+    bottom += np.divide(df_sorted["leanSAT-bb"] + df_sorted["leanSAT-sat"], df_sorted['tot-sum'])*100
+    ax.bar(
+        x,
+        np.divide(df_sorted["leanSAT-lrat"], df_sorted['tot-sum'])*100,
+        width,
+        label="LRAT",
+        bottom=bottom,
+        color=dark_green,
+        edgecolor=dark_green
+    )
+    ax_right.plot(
+        x,
+        df_sorted['tot-sum'],
+        width,
+        label="total time",
+        color=black, 
+    )
+    ax_right.set_yscale("log")
+    ax.set_xticks(np.arange(0,len(df_sorted), 10**np.floor(np.log10(len(df_sorted)))))
+    # ax.set_title("Time to solve theorems from "+bm, pad=20)
+    ax.set_ylabel("Distribution [%]", rotation="horizontal", horizontalalignment="left", y=1.05)
+    ax_right.set_ylabel("Time [ms]", rotation="horizontal", horizontalalignment="right", y=1.08)
+    ax.legend(ncols=4, frameon=False, bbox_to_anchor= [0.5, 1], loc='lower center')
+    plt.gca().spines['right'].set_visible(True) 
     save(fig, "leanSAT_stacked_smtlib_" + bm.split(".")[0] + ".pdf")
 
 
 def leanSAT_smtlib_sat_stacked_perc(df: pd.DataFrame, bm, type):
-    save(fig, "leanSAT_stacked_smtlib_" + bm.split(".")[0] + ".pdf")
+    if type == 'h_tot':
+        matplotlib.rcParams["figure.figsize"] = 14, 5
+    x = np.arange(len(df["benchmark"]))
+    width = 1.0
+    fig, ax = plt.subplots()
+    # df_sorted = df.sort_values(by="leanSAT")
+    # print(df_sorted)
+    df = df.apply(lambda x: x / 10**6 if x.name in
+      ['leanSAT-ld', 'leanSAT-rr', 'leanSAT-ac',
+       'leanSAT-af', 'leanSAT-ecs', 'leanSAT-bb',
+       'leanSAT-sat', 'leanSAT-lrat'] else x)
+    tot_sum = (df["leanSAT-ld"] + df["leanSAT-rr"] + df["leanSAT-ac"] +
+               df["leanSAT-af"] + df["leanSAT-ecs"] + df["leanSAT-bb"] +
+               df["leanSAT-sat"] + df["leanSAT-lrat"])
+    df['tot-sum'] = tot_sum
+    df_sorted = df.sort_values(by="tot-sum")
+    # print(df_sorted)
+    ax_right = ax.twinx()
 
+    bottom = np.zeros_like(df_sorted["leanSAT-ld"])
+    ax.bar(
+        x,
+        np.divide(df_sorted["leanSAT-ld"] + df_sorted["leanSAT-rr"] +
+                  df_sorted["leanSAT-ac"] + df_sorted["leanSAT-af"] +
+                  df_sorted["leanSAT-ecs"], df_sorted['tot-sum'])*100,
+        width,
+        label="rewriting",
+        bottom=bottom,
+        color=dark_blue, 
+        edgecolor = dark_blue
+    )
+    bottom += np.divide(df_sorted["leanSAT-ld"] + df_sorted["leanSAT-rr"] +
+                        df_sorted["leanSAT-ac"] + df_sorted["leanSAT-af"] +
+                        df_sorted["leanSAT-ecs"], df_sorted['tot-sum'])*100
+    ax.bar(
+        x,
+        np.divide(df_sorted["leanSAT-bb"] + df_sorted["leanSAT-sat"],
+                  df_sorted['tot-sum'])*100,
+        width,
+        label="bit-blasting and SAT solving",
+        bottom=bottom,
+        color=light_gray,
+        edgecolor=light_gray
+    )
+    ax_right.plot(
+        x,
+        df_sorted['tot-sum'],
+        width,
+        label="total time",
+        color=black, 
+    )
+    ax_right.set_yscale("log")
+    ax.set_xticks(np.arange(0,len(df_sorted), 10**np.floor(np.log10(len(df_sorted)))))
+    # ax.set_title("Time to solve theorems from "+bm, pad=20)
+    ax.set_ylabel("Distribution [%]", rotation="horizontal", horizontalalignment="left", y=1.05)
+    ax_right.set_ylabel("Time [ms]", rotation="horizontal", horizontalalignment="right", y=1.08)
+    ax.legend(ncols=4, frameon=False, bbox_to_anchor= [0.5, 1], loc='lower center')
+    plt.gca().spines['right'].set_visible(True) 
+    save(fig, "leanSAT_stacked_smtlib_" + bm.split(".")[0] + ".pdf")
 
 def bv_decide_tot_stacked(df, bm):
     x = np.arange(len(df["solved_bv_decide_times_average"]))
@@ -419,9 +544,132 @@ def cumul_solving_time(df, tool1, tool2, bm):
 
 
 def cumul_solving_time_smtlib(df, name):
+    fig, ax = plt.subplots(figsize=(14, 3))
+    plt.rcParams["path.simplify_threshold"] = 1
+    # only consider rows where the problem was actually sat/unsat
+    sorted1 = np.sort(df['time_cpu_bw'])
+    sorted2 = np.sort(df['time_cpu_lwt'])
+    sorted3 = np.sort(df['time_cpu_lw'])
+    sorted4 = np.sort(df['time_cpu_coq'])
+    cumtime1 = [0]
+    cumtime1.extend(np.cumsum(sorted1))
+    cumtime2 = [0]
+    cumtime2.extend(np.cumsum(sorted2))
+    cumtime3 = [0]
+    cumtime3.extend(np.cumsum(sorted3))
+    cumtime4 = [0]
+    cumtime4.extend(np.cumsum(sorted4))
+    ax.plot(
+        cumtime1,
+        np.arange(0, len(df['time_cpu_bw']) + 1),
+        marker="o",
+        color=dark_green,
+        label='bitwuzla',
+    )
+    ax.plot(
+        cumtime2,
+        np.arange(0, len(df['time_cpu_lwt']) + 1),
+        marker="x",
+        color=light_blue,
+        label='bv_decide (no kernel)',
+    )
+    ax.plot(
+        cumtime3,
+        np.arange(0, len(df['time_cpu_lw']) + 1),
+        marker="x",
+        color=dark_blue,
+        label='bv_decide (+ kernel)',
+    )
+    ax.plot(
+        cumtime4,
+        np.arange(0, len(df['time_cpu_coq']) + 1),
+        marker="*",
+        color=dark_red,
+        label='CoqQFBV',
+    )
+    ax.set_xscale('log')
+    ax.set_xlabel("Time [s] - "+name.upper()+" results")
+    ax.set_ylabel("Problems solved", rotation="horizontal", ha="left", y=1)
+    # add a line to highlight the difference in #solved problems between the tools
+    # x_start = cumtime1[len([x for x in np.array(df['time_cpu_lw']) if x > 0])-10]
+    # x_end = np.sum(df['time_cpu_lw'])
+    # y_value = len([x for x in np.array(df['time_cpu_lw']) if x > 0])
+    # ax.plot([x_start, x_end], [len([x for x in np.array(df['time_cpu_lw']) if x > 0]), len([x for x in np.array(df['time_cpu_lw']) if x > 0])], color='black', linestyle='--', linewidth=1)
+    # ax.annotate(f"10^{int(np.round(np.log10(x_end - x_start)))} s", 
+    #         xy=(x_start+(x_end-x_start)/2, y_value), 
+    #         xytext=((x_end-x_start)/2, y_value * 1.01),
+    #         fontsize=12, color='black')
+    
+    # x_start_c = cumtime1[len([x for x in np.array(df['time_cpu_coq']) if x > 0])-10]
+    # x_end_c = np.sum(df['time_cpu_coq'])
+    # y_value_c = len([x for x in np.array(df['time_cpu_coq']) if x > 0])
+    # ax.plot([x_start_c, x_end_c], [len([x for x in np.array(df['time_cpu_coq']) if x > 0]), len([x for x in np.array(df['time_cpu_coq']) if x > 0])], color='black', linestyle='--', linewidth=1)
+    # ax.annotate(f"10^{int(np.round(np.log10(x_end_c - x_start_c)))} s", 
+    #         xy=(x_start_c, y_value_c), 
+    #         xytext=((x_end_c-x_start_c)/2, y_value_c * 1.01),
+    #         fontsize=12, color='black')
+    
+    # y_start = next((i for i, x in enumerate(cumtime2) if x > np.sum(df['time_cpu_bw'])), -1)
+    # y_end = len(df['time_cpu_bw'])
+    # print(cumtime1[-1])
+    # x_value = np.sum(df['time_cpu_bw'])
+    # ax.plot([x_value, x_value], [y_start, y_end], color='black', linestyle='dashed', linewidth=1)
+    # y_end = next((i for i, x in enumerate(cumtime2) if x > np.sum(df['time_cpu_bw'])), -1)
+    # y_start = 0
+    # print(cumtime1[-1])
+    # x_value = np.sum(df['time_cpu_bw'])
+    # ax.plot([x_value, x_value], [y_start, y_end], color='black', linestyle='solid', linewidth=2)
+    # ax.annotate(f"{int(y_end)} problems", 
+    #         xy=(x_start, y_value), 
+    #         xytext=(x_value*1.1, y_end * 0.95),
+    #         fontsize=12, color='black')
+    
+    # x_start_c = cumtime1[len([x for x in np.array(df['time_cpu_coq']) if x > 0])-10]
+    # x_end_c = np.sum(df['time_cpu_coq'])
+    # y_value_c = len([x for x in np.array(df['time_cpu_coq']) if x > 0])
+    # ax.plot([x_start_c, x_end_c], [len([x for x in np.array(df['time_cpu_coq']) if x > 0]), len([x for x in np.array(df['time_cpu_coq']) if x > 0])], color='black', linestyle='--', linewidth=1)
+    # ax.annotate(f"10^{int(np.round(np.log10(x_end_c - x_start_c)))} s", 
+    #         xy=(x_start_c, y_value_c), 
+    #         xytext=((x_end_c-x_start_c)/2, y_value_c * 1.01),
+    #         fontsize=12, color='black')
+
+    # ax.axvline(np.sum(df['time_cpu_bw']), color = black, linestyle = '--')
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
+    # ax.legend(loc="center right", ncols=1, frameon=False, bbox_to_anchor=(1.2, 0.5))
+    ax.legend(loc="upper left", ncols=1, frameon=False)
     save(fig, "cumul_problems_smtlib_"+name+".pdf")
 
 def scatter_solving_time_smtlib(df_sat, df_unsat): 
+    fig, ax = plt.subplots(1, 2, figsize=(14, 4), sharey=True)
+
+    ax[0].scatter(df_sat['time_cpu_bw'], df_sat['time_cpu_lw'], c = "#beaed4",  label = 'SAT results', marker = ".")
+    ax[1].scatter(df_unsat['time_cpu_bw'], df_unsat['time_cpu_lw'], c = "#fdc086",  label = 'UNSAT results', marker=".")
+
+    ax[0].set_xlabel("SAT")
+    ax[1].set_xlabel("UNSAT")
+
+    time_sat_min = min(df_sat["time_cpu_bw"].min(), df_sat["time_cpu_lw"].min())
+    time_sat_max = max(df_sat["time_cpu_bw"].max(), df_sat["time_cpu_lw"].max())
+
+    time_unsat_min = min(df_unsat["time_cpu_bw"].min(), df_unsat["time_cpu_lw"].min())
+    time_unsat_max = max(df_unsat["time_cpu_bw"].max(), df_unsat["time_cpu_lw"].max())
+
+    time_min = min(time_sat_min, time_sat_max)
+    time_max = max(time_sat_max, time_unsat_max)
+
+    ax[0].set_ylim(10**(np.floor(np.log10(time_min))), 10**(np.ceil(np.log10(time_max))))
+    ax[1].set_ylim(10**(np.floor(np.log10(time_min))), 10**(np.ceil(np.log10(time_max))))
+    ax[0].plot([df_sat["time_cpu_bw"].min(), df_sat["time_cpu_bw"].max()], [df_sat["time_cpu_bw"].min(), df_sat["time_cpu_bw"].max()], c = black)
+    ax[1].plot([df_unsat["time_cpu_bw"].min(), df_unsat["time_cpu_lw"].max()], [df_unsat["time_cpu_bw"].min(), df_unsat["time_cpu_lw"].max()], c = black)
+    ax[0].set_xscale('log')
+    ax[0].set_yscale('log')
+    ax[1].set_xscale('log')
+    ax[1].set_yscale('log')
+    # ax[1].text(1.2, 0, "Time [ms]\nbitwuzla", transform=ax[1].transAxes, ha="right")
+    fig.text(0.5, 0.04, "Time [ms] - bitwuzla", ha="center", va="center")
+    ax[0].set_ylabel("Time [s] - bv_decide", rotation="horizontal", ha="left", y=1.05)
+    # ax.legend(loc="lower center", ncols=2, frameon=False)
     save(fig, "scatter_smtlib.pdf")
 
 def scatter_solving_time_instcombine(df):
@@ -528,9 +776,38 @@ def plot_instcombine():
 
 
 def plot_smtlib():
+    # filter out results that are unknown for both solvers
+    df_bitwuzla_46k = pd.read_csv(SMTLIBDataDir + 'bitwuzla_46k.csv')
+    df_leanwuzla_46k = pd.read_csv(SMTLIBDataDir + 'leanwuzla_46k.csv')
+    df_leanwuzla_trusted_46k = pd.read_csv(SMTLIBDataDir + 'leanwuzla_trusted_46k.csv')
+    df_coq_46k = pd.read_csv(SMTLIBDataDir + 'coq_46k.csv')
+    df_bitwuzla_46k_no_unknowns = df_bitwuzla_46k[df_bitwuzla_46k['result'] != 'unknown']
+    df_leanwuzla_46k_no_unknowns = df_leanwuzla_46k[df_leanwuzla_46k['result'] != 'unknown']
+    df_leanwuzla_trusted_46k_no_unknowns = df_leanwuzla_trusted_46k[df_leanwuzla_trusted_46k['result'] != 'unknown']
+    df_leanwuzla_trusted_46k_no_unknowns = df_leanwuzla_trusted_46k_no_unknowns.rename(columns = lambda x : x + '_lwt' if x != 'benchmark' else x)
+    df_coq_46k_no_unknowns = df_coq_46k[df_coq_46k['result'] != 'unknown']
+    df_coq_46k_no_unknowns = df_coq_46k_no_unknowns.rename(columns = lambda x : x + '_coq' if x != 'benchmark' else x)
+    merged_df = pd.merge(df_bitwuzla_46k_no_unknowns, df_leanwuzla_46k_no_unknowns, how = 'outer', on = 'benchmark', suffixes = ('_bw', '_lw'))
+    merged_df = pd.merge(merged_df, df_leanwuzla_trusted_46k_no_unknowns, how = 'outer', on = 'benchmark')
+    merged_df_tot = pd.merge(merged_df, df_coq_46k_no_unknowns, how = 'outer', on = 'benchmark')
+    df_sat = merged_df_tot.loc[lambda x: (x['result_bw'] == 'sat') | (x['result_lw'] == 'sat') | (x['result_coq'] == 'sat')]
+    df_unsat = merged_df_tot.loc[lambda x: (x['result_bw'] == 'unsat') | (x['result_lw'] == 'unsat') | (x['result_coq'] == 'unsat')]
+    # separate sat from unsat 
+    cumul_solving_time_smtlib(df_sat, 'sat')
+    cumul_solving_time_smtlib(df_unsat, 'unsat')
+    scatter_solving_time_smtlib(df_sat, df_unsat)
+    # plot stacked area plot
+    df_leanwuzla_46k_stats = pd.read_csv(SMTLIBDataDir + 'leanwuzla_46k_stats.csv')
+    df_sat_stats = df_leanwuzla_46k_stats[df_leanwuzla_46k_stats['leanSAT-res'] == 'sat']
+    df_unsat_stats = df_leanwuzla_46k_stats[df_leanwuzla_46k_stats['leanSAT-res'] == 'unsat']
+    leanSAT_smtlib_sat_stacked_perc(df_sat_stats, 'sat', 'i')
+    leanSAT_smtlib_unsat_stacked_perc(df_unsat_stats, 'unsat', 'i')
+    df_leanwuzla_trusted_46k_stats = pd.read_csv(SMTLIBDataDir + 'leanwuzla_trusted_46k_stats.csv')
+    df_sat_stats = df_leanwuzla_trusted_46k_stats[df_leanwuzla_trusted_46k_stats['leanSAT-res'] == 'sat']
+    df_unsat_stats = df_leanwuzla_trusted_46k_stats[df_leanwuzla_trusted_46k_stats['leanSAT-res'] == 'unsat']
+    leanSAT_smtlib_sat_stacked_perc(df_sat_stats, 'trusted_sat', 'i')
     leanSAT_smtlib_unsat_stacked_perc(df_unsat_stats, 'trusted_unsat', 'i')
-
-
+    
 def main():
     parser = argparse.ArgumentParser(
         prog="plot",
