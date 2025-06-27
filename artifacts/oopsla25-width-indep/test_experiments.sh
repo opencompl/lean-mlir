4081a8ad42c30a47540491409136865262d7026d
Léo Stefanesco <leo.lveb@gmail.com>
2025-06-27 16:22:38 +0100

    chore: add test script for artifact

--- artifacts/oopsla25-width-indep/test_experiments.sh
+++ artifacts/oopsla25-width-indep/test_experiments.sh
@@ -5,12 +5,12 @@ pushd $(git rev-parse --show-toplevel)
 echo $PWD
 
 pushd bv-evaluation
-./compare-automata-automata-circuit-jsonl.py --db compare.jsonl --prodrun  -j5 
+./compare-automata-automata-circuit-jsonl.py --db compare.jsonl -j4
 ./plot-automata-automata-circuit-jsonl.py compare.jsonl
 popd
 
 pushd SSA/Experimental/Bits/Fast/Dataset2
-./runner.py --db mba.sqlite3 -j10 --prod-run --timeout 120
+./runner.py --db mba.sqlite3 -j4 --num-tests 100 --timeout 120
 ./plotter.py mba.sqlite3
 
 popd
