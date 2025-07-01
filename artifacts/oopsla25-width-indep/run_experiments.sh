ab55fe60beb4c4a536ae4b3a58feeb176ff6962d
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-01 15:21:04 +0100

    chore: set explicit limits for each solver

--- artifacts/oopsla25-width-indep/run_experiments.sh
+++ artifacts/oopsla25-width-indep/run_experiments.sh
@@ -5,12 +5,12 @@ pushd $(git rev-parse --show-toplevel)
 echo $PWD
 
 pushd bv-evaluation
-./compare-automata-automata-circuit-jsonl.py --db compare.jsonl --prodrun  -j5 
+./compare-automata-automata-circuit-jsonl.py --db compare.jsonl --prodrun  -j5
 ./plot-automata-automata-circuit-jsonl.py compare.jsonl
 popd
 
 pushd SSA/Experimental/Bits/Fast/Dataset2
-./runner.py --db mba.sqlite3 -j10 --prod-run --timeout 540
+./runner.py --db mba.sqlite3 -j10 --timeout 540 --memout 8000 --mba 2500 --bv_decide 2500 --bv_automata_classic 2500 --kinduction_verified 1500
 ./plotter.py mba.sqlite3
 
 popd
