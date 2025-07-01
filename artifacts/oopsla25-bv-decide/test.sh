e2eed02f9cebf3524a0d3a85cdb7de1ff99fcfff
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-01 18:29:26 +0100

    chore: add a test file

---
+++ artifacts/oopsla25-bv-decide/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd $(git rev-parse --show-toplevel)
+cd bv-evaluation/
+
+python3 compare.py hackersdelight -j16 -r1
+python3 compare.py instcombine -j16 -r1
+python3 compare.py smtlib -j8 -t60 -m300
+
+python3 collect.py hackersdelight
+python3 collect.py instcombine
+python3 collect.py smtlib
+
+python3 plot.py hackersdelight
+python3 plot.py instcombine
+python3 plot.py smtlib
+
+python3 collect-stats-bv-decide.py
