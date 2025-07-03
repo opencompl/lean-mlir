82076d64bc8ecbe39bf425934367d65384dde41b
Alex Keizer <alex@keizer.dev>
2025-07-03 15:03:52 +0100

    fix: actually include changes to flake.nix to enable docker buikld

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 ARG NIXOS_VERSION="25.05"
 ARG SYSTEM="x86_64-linux"
-FROM nixpkgs/nix:nixos-$NIXOS_VERSION-$SYSTEM
+FROM nixpkgs/nix:nixos-$NIXOS_VERSION-$SYSTEM AS opencompl/lean-mlir
 
 # Enable flakes and set up nix configuration
 RUN mkdir -p /etc/nix && \
@@ -20,3 +20,4 @@ RUN nix profile install ".#"
 COPY lean-toolchain lakefile.* ./
 RUN lean-mlir-init-env
 
+COPY . ./
--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    lean.url = "github:lenianiva/lean4-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, lean }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -40,8 +39,37 @@
 
         pythonEnv = pkgs.python3.withPackages pythonPackages;
 
+        customShellHook = pkgs.writeShellScriptBin "lean-mlir-init-env" ''
+          # Ensure the right Lean toolchain gets installed
+          # And that dependencies are checked out to the right versions,
+          # although we do not check any cached build outputs
+          MATHLIB_NO_CACHE_ON_UPDATE=1 lake update --no-cache
+        '';
+
+        shellPkgs = with pkgs; [
+          pythonEnv
+          elan
+          customShellHook
+        ];
+
       in
       {
+        devShell = pkgs.mkShell {
+          buildInputs = shellPkgs;
+          shellHook = ''
+            lean-mlir-init-env
+          '';
+        };
+
+        packages.default = pkgs.buildEnv {
+          name = "LeanMLIR build environment";
+          paths = shellPkgs;
+          pathsToLink = [
+            "/bin"
+            "/share"
+          ];
+        };
+
         # nix run .#test-experiments
         apps.test-experiments = {
           type = "app";
