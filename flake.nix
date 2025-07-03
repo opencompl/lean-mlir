c5b5f8b4de7dda6d9f4edb7ddebfd198b6097031
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-03 11:40:27 +0100

    chore: setup a flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    lean.url = "github:leanprover/lean-nix";
+    lean.url = "github:lenianiva/lean4-nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, lean }:
@@ -12,40 +12,9 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ lean.overlays.default ];
-        };
-
-        leanToolchain = pkgs.lean-toolchain.override {
-          leanVersion = "nightly-2025-06-27";
-        };
-
-        xdsl = pkgs.python3Packages.buildPythonPackage rec {
-            pname = "xdsl";
-            version = "0.22.0";
-
-            src = pkgs.fetchPypi {
-                inherit version;
-                pname = "xdsl";
-                hash = "sha256-+t8Lic/fm7u7PNEQs9DtSwoVnDjBjdy43f62qtkzPMs=";
-            };
-
-            propagatedBuildInputs = with pkgs.python3Packages; [
-                immutabledict
-                typing-extensions
-                ordered-set
-            ];
-
-            doCheck = false;
-
-            meta = with pkgs.lib; {
-                description = "xDSL: A Python library for building compilers and DSLs";
-                homepage = "https://github.com/xdslproject/xdsl";
-                license = licenses.asl20;
-            };
         };
 
         pythonPackages = ps: with ps; [
-          xdsl
           matplotlib
           pandas
           polars
@@ -65,7 +34,7 @@
           six
           tabulate
           tzdata
-          visidata
+          # visidata
           zipp
         ];
 
@@ -75,7 +44,6 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            leanToolchain
             pythonEnv
             pkgs.unzip
             pkgs.black
@@ -87,15 +55,25 @@
           ];
         };
 
-        apps.run-experiments = {
-          type = "app";
-          program = pkgs.writeShellScript "run-experiments-script" ''
-            #!${pkgs.bash}/bin/bash
-            set -euo pipefail
-            export PATH=${pkgs.lib.makeBinPath [ pythonEnv pkgs.coreutils ]}:$PATH
-            chmod +x artifacts/oopsla25-width-indep/run_experiments.sh
-            ./artifacts/oopsla25-width-indep/run_experiments.sh
-          '';
-        };
+      apps.test-experiments = {
+        type = "app";
+        program = "${pkgs.writeShellScriptBin "run-experiments-script" ''
+          set -euo pipefail
+          export PATH=${pkgs.lib.makeBinPath [ pythonEnv pkgs.coreutils ]}:$PATH
+          chmod +x artifacts/oopsla25-width-indep/test_experiments.sh
+          ./artifacts/oopsla25-width-indep/test_experiments.sh
+        ''}/bin/run-experiments-script";
+      };
+
+      apps.run-experiments = {
+        type = "app";
+        program = "${pkgs.writeShellScriptBin "run-experiments-script" ''
+          set -euo pipefail
+          export PATH=${pkgs.lib.makeBinPath [ pythonEnv pkgs.coreutils ]}:$PATH
+          chmod +x artifacts/oopsla25-width-indep/run_experiments.sh
+          ./artifacts/oopsla25-width-indep/run_experiments.sh
+        ''}/bin/run-experiments-script";
+      };
+
       });
 }
