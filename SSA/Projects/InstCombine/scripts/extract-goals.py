ecfb3d05514f997ea411fe1475e3ef81808ebaf2
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-07 13:42:06 +0100

    feat: add script to extract goals

--- SSA/Projects/InstCombine/scripts/extract-goals.py
+++ SSA/Projects/InstCombine/scripts/extract-goals.py
@@ -1,19 +1,23 @@
-# This script extracts goal states from Lean files in the SSA/Projects/InstCombine/tests/proofs directory.
 #!/usr/bin/env python3
+# This script extracts goal states from Lean files in the
+# 'SSA/Projects/InstCombine/tests/proofs' directory.
+# This ensures that there is one goal state per file,
+# which makes scripting much easier.
 import concurrent.futures
 import subprocess
 import datetime
 import platform
 import argparse
 import shutil
+from typing import List
 from pathlib import Path
+from dataclasses import dataclass, asdict
 import pandas as pd
 import os
 import re
 import logging
 import random
 import json
-from dataclasses import dataclass, asdict
 
 ROOT_DIR = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
 BENCHMARK_DIR = ROOT_DIR + '/SSA/Projects/InstCombine/tests/proofs/'
@@ -22,7 +26,7 @@ GOALS_DIR = ROOT_DIR + '/SSA/Projects/InstCombine/tests/goals/'
 STATUS_FAIL = "❌"
 STATUS_GREEN_CHECK = "✅"
 STATUS_SUCCESS = "🎉"
-STATUS_PROCESSING = "⌛️"
+STATUS_PROCESSING = "🕒"
 
 def sed():
     if platform.system() == "Darwin":
@@ -39,31 +43,31 @@ class Theorem:
 def extract_theorems(raw : str) -> List[Theorem]:
     out = []
     raw = raw.strip()
-    theorems = [x for x in re.split(r'(?=theorem)', raw) if x]
-    for t in theorems:
+    ss = re.findall(r'(theorem.*?:= sorry)', raw, re.DOTALL)
+    for s in ss:
         # grab the theorem name.
-        thm_name = re.search(r'\btheorem\s+([\w._]+)', t).group(1)
+        thm_name = re.search(r'\btheorem\s+([\w._]+)', s).group(1)
         if not thm_name:
-            logging.warning(f"{fileTitle}({file_num}): Failed to extract theorem name from theorem string {t}")
+            logging.warning(f"{fileTitle}({file_num}): Failed to extract theorem name from theorem string {s}")
             continue
-        out.append(Theorem(name=thm_name, text=t))
+        thm = Theorem(name=thm_name, text=s.strip())
+        out.append(thm)
     return out
 
 
-def run_file(db : str, file: str, file_num : int, timeout : int):
+def run_file(db : str, file: str, file_num : int, nfiles : int, timeout : int):
     file_path = BENCHMARK_DIR + file
     fileTitle = file.split('.')[0]
-    logging.info(f"{fileTitle}: writing 'bv_bench_automata' tactic into file #{file_num}.")
     EXTRACT_GOALS = "extract_goals"
+    logging.info(f"{fileTitle}: writing '{EXTRACT_GOALS}' tactic into file #{file_num}.")
     subprocess.Popen(f'{sed()} -i -E \'s,simp_alive_benchmark,{EXTRACT_GOALS},g\' ' + file_path, cwd=ROOT_DIR, shell=True).wait()
-    logging.info(f"{fileTitle}: {STATUS_PROCESSING}")
 
     cmd = 'lake lean ' + file_path
-    logging.info(f"{fileTitle}({file_num}): running '{cmd}'")
+    logging.info(f"{fileTitle}({file_num}/{nfiles}): running '{cmd}'")
 
     # TODO: can check that file exists to skip.
     p = subprocess.Popen(cmd, cwd=ROOT_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True)
-    logging.info(f"{fileTitle}({file_num}): running...")
+    logging.info(f"{fileTitle}({file_num}/{nfiles}): running...")
     try:
         out, err = p.communicate(timeout=timeout)
     except subprocess.TimeoutExpired as e:
@@ -71,25 +75,27 @@ def run_file(db : str, file: str, file_num : int, timeout : int):
         p.kill()
         return False
 
-    logging.info(f"{fileTitle}({file_num}): done processing.")
+    logging.info(f"{fileTitle}({file_num}/{nfiles}): done processing.")
     if p.returncode != 0:
-        logging.error(f"{fileTitle}({file_num}) {STATUS_FAIL}: Expected return code of 0, found {p.returncode}")
+        logging.error(f"{fileTitle}({file_num}/{nfiles}) {STATUS_FAIL}: Expected return code of 0, found {p.returncode}")
 
-    # split 'out' into parts that are delimited by 'theorem ...'.
+    # split 'out' into parts that are delimited by 'theorem ... := sorry'.
     theorems = extract_theorems(out)
     with open(db, "a") as f:
         f.write(json.dumps({
             "fileTitle": fileTitle,
-            "output": out,
+            "contents": out,
             "theorems": [asdict(t) for t in theorems]
             }) + "\n")
 
     for t in theorems:
-        with open(Path(GOALS_DIR) / f"{fileTitle}_{t.name}.lean", 'w') as f:
-            f.write(t)
-            logging.info(f"{fileTitle}({file_num}) {STATUS_SUCESS}: Extracted theorem {t.name}")
+        path = Path(GOALS_DIR) / f"{fileTitle}_{t.name}.lean"
+        with open(path, 'w') as f:
+            f.write(t.text)
+            logging.info(f"{fileTitle}({file_num}/{nfiles}): {STATUS_GREEN_CHECK} Extracted {path}")
+    return True
 
-def process(db : str, jobs: int, nfiles: int, timeout : int)
+def process(db : str, jobs: int, nfiles: int, timeout : int):
     tactic_auto_path = f'{ROOT_DIR}/SSA/Projects/InstCombine/TacticAuto.lean'
 
     if os.path.exists(db):
@@ -101,18 +107,13 @@ def process(db : str, jobs: int, nfiles: int, timeout : int)
     assert gco.returncode == 0, f"git checkout -- {BENCHMARK_DIR} should succeed."
 
 
-    logging.info(f"Running a 'lake exe cache get && lake build'.")
-    lake = subprocess.Popen(f'lake exe cache get && lake build', cwd=ROOT_DIR, shell=True)
-    lake.wait()
-    assert lake.returncode == 0, f"lake build should succeed before running evaluation."
-
     raw_files = list(os.listdir(BENCHMARK_DIR))
 
     files = []
     for ix, file in enumerate(raw_files):
         if "_proof" in file:
             files.append(file)
-            if len(files) > ntests:
+            if len(files) > nfiles:
                 break # quit if we are not doing a production run after 5 files.
 
     total = len(files)
@@ -121,7 +122,7 @@ def process(db : str, jobs: int, nfiles: int, timeout : int)
     future2file = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=jobs) as executor:
         for ix, file in enumerate(files):
-            future = executor.submit(run_file, db, file, ix + 1, timeout)
+            future = executor.submit(run_file, db, file, ix + 1, total, timeout)
             future2file[future] = file
 
     total = len(future2file)
@@ -131,13 +132,15 @@ def process(db : str, jobs: int, nfiles: int, timeout : int)
         try:
             success = future.result()
         except Exception as exc:
+            success = False
             logging.error('%r FAILED with exception: %s' % (file, exc))
         else:
             if not success:
                 logging.error('%r FAILED run' % (file))
 
         percentage = ((ix + 1) / total) * 100
-        logging.info(f'{STATUS_GREEN_CHECK} completed {file} ({percentage:.1f}%)')
+        status_symbol = STATUS_SUCCESS if success else STATUS_FAIL
+        logging.info(f'{status_symbol} completed {file} ({percentage:.1f}%)')
         num_completed += 1
         logging.info(f"total #files processed: {num_completed}/{total}")
 
@@ -153,8 +156,8 @@ def setup_logging(db_name : str):
 if __name__ == "__main__":
   current_time = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
   nproc = os.cpu_count()
-  parser = argparse.ArgumentParser(prog='compare-automata-automata-circuit')
-  default_db = f'automata-circuit-{current_time}.jsonl'
+  parser = argparse.ArgumentParser(prog='extract-goals')
+  default_db = f'extract-goals-{current_time}.jsonl'
   parser.add_argument('--db', default=default_db, help='path to jsonl database')
   parser.add_argument('-j', '--jobs', type=int, default=1)
   parser.add_argument('--nfiles', type=int, default=4, help="number of files to extract")
