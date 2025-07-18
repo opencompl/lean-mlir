2a6c1dd76ac1ec6f793c5fd661935ec43ac1dea1
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-18 14:51:43 +0100

    chore: fix PR

--- SSA/Projects/InstCombine/scripts/test-goals-not-updated.sh
+++ SSA/Projects/InstCombine/scripts/test-goals-not-updated.sh
@@ -3,7 +3,7 @@ set -o xtrace
 set -e # fail on errors.
 
 cd "$(git rev-parse --show-toplevel)"
-python3 SSA/Projects/InstCombine/scripts/extract-goals.py -n 500000 -j20
+python3 SSA/Projects/InstCombine/scripts/extract-goals.py --nfiles 500000 -j20
 CHANGED=$(git diff SSA/Projects/InstCombine/scripts)
 
 echo "$CHANGED"
