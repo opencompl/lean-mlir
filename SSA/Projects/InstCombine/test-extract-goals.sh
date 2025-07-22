4097bed9f7422e4d0c6f0c6e8fb18388d9f429c9
Siddharth Bhat <siddu.druid@gmail.com>
2025-07-22 09:31:10 +0100

    chore: undo changes made by extraction as well

--- SSA/Projects/InstCombine/test-extract-goals.sh
+++ SSA/Projects/InstCombine/test-extract-goals.sh
@@ -6,6 +6,7 @@ pushd "$(git rev-parse --show-toplevel)" || exit 1
 
 python3 SSA/Projects/InstCombine/scripts/extract-goals.py --nfiles 9000 -j7
 OUT=$(git diff SSA/Projects/InstCombine/tests/goals/)
+git checkout -- SSA/Projects/InstCombine/ # undo all changes made by extraction.
 echo "$OUT"
 if [[ -n "$OUT" ]]; then
   echo "Goals have changed. Please commit the changes. ❌"
