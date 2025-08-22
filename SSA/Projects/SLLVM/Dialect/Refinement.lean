11d42f0d79967cfbd0773c70a1e9573f39cc448b
Alex Keizer <alex@keizer.dev>
2025-08-22 11:08:20 +0100

    drop duplicate Dialect

--- SSA/Projects/SLLVM/Dialect/Refinement.lean
+++ SSA/Projects/SLLVM/Dialect/Refinement.lean
@@ -8,16 +8,6 @@ namespace LeanMLIR.SLLVM
 
 open InstCombine.LLVM.Ty (bitvec)
 
-scoped instance : Refinement (BitVec w) := .ofEq
-@[simp, simp_llvm_split] theorem bv_isRefinedBy_iff (x y : BitVec w) : x ⊑ y ↔ x = y := by rfl
--- ^^ declare that for pure bitvectors, refinement is just equality
-
-instance : DialectHRefinement SLLVM SLLVM where
-  MonadIsRefinedBy := inferInstanceAs <| HRefinement (EffectM _) (EffectM _)
-  IsRefinedBy := @fun (.bitvec <| .concrete v) (.bitvec <| .concrete w) => ⟨
-    fun (x y : LLVM.IntW _) => ∃ h : v = w, x ⊑ h ▸ y
-  ⟩
-
 @[simp, simp_denote]
 instance instRefinement : DialectHRefinement SLLVM SLLVM where
   MonadIsRefinedBy := inferInstanceAs <| HRefinement (EffectM _) (EffectM _)
