0af5f9c1ca64e8dc07c8bfba8487801f3662e8d8
luisacicolini <luisacicolini@gmail.com>
2025-08-25 15:28:18 +0100

    chore: sub

--- SSA/Projects/LLVMRiscV/Pipeline/srl.lean
+++ SSA/Projects/LLVMRiscV/Pipeline/srl.lean
@@ -6,51 +6,10 @@ import SSA.Projects.LLVMRiscV.Pipeline.mkRewrite
 open LLVMRiscV
 
 /-!
-  This file implements the lowering for the `llvm.shl` instruction for types: i8, i16, i32, i64.
+  This file implements the lowering for the `llvm.srl` instruction for types: i32, i64.
 -/
 
-@[simp_denote]
-def lshr_llvm_no_flag := [LV| {
-    ^entry (%x: i64, %amount: i64):
-    %1 = llvm.lshr %x, %amount : i64
-    llvm.return %1 : i64
-  }]
-
-@[simp_denote]
-def srl_riscv := [LV| {
-    ^entry (%x: i64, %amount: i64):
-    %base = "builtin.unrealized_conversion_cast"(%x) : (i64) -> (!i64)
-    %shamt = "builtin.unrealized_conversion_cast"(%amount) : (i64) -> (!i64)
-    %res = srl %base, %shamt : !i64
-    %y= "builtin.unrealized_conversion_cast"(%res) : (!i64) -> (i64)
-    llvm.return %y : i64
-  }]
-
-/- # SRL, exact  -/
-
-@[simp_denote]
-def llvm_srl_lower_riscv : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
-  lhs := lshr_llvm_no_flag
-  rhs := srl_riscv
-
-@[simp_denote]
-def lshr_llvm_exact := [LV| {
-    ^entry (%x: i64, %amount: i64):
-    %1 = llvm.lshr exact %x, %amount : i64
-    llvm.return %1 : i64
-  }]
-
-def llvm_srl_lower_riscv_exact : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
-  lhs := lshr_llvm_exact
-  rhs := srl_riscv
-
-def srl_match : List (Σ Γ, Σ ty, PeepholeRewrite LLVMPlusRiscV Γ ty) :=
-  List.map (fun x =>  mkRewrite (LLVMToRiscvPeepholeRewriteRefine.toPeepholeUNSOUND x))
-  [llvm_srl_lower_riscv_exact, llvm_srl_lower_riscv]
-
-/- # SRL i32, not exact
-Logical right shift operation in LLVM: if exact flag is set,
-then returns poison if any nonzero bit is shifted  -/
+/-! ### i32 -/
 
 @[simp_denote]
 def lshr_llvm_no_flag_32 := [LV| {
@@ -74,14 +33,12 @@ def srl_riscv_32 := [LV| {
 --   lhs := lshr_llvm_no_flag_32
 --   rhs := srl_riscv_32
 
-/- # SRL, exact i32  -/
-
-@[simp_denote]
-def lshr_llvm_exact_32 := [LV| {
-    ^entry (%x: i32, %amount: i32):
-    %1 = llvm.lshr exact %x, %amount : i32
-    llvm.return %1 : i32
-  }]
+-- @[simp_denote]
+-- def lshr_llvm_exact_32 := [LV| {
+--     ^entry (%x: i32, %amount: i32):
+--     %1 = llvm.lshr exact %x, %amount : i32
+--     llvm.return %1 : i32
+--   }]
 
 -- def llvm_srl_lower_riscv_exact_32 : LLVMPeepholeRewriteRefine 32 [Ty.llvm (.bitvec 32), Ty.llvm (.bitvec 32)] where
 --   lhs := lshr_llvm_exact_32
@@ -90,3 +47,43 @@ def lshr_llvm_exact_32 := [LV| {
 -- def srl_match_32 : List (Σ Γ, Σ ty, PeepholeRewrite LLVMPlusRiscV Γ ty) :=
 --   List.map (fun x =>  mkRewrite (LLVMToRiscvPeepholeRewriteRefine.toPeepholeUNSOUND x))
 --   [llvm_srl_lower_riscv_exact_32, llvm_srl_lower_riscv_32]
+
+
+/-! ### i64 -/
+
+@[simp_denote]
+def lshr_llvm_no_flag_64 := [LV| {
+    ^entry (%x: i64, %amount: i64):
+    %1 = llvm.lshr %x, %amount : i64
+    llvm.return %1 : i64
+  }]
+
+@[simp_denote]
+def srl_riscv_64 := [LV| {
+    ^entry (%x: i64, %amount: i64):
+    %base = "builtin.unrealized_conversion_cast"(%x) : (i64) -> (!i64)
+    %shamt = "builtin.unrealized_conversion_cast"(%amount) : (i64) -> (!i64)
+    %res = srl %base, %shamt : !i64
+    %y= "builtin.unrealized_conversion_cast"(%res) : (!i64) -> (i64)
+    llvm.return %y : i64
+  }]
+
+@[simp_denote]
+def llvm_srl_lower_riscv_64 : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := lshr_llvm_no_flag_64
+  rhs := srl_riscv_64
+
+@[simp_denote]
+def lshr_llvm_exact_64 := [LV| {
+    ^entry (%x: i64, %amount: i64):
+    %1 = llvm.lshr exact %x, %amount : i64
+    llvm.return %1 : i64
+  }]
+
+def llvm_srl_lower_riscv_exact_64 : LLVMPeepholeRewriteRefine 64 [Ty.llvm (.bitvec 64), Ty.llvm (.bitvec 64)] where
+  lhs := lshr_llvm_exact_64
+  rhs := srl_riscv_64
+
+def srl_match_64 : List (Σ Γ, Σ ty, PeepholeRewrite LLVMPlusRiscV Γ ty) :=
+  List.map (fun x =>  mkRewrite (LLVMToRiscvPeepholeRewriteRefine.toPeepholeUNSOUND x))
+  [llvm_srl_lower_riscv_exact_64, llvm_srl_lower_riscv_64]
