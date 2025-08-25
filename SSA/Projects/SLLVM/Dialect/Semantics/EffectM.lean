30626ed5799a0a0934d881c9f7bcec370e52bc7f
Alex Keizer <alex@keizer.dev>
2025-08-25 17:41:47 +0100

    fix SLLVM proof automation :tada:

--- SSA/Projects/SLLVM/Dialect/Semantics/EffectM.lean
+++ SSA/Projects/SLLVM/Dialect/Semantics/EffectM.lean
@@ -39,8 +39,21 @@ theorem pure_eq (x : α) (s) : (pure x : EffectM _) s = .value (x, s) := rfl
 theorem bind_eq (x : EffectM α) (f : α → EffectM β) (s) :
     (x >>= f) s = x s >>= (fun (x, s) => f x s) := rfl
 
-@[simp, simp_denote] lemma run_pure : StateT.run (pure x : EffectM α) s = .value (x, s) := rfl
-@[simp, simp_denote] lemma run_ub : StateT.run (ub : EffectM α) s = .poison := rfl
+@[simp, simp_denote, simp_sllvm]
+lemma run_pure : StateT.run (pure x : EffectM α) s = .value (x, s) := rfl
+
+@[simp, simp_denote, simp_sllvm]
+lemma run_ub : StateT.run (ub : EffectM α) s = .poison := rfl
+
+@[simp, simp_denote, simp_sllvm]
+lemma run_bind (x : EffectM α) :
+    StateT.run (x >>= f : EffectM β) s
+    = StateT.run x s >>= (fun p => StateT.run (f p.1) p.2) :=
+  rfl
+
+@[simp, simp_denote, simp_sllvm]
+lemma run_map (x : EffectM α) :
+    StateT.run (f <$> x : EffectM β) s = (fun p => (f p.1, p.2)) <$> StateT.run x s := rfl
 
 /-! ## Refinement -/
 section Refinement
