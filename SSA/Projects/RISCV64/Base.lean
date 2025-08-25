5df5e3cb2fc649f902ae15c0f3c9b666848baed9
Tobias Grosser <tobias@grosser.es>
2025-08-25 07:34:10 +0100

    WIP

--- SSA/Projects/RISCV64/Base.lean
+++ SSA/Projects/RISCV64/Base.lean
@@ -378,7 +378,7 @@ instance : DialectSignature RV64 := ⟨Op.signature⟩
 
 def opToString (op : RISCV64.Op) : String :=
   let op  : String := match op with
-  | .li imm => "li"
+  | .li (_imm : BitVec 64) => "li"
   | .mulh => "mulh"
   | .mulhu => "mulhu"
   | .mulhsu => "mulhsu"
