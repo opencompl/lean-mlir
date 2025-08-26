80383f9b1de8ad2f771a5e987b4bc1999c2761c0
Alex Keizer <alex@keizer.dev>
2025-08-26 12:51:25 +0100

    feat: Context & HVector API (#1591)
    This PR extracts the API on `Ctxt` and `HVector` needed for multiple
    return values from #1511.
    
    In particular, for `Ctxt`:
    - We add `grind` tags to existing lemma for better automation
    - We prove that the GetElem instance is Lawful (which needed some moving
    around of lemmas)
    - Refactor `appendIn{l,r}` to use projections over pattern-matching, for
    easier unfolding
    - Add `Var.eq` as a convenient way to state equality of variables with
    different types (but in the same context)
    - Prove `ofFinCases`, a cases-principle in terms of `Fin`
    - Add assorted missing lemmas
    
    And for HVector:
    - Add `toListOf` to convert an HVector into a List (under certain
    conditions)
    - Add a way to append hvectors together
    - Add `idxOf?` to find elements in an HVector

--- SSA/Projects/InstCombine/AliveHandwrittenLargeExamples.lean
+++ SSA/Projects/InstCombine/AliveHandwrittenLargeExamples.lean
@@ -47,8 +47,9 @@ theorem alive_DivRemOfSelect (w : Nat) :
   simp_alive_split
   alive_auto
 
-/--info: 'AliveHandwritten.DivRemOfSelect.alive_DivRemOfSelect' depends on
-axioms: [propext, Quot.sound] -/
+/--
+info: 'AliveHandwritten.DivRemOfSelect.alive_DivRemOfSelect' depends on axioms: [propext, Classical.choice, Quot.sound]
+-/
 #guard_msgs in #print axioms alive_DivRemOfSelect
 
 end DivRemOfSelect
