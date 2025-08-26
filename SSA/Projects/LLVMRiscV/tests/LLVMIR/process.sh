0b74905de2acd80fc9cba9c2811e277241d7fef0
luisacicolini <luisacicolini@gmail.com>
2025-08-26 09:55:50 +0100

    chore: remove tests from changes

--- SSA/Projects/LLVMRiscV/tests/LLVMIR/process.sh
+++ SSA/Projects/LLVMRiscV/tests/LLVMIR/process.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#This script 
+
 # Usage: ./process.sh <input_file>
 cd $(dirname "$0")
 INPUT_FILE="$1"
@@ -9,14 +9,11 @@ if [[ -z "$INPUT_FILE" ]]; then
   exit 1
 fi
 
-BASENAME=$(basename "$INPUT_FILE" .mlir)
-OUT_MLIR="${BASENAME}_out.mlir"
-OUT1_MLIR="${BASENAME}_out1.mlir"
 # Translate LLVM IR to MLIR, optimize, and extract bb0
 mlir-translate -import-llvm "$INPUT_FILE" | \
 mlir-opt --mlir-print-op-generic | \
-./extractbb0.sh > ${OUT_MLIR}
+./extractbb0.sh > out.mlir
 
 # Navigate up to the appropriate directory and run opt tool
 cd ../../../../../ && \
-lake exe opt --passriscv64 SSA/Projects/LLVMRiscV/tests/LLVMIR/${OUT_MLIR} > SSA/Projects/LLVMRiscV/tests/LLVMIR/${OUT1_MLIR}
+lake exe opt SSA/Projects/LLVMRiscV/tests/LLVMIR/out.mlir
