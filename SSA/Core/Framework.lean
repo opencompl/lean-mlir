33d20e89024ebbbccf3e886c01b661a6229cf9ce
Tobias Grosser <tobias@grosser.es>
2025-08-28 19:23:16 +0100

    chore: drop random comment

--- SSA/Core/Framework.lean
+++ SSA/Core/Framework.lean
@@ -61,8 +61,6 @@ theorem _root_.Pure.pure_cast {f} [inst : Pure f] (b : β) (h : β = α) :
   · symm; assumption
   · exact cast_heq ..
 
--- A random comment to force rebuild
-
 /- # Classes -/
 
 abbrev RegionSignature Ty := List (Ctxt Ty × List Ty)
