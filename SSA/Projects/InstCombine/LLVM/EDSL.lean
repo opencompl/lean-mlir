615c25610ea372f283deef35edac099324e35ae7
luisacicolini <luisacicolini@gmail.com>
2025-09-16 08:30:40 +0100

    chore: spacing

--- SSA/Projects/InstCombine/LLVM/EDSL.lean
+++ SSA/Projects/InstCombine/LLVM/EDSL.lean
@@ -41,26 +41,25 @@ def getVarWidth {Γ : Ctxt (MetaLLVM φ).Ty} : (Σ t, Γ.Var t) → Width φ
   | ⟨.bitvec w, _⟩ => w
 
 def parseOverflowFlags (op : AST.Op φ) : ReaderM φ LLVM.NoWrapFlags := do
-   match op.getAttr? "overflowFlags" with
-      | .none => return {}
-      | .some y =>
-        match y with
-        | .opaque_ "llvm.overflow" "nsw" => return (⟨true, false⟩)
-        | .opaque_ "llvm.overflow" "nuw" => return (⟨false, true⟩)
-        | .opaque_ "llvm.overflow" "none" => return (⟨false, false⟩)
-        | .list [.opaque_ "llvm.overflow" "nuw", .opaque_ "llvm.overflow" "nsw"]
-        | .list [.opaque_ "llvm.overflow" "nsw", .opaque_ "llvm.overflow" "nuw"] =>
-             return (⟨true, true⟩)
-        | _ =>  /- This case covers generic MLIR syntax for operations with attributes as
-                MLIR encodes them using integer attributes. -/
-              let ⟨n, _ ⟩ ← op.getIntAttr "overflowFlags"
-              match n with
-              | 0 => return { nsw := false, nuw := false}
-              | 1 => return { nsw := true, nuw := false}
-              | 2 => return { nsw := false, nuw := true}
-              | 3 => return { nsw := true, nuw := true}
-              | s => throw <| .generic s!"The overflow flag with predicate {s} is not allowed. \
-                We currently support nsw (no signed wrap) and nuw (no unsigned wrap)"
+  match op.getAttr? "overflowFlags" with
+  | .none => return {}
+  | .some y => match y with
+    | .opaque_ "llvm.overflow" "nsw" => return (⟨true, false⟩)
+    | .opaque_ "llvm.overflow" "nuw" => return (⟨false, true⟩)
+    | .opaque_ "llvm.overflow" "none" => return (⟨false, false⟩)
+    | .list [.opaque_ "llvm.overflow" "nuw", .opaque_ "llvm.overflow" "nsw"]
+    | .list [.opaque_ "llvm.overflow" "nsw", .opaque_ "llvm.overflow" "nuw"] =>
+          return (⟨true, true⟩)
+    | _ =>  /- This case covers generic MLIR syntax for operations with attributes as
+            MLIR encodes them using integer attributes. -/
+          let ⟨n, _ ⟩ ← op.getIntAttr "overflowFlags"
+          match n with
+          | 0 => return { nsw := false, nuw := false}
+          | 1 => return { nsw := true, nuw := false}
+          | 2 => return { nsw := false, nuw := true}
+          | 3 => return { nsw := true, nuw := true}
+          | s => throw <| .generic s!"The overflow flag with predicate {s} is not allowed. \
+            We currently support nsw (no signed wrap) and nuw (no unsigned wrap)"
 /--
 Maps integer predicate codes (as defined in the MLIR LLVM dialect) to their corresponding
 `LLVM.IntPred` constructors.
