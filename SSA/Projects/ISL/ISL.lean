35f3727cafa790e98bd3d56ccc1886f772271ee9
Alex Keizer <alex@keizer.dev>
2025-09-16 17:19:42 +0100

    add ISL as a project, build it in CI

---
+++ SSA/Projects/ISL/ISL.lean
@@ -0,0 +1,5 @@
+import SSA.Projects.ISL.Explicit.Base
+import SSA.Projects.ISL.Explicit.Syntax
+import SSA.Projects.ISL.Implicit.Base
+import SSA.Projects.ISL.Implicit.Syntax
+import SSA.Projects.ISL.Semantics
