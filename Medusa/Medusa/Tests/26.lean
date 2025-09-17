29906354f693386cc966c687956f493f318c96c1
Léo Stefanesco <leo.lveb@gmail.com>
2025-09-17 14:51:47 +0100

    Split Medusa off to a lake project

---
+++ Medusa/Medusa/Tests/26.lean
@@ -0,0 +1,8 @@
+import Medusa
+
+set_option trace.profiler true
+set_option trace.profiler.threshold 1
+set_option trace.Generalize true
+
+variable {x y : BitVec 8}
+#generalize x <<< 1#8 ^^^ (y <<< 1#8 ^^^ BitVec.ofInt 8 (-68)) = (y ^^^ x) <<< 1#8 ^^^ BitVec.ofInt 8 (-68) ---PASSED gbinophandhshifts_proof/shl_xor_xor_bad_mask_distribute_thm; #26
--- SSA/Experimental/Bits/Generalize/Tests/15.lean
+++
@@ -1,8 +0,0 @@
-import SSA.Experimental.Bits.Fast.Generalize
-
-set_option trace.profiler true
-set_option trace.profiler.threshold 1
-set_option trace.Generalize true
-
-variable {x y : BitVec 8}
-#generalize 28#8 >>> x <<< 3#8 ||| 7#8 = BitVec.ofInt 8 (-32) >>> x ||| 7#8 -- lshr_shl_demand1_thm. Can't generate precondition; #15
