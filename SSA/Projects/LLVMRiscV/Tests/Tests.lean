1204367169862bab9f2fef89e2fc2a20579708a1
luisacicolini <luisacicolini@gmail.com>
2025-09-18 13:39:40 +0100

    chore: remove unused diff

--- SSA/Projects/LLVMRiscV/Tests/Tests.lean
+++ SSA/Projects/LLVMRiscV/Tests/Tests.lean
@@ -1,16 +1,3 @@
-import SSA.Projects.LLVMRiscV.ParseAndTransform
-import SSA.Projects.RISCV64.ParseAndTransform
-import SSA.Projects.InstCombine.LLVM.PrettyEDSL
-import SSA.Projects.InstCombine.TacticAuto
-import SSA.Projects.InstCombine.LLVM.Semantics
-open LLVM
-open BitVec
-
-/--info: builtin.module {
-^bb0(%0 : i64, %1 : i64, %2 : i1):
-  "llvm.return"(%0) : (i64) -> ()
-}
----
-info: 0 -/
-#guard_msgs in
-#eval! passriscv64 "SSA/Projects/LLVMRiscV/Tests/example.mlir"
+/-!
+Import all LLVMRiscV tests here
+-/
