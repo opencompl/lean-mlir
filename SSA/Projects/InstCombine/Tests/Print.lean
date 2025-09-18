c0f71ef762f89eab9b34598b6db9b459fdcc7402
luisacicolini <luisacicolini@gmail.com>
2025-09-18 13:43:06 +0100

    chore: fix test

--- SSA/Projects/InstCombine/Tests/Print.lean
+++ SSA/Projects/InstCombine/Tests/Print.lean
@@ -7,13 +7,13 @@ namespace Tests
 
 /--
 info: builtin.module { ⏎
-^bb0(%0 : i64, %1 : i1):
-  "llvm.return"(%0) : (i64) -> ()
+^bb0(%0 : i1, %1 : i64):
+  "llvm.return"(%0) : (i1) -> ()
  }
 -/
 #guard_msgs in #eval String.toFormat <| Com.toPrint [llvm| {
-  ^bb0(%0: i64, %1: i1):
-    "llvm.return"(%0) : (i64) -> ()
+  ^bb0(%0 : i1, %1 : i64):
+    "llvm.return"(%0) : (i1) -> ()
 }]
 
 /--
