058f01e100005b6768b3bb5ff604d29fdfc17a25
Alex Keizer <alex@keizer.dev>
2025-09-18 12:55:12 +0100

    Wrap test case output in `String.toFormat`

--- SSA/Projects/InstCombine/Tests/Print.lean
+++ SSA/Projects/InstCombine/Tests/Print.lean
@@ -6,15 +6,23 @@ import SSA.Projects.InstCombine.LLVM.PrettyEDSL
 namespace Tests
 
 /--
-info: "builtin.module { \n^bb0(%0 : i1, %1 : i64):\n  \"llvm.return\"(%0) : (i64) -> ()\n }"
+info: builtin.module { ⏎
+^bb0(%0 : i1, %1 : i64):
+  "llvm.return"(%0) : (i64) -> ()
+ }
 -/
-#guard_msgs in #eval Com.toPrint [llvm| {
+#guard_msgs in #eval String.toFormat <| Com.toPrint [llvm| {
   ^bb0(%0: i64, %1: i1):
     "llvm.return"(%0) : (i64) -> ()
 }]
 
-/-- info: "{ \n^entry(%0 : i1, %1 : i64):\n  \"return\"(%0) : (i64) -> ()\n }" -/
-#guard_msgs in #eval Com.toString [llvm| {
+/--
+info: { ⏎
+^entry(%0 : i1, %1 : i64):
+  "return"(%0) : (i64) -> ()
+ }
+-/
+#guard_msgs in #eval String.toFormat <| Com.toString [llvm| {
   ^bb0(%0: i64, %1: i1):
     "llvm.return"(%0) : (i64) -> ()
 }]
