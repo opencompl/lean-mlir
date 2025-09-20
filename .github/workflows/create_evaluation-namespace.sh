896e9d3ffd18be6540e3caca6cc3baad205eaded
Tobias Grosser <tobias@grosser.es>
2025-09-20 09:11:44 +0200

    ci: copy our evaluation and run it on namespace to see the performance difference

---
+++ .github/workflows/create_evaluation-namespace.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$(uname)" == "Darwin" ]; then
+  SED=gsed
+else
+  SED=sed
+fi
+SED=gsed
+
+cp evaluation.yml evaluation-namespace.yml
+${SED} -e 's/Evaluation/Evaluation (Namespace)/' -i evaluation-namespace.yml
+${SED} -e 's/ubuntu-latest/namespace-profile-default/' -i evaluation-namespace.yml
