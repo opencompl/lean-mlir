2fee3a25fbb26f630da7523f56dc5dcc50b23ce9
Siddharth Bhat <siddu.druid@gmail.com>
2025-09-23 08:44:53 +0100

    feat: also make preprocessing robust with telescopes

--- Blase/BlaseTest/Preprocessing1.lean
+++ Blase/BlaseTest/Preprocessing1.lean
@@ -7,18 +7,8 @@ import Blase
 open BitVec
 
 
-@[bv_multi_width_normalize] theorem ofBool_eq_1_iff (x : Bool) :
-  ofBool x = 1#1 ↔ x = True := by sorry
-
-@[bv_multi_width_normalize] theorem not_eq_iff_ne (x y : α) :
-  ¬ (x = y) ↔ x ≠ y := by simp
-
 theorem test_invert_demorgan_logical_or_thm.extracted_1._6 : ∀ (x x_1 : BitVec 64),
   ¬ofBool (x_1 == 27#64) = 1#1 →
     ¬ofBool (x_1 != 27#64) = 1#1 →
       (ofBool (x_1 == 0#64) ||| ofBool (x == 0#64)) ^^^ 1#1 = ofBool (x_1 != 0#64) &&& 0#1 := by
-  intros a b c;
-  bv_multi_width_normalize
-  simp [ofBool_eq_1_iff] at *
-  rw [not_eq_iff_ne] at c
-  bv_multi_width +verbose?
+  bv_multi_width
