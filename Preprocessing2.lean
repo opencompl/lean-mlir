e50e9d0f5e13941abc29d84363bdfa329c3d3bcc
Siddharth Bhat <siddu.druid@gmail.com>
2025-09-24 07:55:38 +0100

    chore: comments

--- Preprocessing2.lean
+++ Preprocessing2.lean
@@ -5,18 +5,18 @@
 import Blase
 open BitVec
 
-#eval (((MultiWidth.Term.Ctx.empty 2).cons (MultiWidth.WidthExpr.var ⟨0, by omega⟩)).cons (MultiWidth.WidthExpr.var ⟨1, by omega⟩)
-      ⟨0, by omega⟩)
-
--- set_option pp.explicit true in
-/-- Bug due to reflection of multiple widths. -/
-theorem minimized :
+-- Check that we correctly reflect this goal state.
+-- The goal state is tricky due to binders with different widths,
+-- which needs us to correctly build environments with the right de bruijn levels.
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in theorem minimized :
     ∀ (v w : Nat) (x_1 : BitVec v) (x_2 : BitVec w), x_1 = 42#v ∨ x_2 = 43#w := by
-  intros;
-  bv_multi_width +verbose?
+  intros
+  fail_if_success bv_multi_width +verbose?
+  sorry
 
 theorem t7_thm.extracted_1._2 :
-    ∀ (x : BitVec 8) (x_1 : BitVec 1) (x_2 : BitVec 8), x_1 = 1#1 → x_2 - 0#8 = 0#8 + x_2 := by
+    ∀ (x_1 : BitVec 1) (x_2 : BitVec 8), x_1 = 1#1 → x_2 - 0#8 = 0#8 + x_2 := by
   intros;
   bv_multi_width
 
