8668292fb6f98665d6682f7b895c37ca094ded7f
Alex Keizer <alex@keizer.dev>
2025-09-24 12:23:57 +0100

    move and fix tests

--- EvaluationHarness/EvaluationHarness/Tests/Basic.lean
+++ EvaluationHarness/EvaluationHarness/Tests/Basic.lean
@@ -7,14 +7,14 @@ namespace EvaluationHarness.Tests
 /-- info: some "foo" -/
 #guard_msgs in #eval do
   let cmd ← `(command| theorem foo : False := sorry)
-  return getDefLikeName cmd
+  return getDefLikeName? cmd
 
 /-- info: some "foo.bar" -/
 #guard_msgs in #eval do
   let cmd ← `(command| theorem foo.bar : False := sorry)
-  return getDefLikeName cmd
+  return getDefLikeName? cmd
 
 /-- info: some "baz" -/
 #guard_msgs in #eval do
   let cmd ← `(command| def baz : False := sorry)
-  return getDefLikeName cmd
+  return getDefLikeName? cmd
