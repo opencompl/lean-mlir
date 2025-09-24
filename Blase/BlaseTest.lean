3d9b99b551e7f6ec2246f882f9897fe30ce60c88
Siddharth <siddu.druid@gmail.com>
2025-09-24 15:29:12 +0100

    feat: show more failure reasons (#1700)
    These examples show two distinct reasons for falure:
    1. We need `ofBool` support, and we should allow ourselves to use
    `Bool`.
    2. We can solve problems with larger number of k-induction iterations,
    so we should make that a parameter.

--- Blase/BlaseTest.lean
+++ Blase/BlaseTest.lean
@@ -2,4 +2,6 @@ import Blase.SingleWidth.Tests
 import Blase.MultiWidth.Tests
 import BlaseTest.Preprocessing1
 import BlaseTest.Preprocessing2
+import BlaseTest.Preprocessing3
+import BlaseTest.Preprocessing4
 
