2264d8be1cdee4015bd7616642bee415bc4ba4ff
Alex Keizer <alex@keizer.dev>
2025-09-24 12:14:14 +0100

    feat: evaluation harness to capture messages and serialize them as a single JSON object

---
+++ EvaluationHarness/EvaluationHarness/Tests.lean
@@ -0,0 +1,20 @@
+import EvaluationHarness.Basic
+
+namespace EvaluationHarness.Tests
+
+/-! ## getDefLikeName -/
+
+/-- info: some "foo" -/
+#guard_msgs in #eval do
+  let cmd ← `(command| theorem foo : False := sorry)
+  return getDefLikeName cmd
+
+/-- info: some "foo.bar" -/
+#guard_msgs in #eval do
+  let cmd ← `(command| theorem foo.bar : False := sorry)
+  return getDefLikeName cmd
+
+/-- info: some "baz" -/
+#guard_msgs in #eval do
+  let cmd ← `(command| def baz : False := sorry)
+  return getDefLikeName cmd
