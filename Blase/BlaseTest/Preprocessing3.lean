14b5fb02bb5e99a8acdd64ec2bcaf8e8e6a9bd60
Siddharth Bhat <siddu.druid@gmail.com>
2025-09-24 15:39:44 +0100

    chore: up default number of iter

--- Blase/BlaseTest/Preprocessing3.lean
+++ Blase/BlaseTest/Preprocessing3.lean
@@ -13,7 +13,8 @@ our solver to interpret 1-width bitvectors always.
 Otherwise, we end up not being able to close many boolean problems.
 This also exposes a TODO: where we should add `ofBool` support to the reflection.
 -/
-theorem bools_multi_uses2_logical_thm.extracted_1._37 : ∀ (x x_1 x_2 : BitVec 1),
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in theorem bools_multi_uses2_logical_thm.extracted_1._37 : ∀ (x x_1 x_2 : BitVec 1),
   ¬x_2 ^^^ 1#1 = 1#1 → ¬x_2 = 1#1 → ¬0#1 = 1#1 → x_1 = 1#1 → 0#1 = 0#1 ^^^ 0#1 := by
   fail_if_success bv_multi_width +verbose?
   sorry
