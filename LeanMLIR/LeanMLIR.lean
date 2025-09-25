a227e2168092e9b7d625cbfb950a4626b556b528
Alex Keizer <alex@keizer.dev>
2025-09-25 17:44:29 +0100

    Add a LeanMLIR.Basic import, which doesn't include the LLVM dialect
    For some reason importing the LLVM dialect changes how bitvectors are printed

--- LeanMLIR/LeanMLIR.lean
+++ LeanMLIR/LeanMLIR.lean
@@ -1,15 +1,8 @@
 /-
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
-import LeanMLIR.Framework
-import LeanMLIR.Tactic
-import LeanMLIR.Util
-import LeanMLIR.MLIRSyntax
 
--- ## Transformations
-import LeanMLIR.Transforms.Rewrite
-import LeanMLIR.Transforms.CSE
-import LeanMLIR.Transforms.DCE
+import LeanMLIR.Basic
 
 -- ## Dialects
 import LeanMLIR.Dialects.LLVM
