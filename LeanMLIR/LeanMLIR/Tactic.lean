0b57cd38208c32dcf5b2771b6a697c88fa02eefd
Alex Keizer <alex@keizer.dev>
2025-09-25 15:04:21 +0100

    drop TacBench import

--- LeanMLIR/LeanMLIR/Tactic.lean
+++ LeanMLIR/LeanMLIR/Tactic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 import LeanMLIR.Framework
 import LeanMLIR.Util
 import LeanMLIR.MLIRSyntax.EDSL
-import TacBench
 import LeanMLIR.Tactic.SimpSet
 
 import Qq
