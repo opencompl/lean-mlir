279b85c6a367f347548286a24dcf2f1e8cb98317
Alex Keizer <alex@keizer.dev>
2025-09-26 09:31:57 +0100

    move tests out of EDSL.lean file

--- LeanMLIR/LeanMLIR/Dialects/LLVM/Syntax/EDSL.lean
+++ LeanMLIR/LeanMLIR/Dialects/LLVM/Syntax/EDSL.lean
@@ -223,17 +223,3 @@ elab "[llvm(" mvars:term,* ")| " reg:mlir_region "]" : term => do
   return res
 
 macro "[llvm| " reg:mlir_region "]" : term => `([llvm()| $reg])
-
-section Test
-open InstCombine.LLVM.Ty (bitvec)
-
-/-- Assert that the elaborator respects variable ordering correctly -/
-private def variable_order1 : Com LLVM ⟨[bitvec 2, bitvec 1]⟩ .pure [bitvec 1] := [llvm()| {
-    ^bb0(%arg1: i1, %arg2 : i2):
-      "llvm.return"(%arg1) : (i1) -> ()
-  }]
-/-- Assert that the elaborator respects variable ordering correctly -/
-private def variable_order2 : Com LLVM ⟨[bitvec 2, bitvec 1]⟩ .pure [bitvec 2] := [llvm()| {
-    ^bb0(%arg1: i1, %arg2 : i2):
-      "llvm.return"(%arg2) : (i2) -> ()
-  }]
