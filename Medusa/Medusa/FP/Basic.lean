cb479b43ce2ad8f086edfe372c02de51446c8e38
Siddharth Bhat <siddu.druid@gmail.com>
2025-09-26 15:43:42 +0200

    chore: FP

---
+++ Medusa/Medusa/FP/Basic.lean
@@ -0,0 +1,342 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henrik Böving, Timi Adeniran, Léo Stefanesco
+-/
+import Lean
+import Lean.Elab.Term
+import Medusa.Generalize
+
+open Lean
+open Elab
+open Lean.Meta
+open Std.Sat
+open Std.Tactic.BVDecide
+
+inductive FpBinOp
+| add
+deriving Hashable, DecidableEq, Repr
+
+
+def FpBinOp.toString : FpBinOp → String
+  | FpBinOp.add => "+"
+
+instance : ToString FpBinOp := ⟨FpBinOp.toString⟩
+
+namespace Generalize
+/--
+Custom floating point expression
+for generalization incorporating all supported expressions
+-/
+inductive FpExpr : Nat → Type where
+  /--
+  A `BitVec` variable, referred to through an index.
+  -/
+  | var (idx : Nat) : FpExpr w
+  /--
+  A binary operation.
+  -/
+  | bin (lhs : FpExpr w) (op : FpBinOp) (rhs : FpExpr w) : FpExpr w
+with
+  @[computed_field]
+  hashCode : (w : Nat) → FpExpr w → UInt64
+    | w, .var idx => mixHash 5 <| mixHash (hash w) (hash idx)
+    | w, .bin lhs op rhs =>
+      mixHash 13 <| mixHash (hash w) <| mixHash (hashCode _ lhs) <| mixHash (hash op) (hashCode _ rhs)
+deriving Repr, DecidableEq, Inhabited
+
+namespace FpExpr
+
+def toString : FpExpr w → String
+  | .var idx => s!"var{idx}#{w}"
+  | .bin lhs op rhs => s!"({lhs.toString} {op.toString} {rhs.toString})"
+
+def size : FpExpr w → Nat
+  | .var _ => 1
+  | .bin lhs _ rhs => 1 + lhs.size + rhs.size
+
+instance : ToString (FpExpr w) := ⟨toString⟩
+
+instance : Hashable (FpExpr w) where
+  hash expr := expr.hashCode _
+
+instance : BEq (FpExpr w) where
+  beq := fun a b => toString a == toString b
+
+instance : Inhabited BVExpr.PackedBitVec where
+  default := { bv := BitVec.ofNat 0 0 }
+
+/-
+/--
+The semantics for `FpExpr`.
+-/
+def eval (assign : Std.HashMap Nat BVExpr.PackedBitVec) : FpExpr w → BitVec w
+  | .var idx =>
+    let packedBv := assign[idx]!
+    /-
+    This formulation improves performance, as in a well formed expression the condition always holds
+    so there is no need for the more involved `BitVec.truncate` logic.
+    -/
+    if h : packedBv.w = w then
+      h ▸ packedBv.bv
+    else
+      packedBv.bv.truncate w
+  | .const val => val
+  | .bin lhs op rhs => op.eval (eval assign lhs) (eval assign rhs)
+-/
+end FpExpr
+
+inductive FpBinaryPredKind
+| eq
+deriving Hashable, DecidableEq, Repr
+
+def FpBinaryPredKind.toString : FpBinaryPredKind → String
+  | FpBinaryPredKind.eq => "="
+
+instance : ToString FpBinaryPredKind := ⟨FpBinaryPredKind.toString⟩
+
+inductive FpPredicate where
+/--
+A binary predicate on `FpExpr`.
+-/
+| bin (lhs : FpExpr w) (op : FpBinaryPredKind) (rhs : FpExpr w)
+deriving Hashable, DecidableEq, Repr
+
+namespace FpPredicate
+
+def size : FpPredicate → Nat
+| .bin lhs _ rhs => 1 + lhs.size + rhs.size
+
+def toString : FpPredicate → String
+  | bin lhs op rhs => s!"({lhs.toString} {op.toString} {rhs.toString})"
+
+instance : ToString FpPredicate := ⟨toString⟩
+
+/-
+/--
+The semantics for `BVPred`.
+-/
+def eval (assign : Std.HashMap Nat BVExpr.PackedBitVec) : FpPredicate → Bool
+  | bin lhs op rhs => op.eval (lhs.eval assign) (rhs.eval assign)
+  | getLsbD expr idx => (expr.eval assign).getLsbD idx
+-/
+
+end FpPredicate
+
+deriving instance Hashable for Gate
+deriving instance BEq for Gate
+deriving instance DecidableEq for Gate
+
+deriving instance Hashable for BoolExpr
+deriving instance BEq for BoolExpr
+deriving instance DecidableEq for BoolExpr
+
+/-- TODO: think if this should live in Medusa lib -/
+def GenFpLogicalExpr := BoolExpr FpPredicate
+
+def GenFpLogicalExpr.toBoolExpr (expr: GenFpLogicalExpr) :
+    BoolExpr FpPredicate :=
+  expr
+
+def GenFpLogicalExpr.ofBoolExpr (expr: BoolExpr FpPredicate) :
+    GenFpLogicalExpr :=
+  expr
+
+
+def GenFpLogicalExpr.size : GenFpLogicalExpr → Nat
+| .literal x => x.size
+| .const _ => 1
+| .not e => 1 + (GenFpLogicalExpr.ofBoolExpr e).size
+| .gate _ e₁ e₂ =>
+  1 + (GenFpLogicalExpr.ofBoolExpr e₁).size + (GenFpLogicalExpr.ofBoolExpr e₂).size
+| .ite e₁ e₂ e₃ => 1 + (GenFpLogicalExpr.ofBoolExpr e₁).size + (GenFpLogicalExpr.ofBoolExpr e₂).size + (GenFpLogicalExpr.ofBoolExpr e₃).size
+
+namespace GenFpLogicalExpr
+
+
+/-
+/--
+The semantics of boolean problems involving BitVec predicates as atoms.
+-/
+def eval (assign : Std.HashMap Nat BVExpr.PackedBitVec)
+  (expr : GenFpLogicalExpr) : Bool :=
+  BoolExpr.eval (·.eval assign) expr
+-/
+
+-- TODO: this instance was defined in terms of toString,
+-- to be fixed.
+instance : BEq GenFpLogicalExpr where
+  beq := fun a b => a.toBoolExpr == b.toBoolExpr
+
+instance : Hashable GenFpLogicalExpr where
+  hash a := hash a.toBoolExpr
+
+end GenFpLogicalExpr
+
+-- | What is this for?
+structure FpExprWrapper where
+  width : Nat
+  bvExpr: FpExpr width
+
+-- | Why BEq and not decidableEq? whatever.
+instance : BEq FpExprWrapper where
+  beq := fun a b => if h : a.width = b.width then
+                      a.bvExpr == h ▸ b.bvExpr
+                    else false
+
+instance : Hashable FpExprWrapper where
+  hash a := hash a.bvExpr
+
+instance : ToString FpExprWrapper where
+  toString w :=
+      s!" FpExprWrapper \{width: {w.width}, bvExpr: {w.bvExpr}}"
+
+instance : Inhabited FpExprWrapper where
+  default := { bvExpr := FpExpr.var 0, width := 0 }
+
+
+def getWidth (expr : Expr) : MetaM (Option Nat) := do
+  match_expr expr with
+  | BitVec n => getNatValue? n
+  | _ => pure none
+
+-- | TODO: rename to setWidth
+def changeBVExprWidth (bvExpr: FpExpr w) (target: Nat) : FpExpr target := Id.run do
+  if h : w = target then
+    return (h ▸ bvExpr)
+
+  match bvExpr with
+  | .var idx => (FpExpr.var idx : FpExpr target)
+  | .bin lhs op rhs => FpExpr.bin (changeBVExprWidth lhs target) op (changeBVExprWidth rhs target)
+
+-- TODO: make this part of Medusa proper?
+def changeBVLogicalExprWidth (bvLogicalExpr: GenFpLogicalExpr) (target: Nat) : GenFpLogicalExpr :=
+  match bvLogicalExpr with
+  | .literal (FpPredicate.bin lhs op rhs) => BoolExpr.literal (FpPredicate.bin (changeBVExprWidth lhs target) op (changeBVExprWidth rhs target))
+  | .not boolExpr =>
+      BoolExpr.not (changeBVLogicalExprWidth boolExpr target)
+  | .gate op lhs rhs =>
+      BoolExpr.gate op (changeBVLogicalExprWidth lhs target) (changeBVLogicalExprWidth rhs target)
+  | .ite constVar auxVar op3 =>
+      BoolExpr.ite (changeBVLogicalExprWidth constVar target) (changeBVLogicalExprWidth auxVar target) (changeBVLogicalExprWidth op3 target)
+  | _ => bvLogicalExpr
+
+-- | What does this do?
+-- TODO: this can be done in Medusa directly?
+-- IT doesn't need to live here?
+def bvExprToSubstitutionValue (map: Std.HashMap Nat FpExprWrapper) :
+      Std.HashMap Nat (SubstitutionValue FpExpr) :=
+      Std.HashMap.ofList (List.map (fun item => (item.fst, SubstitutionValue.genExpr item.snd.bvExpr)) map.toList)
+
+-- What does this tod?
+-- TODO: this can be done in medusa directly?
+def packedBitVecToSubstitutionValue (map: Std.HashMap Nat BVExpr.PackedBitVec) :
+    Std.HashMap Nat (SubstitutionValue FpExpr) :=
+  Std.HashMap.ofList (List.map (fun item => (item.fst, SubstitutionValue.packedBV item.snd)) map.toList)
+
+-- | This does not need to happen here?
+-- TODO: part of this can happen in Medusa dir
+set_option warn.sorry false in
+def substituteBVExpr (bvExpr: FpExpr w) (assignment: Std.HashMap Nat (SubstitutionValue FpExpr)) : FpExpr w :=
+    match bvExpr with
+    | .var idx =>
+      if assignment.contains idx then
+          let value := assignment[idx]!
+          match value with
+          | .genExpr (w := wbv) bv =>
+            let h : w = wbv := sorry
+            h ▸ bv
+          | .packedBV packedBitVec =>  FpExpr.const (BitVec.ofNat w packedBitVec.bv.toNat)
+      else FpExpr.var idx
+    | .bin lhs op rhs =>
+        FpExpr.bin (substituteBVExpr lhs assignment) op (substituteBVExpr rhs assignment)
+    | .const x => .const x
+
+-- | TODO: this can be in Medusa?
+def substitute  (bvLogicalExpr: GenFpLogicalExpr) (assignment: Std.HashMap Nat (SubstitutionValue FpExpr)) :
+          GenFpLogicalExpr :=
+  match bvLogicalExpr with
+  | .literal (FpPredicate.bin lhs op rhs) => BoolExpr.literal (FpPredicate.bin (substituteBVExpr lhs assignment) op (substituteBVExpr rhs assignment))
+  | .not boolExpr =>
+      BoolExpr.not (substitute boolExpr assignment)
+  | .gate op lhs rhs =>
+      BoolExpr.gate op (substitute lhs assignment) (substitute rhs assignment)
+  | .ite conditional pos neg =>
+      BoolExpr.ite (substitute conditional assignment) (substitute pos assignment) (substitute neg assignment)
+  | _ => bvLogicalExpr
+
+def getIdentityAndAbsorptionConstraints (bvLogicalExpr: GenFpLogicalExpr) (symVars : Std.HashSet Nat) : List GenFpLogicalExpr :=
+      match bvLogicalExpr with
+      | .literal (FpPredicate.bin lhs _ rhs) => (getFpExprConstraints lhs) ++ (getFpExprConstraints rhs)
+      | .not boolExpr => getIdentityAndAbsorptionConstraints boolExpr symVars
+      | .gate _ lhs rhs => (getIdentityAndAbsorptionConstraints lhs symVars) ++ (getIdentityAndAbsorptionConstraints rhs symVars)
+      | .ite constVar auxVar op3 =>
+          (getIdentityAndAbsorptionConstraints constVar symVars) ++ (getIdentityAndAbsorptionConstraints auxVar symVars) ++ (getIdentityAndAbsorptionConstraints op3 symVars)
+      | _ => []
+
+      where
+        getFpExprConstraints {w} (bvExpr : FpExpr w) : List GenFpLogicalExpr := Id.run do
+                match bvExpr with
+                | .bin lhs op rhs  =>
+                      match (lhs, rhs) with
+                      | (FpExpr.var lhsId, FpExpr.var rhsId) =>
+                          let mut constraints := []
+
+                          if symVars.contains lhsId then
+                            constraints := getBitwiseConstraints lhs op ++ constraints
+
+                          if symVars.contains rhsId then
+                            constraints := getBitwiseConstraints rhs op ++ constraints
+                          pure constraints
+                      | (FpExpr.var lhsId, _) =>
+                          if !symVars.contains lhsId then
+                            getFpExprConstraints rhs
+                          else
+                            (getBitwiseConstraints lhs op) ++ (getFpExprConstraints rhs)
+                      | (_, FpExpr.var rhsId) =>
+                          if !symVars.contains rhsId then
+                            getFpExprConstraints lhs
+                          else
+                         (getFpExprConstraints lhs)  ++ (getBitwiseConstraints rhs op)
+                      | _ => ((getFpExprConstraints lhs) ++ (getFpExprConstraints rhs))
+                      -- getFpExprConstraints operand
+                | _ =>  []
+
+        getBitwiseConstraints {w} (bvExpr: FpExpr w) (op : FpBinOp) : List GenFpLogicalExpr :=
+            let neqZero := BoolExpr.not (BoolExpr.literal (FpPredicate.bin bvExpr FpBinaryPredKind.eq (FpExpr.const (BitVec.ofNat w 0))))
+            let neqAllOnes := BoolExpr.not (BoolExpr.literal (FpPredicate.bin bvExpr FpBinaryPredKind.eq (FpExpr.const (BitVec.allOnes w))))
+            []
+
+-- | TODO: write this as a filter / map
+def addConstraints (expr: GenFpLogicalExpr) (constraints: List GenFpLogicalExpr) (op: Gate) : GenFpLogicalExpr :=
+  match constraints with
+  | [] => expr
+  | x::xs =>
+      match expr with
+      | BoolExpr.const _ => addConstraints x xs op
+      | _ => addConstraints (BoolExpr.gate op expr x) xs op
+
+
+def sameBothSides (bvLogicalExpr : GenFpLogicalExpr) : Bool :=
+    match bvLogicalExpr with
+  | .literal (FpPredicate.bin lhs _ rhs) => lhs == rhs
+  | _ => false
+
+def evalBVExpr (assignments : Std.HashMap Nat BVExpr.PackedBitVec) (expr: FpExpr w) : BitVec w :=
+  let substitutedBvExpr := substituteBVExpr expr (packedBitVecToSubstitutionValue assignments)
+  sorry
+  -- FpExpr.eval assignments substitutedBvExpr
+
+def evalBVLogicalExpr (assignments : Std.HashMap Nat BVExpr.PackedBitVec) (expr: GenFpLogicalExpr) : Bool :=
+  let substitutedBvExpr := substitute expr (packedBitVecToSubstitutionValue assignments)
+  sorry
+  -- GenFpLogicalExpr.eval assignments substitutedBvExpr
+
+def add (op1 : FpExpr w) (op2 : FpExpr w) : FpExpr w :=
+  FpExpr.bin op1 FpBinOp.add op2
+
+-- | This is untrue.
+def zero (w: Nat) := FpExpr.const (BitVec.ofNat w 0)
+
+def eqToZero (expr: FpExpr w) : GenFpLogicalExpr :=
+  BoolExpr.literal (FpPredicate.bin expr FpBinaryPredKind.eq (zero w))
---
+++ Medusa/Medusa/FP/Reflect.lean
@@ -0,0 +1,270 @@
+import Lean.Elab.Term
+import Lean
+
+import Medusa.Generalize
+import Medusa.Fp.Basic
+
+namespace Generalize
+
+open Lean
+open Lean.Meta
+open Std.Sat
+open Std.Tactic.BVDecide
+open Tactic
+
+-- TODO: where should this live? Why does this not live in Hydra?
+def defaultParsedExprState : ParsedInputState :=
+  { maxFreeVarId := 0
+  , numSymVars := 0, originalWidth := 32
+  , inputVarIdToVariable := {}, symVarToVal := {}
+  , symVarIdToVariable := {}, displayNameToVariable := {}, valToSymVar:= {}}
+
+-- TODO: this should live in Hydra.
+def printParsedFpExprState (s: ParsedInputState) :=
+    s!"ParsedFpExprState:\n" ++
+    s!"  maxFreeVarId: {s.maxFreeVarId}\n" ++
+    s!"  numSymVars: {s.numSymVars}\n" ++
+    s!"  displayNameToVariable: {s.displayNameToVariable}\n" ++
+    s!"  BVExprIdToFreeVar: {s.inputVarIdToVariable}\n" ++
+    s!"  symVarToVal: {s.symVarToVal}" ++
+    s!"  symVarToDisplayName: {s.symVarIdToVariable}"
+
+
+instance : ToMessageData (ParsedInputState) where
+  toMessageData s := printParsedFpExprState s
+
+instance : ToString (ParsedInputState) where
+  toString s := printParsedFpExprState s
+
+-- TODO: can this live in Hydra?
+structure ParsedFpExpr where
+  width : Nat
+  bvExpr: FpExpr width
+  symVars: Std.HashMap Nat BVExpr.PackedBitVec
+  inputVars : Std.HashMap Nat HydraVariable
+
+-- | TODO: Can this live in Hydra?
+abbrev ParsedFpLogicalExpr :=
+  ParsedLogicalExpr ParsedFpExpr GenFpLogicalExpr
+
+-- | TODO: Allow ParseExprM to throw errors, instead of returning an Option in many cases.
+partial def toBVExpr (expr : Expr) (width: Nat) : ParseExprM (Option (FpExprWrapper)) := do
+  go expr
+  where
+  go (x : Expr) : ParseExprM (Option (FpExprWrapper)) := do
+    match_expr x with
+    | HAdd.hAdd _ _ resType _ lhsExpr rhsExpr =>
+        binaryReflection lhsExpr rhsExpr resType FpBinOp.add
+    | _ =>
+        let natVal ← getNatValue? x
+        let bitVecVal ← getBitVecValue? x
+
+        match (natVal, bitVecVal) with
+        | (some v, none) =>
+              let currState: ParsedInputState  ← get
+              let pbv : BVExpr.PackedBitVec := {bv := BitVec.ofNat currState.originalWidth v}
+              -- | TODO: why is this not a separate function?
+              let existingVal :=  currState.valToSymVar[pbv]?
+
+              match existingVal with
+              | none => let numSymVars := currState.numSymVars
+                        let newId := 1001 + numSymVars
+                        let newExpr : FpExpr width := FpExpr.var newId
+                        let userFacingName := Lean.Name.mkSimple s!"C{numSymVars + 1}"
+                        let var : HydraVariable := {name := userFacingName, id := newId, width := width}
+
+                        let updatedState : ParsedInputState  := { currState with
+                                                                numSymVars := numSymVars + 1
+                                                                , symVarToVal := currState.symVarToVal.insert newId pbv
+                                                                , valToSymVar := currState.valToSymVar.insert pbv newId
+                                                                , displayNameToVariable := currState.displayNameToVariable.insert userFacingName var
+                                                                , symVarIdToVariable := currState.symVarIdToVariable.insert newId var}
+                        set updatedState
+                        return some {bvExpr := newExpr, width := width}
+              | some var => let newExpr : FpExpr width := FpExpr.var var
+                            return some {bvExpr := newExpr, width := width}
+
+        | (none, some bvProd) =>
+              let pbv : BVExpr.PackedBitVec := {bv := bvProd.snd: BVExpr.PackedBitVec}
+              return (← processBitVec pbv)
+        | _ =>
+            let currState: ParsedInputState ← get
+            let localDecl ← getFVarLocalDecl x
+            let userFacingName := localDecl.userName
+            let some w ← getWidth? localDecl.type | return none
+
+            let existingVar? := currState.displayNameToVariable[userFacingName]?
+            match existingVar? with
+            | some val => return some {bvExpr := FpExpr.var val.id, width := val.width}
+            | none =>
+                let newId := currState.maxFreeVarId + 1
+                let newExpr : FpExpr w :=  FpExpr.var newId
+
+                let var : HydraVariable := {name := userFacingName, id := newId, width := w}
+
+                let updatedState : ParsedInputState :=  { currState with
+                                                         maxFreeVarId := newId
+                                                         , displayNameToVariable := currState.displayNameToVariable.insert userFacingName var
+                                                         , inputVarIdToVariable := currState.inputVarIdToVariable.insert newId var
+                                                         }
+                set updatedState
+                return some {bvExpr := newExpr, width := w}
+
+  binaryReflection (lhsExpr rhsExpr resType : Expr) (op : FpBinOp) : ParseExprM (Option (FpExprWrapper)) := do
+    let some lhs ← go lhsExpr | return none
+    let some rhs ← go rhsExpr | return none
+    let some w ← getWidth? resType | return none
+
+    if h : w = lhs.width ∧ lhs.width = rhs.width then
+      let rhs' := h.right ▸ rhs.bvExpr
+      return some {bvExpr := FpExpr.bin lhs.bvExpr op rhs', width := lhs.width}
+    else
+      return none
+
+  getConstantBVExpr? (nExpr : Expr) (vExpr : Expr) : ParseExprM (Option (FpExprWrapper)) := do
+        let some n  ← getNatValue? nExpr | return none
+        let some v ← getNatValue? vExpr | return none
+
+        return some {bvExpr := FpExpr.const (BitVec.ofNat n v), width := n}
+
+  -- | TODO: this should be in Medusa/Util?
+  getBitVecValue? (e : Expr) : MetaM (Option ((n : Nat) × BitVec n)) := OptionT.run do
+    match_expr e with
+    | BitVec.ofNat nExpr vExpr =>
+      let n ← getNatValue? nExpr
+      let v ← getNatValue? vExpr
+      return ⟨n, BitVec.ofNat n v⟩
+    | BitVec.ofNatLT nExpr vExpr _ =>
+      let n ← getNatValue? nExpr
+      let v ← getNatValue? vExpr
+      return ⟨n, BitVec.ofNat n v⟩
+    | BitVec.ofInt nExpr vExpr =>
+      let n ← getNatValue? nExpr
+      let v ← getIntValue? vExpr
+      return ⟨n, BitVec.ofInt n v⟩
+    -- | BitVec.ofBool b =>
+    --    return ⟨1, e⟩
+    | BitVec.allOnes nExpr =>
+      let n ← getNatValue? nExpr
+      return ⟨n, BitVec.allOnes n⟩
+    | _ =>
+      let (v, type) ← getOfNatValue? e ``BitVec
+      let n ← getNatValue? (← whnfD type.appArg!)
+      return ⟨n, BitVec.ofNat n v⟩
+
+  -- | TODO: this should be in Medusa/Util?
+  getWidth? (typeExpr : Expr) : MetaM (Option Nat) := do
+    match_expr typeExpr with
+    | BitVec n => getNatValue? n
+    | _ => getNatValue? typeExpr
+
+  processBitVec (pbv : BVExpr.PackedBitVec) : ParseExprM (Option FpExprWrapper) := do
+    let currState: ParsedInputState ← get
+    let existingVal :=  currState.valToSymVar[pbv]?
+    match existingVal with
+    | none =>
+      let numSymVars := currState.numSymVars
+      let newId := 1001 + numSymVars
+      let newExpr : FpExpr pbv.w := FpExpr.var newId
+
+      let userFacingName := Lean.Name.mkSimple s!"C{numSymVars + 1}"
+      let var : HydraVariable := {name := userFacingName, id := newId, width := pbv.w}
+
+      let updatedState : ParsedInputState := { currState with
+                                              numSymVars := numSymVars + 1
+                                              , symVarToVal := currState.symVarToVal.insert newId pbv
+                                              , valToSymVar := currState.valToSymVar.insert pbv newId
+                                              , displayNameToVariable := currState.displayNameToVariable.insert userFacingName var
+                                              , symVarIdToVariable := currState.symVarIdToVariable.insert newId var}
+      set updatedState
+      return some {bvExpr := newExpr, width := pbv.w}
+    | some var => let newExpr : FpExpr pbv.w := FpExpr.var var
+                  return some {bvExpr := newExpr, width := pbv.w}
+
+
+def parseExprs (lhsExpr rhsExpr : Expr) (width : Nat): ParseExprM (Option ParsedFpLogicalExpr)  := do
+  let some lhsRes ← toBVExpr lhsExpr width | throwError "Could not extract lhs: {lhsExpr}"
+
+  let state ← get
+  let lhs: ParsedFpExpr := {bvExpr := lhsRes.bvExpr, width := lhsRes.width, symVars := state.symVarToVal, inputVars := state.inputVarIdToVariable}
+
+  let some rhsRes ← toBVExpr rhsExpr width | throwError "Could not extract rhs: {rhsExpr}"
+  let state ← get
+
+  let rhsInputVars := state.inputVarIdToVariable.filter fun k _ => !lhs.inputVars.contains k
+  let rhsSymVars := state.symVarToVal.filter fun k _ => !lhs.symVars.contains k
+
+  let rhs: ParsedFpExpr := {bvExpr := rhsRes.bvExpr, width := rhsRes.width, symVars := rhsSymVars, inputVars := rhsInputVars}
+
+  trace[Generalize] m! "lhs width: {lhsRes.width}; rhs width: {rhsRes.width}"
+  if h : lhsRes.width = rhsRes.width then
+    let rhsExpr := h ▸ rhsRes.bvExpr
+    -- | TODO: why do we need it to be a 'BoolExpr'? We should have an interface
+    -- that asks for it to be converted to a 'BoolExpr'.
+    let bvLogicalExpr := BoolExpr.literal (FpPredicate.bin lhsRes.bvExpr FpBinaryPredKind.eq rhsExpr)
+    trace[Generalize] m! "BVLogicalExpr: {bvLogicalExpr}"
+    return some {lhs := lhs, rhs := rhs, state := state, logicalExpr := bvLogicalExpr}
+
+  return none
+
+def bvExprToExpr (ParsedFpExpr : ParsedFpLogicalExpr)
+  (bvExpr : FpExpr w) : MetaM Expr := do
+  let ParsedFpExprState := ParsedFpExpr.state
+  let allVars := Std.HashMap.union ParsedFpExprState.inputVarIdToVariable ParsedFpExprState.symVarIdToVariable
+
+  match bvExpr with
+  | .var idx => let some var := allVars[idx]? | throwError m! "Could not find var: {idx} in {bvExpr}"
+                let localDecl ← getLocalDeclFromUserName var.name
+                pure (mkFVar localDecl.fvarId)
+  | .bin lhs op rhs  =>
+    match op with
+    | .add => return mkApp3 (.const ``BitVec.add []) (mkNatLit w) (← bvExprToExpr ParsedFpExpr lhs) (← bvExprToExpr ParsedFpExpr rhs)
+
+
+def beqBitVecInstExpr (width : Expr) : Expr := mkApp2 (.const ``instBEqOfDecidableEq [levelZero]) (mkApp (mkConst ``BitVec) width) (mkApp (.const ``instDecidableEqBitVec []) width)
+def beqBoolInstExpr : Expr := mkApp2 (.const ``instBEqOfDecidableEq [levelZero]) (mkConst ``Bool) (mkConst ``instDecidableEqBool)
+
+def toExpr (ParsedFpExpr : ParsedFpLogicalExpr) (bvLogicalExpr: GenFpLogicalExpr) : MetaM Expr := do
+  go bvLogicalExpr
+  where
+  go (input : GenFpLogicalExpr) := do
+  match input with
+  | .literal (GenBVPred.bin (w := w) lhs op rhs) =>
+      match op with
+      | .eq => return mkApp4 (.const ``BEq.beq [levelZero]) (mkApp (mkConst ``BitVec) (mkNatLit w)) (beqBitVecInstExpr (mkNatLit w)) (← bvExprToExpr ParsedFpExpr lhs) (← bvExprToExpr ParsedFpExpr rhs)
+      | .ult => return mkApp3 (.const ``BitVec.ult []) (mkNatLit w) (← bvExprToExpr ParsedFpExpr lhs) (← bvExprToExpr ParsedFpExpr rhs)
+  | .const b =>
+      match b with
+      | true => return (mkConst ``Bool.true)
+      | _ => return (mkConst ``Bool.false)
+  | .not boolExpr => return mkApp (.const ``Bool.not []) (← go boolExpr)
+  | .gate gate lhs rhs =>
+        match gate with
+        | .or => return mkApp2 (.const ``Bool.or []) (← go lhs) (← go rhs)
+        | .xor => return mkApp2 (.const ``Bool.xor []) (← go lhs) (← go rhs)
+        | .and => return mkApp2 (.const ``Bool.and []) (← go lhs) (← go rhs)
+        | .beq => return mkApp4 (.const ``BEq.beq [levelZero]) (mkConst ``Bool) (beqBoolInstExpr) (← go lhs) (← go rhs)
+  | _ => throwError m! "Unsupported operation"
+
+/- def toBVExpr' (bvExpr : FpExpr w) : GeneralizerStateM (BVExpr w) := do
+  match bvExpr with
+  | .var idx => return BVExpr.var idx
+  | .const val => return BVExpr.const val
+  | .bin lhs op rhs  => return BVExpr.bin (← toBVExpr' lhs) op (← toBVExpr' rhs)
+  | .un op expr =>  return BVExpr.un op (← toBVExpr' expr)
+  | .append lhs rhs h => return BVExpr.append (← toBVExpr' lhs) (← toBVExpr' rhs) h
+  | .replicate n expr h => return BVExpr.replicate n (← toBVExpr' expr) h
+  | .shiftLeft lhs rhs =>  return BVExpr.shiftLeft (← toBVExpr' lhs) (← toBVExpr' rhs)
+  | .shiftRight lhs rhs => return BVExpr.shiftRight (← toBVExpr' lhs) (← toBVExpr' rhs)
+  | .arithShiftRight lhs rhs =>return BVExpr.arithShiftRight (← toBVExpr' lhs) (← toBVExpr' rhs)
+  | _ => throwError m! "Unsupported operation provided: {bvExpr}"
+
+
+def toBVLogicalExpr (bvLogicalExpr: GenFpLogicalExpr) : GeneralizerStateM BVLogicalExpr := do
+  match bvLogicalExpr with
+  | .literal (GenBVPred.bin lhs op rhs) => return BoolExpr.literal (BVPred.bin (← toBVExpr' lhs) op (← toBVExpr' rhs))
+  | .const b => return BoolExpr.const b
+  | .not boolExpr => return BoolExpr.not (← toBVLogicalExpr boolExpr)
+  | .gate gate lhs rhs => return BoolExpr.gate gate (← toBVLogicalExpr lhs) (← toBVLogicalExpr rhs)
+  | _ => throwError m! "Unsupported operation"
+ -/
