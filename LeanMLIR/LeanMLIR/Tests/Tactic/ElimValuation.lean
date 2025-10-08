82b2d676d13e22a15b9460f859cdb9fd4a9564b9
Alex Keizer <alex@keizer.dev>
2025-10-08 13:34:39 +0100

    chore: add a testcase that exposes a bug in elimValuation simproc

---
+++ LeanMLIR/LeanMLIR/Tests/Tactic/ElimValuation.lean
@@ -0,0 +1,33 @@
+import LeanMLIR
+
+/-!
+# ElimValuation simproc
+
+The following is an MWE extracted from `circomvent` (our LLZK experiments repo),
+where the `SSA.elimValuation` triggered a `(kernel) application type mismatch`
+error.
+-/
+
+namespace MWE
+open Ctxt (Valuation)
+
+inductive Ty
+  | felt
+
+def BabyBear := 2^31 - 2^27 + 1
+
+instance : TyDenote Ty where toType
+  | .felt => Fin BabyBear
+
+set_option warn.sorry false in
+example (i : Fin BabyBear) :
+    ∀ (os : Valuation ⟨[Ty.felt]⟩), ∃ (es' : Valuation ⟨[Ty.felt]⟩),
+      (Valuation.cons i <| .cons i <| .nil) = (os ++ es') := by
+  -- simp only [SSA.elimValuation]
+  -- ^^ uncomment the above to trigger a `(kernel) application type mismatch` error
+  stop
+  guard_target = ∀ (e : ⟦Ty.felt⟧), ∃ (es' : Valuation ⟨[Ty.felt]⟩),
+    (Valuation.cons i <| .cons i .nil) = (Valuation.cons e .nil ++ es')
+  sorry
+
+end MWE
