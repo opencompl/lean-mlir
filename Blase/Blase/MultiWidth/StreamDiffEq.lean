a624a2bfe4cc2222a42a82c9d7c38b9fe735d204
Siddharth Bhat <siddu.druid@gmail.com>
2025-10-13 15:16:52 +0800

    chore: stream diff eq proof

--- Blase/Blase/MultiWidth/StreamDiffEq.lean
+++ Blase/Blase/MultiWidth/StreamDiffEq.lean
@@ -2,7 +2,7 @@ import Blase.Fast.Circuit
 import Blase.Fast.FiniteStateMachine
 
 structure InputVar (ι : Type) (npast : Nat) where
-  input : ι
+  inputIx : ι
   past : Fin npast
 deriving DecidableEq, Hashable
 
@@ -35,9 +35,9 @@ instance [instFinEnumI : FinEnum ι] : FinEnum (InputVar ι npast) where
     let instFinEnumFinNpast : FinEnum (Fin npast) := by infer_instance
     instFinEnumI.card * instFinEnumFinNpast.card
   equiv := {
-    toFun := fun { input, past } =>
+    toFun := fun { inputIx, past } =>
       let instFinEnumFinNpast : FinEnum (Fin npast) := by infer_instance
-      let finInput := instFinEnumI.equiv.toFun input
+      let finInput := instFinEnumI.equiv.toFun inputIx
       let finPast := instFinEnumFinNpast.equiv.toFun past
 
       ⟨finInput.val * instFinEnumFinNpast.card + finPast.val, by
@@ -53,7 +53,9 @@ instance [instFinEnumI : FinEnum ι] : FinEnum (InputVar ι npast) where
   }
 
 structure StreamDiffEq (ι : Type) (npast : Nat) where
-  inInit : InputVar ι npast → Bool
+  /-- The output value for the first n steps. -/
+  initialOutputVal : Fin npast → Bool
+  /-- The output as a circuit of the past 'npast' inputs. -/
   outCircuit : Circuit (InputVar ι npast)
 
 
@@ -74,9 +76,7 @@ theorem BitStream.cons_succ (b : Bool) (bs : BitStream) (n : Nat) :
 
 
 /-- Append n bits to the left of a bitstream. -/
-def BitStream.appendLeft (b : BitStream)
-    (n : Nat)
-    (env : Fin n → Bool) : BitStream :=
+def BitStream.appendLeft (n : Nat) (env : Fin n → Bool) (b : BitStream) : BitStream :=
     match n with
     | 0 => b
     | n + 1 => BitStream.cons (env 0) (b.appendLeft n (fun k => env k.succ))
@@ -129,22 +129,29 @@ Produce the output stream differential equation as a bitstream.
 -/
 def StreamDiffEq.toStream [DecidableEq ι]
       (s : StreamDiffEq ι npast) (inputStream : ι → BitStream) : BitStream :=
-  let newStreams : ι → BitStream := fun i =>
-    BitStream.appendLeft (inputStream i) npast
-      (fun kpast => s.inInit ⟨i, kpast⟩)
-  BitStream.ofCircuitPointwise s.outCircuit fun iv =>
-    (newStreams iv.input).drop iv.past
+    fun k =>
+      if h : k < npast then
+        s.initialOutputVal ⟨k, by omega⟩
+      else
+        s.outCircuit.eval (fun input =>
+            inputStream input.inputIx (k - input.past.val)
+        )
+
 
 @[simp]
-theorem StreamDiffEq.toStream_eq_eval_of_lt [DecidableEq ι]
+theorem StreamDiffEq.toStream_eq_initialOutputVal_of_lt [DecidableEq ι]
     (s : StreamDiffEq ι npast)
-    (env : InputVar ι npast → Bool)
-    (inputStream : ι → BitStream) (n : Nat) (hn : ix < npast) :
-    (s.toStream inputStream) ix = s.outCircuit.eval env := by
-  simp [StreamDiffEq.toStream, BitStream.eval_drop]
-  congr
-  ext i
-  sorry
+    (inputStream : ι → BitStream) (hn : k < npast) :
+    (s.toStream inputStream) k = s.initialOutputVal ⟨k, hn⟩ := by
+  simp [StreamDiffEq.toStream, hn]
+
+@[simp]
+theorem StreamDiffEq.toStream_eq_eval_of_le [DecidableEq ι]
+    (s : StreamDiffEq ι npast)
+    (inputStream : ι → BitStream) (hn : npast ≤ k) :
+    (s.toStream inputStream) k =
+      s.outCircuit.eval (fun input => inputStream input.inputIx (k - input.past.val) ) := by
+  simp [StreamDiffEq.toStream, hn]
 
 /--
 Produce the output stream differential equation as a FSM
@@ -152,19 +159,117 @@ Produce the output stream differential equation as a FSM
 def StreamDiffEq.toFSM [DecidableEq ι] [Hashable ι] [FinEnum ι]
     (s : StreamDiffEq ι npast) : FSM ι where
   α := InputVar ι npast
-  initCarry := s.inInit
-  outputCirc := s.outCircuit.map Sum.inl
+  initCarry := fun _ => true
+  -- | completely ignore the current input?
+  -- | No, that seems wrong! Instead, we just need to output the current circuit output.
+  -- What we should do, is for the first N steps, output whatever the output
+  -- says, and after that, just keep outputting the circuit output.
+  outputCirc :=
+    s.outCircuit.map fun iv =>
+      if h : iv.past.val = 0 then
+          .inr iv.inputIx
+      else
+          .inl (InputVar.mk (iv.inputIx) ⟨iv.past.val - 1, by omega⟩)
   -- | we need to rotate, and send bits to the more past state.
   nextStateCirc := fun iv =>
       if h : iv.past.val = 0 then
-        Circuit.var true <| .inr iv.input
+        Circuit.var true <| .inr iv.inputIx
       else
-        Circuit.var true <| .inl ⟨iv.input, ⟨iv.past.val - 1, by omega⟩⟩
+        Circuit.var true <| .inl ⟨iv.inputIx, ⟨iv.past.val - 1, by omega⟩⟩
 
-theorem StreamDiffEq.toFsm_eval_eq_toStream [DecidableEq ι] [Hashable ι]
-    [FinEnum ι] (s : StreamDiffEq ι npast) (inputStream : ι → BitStream) :
-    (s.toFSM.eval inputStream) = (s.toStream inputStream) := by
-  ext i
-  by_cases hi : i < npast
-  · simp
-  · sorry
+/--
+Make an FSM that overrides the output of another FSM for one clock cycle
+to a constant value.
+-/
+def fsmOverrideOutput (f : FSM arity) (b : Bool) : FSM arity where
+  α := Unit ⊕ f.α
+  initCarry := fun i =>
+    match i with
+    | .inl () => true
+    | .inr a => f.initCarry a
+  outputCirc :=
+    Circuit.ite (Circuit.var true <| .inl (.inl ()))
+      (Circuit.ofBool b)
+      (f.outputCirc.map fun v =>
+        match v with
+        | .inl fa => .inl (.inr fa)
+        | .inr a => .inr a)
+  nextStateCirc := fun i =>
+    match i with
+    | .inl () => .ofBool false -- make 'false'.
+    | .inr a =>
+      (f.nextStateCirc a).map fun v =>
+        match v with
+        | .inl fa => .inl (.inr fa)
+        | .inr a => .inr a
+
+@[simp]
+theorem eval_FsmOverrideOutput_zero
+    {f : FSM arity} {b : Bool} {env : arity → BitStream} :
+    (fsmOverrideOutput f b).eval env 0 = b := by
+  simp [FSM.eval, fsmOverrideOutput, FSM.nextBit]
+  split_ifs
+  case pos h => simp [h]
+  case neg h => simp [h]
+
+@[simp]
+theorem carry_fsmOverrideOutput_eq
+    {f : FSM arity} {b : Bool} {env : arity → BitStream} :
+    ∀ (a : f.α), ((fsmOverrideOutput f b).carry env n) (.inr a) = (f.carry env n) a := by
+  induction n generalizing env b
+  case zero =>
+    intros a
+    simp [fsmOverrideOutput, FSM.carry, FSM.nextBit, Circuit.eval_map]
+  case succ n ihn =>
+    intros a
+    simp [fsmOverrideOutput, FSM.carry, FSM.nextBit, Circuit.eval_map]
+    congr
+    ext i
+    rcases i with a | i
+    · simp only [Sum.elim_inl]
+      rw [← ihn (env := env) (b := b)]
+      simp [fsmOverrideOutput]
+    · simp
+
+@[simp]
+theorem eval_FsmOverrideOutput_succ {f : FSM arity} {b : Bool} :
+    (fsmOverrideOutput f b).eval env n =
+      if n = 0 then b else f.eval env n := by
+  -- | TODO: replace all FSM proofs with `eval_induction_1`?
+  -- TODO: Write about this reasoning principle in the paper.
+  induction n using FSM.eval_induction_1
+    (fsm := fsmOverrideOutput f b)
+    (inputs := env)
+    (SInv := fun (i : Nat) (state : Unit ⊕ f.α → Bool) =>
+      (∀ a, state (.inr a) = (f.carry env i) a) ∧ (state (.inl ()) = decide (i = 0)))
+  case hstate0 =>
+    constructor
+    · intros a
+      simp [fsmOverrideOutput]
+    · simp [fsmOverrideOutput]
+  case hStateSucc k state ih =>
+    simp [fsmOverrideOutput, FSM.nextBitState, FSM.nextBit]
+    simp [FSM.carry]
+    simp [FSM.nextBit, Circuit.eval_map]
+    intros a
+    congr
+    ext i
+    rcases i with a | i
+    · simp [ih]
+    · simp
+  case hEval k state ih =>
+    simp [fsmOverrideOutput, FSM.nextBitOutput, FSM.nextBit]
+    obtain ⟨ih1, ih2⟩ := ih
+    simp [ih2]
+    split
+    case isTrue hk =>
+      subst hk
+      rcases hb : b <;> simp
+    case isFalse hk =>
+      simp [Circuit.eval_map]
+      simp [FSM.eval, FSM.nextBit]
+      congr
+      ext i
+      rcases i with a | i
+      · simp [ih1]
+      · simp
