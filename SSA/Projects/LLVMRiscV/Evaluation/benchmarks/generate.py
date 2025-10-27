4e65f1dad6c3def402353de7665ae82c59e7a7f0
luisacicolini <luisacicolini@gmail.com>
2025-10-27 07:19:58 +0000

    chore: fix args

--- SSA/Projects/LLVMRiscV/Evaluation/benchmarks/generate.py
+++ SSA/Projects/LLVMRiscV/Evaluation/benchmarks/generate.py
@@ -577,13 +577,14 @@ def main():
         "-llvm", "--llvm_opt", 
         help="Optimization level for LLVM.",
         nargs="+",
-        choices=["O3", "O2", "O1", "O0", "default"]
+        choices=["O3", "O2", "O1", "O0", "default"], 
+        default="default"
     )
     
     args = parser.parse_args()
     
     opts_to_evaluate = (
-        ["O3", "Os", "default"] if "all" in args.llvm_opt else args.llvm_opt
+        ["O3", "O2", "O1", "O0", "default"] if "all" in args.llvm_opt else args.llvm_opt
     )
 
 
