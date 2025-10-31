672155fc081b2c3f3ce9a264b606a20a51f0848d
Siddharth Bhat <siddu.druid@gmail.com>
2025-10-31 14:17:43 +0000

    chore: add test that we create an ill typed term at 'truncate'

--- Blase/BlaseTest/WidthGeneralize.lean
+++ Blase/BlaseTest/WidthGeneralize.lean
@@ -10,3 +10,14 @@ trace: ⊢ ∀ (w w_1 : ℕ) (x : BitVec w) (y : BitVec w_1), BitVec.zeroExtend 
       x.zeroExtend _ + y = y + x.zeroExtend _ := by
   bv_generalize
   trace_state
+
+
+/- See that this produces an ill-typed term, so we fail. -/
+open BitVec in
+theorem test_thm.extracted_1._1 : ∀ (x : BitVec 64), zeroExtend 64 (truncate 32 x) = x &&& 4294967295#64 := by
+  intros
+  bv_generalize +specialize
+  intros
+  bv_decide
+  done
+
