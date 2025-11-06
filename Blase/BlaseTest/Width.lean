55b8f24b20ec1391359906f8eeaa8acacf80cd2a
Siddharth Bhat <siddu.druid@gmail.com>
2025-11-06 01:44:56 +0000

    chore: add width tests

--- Blase/BlaseTest/Width.lean
+++ Blase/BlaseTest/Width.lean
@@ -1,5 +1,6 @@
 import Blase
 open BitVec
+set_option warn.sorry false
 
 theorem width1 {v w : Nat} (x : BitVec v) :
     x.zeroExtend (min v (max v w)) = x.signExtend (min v (max v w)) := by
@@ -8,4 +9,14 @@ theorem width1 {v w : Nat} (x : BitVec v) :
 theorem width2 {v w : Nat} (x : BitVec v) :
     x.signExtend (max v (min v w)) = x.zeroExtend (max v (min v w)) := by
   bv_multi_width
+
+theorem add_incr_right (x : BitVec v) : 
+    x.zeroExtend (v + 2) = (x.zeroExtend (v + 1)).zeroExtend (v + 2) := by
+  fail_if_success bv_multi_width
+  sorry
+
+theorem add_incr_left (x : BitVec v) : 
+    x.zeroExtend (2 + v) = (x.zeroExtend (1 + v)).zeroExtend (2 + v) := by
+  fail_if_success bv_multi_width
+  sorry
 
