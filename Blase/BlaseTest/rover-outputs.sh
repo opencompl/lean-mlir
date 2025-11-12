1c916cb8a155fa5998660c16272daf25bca1b606
Siddharth Bhat <siddu.druid@gmail.com>
2025-11-12 00:55:26 +0000

    chore: fix script that produces ROVER info

--- Blase/BlaseTest/rover-outputs.sh
+++ Blase/BlaseTest/rover-outputs.sh
@@ -4,12 +4,12 @@ rm "rover-stats.tex" || true
 exec &> >(tee -a "rover-stats.tex")
 
 ROVER_NUM_PROBLEMS=$(cat Rover.lean | grep -c '\"name\":')
-echo "\\newcommand{RoverTotalProblems}{$ROVER_NUM_PROBLEMS}" 
+echo "\\newcommand{\\RoverTotalProblems}{$ROVER_NUM_PROBLEMS}" 
 NUM_SOLVED_MULTI_WIDTH=$(cat Rover.lean | grep -c "MULTIWIDTH")
-echo "\\newcommand{RoverNumSolvedMultiWidth}{$NUM_SOLVED_MULTI_WIDTH}" 
+echo "\\newcommand{\\RoverNumSolvedMultiWidth}{$NUM_SOLVED_MULTI_WIDTH}" 
 NUM_SOLVED_BMC=$(cat Rover.lean | grep -c "BMC")
-echo "\\newcommand{RoverNumSolvedBmc}{$NUM_SOLVED_BMC}" 
+echo "\\newcommand{\\RoverNumSolvedBmc}{$NUM_SOLVED_BMC}" 
 NUM_INEXPRESSIBLE=$(cat Rover.lean | grep -c "INEXPRESSIBLE")
-echo "\\newcommand{RoveNumInexpressible}{$NUM_INEXPRESSIBLE}" 
+echo "\\newcommand{\\RoveNumInexpressible}{$NUM_INEXPRESSIBLE}" 
 NUM_TODO=$(cat Rover.lean | grep -c "TODO")
-echo "\\newcommand{RoveNumTodo}{$NUM_TODO}" 
+echo "\\newcommand{\\RoveNumTodo}{$NUM_TODO}" 
