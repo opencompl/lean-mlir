5b9a82703563854d9ff65b5d6805d25152ec26bf
luisacicolini <luisacicolini@gmail.com>
2025-11-12 14:43:10 +0000

    chore: add bug 69876

---
+++ SSA/Projects/LLVMRiscV/Evaluation/LLVM-bug-reports/issue_59876.lean
@@ -0,0 +1,33 @@
+import SSA.Projects.RISCV64.PrettyEDSL
+import LeanMLIR.Framework
+import SSA.Projects.LLVMRiscV.PeepholeRefine
+import SSA.Projects.LLVMRiscV.Simpproc
+import SSA.Projects.RISCV64.Tactic.SimpRiscV
+import SSA.Projects.LLVMRiscV.Pipeline.mkRewrite
+
+/-!
+  Bug reported in https://github.com/llvm/llvm-project/issues/59876
+-/
+
+namespace BitVec
+open LLVMRiscV
+
+def original := [LV| {
+  ^entry (%x : i1):
+  %1 = llvm.mlir.constant (1) : i1
+  %0 = llvm.mul %x, %1 : i1
+  llvm.return %0 : i1
+}]
+
+def optimized_incorrect := [LV| {
+  ^entry (%x : i1):
+  %0 = llvm.mlir.constant (0) : i1
+  llvm.return %0 : i1
+  }]
+
+def bug : LLVMPeepholeRewriteRefine 1 [Ty.llvm (.bitvec 1)] where
+  lhs:= original
+  rhs:= optimized_incorrect
+  correct := by
+    simp_lowering
+    <;> sorry
