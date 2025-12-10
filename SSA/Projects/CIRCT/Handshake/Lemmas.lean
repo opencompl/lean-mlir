1724f5292ebba929d3be285a307b7c43259eb36e
luisacicolini <luisacicolini@gmail.com>
2025-12-10 13:15:09 +0000

    chore: Handhsake

--- SSA/Projects/CIRCT/Handshake/Lemmas.lean
+++ SSA/Projects/CIRCT/Handshake/Lemmas.lean
@@ -1,33 +1,24 @@
-import SSA.Core
-import SSA.Projects.CIRCT.Handshake.Handshake
-import SSA.Projects.CIRCT.Stream.Stream
-import SSA.Projects.CIRCT.Stream.WeakBisim
-import SSA.Core.Tactic
+import LeanMLIR
 
-namespace CIRCTStream
+import SSA.Projects.CIRCT.Stream.Basic
+import SSA.Projects.CIRCT.Handshake.Handshake
+import Init.Data.String.Basic
+
+open HandshakeStream
 namespace Stream.Bisim
 
 /-! We implement the rewrites from https://dl.acm.org/doi/pdf/10.1145/3676641.3715993 -/
 
-namespace CIRCTStream
 
-namespace Strean.Bisim
+theorem corec_prod_eq_val (x : Stream α):
+  (corec_prod x fun x => Id.run (x 0, x 0, tail x)) = (corec_prod x fun x => Id.run (x 0, x 0, x.tail)) := by
+  exact _root_.rfl
 
-theorem corec₂_eq_val (x : Stream α):
-  (corec₂ x fun x => Id.run (x 0, x 0, tail x)) = (corec₂ x fun x => Id.run (x 0, x 0, x.tail)) := by
-  apply corec₂_eq_corec₂_of
-  rotate_left 2
-  · exact Eq.refl _
-  · intro b₁ b₂ h
-    simp [h]
-  · intro b₁ b₂ h
-    simp [h]
+theorem corec_prod_corec1 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
+  (corec_prod s f).1 = corec s (fun s' => let ⟨ a, _, b ⟩ := f s'; (a, b) ) := by rfl
 
-theorem corec₂_corec1 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
-  (corec₂ s f).1 = corec s (fun s' => let ⟨ a, _, b ⟩ := f s'; (a, b) ) := by rfl
-
-theorem corec₂_corec2 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
-  (corec₂ s f).2 = corec s (fun s' => let ⟨ _, a, b ⟩ := f s'; (a, b) ) := by rfl
+theorem corec_prod_corec2 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
+  (corec_prod s f).2 = corec s (fun s' => let ⟨ _, a, b ⟩ := f s'; (a, b) ) := by rfl
 
 
 theorem rewrite_a_fst (stream_d : Stream α) (stream_c : Stream (BitVec 1)):
@@ -36,7 +27,7 @@ theorem rewrite_a_fst (stream_d : Stream α) (stream_c : Stream (BitVec 1)):
      := by
   simp only [HandshakeOp.branch, BitVec.ofNat_eq_ofNat, HandshakeOp.supp, HandshakeOp.fork,
     HandshakeOp.not]
-  simp [corec₂_corec1]
+  simp [corec_prod_corec1]
 
 
   sorry
