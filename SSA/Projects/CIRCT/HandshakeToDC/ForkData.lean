d88dfcd922e3853a49b079dffd3720eac90efd02
luisacicolini <luisacicolini@gmail.com>
2025-12-11 09:06:09 +0000

    chore: clean up

---
+++ SSA/Projects/CIRCT/HandshakeToDC/ForkData.lean
@@ -0,0 +1,273 @@
+import LeanMLIR
+
+import SSA.Projects.CIRCT.Stream.Basic
+import SSA.Projects.CIRCT.Stream.Lemmas
+import SSA.Projects.CIRCT.DC.DC
+import SSA.Projects.CIRCT.DCxComb.DCxCombFunctor
+import SSA.Projects.CIRCT.HSxComb.HSxCombFunctor
+import Init.Data.String.Basic
+
+namespace HandshakeStream
+
+theorem corec_prod_eq_tok (x : DCOp.TokenStream):
+  (corec_prod x fun x => Id.run (x 0, x 0, tail x)) = (corec_prod x fun x => Id.run (x 0, x 0, x.tail)) := by
+  apply corec_prod_eq_corec_prod_of
+  sorry
+  sorry
+  sorry
+  sorry
+  -- rotate_left 2
+  -- · exact Eq.refl _
+  -- · intro b₁ b₂ h
+  --   simp [h]
+  -- · intro b₁ b₂ h
+  --   simp [h]
+
+theorem corec_prod_eq_val (x : Stream α):
+  (corec_prod x fun x => Id.run (x 0, x 0, tail x)) = (corec_prod x fun x => Id.run (x 0, x 0, x.tail)) := by
+  apply corec_prod_eq_corec_prod_of
+  sorry
+  sorry
+  sorry
+  sorry
+  -- rotate_left 2
+  -- · exact Eq.refl _
+  -- · intro b₁ b₂ h
+  --   simp [h]
+  -- · intro b₁ b₂ h
+  --   simp [h]
+
+-- theorem EqIsBisim {α} : IsBisim α Eq := by
+--   simp [IsBisim]
+--   intros a; exists 0; exists 0
+--   and_intros; all_goals first | rfl | intros _ h; cases h
+
+theorem fork_hs_dc_equiv_fst (x : DCOp.TokenStream):
+    (DCOp.fork (x)).fst ~ (HandshakeOp.fork (x)).fst := by sorry
+  -- simp [Bisim, DCOp.fork, HandshakeOp.fork]
+  -- exists Eq
+  -- and_intros
+  -- · rw [corec_prod_eq_tok]
+  --   rfl
+  -- · apply EqIsBisim
+
+theorem fork_hs_dc_equiv_snd (x : DCOp.TokenStream):
+    (DCOp.fork (x)).snd ~ (HandshakeOp.fork (x)).snd := by sorry
+  -- simp [Bisim, DCOp.fork, HandshakeOp.fork]
+  -- exists Eq
+  -- and_intros
+  -- · rw [corec_prod_eq_tok]
+  --   rfl
+  -- · apply EqIsBisim
+
+
+/- prove that dc fork with a value is eqv to handshake fork -/
+
+/- step 1: write dc circuit -/
+
+unseal String.splitOnAux in
+def DCFork := [DC_com| {
+  ^entry(%0 : !ValueStream_8):
+    %unpack12 = "DC.unpack" (%0) : (!ValueStream_8) -> (!ValueTokenStream_8)
+    %unpack1 = "DC.fstVal" (%unpack12) : (!ValueTokenStream_8) -> (!ValueStream_8)
+    %unpack2 = "DC.sndVal" (%unpack12) : (!ValueTokenStream_8) -> (!TokenStream)
+    %fork12 = "DC.fork" (%unpack2) : (!TokenStream) -> (!TokenStream2)
+    %fork1 = "DC.fst" (%fork12) : (!TokenStream2) -> (!TokenStream)
+    %fork2 = "DC.snd" (%fork12) : (!TokenStream2) -> (!TokenStream)
+    %pack1 = "DC.pack" (%unpack1, %fork1) : (!ValueStream_8, !TokenStream) -> (!ValueStream_8)
+    %pack2 = "DC.pack" (%unpack1, %fork2) : (!ValueStream_8, !TokenStream) -> (!ValueStream_8)
+    %pack12 = "DC.pair" (%pack1, %pack2) : (!ValueStream_8, !ValueStream_8) -> (!ValueStream2_8)
+    "return" (%pack12) : (!ValueStream2_8) -> ()
+  }]
+
+/- step 2: denote dc circuit -/
+
+#check DCFork
+#eval DCFork
+#reduce DCFork
+#check DCFork.denote
+#print axioms DCFork
+
+def ofList' (vals : List (Option α)) : Stream α :=
+  fun i => (vals[i]?).join
+
+def x : DCOp.ValueStream (BitVec 8) := ofList' [some 1, none, some 2, some 5, none]
+
+-- def test : DCOp.ValueStream (BitVec 8) × DCOp.ValueStream (BitVec 8) :=
+--   DCFork.denote (Ctxt.Valuation.ofHVector (.cons x <| .nil))
+
+/- step 3: prove equivalence -/
+
+theorem corec_prod_corec1 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
+  (corec_prod s f).1 = corec s (fun s' => let ⟨ a, _, b ⟩ := f s'; (a, b) ) := by rfl
+
+theorem corec_prod_corec2 (s : Stream γ) (f : Stream γ -> Option α × Option β × Stream γ) :
+  (corec_prod s f).2 = corec s (fun s' => let ⟨ _, a, b ⟩ := f s'; (a, b) ) := by rfl
+
+-- this function maps a stream α to a stream α × stream unit st stream unit stores
+-- whether the stream has something in there
+-- def map_to_unit_pair (x : Stream α) (z : Stream α × Stream Unit) : Prop :=
+--     x = z.1 ∧ x.map (·.map (λ _ => ())) = z.2
+
+theorem tail_iterate'' {α} {n} {s : Stream' α} : Stream'.iterate Stream'.tail s n m = s (n + m) := by
+  induction n generalizing m; dsimp [Stream'.iterate]; simp
+  dsimp [Stream'.iterate]
+  rename_i n hn
+  have : (Stream'.iterate Stream'.tail s n).tail m = Stream'.iterate Stream'.tail s n (m + 1) := by rfl
+  rw [this]; rw [hn];
+  have : n + (m + 1) = n + 1 + m := by omega
+  rw [this]
+
+theorem tail_iterate' {α} {n} {s : Stream' α} : Stream'.iterate Stream'.tail s n 0 = s n :=
+  tail_iterate''
+
+-- open Ctxt in
+-- theorem equiv_fork_fst (streamInt : DCOp.ValueStream (BitVec 8)) :
+--   (HandshakeOp.fork streamInt).fst ~ (DCFork.denote (Valuation.ofHVector (.cons streamInt <| .nil))).fst := by
+--   unfold HandshakeOp.fork DCFork
+--   simp_peephole
+--   unfold Bisim; exists Eq
+--   rw [corec_prod_corec1]
+--   refine
+--     (and_symm_left
+--           (fun x =>
+--             DCOp.pack (fun x => (DCOp.unpack fun x => streamInt x).1 x)
+--               (fun x => (DCOp.fork fun x => (DCOp.unpack fun x => streamInt x).2 x).1 x) x)
+--           (corec streamInt fun s' =>
+--             match Id.run (s' 0, s' 0, s'.tail) with
+--             | (a, fst, b) => (a, b))
+--           (IsBisim Eq)).mp
+--       ?_
+--   unfold DCOp.pack DCOp.unpack DCOp.fork
+--   simp_peephole
+
+--   · sorry
+      -- and_intros
+      -- all_goals
+      --   unfold CIRCTStream.Stream.tail
+      --   rcases hm with ⟨ heq, hmap ⟩; subst_vars
+      --   rcases b with ⟨ b1, b2 ⟩; dsimp only at *
+      --   rcases hb1 : b1 0 with _ | val <;> cases hb2 : b2 0
+      --     <;> first | rfl | simp [Id.run, Stream'.map_tail, hmap]
+      --   rw [Stream'.ext_iff] at hmap; specialize hmap 0
+      --   simp_all [Stream'.get, Stream'.map]
+    -- · unfold map_to_unit_pair
+    --   and_intros
+    --   · simp only; rw [corec_prod_corec1]
+    --     have h : ((fun s' => match
+    --         Id.run (match s' 0 with
+    --           | some _ => (s' 0, some (), s'.tail)
+    --           | none => (none, none, s'.tail)) with
+    --       | (a, _, b) => (a, b)) : Stream Int → Option Int × Stream Int) =
+    --       (fun s' => (s' 0, s'.tail)) := by
+    --       ext s' <;> cases h : s' 0 <;> simp_all [Id.run]
+    --     dsimp only [MLIR2DC.instTyDenoteTy2, TyDenote.toType] at *;
+    --     unfold DC.unpack.match_1 equiv_fork_fst.match_1 at *
+    --     rw [h]
+    --     unfold corec
+    --     rw [Stream'.ext_iff]; intro n
+    --     clear h
+    --     rw [Stream'.corec_def]; simp
+    --     unfold Stream Stream.tail at *
+    --     have {α} : ((fun x => x.tail) : Stream' α → Stream' α) = Stream'.tail := by rfl
+    --     rw [this]; unfold Stream'.get
+    --     rw [tail_iterate']
+    --   · simp only; rw [corec_prod_corec1, corec_prod_corec2]
+    --     dsimp only [Id.run]
+    --     have h : ((fun s' => match
+    --         Id.run (match s' 0 with
+    --           | some _ => (s' 0, some (), s'.tail)
+    --           | none => (none, none, s'.tail)) with
+    --       | (_, a, b) => (a, b)) : Stream Int → Option Unit × Stream Int) =
+    --       (fun s' => (Option.map (fun _ => ()) (s' 0), s'.tail)) := by
+    --       ext s' <;> cases h : s' 0 <;> simp_all [Id.run]
+    --     unfold CIRCTStream.DC.unpack.match_1 CIRCTStream.Stream.Bisim.corec_prod_corec1.match_1 CIRCTStream.Stream.Bisim.equiv_fork_fst.match_2 CIRCTStream.Stream.Bisim.equiv_fork_fst.match_1 at *
+    --     dsimp only [Id.run, TyDenote.toType] at *
+    --     rw [h]; clear h
+    --     unfold corec
+    --     rw [Stream'.ext_iff]; intro n
+    --     rw [Stream'.corec_def]; simp only [Stream'.get_map]
+    --     unfold Stream Stream.tail at *
+    --     have {α} : ((fun x => x.tail) : Stream' α → Stream' α) = Stream'.tail := by rfl
+    --     rw [this]; unfold Stream'.get
+    --     rw [tail_iterate', Stream'.corec_def, this]
+    --     unfold Stream'.map Stream'.get
+  --   --     dsimp only
+  --   --     rw [tail_iterate']
+  -- · apply EqIsBisim
+
+theorem stream_pair_1 (s : Stream α) (f : Stream α → Option α × Option α × Stream α):
+    (corec_prod s f).1 = corec s (fun x => let ⟨f1, _, f2⟩ := f x; (f1, f2)) := by
+  rfl
+
+
+inductive concrete_bisim (x y : Stream α) : Prop
+
+-- pb: generate bisim relation defined over streams (a concrete one)
+-- needs to rely on cores
+
+def IsBisim' (R' : Stream α → Stream α → Prop) : Prop :=
+  ∀ a b, R' a b → ∃ n m,
+    R' (a.drop (n+1)) (b.drop (m+1))
+    ∧ a.get n = b.get m
+    ∧ (∀ i < n, a.get i = none)
+    ∧ (∀ j < m, b.get j = none)
+
+theorem corec_prod_eq_corec_of_corec_prod (streamInt: DCOp.ValueStream Int) :
+    (corec_prod streamInt fun x => (x 0, x 0, x.tail)).1 ≈
+    corec
+      ((corec_prod streamInt fun (x : DCOp.ValueStream Int) =>
+            (match x 0 with
+              | some val => (x 0, some (), x.tail)
+              | none => (none, none, x.tail))).1,
+        (corec_prod
+            (corec_prod streamInt fun x =>
+                (match x 0 with
+                  | some val => (x 0, some (), x.tail)
+                  | none => (none, none, x.tail))).2
+            fun x => Id.run (x 0, x 0, tail x)).1)
+      fun x =>
+      match x.1 0, x.2 0 with
+      | some x₀, some val => (some x₀, tail x.1, tail x.2)
+      | some val, none => (none, x.1, tail x.2)
+      | none, some val => (none, tail x.1, x.2)
+      | none, none => (none, tail x.1, tail x.2) := by
+  rw [stream_pair_1]
+  unfold HasEquiv.Equiv
+  unfold instHasEquivOfSetoid
+  simp only
+  unfold Setoid.r StreamSetoid
+  simp only
+  unfold Bisim
+  sorry
+  -- simp only
+  -- exists Eq
+  -- and_intros
+  -- · sorry
+  -- · sorry
+
+
+
+
+
+
+
+
+-- theorem determinate :
+--   Set.Subsingleton (nondeterminify2 (fun s1 s2 => BranchEg1.denote (Ctxt.Valuation.ofPair s1 s2)) (s1', s2')) := by
+--   intro x Hx y  Hy
+--   simp [Stream.nondeterminify2, Stream.StreamWithoutNones.hasStream] at *
+--   rcases Hx with ⟨ x1Stream, x1, x2Stream, x2, rfl ⟩
+--   rcases Hy with ⟨ y1Stream, y1, y2Stream, y2, rfl ⟩
+--   apply Quotient.sound
+--   -- simp [BranchEg1]
+--   -- simp [Stream.Bisim, Stream.IsBisim]
+--   subst s2'; subst s1'
+--   have y1' := Quotient.exact y1
+--   have y2' := Quotient.exact y2
+--   clear y1; clear y2
+--   trans x1Stream
+--   apply (equiv_arg1 _ _).symm
+--   trans y1Stream
+--   · assumption
+--   · apply equiv_arg1
---
+++ SSA/Projects/CIRCT/HandshakeToHW/fork.lean
@@ -0,0 +1,82 @@
+import SSA.Projects.CIRCT.Stream.Basic
+import SSA.Projects.CIRCT.Stream.Lemmas
+import SSA.Projects.CIRCT.Register.Basic
+import SSA.Projects.CIRCT.Register.Lemmas
+
+namespace HandshakeStream
+
+/-- We define the module as a function with inputs and outputs.
+  we use `Stream'` type, which does not contain `Option` values, because at this level
+  of abstractions the content of streams has been concretized
+
+  Initial handshake program:
+
+    handshake.func @test_fork(%arg0: none, %arg1: none, ...) -> (none, none, none) {
+      %0:2 = fork [2] %arg0 : none
+      return %0#0, %0#1, %arg1 : none, none, none
+    }
+
+  Lowered program (https://github.com/opencompl/DC-semantics-simulation-evaluation/commit/28ef888954a8726d4858bed925ad067729207655)
+
+  module {
+    hw.module @test_fork(in %arg0 : !esi.channel<i0>, in %arg1 : !esi.channel<i0>, in %clk : !seq.clock {dc.clock}, in %rst : i1 {dc.reset}, out out0 : !esi.channel<i0>, out out1 : !esi.channel<i0>, out out2 : !esi.channel<i0>) {
+      %rawOutput, %valid = esi.unwrap.vr %arg0, %12 : i0
+      %c0_i0 = hw.constant 0 : i0
+      %chanOutput, %ready = esi.wrap.vr %c0_i0, %3 : i0
+      %c0_i0_0 = hw.constant 0 : i0
+      %chanOutput_1, %ready_2 = esi.wrap.vr %c0_i0_0, %9 : i0
+      %false = hw.constant false
+      %true = hw.constant true
+      %0 = comb.xor %12, %true : i1
+      %1 = comb.and %5, %0 : i1
+      %emitted_0 = seq.compreg sym @emitted_0 %1, %clk reset %rst, %false : i1
+      %2 = comb.xor %emitted_0, %true : i1
+      %3 = comb.and %2, %valid : i1
+      %4 = comb.and %ready, %3 : i1
+      %5 = comb.or %4, %emitted_0 {sv.namehint = "done0"} : i1
+      %6 = comb.xor %12, %true : i1
+      %7 = comb.and %11, %6 : i1
+      %emitted_1 = seq.compreg sym @emitted_1 %7, %clk reset %rst, %false : i1
+      %8 = comb.xor %emitted_1, %true : i1
+      %9 = comb.and %8, %valid : i1
+      %10 = comb.and %ready_2, %9 : i1
+      %11 = comb.or %10, %emitted_1 {sv.namehint = "done1"} : i1
+      %12 = comb.and %5, %11 {sv.namehint = "allDone"} : i1
+      hw.output %chanOutput, %chanOutput_1, %arg1 : !esi.channel<i0>, !esi.channel<i0>, !esi.channel<i0>
+    }
+  }
+
+-/
+def module
+      (arg_0 : Stream' (BitVec 1))
+      (arg_0_valid : Stream' (BitVec 1))
+      (arg_1 : Stream' (BitVec 1))
+      (arg_1_valid : Stream' (BitVec 1))
+      (out0_ready : Stream' (BitVec 1))
+      (out1_ready : Stream' (BitVec 1))
+      (out2_ready : Stream' (BitVec 1))
+  : Vector (Stream' (BitVec 1)) 8 :=
+    let vec_streams := HandshakeStream.streams_to_vec
+                        (#v[arg_0, arg_0_valid, arg_1, arg_1_valid, out0_ready, out1_ready, out2_ready])
+    vec_to_streams <| register_wrapper
+      (inputs := vec_streams)
+      (init_regs := #v[0#1, 0#1])
+      (update_fun := -- (Vector (BitVec 1) 7 × Vector (BitVec 1) 2) → (Vector α 9 × Vector α 2)
+        fun (inp, regs) =>
+          let v2 := BitVec.xor regs[0] 1#1
+          let v3 := BitVec.and v2 inp[1]
+          let v4 := BitVec.and inp[4] v3
+          let v5 := BitVec.or v4 regs[0]
+          let v8 := BitVec.xor regs[1] 1#1
+          let v9 := BitVec.and v8 inp[1]
+          let v10 := BitVec.and inp[5] v9
+          let v11 := BitVec.or v10 regs[1]
+          let v12 := BitVec.and v5 v11
+          let v6 := BitVec.xor v12 1#1
+          let v7 := BitVec.and v11 v6
+          let v0 := BitVec.xor v12 1#1
+          let v1 := BitVec.and v5 v0
+          let updated_reg0 := v1
+          let updated_reg1 := v7
+          (#v[v12, inp[6], 0#1, v3, 0#1, v9, inp[2], inp[3]], #v[updated_reg0, updated_reg1])
+      )
