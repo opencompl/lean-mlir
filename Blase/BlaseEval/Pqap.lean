7cc68e5d0afe6eead517469430a0f65a454ad2c5
Siddharth Bhat <siddu.druid@gmail.com>
2025-12-23 11:19:45 +0100

    chore: start writing shift right elim

--- Blase/BlaseEval/Pqap.lean
+++ Blase/BlaseEval/Pqap.lean
@@ -80,8 +80,21 @@ end AbsDiffSharedSubSigned
 namespace UnsignedRounding
 
 -- | TODO: what is the precise semantics in 
+@[bv_multi_width_normalize]
 def addExtending (a : BitVec w) (b : BitVec v) : BitVec ow :=
   a.zeroExtend _ + b.zeroExtend _
+
+@[bv_multi_width_normalize]
+def topBits (a : BitVec w) (n : Nat) : BitVec n :=
+  ((a >>> (w - n))).truncate n
+
+@[bv_multi_width_normalize]
+def bottomBits (a : BitVec w) (n : Nat) : BitVec n :=
+  a.truncate n
+
+notation a "[: " n "]" => topBits a n
+notation a "[" n ":]" => bottomBits a n
+
 
 /-
 NOTE (sid): Here we do some preprocessing, where we explicitly write `w2` in terms of `w1 + 1`.
@@ -96,14 +109,121 @@ input a : w1
 output z : w2
 c <- a[:w2]
 d: 1 <- a[1:]
-constraint w1 == w2 + 1
+constraint w1 == 1 + w2
 constraint w2 > 1
 z <- c + d
 -/
-def v0  (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (w2 + 1)) (z : BitVec w2) (hw2 : w2 > 1)
-  (ha : a = alo.zeroExtend (w2 + 1) || (ahi.zeroExtend (w2 + 1) <<< 1)) : 
+def v0  (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (1 + w2)) (hw2 : w2 > 1)
+  (ha : a = (alo.zeroExtend (1 + w2) ||| (ahi.zeroExtend (1 + w2) <<< 1))) : BitVec w2 :=
       let c := ahi
       let d := alo
-      let z := c.zeroExtend w2 + d.zeroExtend w2
-      z := z
+      let z := addExtending c d
+      z
+
+/-
+input a: w1
+output z: w2
+c <- a[:w2]
+d <- a[1:].zero_extend(2)
+constraint w1 == 1 + w2
+constraint w2 > 1
+z <- c + d
+-/
+
+def v1 (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (1 + w2)) 
+  (hw2 : w2 > 1)
+  (ha : a = (alo.zeroExtend (1 + w2) ||| (ahi.zeroExtend (1 + w2) <<< 1))) : BitVec w2 :=
+      let c := ahi
+      let d := alo.zeroExtend 2
+      let z := addExtending c d
+      z
+
+theorem v0_eq_v1 (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (1 + w2))
+  (hw2 : w2 > 1)
+  (ha : a = (alo.zeroExtend (1 + w2) ||| (ahi.zeroExtend (1 + w2) <<< 1))) :
+  v0 alo ahi a hw2 ha = v1 alo ahi a hw2 ha := by
+    simp only [v0, v1]
+    bv_multi_width
+
+/-
+input a: w1
+output z: w2
+c: w1 <- a + 1'b1
+constraint w1 == 1 + w2
+constraint w2 > 1
+z <- c[:w2]
+-/
+def v2 (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (1 + w2)) 
+  (hw2 : w2 > 1)
+  (ha : a = (alo.zeroExtend (1 + w2) ||| (ahi.zeroExtend (1 + w2) <<< 1))) : BitVec w2 :=
+      let c := a + 1
+      let z := c[:w2]
+      z
+
+@[simp, bv_multi_width_normalize]
+def bvOnes (n : Nat) : BitVec w :=
+    ((1#1).signExtend n).zeroExtend w
+
+theorem shiftRight_eq_shiftLeft (x : BitVec w) (n : Nat) (y : BitVec w) :
+      ((x >>> n) = y) ↔ ((y <<< n = (x &&& (~~~(bvOnes n)))) ∧ (y &&& ~~~ bvOnes n = 0#w)) := by
+  constructor
+  · intros h 
+    subst h 
+    simp 
+    constructor
+    · ext i 
+      simp 
+      by_cases hi : i = 0 
+      · simp [hi]
+        rw [BitVec.getLsbD_signExtend]
+        subst hi
+        simp
+        by_cases hn : 0 < n 
+        · simp [hn]
+        · simp [show n = 0 by omega]
+          rfl
+      · by_cases hi : i < n 
+        · simp [hi]
+          intros hx 
+          simp [getElem_signExtend]
+        · simp [hi]
+          simp [show n + (i - n) = i by omega]
+          simp [getLsbD_signExtend, hi]
+          rfl
+    · ext i 
+      simp 
+      rw [BitVec.getLsbD_signExtend]
+      by_cases hi : i < n 
+      · simp [hi]
+      · simp [hi]
+  · intros h 
+    ext i 
+    have := congrFun (congrArg BitVec.getLsbD h) i
+    simp [BitVec.getLsbD_signExtend] at this
+    simp 
+    by_cases hi1 : i < w 
+    · simp [hi1] at this ⊢
+      by_cases hn1 : i < n 
+      · simp [hn1] at this ⊢
+        have := congrFun (congrArg BitVec.getLsbD h) (n + i)
+        simp [BitVec.getLsbD_signExtend, hi1, hn1] at this
+        by_cases hin : n + i < w 
+        · simp [hin] at this ⊢
+          exact this.symm
+        · simp [hin] at this
+          omega
+      sorry
+    · simp [hi1] at this ⊢
+      sorry
+        
+
+
+theorem v0_eq_v2 (alo : BitVec 1) (ahi : BitVec w2) (a : BitVec (1 + w2))
+  (hw2 : w2 > 1)
+  (ha : a = (alo.zeroExtend (1 + w2) ||| (ahi.zeroExtend (1 + w2) <<< 1))) :
+  v0 alo ahi a hw2 ha = v2 alo ahi a hw2 ha := by
+    simp only [v0, v2]
+    simp [topBits, show 1 + w2 - w2 = 1 by omega]
+    bv_multi_width -- TODO: need to add right shift by constant support via quantifiers.
+
 end UnsignedRounding
