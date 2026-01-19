97032dd9c7a4cfde8a3476f035dd855cba1e7860
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2026-01-19 14:29:43 +0000

    feat: `fork` circuit with buffer (#1926)
    This PR adds the circuits verify the correctness of the Handshake-to-HW
    lowering of a fork component with buffer (`fork_buffer`). We also
    refactor the `fork` circuit for consistency.
    
    We also add some infrastructure for registers and streams, to state the
    correctness lemma.

--- SSA/Projects/CIRCT/HandshakeToHW/fork.lean
+++ SSA/Projects/CIRCT/HandshakeToHW/fork.lean
@@ -6,97 +6,154 @@ import SSA.Projects.CIRCT.Register.Lemmas
 namespace HandshakeStream
 
 /-!
-  Handshake program:
+  # Fork
 
-    handshake.func @test_fork(%arg0: none, %arg1: none, ...) -> (none, none, none) {
-      %0:2 = fork [2] %arg0 : none
-      return %0#0, %0#1, %arg1 : none, none, none
-    }
-
-  Lowered program (https://github.com/opencompl/DC-semantics-simulation-evaluation/commit/28ef888954a8726d4858bed925ad067729207655)
-
-  module {
-    hw.module @test_fork(in %arg0 : !esi.channel<i0>, in %arg1 : !esi.channel<i0>, in %clk : !seq.clock {dc.clock}, in %rst : i1 {dc.reset}, out out0 : !esi.channel<i0>, out out1 : !esi.channel<i0>, out out2 : !esi.channel<i0>) {
-      %rawOutput, %valid = esi.unwrap.vr %arg0, %12 : i0
-      %c0_i0 = hw.constant 0 : i0
-      %chanOutput, %ready = esi.wrap.vr %c0_i0, %3 : i0
-      %c0_i0_0 = hw.constant 0 : i0
-      %chanOutput_1, %ready_2 = esi.wrap.vr %c0_i0_0, %9 : i0
-      %false = hw.constant false
-      %true = hw.constant true
-      %0 = comb.xor %12, %true : i1
-      %1 = comb.and %5, %0 : i1
-      %emitted_0 = seq.compreg sym @emitted_0 %1, %clk reset %rst, %false : i1
-      %2 = comb.xor %emitted_0, %true : i1
-      %3 = comb.and %2, %valid : i1
-      %4 = comb.and %ready, %3 : i1
-      %5 = comb.or %4, %emitted_0 {sv.namehint = "done0"} : i1
-      %6 = comb.xor %12, %true : i1
-      %7 = comb.and %11, %6 : i1
-      %emitted_1 = seq.compreg sym @emitted_1 %7, %clk reset %rst, %false : i1
-      %8 = comb.xor %emitted_1, %true : i1
-      %9 = comb.and %8, %valid : i1
-      %10 = comb.and %ready_2, %9 : i1
-      %11 = comb.or %10, %emitted_1 {sv.namehint = "done1"} : i1
-      %12 = comb.and %5, %11 {sv.namehint = "allDone"} : i1
-      hw.output %chanOutput, %chanOutput_1, %arg1 : !esi.channel<i0>, !esi.channel<i0>, !esi.channel<i0>
-    }
-  }
--/
-
-/--
   `fork` replicates the content of an input stream and dispatches it to two channels.
+  At the handshake level, `fork` operates on the `Stream' (Option α)` type (i.e., `Stream`).
 
   The hardware (lowered) module is a function over the `Stream'` type,
   which does not contain `Option` values, because at this level
   of abstractions the content of streams has been concretized.
+  We ignore buffers.
+
+  See: https://github.com/opencompl/DC-semantics-simulation-evaluation/tree/95684e396cffaaae0aa31e57c6d1d056305195ea/benchmarks/fork
+
 -/
-def fork
-      (arg_0 : Stream' (BitVec 1))
-      (arg_0_valid : Stream' (BitVec 1))
-      (arg_1 : Stream' (BitVec 1))
-      (arg_1_valid : Stream' (BitVec 1))
-      (out0_ready : Stream' (BitVec 1))
-      (out1_ready : Stream' (BitVec 1))
-      (out2_ready : Stream' (BitVec 1))
-  : Vector (Stream' (BitVec 1)) 8 :=
-    let vec_streams := streams_to_vec'
-                        (#v[arg_0, arg_0_valid, arg_1, arg_1_valid, out0_ready, out1_ready, out2_ready])
-    vec_to_streams' <| register_wrapper
-      (inputs := vec_streams)
-      (init_regs := #v[0#1, 0#1])
-      (update_fun := -- (Vector (BitVec 1) 7 × Vector (BitVec 1) 2) → (Vector α 9 × Vector α 2)
-        fun (inp, regs) =>
-          let v2 := BitVec.xor regs[0] 1#1
-          let v3 := BitVec.and v2 inp[1]
-          let v4 := BitVec.and inp[4] v3
-          let v5 := BitVec.or v4 regs[0]
-          let v8 := BitVec.xor regs[1] 1#1
-          let v9 := BitVec.and v8 inp[1]
-          let v10 := BitVec.and inp[5] v9
-          let v11 := BitVec.or v10 regs[1]
-          let v12 := BitVec.and v5 v11
-          let v6 := BitVec.xor v12 1#1
-          let v7 := BitVec.and v11 v6
-          let v0 := BitVec.xor v12 1#1
-          let v1 := BitVec.and v5 v0
-          let updated_reg0 := v1
-          let updated_reg1 := v7
-          (#v[v12, inp[6], 0#1, v3, 0#1, v9, inp[2], inp[3]], #v[updated_reg0, updated_reg1])
-      )
 
-/-- We prove that the description at handshake level and the description at lower (RTL) level are bisimilar. -/
-theorem fork_bisim
-    (arg0_data arg0_valid arg1_data arg1_valid out0_ready out1_ready out2_ready : Stream' (BitVec 1))
-    (arg0 arg1 out0 out1 out2 : Stream (BitVec 1)) :
-    let := ReadyValid #v[arg0_data, arg0_valid, arg0_ready] arg0
-    let := ReadyValid #v[arg1_data, arg1_valid, arg1_ready] arg1
-    let := ReadyValid #v[out0_data, out0_valid, out0_ready] out0
-    let := ReadyValid #v[out1_data, out1_valid, out1_ready] out1
-    let := ReadyValid #v[out2_data, out2_valid, out2_ready] out2
-    Bisim (iso_unary' ((fork arg0_data arg0_valid arg1_data arg1_valid out0_ready oue1_ready out2_ready).get 0))  (HandshakeOp.fork arg0).fst := by
-  unfold iso_unary' HandshakeOp.fork
-  unfold Bisim
-  simp
+/--
+  Handshake program:
 
-  sorry
+  handshake.func @test_fork(%arg0: none, %arg1: none, ...) -> (none, none, none) {
+    %0:2 = fork [2] %arg0 : none
+    return %0#0, %0#1, %arg1 : none, none, none
+  }
+-/
+def fork_handshake (arg : Stream (BitVec 1)) :=
+  corec_prod arg
+    fun x => Id.run <| do
+      let x0 := x 0
+      let x' := x.tail
+      (x0, x0, x')
+
+/--
+  First RTL module:
+
+  hw.module @handshake_fork_1ins_2outs_ctrl(in %in0 : i0, in %in0_valid : i1, in %clock : !seq.clock, in %reset : i1, in %out0_ready : i1, in %out1_ready : i1, out in0_ready : i1, out out0 : i0, out out0_valid : i1, out out1 : i0, out out1_valid : i1) {
+    %true = hw.constant true
+    %false = hw.constant false
+    %0 = comb.xor %12, %true : i1
+    %1 = comb.and %5, %0 : i1
+    %emitted_0 = seq.compreg sym @emitted_0 %1, %clock reset %reset, %false : i1
+    %2 = comb.xor %emitted_0, %true : i1
+    %3 = comb.and %2, %in0_valid : i1
+    %4 = comb.and %out0_ready, %3 : i1
+    %5 = comb.or %4, %emitted_0 {sv.namehint = "done0"} : i1
+    %6 = comb.xor %12, %true : i1
+    %7 = comb.and %11, %6 : i1
+    %emitted_1 = seq.compreg sym @emitted_1 %7, %clock reset %reset, %false : i1
+    %8 = comb.xor %emitted_1, %true : i1
+    %9 = comb.and %8, %in0_valid : i1
+    %10 = comb.and %out1_ready, %9 : i1
+    %11 = comb.or %10, %emitted_1 {sv.namehint = "done1"} : i1
+    %12 = comb.and %5, %11 {sv.namehint = "allDone"} : i1
+    hw.output %12, %in0, %3, %in0, %9 : i1, i0, i1, i0, i1
+  }
+-/
+def handshake_fork_1ins_2outs_ctrl
+      (inp : wiresStruc 1 3 1)
+      (regs : wiresStruc 0 2 1)
+  : wiresStruc 2 5 1 :=
+    -- inputs
+    let in0 := inp.result[0]
+    let in0_valid := inp.signals[0]
+    let out0_ready := inp.signals[1]
+    let out1_ready := inp.signals[2]
+    -- regs
+    let reg_0 := regs.signals[0]
+    let reg_1 := regs.signals[1]
+    -- circuit
+    let v2 := BitVec.xor reg_0 1#1
+    let v3 := BitVec.and v2 in0_valid
+    let v4 := BitVec.and out0_ready v3
+    let v5 := BitVec.or reg_0 v4
+    let v8 := BitVec.xor reg_1 1#1
+    let v9 := BitVec.and v8 in0_valid
+    let v10 := BitVec.and out1_ready v9
+    let v11 := BitVec.or v10 reg_1
+    let v12 := BitVec.and v5 v11
+    let v0 := BitVec.xor v12 1#1
+    let v1 := BitVec.and v5 v0
+    let v6 := BitVec.xor v12 1#1
+    let v7 := BitVec.and v11 v6
+    let reg_0 := v1
+    let reg_1 := v7
+    {result := #v[in0, in0], signals := #v[v12, v3, v9, reg_0, reg_1]}
+
+/--
+  Second RTL module:
+
+  hw.module @fork(in %arg0 : i0, in %arg0_valid : i1, in %arg1 : i0, in %arg1_valid : i1, in %clock : !seq.clock, in %reset : i1, in %out0_ready : i1, in %out1_ready : i1, in %out2_ready : i1, out arg0_ready : i1, out arg1_ready : i1, out out0 : i0, out out0_valid : i1, out out1 : i0, out out1_valid : i1, out out2 : i0, out out2_valid : i1) {
+    %handshake_fork0.in0_ready, %handshake_fork0.out0, %handshake_fork0.out0_valid, %handshake_fork0.out1, %handshake_fork0.out1_valid = hw.instance "handshake_fork0" @handshake_fork_1ins_2outs_ctrl(in0: %arg0: i0, in0_valid: %arg0_valid: i1, clock: %clock: !seq.clock, reset: %reset: i1, out0_ready: %out0_ready: i1, out1_ready: %out1_ready: i1) -> (in0_ready: i1, out0: i0, out0_valid: i1, out1: i0, out1_valid: i1)
+    hw.output %handshake_fork0.in0_ready, %out2_ready, %handshake_fork0.out0, %handshake_fork0.out0_valid, %handshake_fork0.out1, %handshake_fork0.out1_valid, %arg1, %arg1_valid : i1, i1, i0, i1, i0, i1, i0, i1
+  }
+-/
+def fork_rtl (inp : Stream' (wiresStruc 2 5 1)) : Stream' (wiresStruc 3 5 1) :=
+  register_wrapper_generalized
+    (inputs := inp)
+    (init_regs := {result := #v[], signals := Vector.replicate 2 (0#1)})
+    (outops := 3)
+    (outsigs := 5)
+    (update_fun :=
+      fun (inp, regs) =>
+        -- inputs
+        let arg0 := inp.result[0]
+        let arg1 := inp.result[1]
+        let arg0_valid := inp.signals[0]
+        let arg1_valid := inp.signals[1]
+        let out0_ready := inp.signals[2]
+        let out1_ready := inp.signals[3]
+        let out2_ready := inp.signals[4]
+        -- regs
+        let reg_0 := regs.signals[0]
+        let reg_1 := regs.signals[1]
+        -- circuit
+        let fork0_inp := {result := #v[arg0], signals := #v[arg0_valid, out0_ready, out1_ready]}
+        let fork0_regs := regs
+        let fork0_out := handshake_fork_1ins_2outs_ctrl fork0_inp fork0_regs
+        let fork0_out0 := fork0_out.result[0]
+        let fork0_out1 := fork0_out.result[1]
+        let fork0_in0_ready := fork0_out.signals[0]
+        let fork0_out0_valid := fork0_out.signals[1]
+        let fork0_out1_valid := fork0_out.signals[2]
+        let reg_0 := fork0_out.signals[3]
+        let reg_1 := fork0_out.signals[4]
+        let updatedRegs := {result := #v[], signals := #v[reg_0, reg_1]}
+        let out := {result := #v[fork0_out0, fork0_out1, arg1], signals := #v[fork0_in0_ready, out2_ready, fork0_out0_valid, fork0_out1_valid, arg1_valid]}
+        ⟨out, updatedRegs⟩
+    )
+
+
+
+
+-- theorem lowering_correctness
+--   (arg0 arg1 : Stream (BitVec 1))
+--   (arg0_readyvalid arg1_readyvalid : wiresStructStream 1 2 1)
+--   (harg0 : ReadyValidStruc arg0_readyvalid arg0)
+--   (harg1 : ReadyValidStruc arg1_readyvalid arg1)
+--   (out2_ready : Stream' (BitVec 1)) :
+
+
+        --   let arg0 := inp.result[0]
+        -- let arg1 := inp.result[1]
+        -- let arg0_valid := inp.signals[0]
+        -- let arg1_valid := inp.signals[1]
+        -- let out0_ready := inp.signals[2]
+        -- let out1_ready := inp.signals[3]
+        -- let out2_ready := inp.signals[4]
+
+    -- let input_fork_rtl' := {
+    --   result := #v[arg0_readyvalid.result[0], arg1_readyvalid.result[0]],
+    --   signals := #v[arg0_readyvalid.signals[0], arg1_readyvalid.signals[0],
+    --                 arg0_readyvalid.signals[1], arg1_readyvalid.signals[1], ]
+    -- }
+    -- Bisim' (fork_handshake arg0).fst fork_rtl'[2] := by sorry
