ea3592895a1f0cc3bde5ccfeecfbf8f35104dd3a
luisacicolini <luisacicolini@gmail.com>
2026-01-29 13:46:37 +0000

    chore: sink

--- SSA/Projects/CIRCT/HandshakeToHW/simple_example.lean
+++ SSA/Projects/CIRCT/HandshakeToHW/simple_example.lean
@@ -10,12 +10,16 @@ def opt_type (n : Nat) : Type := Option (BitVec n)
 def convertVecToStream : HVector n_type l -> Stream' (HVector opt_type l) := sorry
 def convertStreamToVec : Stream' (HVector opt_type l) -> (HVector n_type l) := sorry
 
-
+/--
+  A module with a register that keeps the stores the "ready" value and outputs a couple where:
+  · the first element is true if the value of the register and the current input are true
+  · the second element is true
+-/
 def output_const (input : HVector n_type [1]) : HVector n_type [1, 1] :=
   let hv := het_register_wrapper
               (ls := [1]) (lout := [1, 1])
               (inputs := convertVecToStream input)
-              (init_regs := HVector.cons (some (1 : BitVec 1)) HVector.nil)
+              (init_regs := HVector.cons (some true) HVector.nil)
               (update_fun :=
                 fun (inp, regs) =>
                   let e := (regs.get ⟨0, by simp⟩)
@@ -30,18 +34,42 @@ def output_const (input : HVector n_type [1]) : HVector n_type [1, 1] :=
                       ⟨updatedreg, output⟩
                   | _, _ =>
                     let output : HVector opt_type [1, 1] := HVector.cons (some false) (HVector.cons (some true) HVector.nil)
-                    ⟨updatedreg, output⟩
-              )
+                    ⟨updatedreg, output⟩)
   convertStreamToVec hv
 
-  -- it has a reg, which keeps the previous "ready" value of the sink
-  -- it then outputs true, in the first spot of the HVector n_type [1, 1], if the value of the reg and the current input are true
-  -- To output, you also have to set the second spot of HVector to true.
 
-def sink (input : HVector n_type [1, 1]) : HVector n_type [1] := sorry
-  -- register which flips between [true, true, false].
-  -- the "ready" output should reflect the state of the register.
-  -- it doesn't have to do anything with the inputs.
+
+/--
+  A module with a register that flips between [true, false].
+  The "ready" output should reflect the state of the register.
+  It doesn't have to do anything with the inputs.
+-/
+def sink (input : HVector n_type [1, 1]) : HVector n_type [1] :=
+    let values := [true, true, false]
+    let hv := het_register_wrapper
+              (ls := [1]) (lout := [1])
+              (inputs := convertVecToStream input)
+              (init_regs := HVector.cons (some true) HVector.nil)
+              (update_fun :=
+                fun (inp, regs) =>
+                  let e := (regs.get ⟨0, by simp⟩)
+                  match e with
+                  | some e' =>
+                    if e'[0] = true then
+                      /- TODO: Luisa could not find a way to make it flip [true, true, false] -/
+                      let output : HVector opt_type [1] := HVector.cons (some true) HVector.nil
+                      let updatedreg : HVector opt_type [1] := HVector.cons (some false) HVector.nil
+                      ⟨updatedreg, output⟩
+                    else
+                      let output : HVector opt_type [1] := HVector.cons (some false) HVector.nil
+                      let updatedreg : HVector opt_type [1] := HVector.cons (some true) HVector.nil
+                      ⟨updatedreg, output⟩
+                  | none =>
+                    let output : HVector opt_type [1] := HVector.cons none HVector.nil
+                    let updatedreg : HVector opt_type [1] := HVector.cons none HVector.nil
+                    ⟨updatedreg, output⟩)
+    convertStreamToVec hv
+
 
 -- def complete_module := sorry -- implemented with `fix_wrapper`
 
