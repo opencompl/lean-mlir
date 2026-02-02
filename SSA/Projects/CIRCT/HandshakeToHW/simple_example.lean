a5f8986506a1b0c78fb120e504fda937ffabfc38
luisacicolini <luisacicolini@gmail.com>
2026-02-02 11:28:40 +0000

    chore: complete defs/

--- SSA/Projects/CIRCT/HandshakeToHW/simple_example.lean
+++ SSA/Projects/CIRCT/HandshakeToHW/simple_example.lean
@@ -3,12 +3,41 @@ import SSA.Projects.CIRCT.Stream.Basic
 
 open HandshakeStream
 
+def streams_to_vec' {α : Type u} {n : Nat} (xv : Vector (Stream' α) n) : Stream' (Vector α n) :=
+  /- `map` applies `fun (s : Stream' α) => s i` to every element in `xv` -/
+  fun (i : Nat) => xv.map (fun (s : Stream' α) => s i)
+
+-- /--
+--   Map the `i`-th element of stream `s` in `xv` to the output heterogeneous vector, using `Stream'`.
+-- -/
+-- def streams'_to_hvec {α : Type u} {n : Nat} (xv : HVector f lin) : Stream' (HVector f lin) :=
+--   /- `map` applies `fun (s : Stream' α) => s i` to every element in `xv` -/
+--   fun (f, lin) => xv.map (fun (s : Stream' α) => s i)
+
+/--
+  Map each element at the `k`-th position of `xv` to the `k`-th stream of the output, using `Stream'`.
+-/
+def vec_to_streams' {α : Type u} {n : Nat} (xv : Stream' (Vector α n)) : Vector (Stream' α) n :=
+  /- `.ofFn` creates a vector with `n` elements (for each `k` from `0` to `n - 1`),
+      where each element is a stream `fun (i : Nat) => ...` containing the `k`-th element of the
+      `i`-th element of stream `xv`.
+  -/
+  Vector.ofFn (fun (k : Fin n) => fun (i : Nat) => (xv i).get k)
+
+
+
 def n_type (n : Nat) : Type := Stream' (Option (BitVec n))
 
 
 def opt_type (n : Nat) : Type := Option (BitVec n)
-def convertVecToStream : HVector n_type l -> Stream' (HVector opt_type l) := sorry
-def convertStreamToVec : Stream' (HVector opt_type l) -> (HVector n_type l) := sorry
+
+
+def convertVecToStream {l : List Nat} (xv : HVector n_type l) : Stream' (HVector opt_type l) :=
+  fun (i : Nat) =>
+    xv.map (fun {n : Nat} (s : n_type n) => s i)
+
+def convertStreamToVec {l : List Nat} (xv : Stream' (HVector opt_type l)) : HVector n_type l :=
+  HVector.ofFn n_type l (fun (k : Fin l.length) => fun (i : Nat) => (xv i).get k)
 
 /--
   A module with a register that keeps the stores the "ready" value and outputs a couple where:
@@ -36,7 +65,6 @@ def output_const (input : HVector n_type [1]) : HVector n_type [1, 1] :=
                     let output : HVector opt_type [1, 1] := HVector.cons (some false) (HVector.cons (some true) HVector.nil)
                     ⟨updatedreg, output⟩)
   convertStreamToVec hv
-
 
 
 /--
@@ -70,11 +98,28 @@ def sink (input : HVector n_type [1, 1]) : HVector n_type [1] :=
                     ⟨updatedreg, output⟩)
     convertStreamToVec hv
 
-def het_fix_wrapper (s : HVector n_type l) (f : HVector opt_type l → HVector opt_type l) :
-    Option (HVector n_type l) := sorry
 
+def het_fix_wrapper (fuel : Nat) (s : Stream' (HVector opt_type l)) (f : Stream' (HVector opt_type l) → Stream' (HVector opt_type l)) :
+    Stream' (HVector opt_type l) :=
+  match fuel with
+  | 0 => s
+  | n+1 => het_fix_wrapper n (f s) f
 
-def complete_module (input : HVector n_type [1]) :=
-  let const_module := output_const input
-  let sink_module := sink const_module
-  het_fix_wrapper sink_module id
+def het_fix_wrapper_single (fuel : Nat) (s : HVector opt_type l) (f : HVector opt_type l → HVector opt_type l) : (HVector opt_type l) :=
+  match fuel with
+  | 0 => s
+  | n+1 => het_fix_wrapper_single n (f s) f
+
+def compute_module' (input : Stream' (HVector opt_type [1])) : Stream' (HVector opt_type [1]) :=
+  let inputStream := convertStreamToVec input
+  let const_outs := output_const inputStream
+  convertVecToStream <| sink const_outs
+
+/- def compute_module_single' (input : Stream' (HVector opt_type [1])) : Stream' (HVector opt_type [1]) :=
+  let inputStream := convertStreamToVec input
+  let const_outs := output_const inputStream
+  let sink_outs := sink const_outs
+  convertVecToStream <| (Stream'.map (het_fix_wrapper_single 100 (f := ??)) sink_outs) -/
+
+def complete_module :=
+  het_fix_wrapper 10 (fun _ => .cons .none <| .nil) compute_module'
